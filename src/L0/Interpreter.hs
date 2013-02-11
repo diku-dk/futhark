b5ed5211905abb0fc68f06472991986570b24412
Cosmin Oancea <coancea76@yahoo.com>
2013-02-11 01:07:47 +0100

    fixed typo in replicate. type result array type was set to int

--- src/L0/Interpreter.hs
+++ src/L0/Interpreter.hs
@@ -220,12 +220,12 @@ evalExp (Iota e pos) = do
       | x >= 0    -> return $ ArrayVal (map (`IntVal` pos) [0..x-1]) (Int pos) pos
       | otherwise -> bad $ NegativeIota pos x
     _ -> bad $ TypeError pos "evalExp Iota"
-evalExp (Replicate e1 e2 _ pos) = do
+evalExp (Replicate e1 e2 (Identity outtype) pos) = do
   v1 <- evalExp e1
   v2 <- evalExp e2
   case v1 of
     IntVal x _
-      | x >= 0    -> return $ ArrayVal (replicate x v2) (Int pos) pos
+      | x >= 0    -> return $ ArrayVal (replicate x v2) outtype pos
       | otherwise -> bad $ NegativeReplicate pos x
     _ -> bad $ TypeError pos "evalExp Replicate"
 evalExp (Reshape shapeexp arrexp _ (Identity outtype) pos) = do
