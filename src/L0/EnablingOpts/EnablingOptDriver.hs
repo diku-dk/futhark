39d94ab1e9fca3d92a39e365cc6bd9e022c8e489
Cosmin Oancea <coancea76@yahoo.com>
2013-02-23 02:30:27 +0100

    place enabling optimizations in a separate folder and modules, and added a simple test for inlining

---
+++ src/L0/EnablingOpts/EnablingOptDriver.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, ScopedTypeVariables #-}
+
+module L0.EnablingOpts.EnablingOptDriver ( 
+                                    enablingOpts
+                                  , copyCtProp
+                                  , CallGraph
+                                  , buildCG
+                                  , aggInlineDriver
+                                  , EnablingOptError(..)
+                            )
+  where
+ 
+ 
+--import Data.Either
+
+import L0.AbSyn
+
+import L0.EnablingOpts.InliningDeadFun
+import L0.EnablingOpts.CopyCtPropFold
+import L0.EnablingOpts.EnablingOptErrors
+
+
+--------------------------------------------------------------
+---- Enabling Optimization Driver
+--------------------------------------------------------------
+
+enablingOpts :: TypeBox tf => Prog tf -> Either EnablingOptError (Prog tf)
+enablingOpts prog = do
+    proginl <- aggInlineDriver prog
+
+    (success, progcp) <- copyCtProp proginl
+    if(success) 
+    then enablingOpts progcp
+    else return progcp
+
---
+++ src/L0/EnablingOpts/EnablingOptErrors.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, ScopedTypeVariables #-}
+
+module L0.EnablingOpts.EnablingOptErrors ( EnablingOptError(..) )
+  where
+  
+import Data.Loc
+import L0.AbSyn
+
+-- | Information about an error during type checking.  The 'Show'
+-- instance for this type produces a human-readable description.
+data EnablingOptError = EnablingOptError SrcLoc String
+               -- ^ A general error happened at the given position and
+               -- for the given reason.
+               | DupParamError String String SrcLoc
+               -- ^ Two function parameters share the same name.
+               | CopyCtPropError SrcLoc String 
+               -- ^ Copy/Constant Propagation Error
+               | TypeError SrcLoc String
+               | Div0Error SrcLoc
+               | DupDefinitionError String SrcLoc SrcLoc
+               | FunctionNotInFtab  String
+
+instance Show EnablingOptError where
+    show (EnablingOptError pos msg) =
+        "Enabling Optimization Error at " ++ locStr pos ++ ":\n" ++ msg
+    show (DupParamError funname paramname pos) =
+        "Parameter " ++ paramname ++
+        " mentioned multiple times in argument list of function " ++
+        funname ++ " at " ++ locStr pos ++ "."
+    show (CopyCtPropError pos msg ) = --ee
+        "Copy or Constant Folding and Propagation Implementation Error " ++ 
+        msg ++ " at " ++ locStr pos -- ++ ppExp 0 ee 
+    show (TypeError pos s) =
+        "Type error at " ++ locStr pos ++ " in " ++ s ++ 
+        " during interpretation.  This implies a bug in the type checker."
+    show (Div0Error pos) =
+        "Division by zero Error detected during copy/constant propagation and folding at line: " 
+        ++ locStr pos 
+    show (DupDefinitionError name pos1 pos2) =
+        "Duplicate definition of function " ++ name ++ ".  Defined at " ++
+        locStr pos1 ++ " and " ++ locStr pos2 ++ "."
+    show (FunctionNotInFtab fname) = 
+        "Function " ++ fname ++ " not found in Function Symbol Table"
+
