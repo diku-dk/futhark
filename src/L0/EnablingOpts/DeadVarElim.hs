a8a8d58e1233959897ebc41a33e230143e8700e7
Cosmin Oancea <coancea76@yahoo.com>
2013-03-26 03:07:53 +0100

    replacing the result map with a set

--- src/L0/EnablingOpts/DeadVarElim.hs
+++ src/L0/EnablingOpts/DeadVarElim.hs
@@ -12,6 +12,7 @@ import Control.Monad.Writer
 import Data.Data
 import Data.Generics
 
+import qualified Data.Set as S
 import qualified Data.Map as M
 
 import L0.AbSyn
@@ -21,7 +22,6 @@ import L0.EnablingOpts.InliningDeadFun
 import L0.EnablingOpts.EnablingOptErrors
 
 
-
 -----------------------------------------------------------------
 -----------------------------------------------------------------
 ---- This file implements Dead-Variable Elimination          ----
@@ -29,22 +29,22 @@ import L0.EnablingOpts.EnablingOptErrors
 -----------------------------------------------------------------
 
 data DCElimEnv tf = DCElimEnv {   
-                        envVtable  :: M.Map String Int
+                        envVtable  :: S.Set String
                       , callGraph  :: CallGraph
                   }
 
 data DCElimRes tf = DCElimRes {
     resSuccess :: Bool
   -- ^ Whether we have changed something.
-  , resMap     :: M.Map String Int
+  , resMap     :: S.Set String
   -- ^ The hashtable recording the uses
   }
 
 
 instance Monoid (DCElimRes tf) where
   DCElimRes s1 m1 `mappend` DCElimRes s2 m2 =
-    DCElimRes (s1 || s2) (M.union m1 m2)   
-  mempty = DCElimRes False M.empty
+    DCElimRes (s1 || s2) (S.union m1 m2)   
+  mempty = DCElimRes False S.empty
 
 newtype DCElimM tf a = DCElimM (WriterT (DCElimRes tf) (ReaderT (DCElimEnv tf) (Either EnablingOptError)) a)
     deriving (MonadWriter (DCElimRes tf),
@@ -56,24 +56,21 @@ newtype DCElimM tf a = DCElimM (WriterT (DCElimRes tf) (ReaderT (DCElimEnv tf) (
 runDCElimM :: DCElimM tf a -> DCElimEnv tf -> Either EnablingOptError (a, DCElimRes tf)
 runDCElimM  (DCElimM a) env = runReaderT (runWriterT a) env
 
-badCPropM :: EnablingOptError -> DCElimM tf a
-badCPropM = DCElimM . lift . lift . Left
+badDCElimM :: EnablingOptError -> DCElimM tf a
+badDCElimM = DCElimM . lift . lift . Left
 
 
 
 collectRes :: [String] -> DCElimM tf a -> DCElimM tf (a, Bool)
 collectRes mvars m = pass collect
   where wasNotUsed hashtab vnm = do
-          let tmp = M.lookup vnm hashtab
-          case tmp of
-            Nothing -> return True
-            Just _  -> return False
+          return $ not (S.member vnm hashtab)
+
         collect = do
           (x,res) <- listen m
           tmps    <- mapM (wasNotUsed (resMap res)) mvars
           let unused = foldl (&&) True tmps
           return ( (x, unused), const $ res )
-                  -- const $ res) { resSuccess = resSuccess res || unused })
 
 changed :: DCElimM tf a -> DCElimM tf a
 changed m = pass collect
@@ -84,43 +81,31 @@ changed m = pass collect
 
 -- | Bind a name as a common (non-merge) variable.
 -- TypeBox tf => 
-bindVar :: DCElimEnv tf -> (String, Int) -> DCElimEnv tf
-bindVar env (name,val) =
-  env { envVtable = M.insert name val $ envVtable env }
+bindVar :: DCElimEnv tf -> String -> DCElimEnv tf
+bindVar env name =
+  env { envVtable = S.insert name $ envVtable env }
 
-bindVars :: DCElimEnv tf -> [(String, Int)] -> DCElimEnv tf
+bindVars :: DCElimEnv tf -> [String] -> DCElimEnv tf
 bindVars = foldl bindVar
 
-binding :: [(String, Int)] -> DCElimM tf a -> DCElimM tf a
+binding :: [String] -> DCElimM tf a -> DCElimM tf a
 binding bnds = local (`bindVars` bnds)
 
--- | Remove the binding for a name.
--- TypeBox tf => 
---remVar :: DCElimEnv tf -> String -> DCElimEnv tf
---remVar env name = env { envVtable = M.delete name $ envVtable env }
---
---remVars :: DCElimEnv tf -> [String] -> DCElimEnv tf
---remVars = foldl remVar
---
---remBindings :: [String] -> DCElimM tf a -> DCElimM tf a
---remBindings keys = local (`remVars` keys)
 
--- | Applies Copy/Constant Propagation and Folding to an Entire Program.
+-- | Applies Dead-Code Elimination to an Entire Program.
 -- TypeBox tf => 
 deadCodeElim :: TypeBox tf => Prog tf -> Either EnablingOptError (Bool, Prog tf)
 deadCodeElim prog = do
     cg <- buildCG prog
-    let env = DCElimEnv { envVtable = M.empty, callGraph = cg }
+    let env = DCElimEnv { envVtable = S.empty, callGraph = cg }
     (rs, res) <- runDCElimM (mapM deadCodeElimFun prog) env
     return (resSuccess res, rs)
 
 deadCodeElimFun :: TypeBox tf => FunDec tf -> DCElimM tf (FunDec tf)
 deadCodeElimFun (fname, rettype, args, body, pos) = do
     let ids = map identName args
-    let bnds= zip ids (replicate (length ids) 0)
-    body' <- binding bnds $ deadCodeElimExp body
+    body' <- binding ids $ deadCodeElimExp body
     return (fname, rettype, args, body', pos)
-    --return (fname, rettype, args, body, pos)
 
 --------------------------------------------------------------------
 --------------------------------------------------------------------
@@ -133,8 +118,7 @@ deadCodeElimExp :: TypeBox tf => Exp tf -> DCElimM tf (Exp tf)
 
 deadCodeElimExp (LetPat pat e body pos) = do
     let ids = getBnds pat
-    let bnds= zip ids (replicate (length ids) 0)
-    (body', noref) <- collectRes ids $ binding bnds $ deadCodeElimExp body
+    (body', noref) <- collectRes ids $ binding ids $ deadCodeElimExp body
 
     cg  <- asks $ callGraph
     let torem = noref && not (hasIO cg e)
@@ -146,7 +130,7 @@ deadCodeElimExp (LetPat pat e body pos) = do
 
 
 deadCodeElimExp (LetWith nm src inds el body pos) = do
-    (body', noref) <- collectRes [identName nm] $ binding [(identName nm, 0)] $ deadCodeElimExp body
+    (body', noref) <- collectRes [identName nm] $ binding [identName nm] $ deadCodeElimExp body
 
     cg  <- asks $ callGraph
     
@@ -155,40 +139,41 @@ deadCodeElimExp (LetWith nm src inds el body pos) = do
     then changed $ return body'
     else do
             let srcnm = identName src
-            bnd   <- asks $ M.lookup srcnm . envVtable
-            case bnd of
-                Nothing -> badCPropM $ TypeError pos  ("Var "++srcnm++" not in vtable!")
-                Just _  -> do
-                    _ <- tell $ DCElimRes False (M.fromList [(srcnm, 1)])
+            in_vtab <- asks $ S.member srcnm . envVtable
+            if not in_vtab
+            then badDCElimM $ TypeError pos  ("Var "++srcnm++" not in vtable!")
+            else do
+                    _ <- tell $ DCElimRes False (S.insert srcnm S.empty)
                     inds' <- mapM deadCodeElimExp inds
                     el'   <- deadCodeElimExp el
                     return $ LetWith nm src inds' el' body' pos
 
 
 deadCodeElimExp e@(Var (Ident vnm _ pos)) = do 
-    bnd <- asks $ M.lookup vnm . envVtable
-    case bnd of
-        Nothing -> badCPropM $ TypeError pos  ("Var "++vnm ++" not in vtable!")
-        Just _  -> do
-            _ <- tell $ DCElimRes False (M.fromList [(vnm, 1)])
+    in_vtab <- asks $ S.member vnm . envVtable
+    if not in_vtab
+    then badDCElimM $ TypeError pos  ("Var "++vnm ++" not in vtable!")
+    else do
+            _ <- tell $ DCElimRes False (S.insert vnm S.empty)
             return e
+
 
 deadCodeElimExp (Index s idxs t1 t2 pos) = do
     let vnm = identName s
-    bnd <- asks $ M.lookup vnm . envVtable
-    case bnd of
-        Nothing -> badCPropM $ TypeError pos  ("Var "++vnm ++" not in vtable!")
-        Just _  -> do
-            _ <- tell $ DCElimRes False (M.fromList [(vnm, 1)])
+    in_vtab <- asks $ S.member vnm . envVtable
+    if not in_vtab
+    then badDCElimM $ TypeError pos  ("Var "++vnm ++" not in vtable!")
+    else do
+            _ <- tell $ DCElimRes False (S.insert vnm S.empty)
             idxs' <- mapM deadCodeElimExp idxs
             return $ Index s idxs' t1 t2 pos
 
+
 deadCodeElimExp (DoLoop ind n body mergevars pos) = do
     n'    <- deadCodeElimExp n
-    body' <- binding [(identName ind, 0)] $ deadCodeElimExp body
+    body' <- binding [identName ind] $ deadCodeElimExp body
     let mergenms = map identName mergevars
-    let mergebnds= zip mergenms (replicate (length mergenms) 1)
-    _ <- tell $ DCElimRes False (M.fromList mergebnds)
+    _ <- tell $ DCElimRes False (S.fromList mergenms)
     return $ DoLoop ind n' body' mergevars pos
 
 
@@ -209,8 +194,7 @@ deadCodeElimExpPair (e,t) = do e' <- deadCodeElimExp e
 deadCodeElimLambda :: TypeBox tf => Lambda tf -> DCElimM tf (Lambda tf)
 deadCodeElimLambda (AnonymFun params body ret pos) = do
     let ids  = map identName params
-    let bnds = zip ids (replicate (length ids) 0)
-    body' <- binding bnds $ deadCodeElimExp body
+    body' <- binding ids $ deadCodeElimExp body
     return $ AnonymFun params body' ret pos
 
 deadCodeElimLambda (CurryFun fname curryargexps rettype pos) = do
