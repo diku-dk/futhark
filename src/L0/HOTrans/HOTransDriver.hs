fb901da2791a488beb397e9590d200f6398f5d62
Cosmin Oancea <coancea76@yahoo.com>
2013-06-04 14:13:38 +0200

    adding higher-order transformation driver

---
+++ src/L0/HOTrans/HOTransDriver.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module L0.HOTrans.HOTransDriver ( 
+                                    highOrdTransf
+                                )
+  where
+ 
+ 
+--import Data.Either
+ 
+import L0.AbSyn
+--import L0.Renamer
+
+import L0.EnablingOpts.EnablingOptDriver
+
+import L0.HOTrans.Fusion
+
+--------------------------------------------------------------
+---- HigherOrderTransformation Driver
+--------------------------------------------------------------
+
+--enablingOpts :: TypeBox tf => Prog tf -> Either EnablingOptError (Prog tf)
+highOrdTransf :: Prog Type -> Either EnablingOptError (Prog Type)
+highOrdTransf prog = do
+    
+{-
+    (succc0, prog') <- normCopyDeadOpts prog  >>= fuseProg
+    (succc1, prog1) <- normCopyDeadOpts prog' >>= fuseProg
+    (succc2, prog2) <- normCopyDeadOpts prog1 >>= fuseProg
+    (succc3, prog3) <- normCopyDeadOpts prog2 >>= fuseProg
+    (succc4, prog4) <- normCopyDeadOpts prog3 >>= fuseProg
+    return prog4
+-}
+
+    (succc, prog') <- fuseProg prog
+    if succc 
+    then normCopyDeadOpts prog' >>= highOrdTransf 
+    else return prog'
+
