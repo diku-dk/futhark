df3bf001fca00c628bc50cd6fbf98fb94e878e53
Troels Henriksen <athas@sigkill.dk>
2013-06-28 12:29:41 +0200

    Remove accidentally added file.

--- src/L0C/Util.hs
+++
@@ -1,7 +0,0 @@
-module L0.Util ( all2 )
-  where
-
--- | Like 'all', but with two lists.  The lists need not have the same
--- length for the predicate to be true.
-all2 :: (a -> b -> Bool) -> [a] -> [b] -> Bool
-all2 p xs ys = all id $ zipWith p xs ys
