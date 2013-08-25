7e75543d82de04563d2b1c18a9b17414dbb5f8b6
Troels Henriksen <athas@sigkill.dk>
2013-08-25 20:40:18 +0200

    Let the hoister insert entirely new bindings.
    It currently ends up inserting a lot, which must be removed by the
    dead code remover.  Perhaps this could be improved.

--- src/L0C/EnablingOpts/Hoisting.hs
+++ src/L0C/EnablingOpts/Hoisting.hs
@@ -11,16 +11,18 @@ module L0C.EnablingOpts.Hoisting
 import Control.Applicative
 import Control.Monad.Writer
 import Control.Monad.Reader
+import Control.Monad.State
+import Control.Monad.RWS
 
 import Data.Graph
 import Data.List
 import Data.Loc
-import Data.Maybe
 import qualified Data.Map as M
 import Data.Ord
 import qualified Data.Set as S
 
 import L0C.L0
+import L0C.FreshNames
 
 data BindNeed = LoopBind TupIdent Exp Ident Exp Exp
               | LetBind TupIdent Exp [(TupIdent, Exp)]
@@ -40,7 +42,9 @@ asTail (LetWithBind dest src is ve) =
   LetWith dest src is ve (Var dest) $ srclocOf dest
 
 requires :: BindNeed -> S.Set VName
-requires = S.map identName . freeInExp . asTail
+requires (LetBind pat e ((altpat,alte):alts)) =
+  requires (LetBind pat e []) <> requires (LetBind altpat alte alts)
+requires bnd = S.map identName $ freeInExp $ asTail bnd
 
 provides :: BindNeed -> S.Set VName
 provides (LoopBind mergepat _ _ _ _) = patNames mergepat
@@ -97,14 +101,40 @@ lookupShapeBindings k env =
             DimSizes $ replicate strip Nothing ++ sz
 
 
-newtype HoistM a = HoistM (WriterT Need (Reader Env) a)
+newtype HoistM a = HoistM (RWS
+                           Env                -- Reader
+                           Need               -- Writer
+                           (NameSource VName) -- State
+                           a)
   deriving (Applicative, Functor, Monad,
-            MonadWriter Need, MonadReader Env)
+            MonadWriter Need, MonadReader Env, MonadState (NameSource VName))
 
-runHoistM :: HoistM a -> (a, Need)
-runHoistM (HoistM m) = runReader (runWriterT m) emptyEnv
+runHoistM :: HoistM a -> NameSource VName -> a
+runHoistM (HoistM m) src = let (x, _, _) = runRWS m emptyEnv src
+                           in x
+
+new :: String -> HoistM VName
+new k = do (name, src) <- gets $ newVName k
+           put src
+           return name
+
+addNewBinding :: String -> Exp -> HoistM Ident
+addNewBinding k e = do
+  k' <- new k
+  let ident = Ident { identName = k'
+                    , identType = typeOf e
+                    , identSrcLoc = srclocOf e }
+  addBinding (Id ident) e
+  return ident
 
 addBinding :: TupIdent -> Exp -> HoistM ()
+addBinding pat e@(Size i (Var x) loc) = do
+  let mkAlt (Slice ydims y) = [(pat, Size (i+ydims) (Var y) loc)]
+      mkAlt (DimSizes ses) = case drop i ses of
+                               Just se:_ -> [(pat, se)]
+                               _        -> []
+  alts <- concatMap mkAlt <$> asks (lookupShapeBindings x)
+  tell $ Need $ S.singleton $ LetBind pat e alts
 addBinding pat e =
   tell $ Need $ S.singleton $ LetBind pat e []
 
@@ -124,25 +154,22 @@ bindLet pat@(Id dest) e@(Replicate (Var x) _ _) m = do
 
 bindLet pat@(TupId [dest1, dest2] _) e@(Split (Var n) (Var src) _ loc) m = do
   addBinding pat e
-  szs <- asks $ lookupShapeBindings src
-  withShapes [(dest1, Var n : rest)] $ bind $ mapMaybe dim0 szs
+  src_sz <- addNewBinding "split_src_sz" $ Size 0 (Var src) loc
+  split_sz <- addNewBinding "split_sz" $
+              BinOp Minus (Var src_sz) (Var n) (Elem Int) loc
+  withShapes [(dest1, Var n : rest),
+              (dest2, Var split_sz : rest)] m
     where rest = [ Size i (Var src) loc
                    | i <- [1.. arrayDims (identType src) - 1]]
-          dim0 (Slice d k) = Just $ Size d (Var k) loc
-          dim0 (DimSizes (Just se:_)) = Just se
-          dim0 (DimSizes _) = Nothing
-          bind =
-            foldr (\se ->
-                   withShapes [(dest2, BinOp Minus se (Var n) (Elem Int) loc:rest)])
-            m
 
 bindLet pat@(Id dest) e@(Concat (Var x) (Var y) loc) m = do
-  addBinding pat e
-  withShape dest (outerdim :
+  concat_x <- addNewBinding "concat_x" $ Size 0 (Var x) loc
+  concat_y <- addNewBinding "concat_y" $ Size 0 (Var y) loc
+  concat_sz <- addNewBinding "concat_sz" $
+               BinOp Plus (Var concat_x) (Var concat_y) (Elem Int) loc
+  withShape dest (Var concat_sz :
                   [Size i (Var x) loc | i <- [1..arrayDims (identType x) - 1]]
-                 ) m
-  where outerdim = BinOp Plus (Size 0 (Var x) loc) (Size 0 (Var y) loc)
-                   (Elem Int) loc
+                 ) $ addBinding pat e >> m
 
 bindLet pat@(Id dest) e@(Map _ (Var src) _ loc) m = do
   addBinding pat e
@@ -171,17 +198,8 @@ bindLet pat@(Id dest) e@(Transpose k n (Var src) loc) m = do
                  [Size i (Var src) loc
                   | i <- [0..arrayDims (identType src) - 1]]
 
-bindLet pat e@(Size i (Var x) loc) m = do
-  let mkAlt (Slice ydims y) = [(pat, Size (i+ydims) (Var y) loc)]
-      mkAlt (DimSizes ses) = case drop i ses of
-                               Just se:_ -> [(pat, se)]
-                               _        -> []
-  alts <- concatMap mkAlt <$> asks (lookupShapeBindings x)
-  tell $ Need $ S.singleton $ LetBind pat e alts
-  m
-
 bindLet pat e m = do
-  tell $ Need $ S.singleton $ LetBind pat e []
+  addBinding pat e
   m
 
 bindLetWith :: Ident -> Ident -> [Exp] -> Exp -> HoistM a -> HoistM a
@@ -222,42 +240,76 @@ withShapes (_:rest) m =
 -- taken place - the order of bindings may simply have been
 -- rearranged.  The function is idempotent, however.
 transformProg :: Prog -> Prog
-transformProg = Prog . map transformFun . progFunctions
+transformProg prog =
+  Prog $ runHoistM (mapM transformFun $ progFunctions prog) namesrc
+  where namesrc = newNameSourceForProg prog
 
-transformFun :: FunDec -> FunDec
-transformFun (fname, rettype, params, body, loc) =
-  (fname, rettype, params, addBindings need body', loc)
-  where (body', need) = runHoistM $ hoistInExp body
+transformFun :: FunDec -> HoistM FunDec
+transformFun (fname, rettype, params, body, loc) = do
+  body' <- blockAllHoisting $ hoistInExp body
+  return (fname, rettype, params, body', loc)
 
 addBindings :: Need -> Exp -> Exp
 addBindings need =
   snd $ foldl comb (M.empty, id) (inDepOrder $ S.toList $ needBindings need)
   where comb (m, outer) bind@(LoopBind mergepat mergeexp loopvar
                               boundexp loopbody) =
-          (m `M.union` distances m (provides bind) (requires bind),
+          (m `M.union` distances m bind,
            \inner -> outer $
                      DoLoop mergepat mergeexp loopvar boundexp
                      loopbody inner $ srclocOf inner)
-        comb (m, outer) bind@(LetBind pat e alts) =
+        comb (m, outer) (LetBind pat e alts) =
           let add pat' e' =
-                (m `M.union` distances m (provides bind) (S.map identName $ freeInExp e),
+                (m `M.union` distances m (LetBind pat' e' []),
                  \inner -> outer $ LetPat pat' e' inner $ srclocOf inner)
           in case map snd $ sortBy (comparing fst) $ map (score m) $ (pat,e):alts of
                (pat',e'):_ -> add pat' e'
                _           -> add pat e
         comb (m, outer) bind@(LetWithBind dest src is ve) =
-          (m `M.union` distances m (provides bind) (requires bind),
+          (m `M.union` distances m bind,
            \inner -> outer $ LetWith dest src is ve inner $ srclocOf inner)
 
 score :: M.Map VName Int -> (TupIdent, Exp) -> (Int, (TupIdent, Exp))
-score m (p, e) = (S.fold f 0 $ S.map identName $ freeInExp e, (p, e))
+score m (p, e) = (S.fold f 0 $ freeNamesInExp e, (p, e))
   where f k x = case M.lookup k m of
                   Just y  -> max x y
                   Nothing -> x
 
-distances :: M.Map VName Int -> S.Set VName -> S.Set VName -> M.Map VName Int
-distances m outs ins = M.fromList [ (k, d+1) | k <- S.toList outs ]
+expCost :: Exp -> Int
+expCost (Map {}) = 1
+expCost (Map2 {}) = 1
+expCost (Filter {}) = 1
+expCost (Filter2 {}) = 1
+expCost (Reduce {}) = 1
+expCost (Reduce2 {}) = 1
+expCost (Scan {}) = 1
+expCost (Scan2 {}) = 1
+expCost (Redomap {}) = 1
+expCost (Redomap2 {}) = 1
+expCost (Transpose {}) = 1
+expCost (Copy {}) = 1
+expCost (Concat {}) = 1
+expCost (Split {}) = 1
+expCost (Reshape {}) = 1
+expCost (DoLoop {}) = 1
+expCost _ = 0
+
+distances :: M.Map VName Int -> BindNeed -> M.Map VName Int
+distances m need = M.fromList [ (k, d+cost) | k <- S.toList outs ]
   where d = S.fold f 0 ins
+        (outs, ins, cost) =
+          case need of
+            LetBind pat e _ ->
+              (patNames pat, freeNamesInExp e, expCost e)
+            LetWithBind dest src is ve ->
+              (S.singleton $ identName dest,
+               identName src `S.insert`
+               mconcat (map freeNamesInExp (ve:is)),
+               1)
+            LoopBind pat mergeexp _ bound loopbody ->
+              (patNames pat,
+               mconcat $ map freeNamesInExp [mergeexp, bound, loopbody],
+               1)
         f k x = case M.lookup k m of
                   Just y  -> max x y
                   Nothing -> x
