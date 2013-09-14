d8c12a6bf304e32a995c6ce874d7a304d8f60594
Rasmus Wriedt Larsen <rasmuswriedtlarsen@gmail.com>
2013-09-14 18:21:39 +0200

    can now show SimplifyError

--- src/L0C/EnablingOpts/EnablingOptErrors.hs
+++ src/L0C/EnablingOpts/EnablingOptErrors.hs
@@ -1,10 +1,10 @@
 -- | Common error definition for all the enabling optimisation submodules.
 module L0C.EnablingOpts.EnablingOptErrors ( EnablingOptError(..) )
   where
-  
+
 import Data.Loc
 import L0C.L0
- 
+
 -- | Information about an error during enabling optimisations.  The 'Show'
 -- instance for this type produces a human-readable description.
 data EnablingOptError = EnablingOptError SrcLoc String
@@ -29,18 +29,20 @@ instance Show EnablingOptError where
         " mentioned multiple times in argument list of function " ++
         nameToString funname ++ " at " ++ locStr pos ++ "."
     show (CopyCtPropError pos msg ) = --ee
-        "Copy or Constant Folding and Propagation Implementation Error " ++ 
-        msg ++ " at " ++ locStr pos -- ++ ppExp 0 ee 
+        "Copy or Constant Folding and Propagation Implementation Error " ++
+        msg ++ " at " ++ locStr pos -- ++ ppExp 0 ee
     show (TypeError pos s) =
-        "Type error at " ++ locStr pos ++ " in " ++ s ++ 
+        "Type error at " ++ locStr pos ++ " in " ++ s ++
         " during interpretation.  This implies a bug in the type checker."
     show (Div0Error pos) =
-        "Division by zero Error detected during copy/constant propagation and folding at line: " 
-        ++ locStr pos 
+        "Division by zero Error detected during copy/constant propagation and folding at line: "
+        ++ locStr pos
     show (DupDefinitionError name pos1 pos2) =
         "Duplicate definition of function " ++ nameToString name ++ ".  Defined at " ++
         locStr pos1 ++ " and " ++ locStr pos2 ++ "."
-    show (FunctionNotInFtab fname) = 
+    show (FunctionNotInFtab fname) =
         "Function " ++ nameToString fname ++ " not found in Function Symbol Table"
     show (VarNotInFtab pos name) =
         "Variable " ++ textual name ++ " not found in symbol table at " ++ locStr pos ++ "."
+    show (SimplifyError pos msg) =
+        "Simplify Error at " ++ locStr pos ++ ":\n" ++ msg
