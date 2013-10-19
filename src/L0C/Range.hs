614c78bb172fb0bbbb5d031317ea64bb04e9e7ba
Jonas Brunsgaard <jonas.brunsgaard@gmail.com>
2013-10-19 13:34:46 +0200

    for Rasmus to add signs

--- src/L0C/Range.hs
+++ src/L0C/Range.hs
@@ -46,7 +46,19 @@ substitute i r (RExp (BinOp Plus e1 e2 ty pos)) =
     let ub = simplExp(BinOp Plus (extractExp e1ub) (extractExp e2ub) ty pos) in
     (RExp lb, RExp ub) 
 
+substitute i r (RExp (BinOp Minus e1 e2 ty pos)) =
+    let (e1lb, e1ub) = substitute i r (RExp e1) in
+    let (e2lb, e2ub) = substitute i r (RExp e2) in
+    let lb = simplExp(BinOp Minus (extractExp e1lb) (extractExp e2lb) ty pos) in
+    let ub = simplExp(BinOp Minus (extractExp e1ub) (extractExp e2ub) ty pos) in
+    (RExp lb, RExp ub) 
 
+substitute i r (RExp (BinOp Mul e1 e2 ty pos)) =
+    let (e1lb, e1ub) = substitute i r (RExp e1) in
+    let (e2lb, e2ub) = substitute i r (RExp e2) in
+    let lb = simplExp(BinOp Plus (extractExp e1lb) (extractExp e2lb) ty pos) in
+    let ub = simplExp(BinOp Plus (extractExp e1ub) (extractExp e2ub) ty pos) in
+    (RExp lb, RExp ub) 
 
 -- TESTING
 
