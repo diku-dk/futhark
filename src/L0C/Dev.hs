d5e1a3254c9c19610b0c05d685bb7f66288db0f0
Troels Henriksen <athas@sigkill.dk>
2013-10-27 09:56:50 +0100

    Add lambda function to Dev module.

--- src/L0C/Dev.hs
+++ src/L0C/Dev.hs
@@ -11,31 +11,29 @@ module L0C.Dev
   , tident
   , expr
   , typ
+  , lambda
   )
 where
 
 import Data.IORef
 import Data.Loc
+import qualified Data.Map as M
 import qualified Data.Set as S
 import System.IO.Unsafe
 
 import Language.L0.Parser
 
+import L0C.FreshNames
 import L0C.L0
 import L0C.Renamer
 import L0C.TypeChecker
 
-counter :: IORef Int
-counter = unsafePerformIO $ newIORef 1000000
-
--- | Return a unique name.  Yes, the type of this function is
--- non-monadic.  Yes, the name will have a unique tag (at least
--- compared to anything else returned from this function).  No, don't
--- think too much about it.
+-- | Return a tagged name based on a string.
 name :: String -> VName
-name k = unsafePerformIO $ do
-           i <- atomicModifyIORef counter $ \x -> (x+1,x)
-           return $ ID (nameFromString k, i)
+name k = unsafePerformIO $
+           atomicModifyIORef' uniqueNameSource $ \src ->
+            let (k', src') = newID src $ nameFromString k
+            in (src', k')
 
 -- | Return a new, unique identifier.  Uses 'name'.
 ident :: String -> Type -> Ident
@@ -48,13 +46,49 @@ tident s = case words s of
              [t,k] -> ident k $ typ t
              _ -> error "Bad ident"
 
+uniqueNameSource :: IORef (NameSource VName)
+uniqueNameSource = unsafePerformIO $ newIORef newUniqueNameSource
+  where newUniqueNameSource = NameSource $ generator 0 M.empty
+        generator i m s =
+          case M.lookup (baseName s) m of
+            Just s' -> (s', NameSource $ generator i m)
+            Nothing ->
+              let s' = s `setID` i
+                  m' = M.insert (baseName s) s' m
+              in (s', NameSource $ generator (i+1) m')
+
+uniqueTag :: (NameSource VName -> f -> (t, NameSource VName)) -> f -> t
+uniqueTag f x =
+  x `seq` unsafePerformIO $ atomicModifyIORef' uniqueNameSource $ \src ->
+    let (x', src') = f src x
+    in (src', x')
+
+uniqueTagExp :: TypeBox ty => ExpBase ty Name -> ExpBase ty VName
+uniqueTagExp = uniqueTag tagExp'
+
+uniqueTagType :: TypeBox ty => ty Name -> ty VName
+uniqueTagType = uniqueTag tagType'
+
+uniqueTagLambda :: TypeBox ty => LambdaBase ty Name -> LambdaBase ty VName
+uniqueTagLambda = uniqueTag tagLambda'
+
 rightResult :: Show a => Either a b -> b
 rightResult = either (error . show) id
 
 -- | Parse a string to an expression.
 expr :: String -> Exp
-expr = tagExp . rightResult . checkClosedExp . rightResult . parseExp "input"
+expr = uniqueTagExp . rightResult . checkClosedExp . rightResult . parseExp "input"
 
 -- | Parse a string to a type.
 typ :: String -> Type
-typ = tagType . (`setAliases` S.empty) . rightResult . parseType "input"
+typ = uniqueTagType . (`setAliases` S.empty) . rightResult . parseType "input"
+
+-- | Parse a string to an anonymous function.  Does not handle curried functions.
+lambda :: String -> Lambda
+lambda = uniqueTagLambda . rightResult . checkClosedLambda . rightResult . parseLambda "input"
+  where checkClosedLambda (AnonymFun params body rettype loc) = do
+          body' <- checkOpenExp env body
+          return $ AnonymFun params body' rettype loc
+            where env = M.fromList [ (identName param, fromDecl $ identType param)
+                                     | param <- params ]
+        checkClosedLambda (CurryFun {}) = error "Curries not handled"
