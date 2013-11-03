72b0457c71c64fdd384987251065bf488f3231dd
Jonas Brunsgaard <jonas.brunsgaard@gmail.com>
2013-11-03 16:30:39 +0100

    Found bug, Hurra

--- src/L0C/EnablingOpts/CopyCtPropFold.hs
+++ src/L0C/EnablingOpts/CopyCtPropFold.hs
@@ -490,9 +490,9 @@ ctFoldBinOp e@(BinOp Mod e1 e2 _ pos)
 ctFoldBinOp e@(BinOp Pow e1 e2 _ pos)
   | isCt0 e1 || isCt1 e1 || isCt1 e2 = changed e1
   | isCt0 e2 =
-    case e1 of
-      Literal (IntVal  _) _ -> changed $ Literal (IntVal  1) pos
-      Literal (RealVal _) _ -> changed $ Literal (RealVal 1.0) pos
+    case typeOf e1 of
+      Elem Int  -> changed $ Literal (IntVal  1) pos
+      Elem Real -> changed $ Literal (RealVal 1.0) pos
       _ -> badCPropM $ TypeError pos  " pow operands not of (the same) numeral type! "
   |  isValue e1, isValue e2 =
     case (e1, e2) of
