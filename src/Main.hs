26cdbe2fbc57e32da102e4d9ec415dea516cb63c
Troels Henriksen <athas@sigkill.dk>
2013-11-12 18:43:49 +0100

    Add -s flag for using a sensible compilation pipeline.

--- src/Main.hs
+++ src/Main.hs
@@ -80,15 +80,18 @@ commandLineOptions =
   , Option "i" ["interpret"]
     (NoArg $ \opts -> opts { l0action = interpret })
     "Run the program via an interpreter."
-  , rename "r" ["rename"]
-  , hoist "o" ["hoist"]
-  , hoistAggr "O" ["hoist-aggressively"]
-  , normalize "n" ["normalize"]
-  , uttransform "u" ["untrace"]
-  , fotransform "f" ["first-order-transform"]
-  , tatransform "t" ["tuple-of-arrays-transform"]
-  , eotransform "e" ["enabling-optimisations"]
-  , hotransform "h" ["higher-order-optimizations"]
+  , renameOpt "r" ["rename"]
+  , hoistOpt "o" ["hoist"]
+  , hoistAggrOpt "O" ["hoist-aggressively"]
+  , normalizeOpt "n" ["normalize"]
+  , uttransformOpt "u" ["untrace"]
+  , fotransformOpt "f" ["first-order-transform"]
+  , tatransformOpt "t" ["tuple-of-arrays-transform"]
+  , eotransformOpt "e" ["enabling-optimisations"]
+  , hotransformOpt "h" ["higher-order-optimizations"]
+  , Option "s" ["standard"]
+    (NoArg $ \opts -> opts { l0pipeline = standardPipeline })
+    "Use the recommended optimised pipeline."
   ]
 
 interpret :: Prog -> IO ()
@@ -111,75 +114,101 @@ interpret prog =
                        exitWith $ ExitFailure 2
         Right val  -> putStrLn $ "Result of evaluation: " ++ ppValue val
 
+rename :: Pass
+rename = Pass { passName = "renamer"
+              , passOp = return . renameProg
+              }
+
+hoist :: Pass
+hoist = Pass { passName = "rebinder"
+             , passOp = return . RB.transformProg
+             }
+
+hoistAggr :: Pass
+hoistAggr = Pass { passName = "rebinder (aggressive)"
+                 , passOp = return . RB.transformProgAggr
+                 }
+
+normalize :: Pass
+normalize = Pass { passName = "full normalizer"
+                 , passOp = return . FN.normalizeProg
+                 }
+
+fotransform :: Pass
+fotransform = Pass { passName = "first-order transform"
+                   , passOp = return . FOT.transformProg
+                   }
+
+tatransform :: Pass
+tatransform = Pass { passName = "tuple-transform"
+                   , passOp = return . TT.transformProg
+                   }
+
+uttransform :: Pass
+uttransform = Pass { passName = "debugging annotation removal"
+                   , passOp = return . untraceProg
+                   }
+
+eotransform :: Pass
+eotransform = Pass { passName = "enabling optimations"
+                   , passOp = liftPass enablingOpts
+                   }
+
+hotransform :: Pass
+hotransform = Pass { passName = "higher-order optimisations"
+                   , passOp = liftPass highOrdTransf
+                   }
+
+standardPipeline :: [Pass]
+standardPipeline =
+  reverse [ uttransform, tatransform, normalize, hoist, eotransform,
+            hotransform, normalize, hoistAggr, eotransform ]
+
 passoption :: String -> Pass -> String -> [String] -> L0Option
 passoption desc pass short long =
   Option short long
   (NoArg $ \opts -> opts { l0pipeline = pass : l0pipeline opts })
   desc
 
-rename :: String -> [String] -> L0Option
-rename =
-  passoption "Rename all non-function identifiers to be unique."
-  Pass { passName = "renamer"
-       , passOp = return . renameProg
-       }
+renameOpt :: String -> [String] -> L0Option
+renameOpt =
+  passoption "Rename all non-function identifiers to be unique." rename
 
-hoist :: String -> [String] -> L0Option
-hoist =
-  passoption "Rebinder - hoisting, CSE, dependency graph compression."
-  Pass { passName = "rebinder"
-       , passOp = return . RB.transformProg
-       }
+hoistOpt :: String -> [String] -> L0Option
+hoistOpt =
+  passoption "Rebinder - hoisting, CSE, dependency graph compression." hoist
 
-hoistAggr :: String -> [String] -> L0Option
-hoistAggr =
+hoistAggrOpt :: String -> [String] -> L0Option
+hoistAggrOpt =
   passoption "Rebinder - hoisting, CSE, dependency graph compression (aggressively)."
-  Pass { passName = "rebinder (aggressive)"
-       , passOp = return . RB.transformProgAggr
-       }
+  hoistAggr
 
-normalize :: String -> [String] -> L0Option
-normalize =
-  passoption "Full normalization"
-  Pass { passName = "full normalizer"
-       , passOp = return . FN.normalizeProg
-       }
+normalizeOpt :: String -> [String] -> L0Option
+normalizeOpt =
+  passoption "Full normalization" normalize
 
-fotransform :: String -> [String] -> L0Option
-fotransform =
+fotransformOpt :: String -> [String] -> L0Option
+fotransformOpt =
   passoption "Transform all second-order array combinators to for-loops."
-  Pass { passName = "first-order transform"
-       , passOp = return . FOT.transformProg
-       }
+  fotransform
 
-tatransform :: String -> [String] -> L0Option
-tatransform =
-  passoption "Transform most uses of tuples away"
-  Pass { passName = "tuple-transform"
-       , passOp = return . TT.transformProg
-       }
+tatransformOpt :: String -> [String] -> L0Option
+tatransformOpt =
+  passoption "Transform most uses of tuples away" tatransform
 
-uttransform :: String -> [String] -> L0Option
-uttransform =
-  passoption "Remove debugging annotations from program."
-  Pass { passName = "debugging annotation removal"
-       , passOp = return . untraceProg
-       }
+uttransformOpt :: String -> [String] -> L0Option
+uttransformOpt =
+  passoption "Remove debugging annotations from program." uttransform
 
-
-eotransform :: String -> [String] -> L0Option
-eotransform =
+eotransformOpt :: String -> [String] -> L0Option
+eotransformOpt =
   passoption "Perform simple enabling optimisations."
-  Pass { passName = "enabling optimations"
-       , passOp = liftPass enablingOpts
-       }
+  eotransform
 
-hotransform :: String -> [String] -> L0Option
-hotransform =
+hotransformOpt :: String -> [String] -> L0Option
+hotransformOpt =
   passoption "Perform higher-order optimisation, i.e., fusion."
-  Pass { passName = "higher-order optimisations"
-       , passOp = liftPass highOrdTransf
-       }
+  hotransform
 
 -- | Entry point.  Non-interactive, except when reading interpreter
 -- input from standard input.
