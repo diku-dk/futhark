26256f213dc1652175868fa4bfb04bfc1621bbe8
Troels Henriksen <athas@sigkill.dk>
2013-12-09 13:21:54 +0100

    Make map^n recognition better.

--- src/L0C/HOTrans/SOAC.hs
+++ src/L0C/HOTrans/SOAC.hs
@@ -149,9 +149,19 @@ mapNfromSOAC (Map2 cs l as _ loc) =
   let (inner_cs, inner_l, inner_params) = descend l
   in Just (cs++inner_cs, inner_l, inner_params, as, loc)
   where descend l'
-          | Just (Map2 inner_cs inner_l inner_as _ _) <-
-              fromExp (tupleLambdaBody l'),
-            Just ks <- vars inner_as,
+          | Just (Map2 inner_cs inner_l inner_as _ _)
+              <- fromExp (tupleLambdaBody l') =
+              foundMap l' inner_cs inner_l inner_as
+          | (L0.LetPat
+                 (TupId pats _)
+                 (L0.Map2 inner_cs inner_l inner_as _ _)
+                 (L0.TupLit tupes _) _) <- tupleLambdaBody l',
+            Just tupvs <- vars tupes, map Id tupvs == pats =
+              foundMap l' inner_cs inner_l inner_as
+          | otherwise = ([], l', [])
+
+        foundMap l' inner_cs inner_l inner_as
+          | Just ks <- vars inner_as,
             tupleLambdaParams l' `matches` ks =
               let (inner_inner_cs, inner_inner_l, inner_inner_params) = descend inner_l
               in (inner_cs++inner_inner_cs,
