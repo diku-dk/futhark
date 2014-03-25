7999b5ed59a79af770c512d91d330684c9fb9fdd
Troels Henriksen <athas@sigkill.dk>
2014-03-25 19:42:40 +0100

    Document InternalRep a bit.

--- src/L0C/InternalRep.hs
+++ src/L0C/InternalRep.hs
@@ -1,3 +1,5 @@
+-- | This module exports the full internal reprensetation of the L0
+-- language.
 module L0C.InternalRep
   ( module L0C.InternalRep.Syntax
   , module L0C.InternalRep.Attributes
@@ -24,6 +26,9 @@ import L0C.FreshNames
 newNameSourceForProg :: Prog -> VNameSource
 newNameSourceForProg = newNameSource . progNames
 
+-- | A list.  Its 'Pretty' instance produces a comma-separated
+-- sequence enclosed in braces if the list has anything but a single
+-- element.
 newtype Several a = Several [a]
   deriving (Eq, Ord, Show)
 
@@ -31,5 +36,6 @@ instance Pretty a => Pretty (Several a) where
   ppr (Several [t]) = ppr t
   ppr (Several ts)  = braces $ commasep $ map ppr ts
 
+-- | Turn a single value into a singleton list.
 justOne :: a -> Several a
 justOne x = Several [x]
