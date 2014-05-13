8f83b81d9982b5c861c62e91d6e891b3d5c1e651
Troels Henriksen <athas@sigkill.dk>
2014-05-13 16:36:53 +0200

    Add range tracking across filter.

--- src/Futhark/EnablingOpts/SymbolTable.hs
+++ src/Futhark/EnablingOpts/SymbolTable.hs
@@ -116,6 +116,9 @@ insert name e vtable = insertEntry name bind vtable
             subExpRange se vtable
           Index _ v _ _ ->
             lookupRange (identName v) vtable
+          Filter _ _ [Var v] _ _ ->
+            lookupRange (identName v) vtable -- FIXME: Support filters
+                                             -- with more outputs.
           _ -> (Nothing, Nothing)
         zero = Val $ IntVal 0
 
