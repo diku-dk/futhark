e9b57149a4b0f0b628e149481901cc9a0243d9f2
Troels Henriksen <athas@sigkill.dk>
2014-05-14 15:03:46 +0200

    Simplification and dead code elim cannot fail.

--- src/Futhark/EnablingOpts/EnablingOptDriver.hs
+++ src/Futhark/EnablingOpts/EnablingOptDriver.hs
@@ -24,18 +24,15 @@ import Futhark.EnablingOpts.EnablingOptErrors
 
 enablingOpts :: Prog -> Either EnablingOptError Prog
 enablingOpts prog = do
-    prog_enopt1 <- normCopyDeadOpts prog
-    prog_enopt2 <- normCopyDeadOpts prog_enopt1
-    prog_deadf2 <- deadFunElim      prog_enopt2
-    prog_flat_opt <- normCopyDeadOpts prog_deadf2
+  let prog_enopt1 = normCopyDeadOpts prog
+  let prog_enopt2 = normCopyDeadOpts prog_enopt1
+  prog_deadf2 <- deadFunElim      prog_enopt2
+  let prog_flat_opt = normCopyDeadOpts prog_deadf2
 
-    normCopyDeadOpts prog_flat_opt
+  return $ normCopyDeadOpts prog_flat_opt
 
-normCopyDeadOpts :: Prog -> Either EnablingOptError Prog
-normCopyDeadOpts prog = do
-  let prog_cp  =  simplifyProg prog
-  let prog_dce = deadCodeElim prog_cp
-  return prog_dce
+normCopyDeadOpts :: Prog -> Prog
+normCopyDeadOpts = deadCodeElim . simplifyProg
 
 normCopyOneLambda :: MonadFreshNames m => Prog -> Lambda ->
                      m Lambda
