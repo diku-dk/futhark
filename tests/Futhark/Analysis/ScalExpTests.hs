2d0fb2f80c501b3e66fc89b3c196212f5a8745e2
Troels Henriksen <athas@sigkill.dk>
2014-06-11 20:42:08 +0200

    Add some trivial algebraic simplification tests.

---
+++ tests/Futhark/Analysis/ScalExpTests.hs
@@ -0,0 +1,123 @@
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Futhark.Analysis.ScalExpTests
+  ( tests
+  , parseScalExp
+  , parseScalExp'
+  )
+where
+
+import Test.Framework
+
+import Control.Applicative
+import Control.Monad.Identity
+import qualified Data.Map as M
+import Data.Loc
+import Text.Parsec hiding (token, (<|>), many)
+import Text.Parsec.Expr
+import Text.Parsec.Language
+import qualified Text.Parsec.Token as Token
+
+import Futhark.Analysis.ScalExp
+import Futhark.InternalRep hiding (constant)
+
+tests :: [Test]
+tests = []
+
+languageDef :: GenLanguageDef String u Identity
+languageDef =
+  emptyDef { Token.commentStart    = "/*"
+           , Token.commentEnd      = "*/"
+           , Token.commentLine     = "//"
+           , Token.identStart      = letter
+           , Token.identLetter     = alphaNum
+           , Token.reservedNames   = ["max", "min", "False", "True"]
+           , Token.reservedOpNames = ["+", "-", "*", "/"
+                                     , "<", ">", "<=", ">="
+                                     , "&&", "||", "not"
+                                     ]
+           }
+
+lexer :: Token.GenTokenParser String ParserState Identity
+lexer = Token.makeTokenParser languageDef
+
+parseScalExp :: String -> ScalExp
+parseScalExp = parseScalExp' M.empty
+
+parseScalExp' :: M.Map String (Int, Type) -> String -> ScalExp
+parseScalExp' m s = case runParser expr (0, m) ("string: " ++ s) s of
+  Left err -> error $ show err
+  Right e  -> e
+
+type ParserState = (Int, M.Map String (Int, Type))
+type Parser = ParsecT String ParserState Identity
+
+newVar :: String -> Type -> Parser Ident
+newVar s t = do (x, m) <- getState
+                case M.lookup s m of
+                  Just _ -> fail $ "Variable " ++ s ++ " double-declared."
+                  Nothing -> do setState (x+1, M.insert s (x,t) m)
+                                return $ Ident (ID (nameFromString s, x)) t noLoc
+
+knownVar :: String -> Parser Ident
+knownVar s = do (_, m) <- getState
+                case M.lookup s m of
+                  Just (y,t) -> return $ Ident (ID (nameFromString s, y)) t noLoc
+                  Nothing -> fail $ "Undeclared variable " ++ s
+
+reservedOp :: String -> Parser ()
+reservedOp = Token.reservedOp lexer
+
+reserved :: String -> Parser ()
+reserved = Token.reserved lexer
+
+parens :: Parser a -> Parser a
+parens = Token.parens lexer
+
+identifier :: Parser Ident
+identifier = do s <- (:) <$> letter <*> many alphaNum
+                varDecl s <|> knownVar s
+  where varDecl s = do
+          t <- parens $
+               (reserved "int" >> pure (Basic Int)) <|>
+               (reserved "real" >> pure (Basic Real)) <|>
+               (reserved "bool" >> pure (Basic Bool))
+          newVar s t
+
+constant :: Parser ScalExp
+constant = (reserved "True" >> pure (Val $ LogVal True)) <|>
+           (reserved "False" >> pure (Val $ LogVal True)) <|>
+           (Val . IntVal <$> integer)
+  where integer = fromIntegral <$> Token.integer lexer
+
+expr :: Parser ScalExp
+expr = buildExpressionParser operators prim
+
+prim :: Parser ScalExp
+prim = parens expr <|>
+       constant <|>
+       maxapp <|>
+       minapp <|>
+       (Id <$> identifier)
+  where maxapp = reserved "max" >> MaxMin False <$> parens (expr `sepBy` comma)
+        minapp = reserved "min" >> MaxMin True <$> parens (expr `sepBy` comma)
+        comma = Token.comma lexer
+
+operators :: [[Operator String ParserState Identity ScalExp]]
+operators = [ [Prefix (reservedOp "-"   >> return SNeg)          ]
+            , [Infix  (reservedOp "*"   >> return STimes) AssocLeft]
+            , [Infix  (reservedOp "pow"   >> return SPow) AssocLeft]
+            , [Infix  (reservedOp "/"   >> return SDivide) AssocLeft]
+            , [Infix  (reservedOp "+"   >> return SPlus) AssocLeft]
+            , [Infix  (reservedOp "-"   >> return SMinus) AssocLeft]
+            , [Infix  (reservedOp "<="   >> return leq) AssocLeft]
+            , [Infix  (reservedOp "<"   >> return lth) AssocLeft]
+            , [Infix  (reservedOp ">="   >> return (flip leq)) AssocLeft]
+            , [Infix  (reservedOp ">"   >> return (flip lth)) AssocLeft]
+            , [Infix  (reservedOp "&&"   >> return SLogAnd) AssocLeft]
+            , [Infix  (reservedOp "||"   >> return SLogOr) AssocLeft]
+            ]
+  where leq x y =
+          RelExp LEQ0 $ x `SMinus` y
+        lth x y =
+          RelExp LTH0 $ x `SMinus` y
--- tests/futhark_tests.hs
+++ tests/futhark_tests.hs
@@ -2,14 +2,15 @@ module Main (main) where
 
 import qualified Language.Futhark.CoreTests
 import qualified Futhark.InternalRep.AttributesTests
+import qualified Futhark.Optimise.AlgSimplifyTests
 
 import Test.Framework (defaultMain, testGroup, Test)
-
 
 allTests :: [Test]
 allTests =
   [ testGroup "CoreTests" Language.Futhark.CoreTests.tests
   , testGroup "AttributesTests" Futhark.InternalRep.AttributesTests.tests
+  , testGroup "AlgSimplifyTests" Futhark.Optimise.AlgSimplifyTests.tests
   ]
 
 main :: IO ()
