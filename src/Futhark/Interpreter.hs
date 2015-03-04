081df28d991342cc0caa858816b7897618fb2d0e
Troels Henriksen <athas@sigkill.dk>
2015-03-04 10:50:23 +0100

    Remove trace import.

--- src/Futhark/Interpreter.hs
+++ src/Futhark/Interpreter.hs
@@ -22,7 +22,7 @@ import Control.Applicative
 import Control.Monad.Reader
 import Control.Monad.Writer
 import Control.Monad.Except
-import Debug.Trace
+
 import Data.Array
 import Data.Bits
 import Data.List
