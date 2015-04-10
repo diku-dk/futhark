541c8028a1f8930a2f2394dd123c523d2e955af6
Troels Henriksen <athas@sigkill.dk>
2015-04-10 13:56:40 +0200

    Some work on TypeEnv.

--- src/Futhark/Representation/AST/Attributes/TypeEnv.hs
+++ src/Futhark/Representation/AST/Attributes/TypeEnv.hs
@@ -1,7 +1,10 @@
-{-# LANGUAGE FlexibleInstances, TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances, TypeSynonymInstances, GeneralizedNewtypeDeriving #-}
 module Futhark.Representation.AST.Attributes.TypeEnv
        ( HasTypeEnv (..)
        , TypeEnv
+         -- * Extended type environment
+       , ExtendedTypeEnv
+       , extendedTypeEnv
        ) where
 
 import Control.Applicative
@@ -19,11 +22,29 @@ type TypeEnv = HM.HashMap VName Type
 class Applicative m => HasTypeEnv m where
   lookupType :: VName -> m Type
   lookupType name =
-    maybe notFound id <$> HM.lookup name <$> askTypeEnv
-    where notFound = error $ "lookupType: Name " ++ textual name ++
-                     " not found in type environment."
+    fromMaybe notFound <$> HM.lookup name <$> askTypeEnv
+    where notFound =
+            error $ "TypeEnv.lookupType: Name " ++ textual name ++
+            " not found in type environment."
 
   askTypeEnv :: m TypeEnv
 
 instance (Applicative m, Monad m) => HasTypeEnv (ReaderT TypeEnv m) where
   askTypeEnv = ask
+
+-- | A monad transformer that carries around an extended 'TypeEnv'.
+-- Its 'lookupType' method will first look in the extended 'TypeEnv',
+-- and then use the 'lookupType' method of the underlying monad.
+newtype ExtendedTypeEnv m a = ExtendedTypeEnv (ReaderT TypeEnv m a)
+                            deriving (Functor, Applicative, Monad,
+                                      MonadReader TypeEnv)
+
+instance (HasTypeEnv m, Monad m) => HasTypeEnv (ExtendedTypeEnv m) where
+  lookupType name = do
+    res <- asks $ HM.lookup name
+    maybe (ExtendedTypeEnv $ lift $ lookupType name) return res
+  askTypeEnv = HM.union <$> ask <*> ExtendedTypeEnv (lift askTypeEnv)
+
+-- Run a computation in the extended type environment.
+extendedTypeEnv :: ExtendedTypeEnv m a -> TypeEnv -> m a
+extendedTypeEnv (ExtendedTypeEnv m) = runReaderT m
