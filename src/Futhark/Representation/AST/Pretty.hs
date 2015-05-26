1d7a33aaa479a73eedbfea3629789810156489c1
Troels Henriksen <athas@sigkill.dk>
2015-05-26 16:43:44 +0200

    Do not print IntDivide as mod.

--- src/Futhark/Representation/AST/Pretty.hs
+++ src/Futhark/Representation/AST/Pretty.hs
@@ -299,7 +299,7 @@ instance Pretty BinOp where
   ppr Pow = text "pow"
   ppr Times = text "*"
   ppr Divide = text "/"
-  ppr IntDivide = text "mod"
+  ppr IntDivide = text "div"
   ppr Mod = text "%"
   ppr ShiftR = text ">>"
   ppr ShiftL = text "<<"
