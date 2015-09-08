5e920d6310e1c6935bf64dc243ca1fa9a7b245ce
Troels Henriksen <athas@sigkill.dk>
2015-09-08 10:01:47 +0200

    Put OpenCL kernels in their own module.

--- src/Futhark/CodeGen/Backends/COpenCL.hs
+++ src/Futhark/CodeGen/Backends/COpenCL.hs
@@ -15,6 +15,7 @@ import Prelude
 import qualified Language.C.Syntax as C
 import qualified Language.C.Quote.OpenCL as C
 
+import qualified Futhark.CodeGen.OpenCL.Kernels as Kernels
 import Futhark.Representation.ExplicitMemory (Prog, pretty)
 import qualified Futhark.CodeGen.Backends.GenericC as GenericC
 import Futhark.CodeGen.Backends.SimpleRepresentation
@@ -352,53 +353,8 @@ compileKernel (Kernel kernel) =
             OpenClRequirements (used_funs ++ funs32_used ++ funs64_used) extra_pragmas)
 
 compileKernel kernel@(MapTranspose bt _ _ _ _ _ _ _) =
-  Right ([C.cfun|
-  // This kernel is optimized to ensure all global reads and writes are coalesced,
-  // and to avoid bank conflicts in shared memory.  The shared memory array is sized
-  // to (BLOCK_DIM+1)*BLOCK_DIM.  This pads each row of the 2D block in shared memory
-  // so that bank conflicts do not occur when threads address the array column-wise.
-  __kernel void $id:(kernelName kernel)(__global $ty:ty *odata,
-                                        unsigned int odata_offset,
-                                        __global $ty:ty *idata,
-                                        unsigned int idata_offset,
-                                        unsigned int width,
-                                        unsigned int height,
-                                        __local $ty:ty* block) {
-    unsigned int x_index;
-    unsigned int y_index;
-    unsigned int our_array_offset;
-
-    // Adjust the input and output arrays with the basic offset.
-    odata += odata_offset;
-    idata += idata_offset;
-
-    // Adjust the input and output arrays for the third dimension.
-    our_array_offset = get_global_id(2) * width * height;
-    odata += our_array_offset;
-    idata += our_array_offset;
-
-    // read the matrix tile into shared memory
-    x_index = get_global_id(0);
-    y_index = get_global_id(1);
-
-    if((x_index < width) && (y_index < height))
-    {
-        unsigned int index_in = y_index * width + x_index;
-        block[get_local_id(1)*(FUT_BLOCK_DIM+1)+get_local_id(0)] = idata[index_in];
-    }
-
-    barrier(CLK_LOCAL_MEM_FENCE);
-
-    // Write the transposed matrix tile to global memory.
-    x_index = get_group_id(1) * FUT_BLOCK_DIM + get_local_id(0);
-    y_index = get_group_id(0) * FUT_BLOCK_DIM + get_local_id(1);
-    if((x_index < height) && (y_index < width))
-    {
-        unsigned int index_out = y_index * height + x_index;
-        odata[index_out] = block[get_local_id(0)*(FUT_BLOCK_DIM+1)+get_local_id(1)];
-    }
-  }|],
-         OpenClRequirements [] [blockDimPragma])
+  Right (Kernels.mapTranspose (kernelName kernel) ty,
+         mempty)
   where ty = GenericC.scalarTypeToCType bt
 
 openClProgramHeader :: OpenClRequirements -> [C.Definition]
