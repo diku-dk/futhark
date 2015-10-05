176ed6a8bff94f813a98b81b3eefa441aa795c67
Daniel Gavin <danielgavin5@hotmail.com>
2015-10-05 16:28:48 +0200

    moved all pretty print over to GenericPythonAst.hs

--- src/Futhark/CodeGen/Backends/GenericPython.hs
+++ src/Futhark/CodeGen/Backends/GenericPython.hs
@@ -7,16 +7,3 @@ import Futhark.CodeGen.Backends.GenericPythonAst
 
 compileProg :: Program () -> String
 compileProg p = "print 'Hello World!'\n"
-
-instance Pretty PyStmt where
-  ppr (If cond tbranch fbranch) =
-    text "if" <+> ppr cond <+> text ":" </>
-    indent 2 (ppr tbranch) </>
-    text "else:" </>
-    indent 2 (ppr fbranch)
-  ppr (While cond body) =
-    text "while" <+> ppr cond <+> text ":" </>
-    indent 2 (ppr body)
-  ppr (For i limit body) =
-    text  "for" <+> ppr i <+> text "in range(" <+> ppr limit <+> text ")" </>
-    indent 2 (ppr body)
--- src/Futhark/CodeGen/Backends/GenericPythonAst.hs
+++ src/Futhark/CodeGen/Backends/GenericPythonAst.hs
@@ -30,6 +30,18 @@ instance Pretty PyExp where
     ppr (SetScalar v e) = text v <> text "=" <> ppr e 
   
 
+instance Pretty PyStmt where
+  ppr (If cond tbranch fbranch) =
+    text "if" <+> ppr cond <+> text ":" </>
+    indent 2 (ppr tbranch) </>
+    text "else:" </>
+    indent 2 (ppr fbranch)
+  ppr (While cond body) =
+    text "while" <+> ppr cond <+> text ":" </>
+    indent 2 (ppr body)
+  ppr (For i limit body) =
+    text  "for" <+> ppr i <+> text "in range(" <+> ppr limit <+> text ")" </>
+    indent 2 (ppr body)
 
             
             
