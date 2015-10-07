0d3986c8aabdc63fa1afff6fd804b319bc94e9f2
Daniel Gavin <danielgavin5@hotmail.com>
2015-10-07 12:30:37 +0200

    removed GenericPython.hs and GenericPythonAst.hs from Backends

--- src/Futhark/CodeGen/Backends/GenericPython.hs
+++
@@ -1,9 +0,0 @@
-module Futhark.CodeGen.Backends.GenericPython
-  ( compileProg ) where
-
-import Futhark.CodeGen.ImpCode
-
-import Futhark.CodeGen.Backends.GenericPythonAst
-
-compileProg :: Program () -> String
-compileProg p = "print 'Hello World!'\n"
