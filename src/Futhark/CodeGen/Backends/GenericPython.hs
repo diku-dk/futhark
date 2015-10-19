5a98f0f2ddc218d2179b49661e8fa263057bfbfb
Abelskov <hjalte.diku@gmail.com>
2015-10-19 20:43:12 +0200

    Added UnOp and Cond

--- src/Futhark/CodeGen/Backends/GenericPython.hs
+++ src/Futhark/CodeGen/Backends/GenericPython.hs
@@ -26,38 +26,29 @@ import Futhark.Util.Pretty(pretty)
 data CompilerEnv = CompilerEnv {
   envFtable     :: HM.HashMap Name [Imp.Type]
 }
-newCompilerEnv :: Imp.Program () 
-               -> CompilerEnv
-newCompilerEnv (Imp.Program funs) =
-  CompilerEnv { 
-               envFtable = ftable <> builtinFtable
-              }
-  where ftable = HM.fromList $ map funReturn funs
-        funReturn (name, Imp.Function outparams _ _ _ _) =
-          (name, paramsTypes outparams)
-        builtinFtable =
-          HM.map (map Imp.Scalar . snd) builtInFunctions
 
+newCompilerEnv :: Imp.Program () -> CompilerEnv
+newCompilerEnv (Imp.Program funs) =
+  CompilerEnv { envFtable = ftable <> builtinFtable }
+  where ftable = HM.fromList $ map funReturn funs
+        funReturn (name, Imp.Function outparams _ _ _ _) = (name, paramsTypes outparams)
+        builtinFtable = HM.map (map Imp.Scalar . snd) builtInFunctions
 
 data CompilerState = CompilerState {
   compNameSrc :: VNameSource
 }
-newCompilerState :: VNameSource -> CompilerState
-newCompilerState src = CompilerState {
-                                      compNameSrc = src
-                                     }
 
+newCompilerState :: VNameSource -> CompilerState
+newCompilerState src = CompilerState { compNameSrc = src }
 
 newtype CompilerM a = CompilerM (RWS
-                                  CompilerEnv
-                                  [PyStmt]
-                                  CompilerState a)
+                                     CompilerEnv
+                                     [PyStmt]
+                                     CompilerState a)
   deriving (Functor, Applicative, Monad,
             MonadState CompilerState,
             MonadReader CompilerEnv,
             MonadWriter [PyStmt])
-
-
 
 collect :: CompilerM () -> CompilerM [PyStmt]
 collect m = pass $ do
@@ -80,8 +71,7 @@ paramsTypes = map paramType
   where paramType (Imp.MemParam _ size space) = Imp.Mem size space
         paramType (Imp.ScalarParam _ t) = Imp.Scalar t
 
-
-runCompilerM :: Imp.Program () -> VNameSource 
+runCompilerM :: Imp.Program () -> VNameSource
              -> CompilerM a
              -> a
 runCompilerM prog src (CompilerM m) =
@@ -93,16 +83,58 @@ compileProg prog@(Imp.Program funs) =
   pretty $ PyProg $ runCompilerM prog blankNameSource (mapM compileFunc funs)
 
 compileFunc :: (Name, Imp.Function ()) -> CompilerM PyFunc
-compileFunc (fname, (Imp.Function outputs inputs body _ _)) = do
+compileFunc (fname, Imp.Function outputs inputs body _ _) = do
   body' <- collect $ compileCode body
   let inputs' = map (pretty . Imp.paramName) inputs
+  --let pyrettype = typeToPyType $ paramsTypes outputs
+  --let retval = PyStmt
   return $ PyFunc (nameToString fname) inputs' body'
+
+--data UnOp = Not -- ^ Boolean negation.
+--          | Complement -- ^ Bitwise complement.
+--          | Negate -- ^ Numerical negation.
+--          | Abs -- ^ Absolute/numerical value.
+--          | Signum -- ^ Sign function.
+--            deriving (Eq, Show)
+
+compileUnOp :: Imp.UnOp -> String
+compileUnOp op =
+  case op of
+    Imp.Not -> "!"
+    Imp.Complement -> "~"
+    Imp.Negate -> "-"
+    Imp.Abs -> "abs"
+    Imp.Signum -> undefined
+
+--data BinOp = Plus -- Binary Ops for Numbers
+--           | Minus
+--           | Pow
+--           | Times
+--           | FloatDiv
+--           | Div -- ^ Rounds towards negative infinity.
+--           | Mod
+--           | Quot -- ^ Rounds towards zero.
+--           | Rem
+--           | ShiftR
+--           | ShiftL
+--           | Band
+--           | Xor
+--           | Bor
+--           | LogAnd
+--           | LogOr
+--           -- Relational Ops for all basic types at least
+--           | Equal
+--           | Less
+--           | Leq
+--           deriving (Eq, Ord, Enum, Bounded, Show)
 
 compileBinOp :: BinOp -> String
 compileBinOp op =
   case op of
     Plus -> "+"
     Minus -> "-"
+    Div -> "/"
+    Times -> "*"
     Equal -> "=="
     _ -> undefined
 
@@ -110,6 +142,8 @@ compileExp :: Imp.Exp -> PyExp
 compileExp (Imp.Constant v) = Constant v
 compileExp (Imp.ScalarVar vname) = ScalarVar $ pretty vname
 compileExp (Imp.BinOp op exp1 exp2) = BinaryOp (compileBinOp op) (compileExp exp1) (compileExp exp2)
+compileExp (Imp.UnOp op exp) = UnOp (compileUnOp op) (compileExp exp)
+compileExp (Imp.Cond exp1 exp2 exp3) = Cond (compileExp exp1) (compileExp exp2) (compileExp exp3)
 compileExp _ = undefined
 
 -- get everything to work except memblocks and function calls
@@ -119,14 +153,24 @@ compileCode (Imp.If cond tb fb) = do
   tb' <- collect $ compileCode tb
   fb' <- collect $ compileCode fb
   stm $ If cond' tb' fb'
+
 compileCode (c1 Imp.:>>: c2) = do
   compileCode c1
   compileCode c2
---compileCode (Imp.While cond body) = While (compileExp cond) (compileCode body)
---compileCode (Imp.For i bound body) = For (baseString i) (compileExp bound) (compileCode body)
+
+compileCode (Imp.While cond body) = do
+  let cond' = compileExp cond
+  body' <- collect $ compileCode body
+  stm $ While cond' body'
+
+compileCode (Imp.For i bound body) = do
+  let bound' = compileExp bound
+  body' <- collect $ compileCode body
+  stm $ For (pretty i) bound' body'
+
 compileCode (Imp.SetScalar vname exp1) =
   stm $ AssignVar (pretty vname) (compileExp exp1)
+
 compileCode (Imp.DeclareScalar _ _) = return ()
+
 compileCode c = fail $ "This is not handled yet: " ++ pretty c
-
-
--- src/Futhark/CodeGen/Backends/GenericPython/AST.hs
+++ src/Futhark/CodeGen/Backends/GenericPython/AST.hs
@@ -12,18 +12,31 @@ import Text.PrettyPrint.Mainland hiding (space)
 
 type VariableName = String
 
+data UnOp = Not -- ^ Boolean negation.
+          | Complement -- ^ Bitwise complement.
+          | Negate -- ^ Numerical negation.
+          | Abs -- ^ Absolute/numerical value.
+            deriving (Eq, Show)
+
 data PyExp = Constant BasicValue
            | ScalarVar VariableName
            | BinaryOp String PyExp PyExp
+           | UnOp String PyExp
+           | Cond PyExp PyExp PyExp
              deriving (Eq, Show)
 
 data PyStmt = If PyExp [PyStmt] [PyStmt]
-            | While PyExp PyStmt
-            | For VariableName PyExp PyStmt
+            | While PyExp [PyStmt]
+            | For VariableName PyExp [PyStmt]
             | AssignVar VariableName PyExp
             deriving (Eq, Show)
 
-data PyFunc = PyFunc String [String] [PyStmt]
+data PyReturn = PyReturnScalar PyExp
+              | PyReturnTuple [PyExp]
+              | PyReturnList [PyExp]
+
+
+data PyFunc = PyFunc String [String] [PyStmt] --PyReturn
 
 data PyProg = PyProg [PyFunc]
 
@@ -31,6 +44,8 @@ instance Pretty PyExp where
     ppr (Constant v) = ppr v
     ppr (ScalarVar n) = text n
     ppr (BinaryOp s e1 e2) = ppr e1 <> text s <> ppr e2
+    ppr (UnOp s e) = text s <> parens (ppr e)
+    ppr (Cond e1 e2 e3) = ppr e2 <+> text "if" <+> ppr e1 <+> text "else" <+> ppr e3
 
 instance Pretty PyStmt where
   ppr (If cond tbranch fbranch) =
@@ -40,11 +55,16 @@ instance Pretty PyStmt where
     indent 2 (stack $ map ppr fbranch)
   ppr (While cond body) =
     text "while" <+> ppr cond <+> text ":" </>
-    indent 2 (ppr body)
+    indent 2 (stack $ map ppr body)
   ppr (For i limit body) =
     text  "for" <+> ppr i <+> text "in range" <+> parens (ppr limit) <+> text ":" </>
-    indent 2 (ppr body)
+    indent 2 (stack $ map ppr body)
   ppr (AssignVar v e) = text v <+> text "=" <+> ppr e
+
+instance Pretty PyReturn where
+  ppr (PyReturnScalar expr) = ppr expr
+  ppr (PyReturnTuple exprs) = parens (stack $ map ppr exprs)
+  ppr (PyReturnList exprs) = text "[" <> stack (map ppr exprs) <> text "]"
 
 instance Pretty PyFunc where
   ppr (PyFunc fname params body) =
