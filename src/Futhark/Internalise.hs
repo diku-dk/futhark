6974677e8f967a0359ab2ea96397cf50e1a1a252
Troels Henriksen <athas@sigkill.dk>
2016-01-25 10:16:45 +0100

    Fix internalisation of ToInt.

--- src/Futhark/Internalise.hs
+++ src/Futhark/Internalise.hs
@@ -633,7 +633,7 @@ internaliseExp desc (E.UnOp (E.ToInt int_to) e _) = do
   e' <- internaliseExp1 "trunc_arg" e
   case E.typeOf e of
     E.Prim (E.IntType int_from) ->
-      let op = if int_to < int_from then I.SExt else I.Trunc
+      let op = if int_to < int_from then I.Trunc else I.SExt
       in letTupExp' desc $ I.PrimOp $ I.ConvOp (op int_from int_to) e'
     E.Prim (E.FloatType float_from) ->
       letTupExp' desc $ I.PrimOp $ I.ConvOp (I.FPToSI float_from int_to) e'
