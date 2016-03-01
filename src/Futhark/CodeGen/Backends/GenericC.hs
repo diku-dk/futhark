d30b720282c85a203596c7757713389ef729dd5d
Troels Henriksen <athas@sigkill.dk>
2016-03-01 10:51:08 +0100

    Move timeval_subtract to avoid warnings.

--- src/Futhark/CodeGen/Backends/GenericC.hs
+++ src/Futhark/CodeGen/Backends/GenericC.hs
@@ -544,6 +544,15 @@ $esc:("#include <assert.h>")
 $esc:("#include <err.h>")
 $esc:("#include <getopt.h>")
 
+int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1)
+{
+    unsigned int resolution=1000000;
+    long int diff = (t2->tv_usec + resolution * t2->tv_sec) - (t1->tv_usec + resolution * t1->tv_sec);
+    result->tv_sec = diff / resolution;
+    result->tv_usec = diff % resolution;
+    return (diff<0);
+}
+
 $edecls:decls
 
 $edecls:(typeDefinitions endstate)
@@ -557,15 +566,6 @@ $edecls:builtin
 $edecls:(map funcToDef definitions)
 
 $edecls:readerFunctions
-
-int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1)
-{
-    unsigned int resolution=1000000;
-    long int diff = (t2->tv_usec + resolution * t2->tv_sec) - (t1->tv_usec + resolution * t1->tv_sec);
-    result->tv_sec = diff / resolution;
-    result->tv_usec = diff % resolution;
-    return (diff<0);
-}
 
 static typename FILE *runtime_file;
 
