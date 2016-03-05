7674df18418ac7db451ffc3c49c0a203fbe583d6
Troels Henriksen <athas@sigkill.dk>
2016-03-05 23:09:05 +0100

    Also report total OpenCL kernel runtimes.

--- src/Futhark/CodeGen/Backends/COpenCL/Boilerplate.hs
+++ src/Futhark/CodeGen/Backends/COpenCL/Boilerplate.hs
@@ -372,8 +372,9 @@ openClReport = map reportKernel
               total_runtime = name ++ "_total_runtime"
           in [C.cstm|
                fprintf(stderr,
-                       "Kernel %s executed %d times, with average runtime:\t %6ldus\n",
+                       "Kernel %s executed %d times, with average runtime:\t %6ldus\tand total runtime:\t %6ld\n",
                        $string:name,
                        $id:runs,
-                       (long int) $id:total_runtime / ($id:runs != 0 ? $id:runs : 1));
+                       (long int) $id:total_runtime / ($id:runs != 0 ? $id:runs : 1),
+                       (long int) $id:total_runtime);
              |]
