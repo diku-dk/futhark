2dc391f68f737dfe43b9ad5e64e86d14b4e07693
Troels Henriksen <athas@sigkill.dk>
2016-04-04 10:47:47 +0200

    Remove unused function.

--- src/Futhark/CodeGen/Backends/CUtils.hs
+++ src/Futhark/CodeGen/Backends/CUtils.hs
@@ -2,7 +2,6 @@
 -- | Simple C utility combinators.  You should import this qualified, as it shadows some standard names.
 module Futhark.CodeGen.Backends.CUtils
   ( product
-  , sum
   , var
   )
   where
@@ -18,13 +17,6 @@ product :: [C.Exp] -> C.Exp
 product []     = [C.cexp|1|]
 product (e:es) = foldl mult e es
   where mult x y = [C.cexp|$exp:x * $exp:y|]
-
--- | Return an expression summing together the given expressions.
--- If an empty list is given, the expression @0@ is returned.
-sum :: [C.Exp] -> C.Exp
-sum []     = [C.cexp|0|]
-sum (e:es) = foldl add e es
-  where add x y = [C.cexp|$exp:x + $exp:y|]
 
 -- | Turn a name into a C expression consisting of just that name.
 var :: C.ToIdent a => a -> C.Exp
