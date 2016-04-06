679b864ad100cc0992e2dc80f7129ec563c2de1c
Troels Henriksen <athas@sigkill.dk>
2016-04-06 09:29:24 +0200

    COpenCL also has local memory space.
    Kind of a hack, but eh.

--- src/Futhark/CodeGen/Backends/COpenCL.hs
+++ src/Futhark/CodeGen/Backends/COpenCL.hs
@@ -28,7 +28,7 @@ compileProg prog = do
     Left err -> return $ Left err
     Right (Program opencl_code opencl_prelude kernel_names prog') -> do
       Right <$> GenericC.compileProg operations ()
-                [Space "device", DefaultSpace]
+                [Space "device", Space "local", DefaultSpace]
                 (openClDecls transposeBlockDim kernel_names opencl_code opencl_prelude)
                 openClInit
                 [[C.cstm|OPENCL_SUCCEED(clFinish(fut_cl_queue));|]]
