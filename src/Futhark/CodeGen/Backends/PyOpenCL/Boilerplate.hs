f43f54296ad9929c9745c7337b4f624bbe748823
Troels Henriksen <athas@sigkill.dk>
2016-04-12 16:13:35 +0200

    Use newest neat-interpolation.

--- src/Futhark/CodeGen/Backends/PyOpenCL/Boilerplate.hs
+++ src/Futhark/CodeGen/Backends/PyOpenCL/Boilerplate.hs
@@ -3,14 +3,17 @@ module Futhark.CodeGen.Backends.PyOpenCL.Boilerplate
   ( openClInit
   ) where
 
-import NeatInterpolation
+import qualified Data.Text as T
+
+import NeatInterpolation (text)
 
 openClInit :: String -> String
-openClInit assign = [string|
+openClInit assign = T.unpack [text|
 self.ctx = cl.create_some_context(interactive=False)
 self.queue = cl.CommandQueue(self.ctx)
 if (len(fut_opencl_src) >= 0):
   program = cl.Program(self.ctx, fut_opencl_src).build(["-DFUT_BLOCK_DIM={}".format(FUT_BLOCK_DIM), "-DWAVE_SIZE=32"])
 
-$assign
+$assign'
 |]
+  where assign' = T.pack assign
