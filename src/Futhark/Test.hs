167eee55c96b7643a5dffaf4d3970a21180cc626
Troels Henriksen <athas@sigkill.dk>
2016-04-16 12:27:31 +0200

    Never use zero tolerance.

--- src/Futhark/Test.hs
+++ src/Futhark/Test.hs
@@ -344,7 +344,7 @@ minTolerance :: Fractional a => a
 minTolerance = 0.002 -- 0.2%
 
 tolerance :: A.Array Int PrimValue -> Double
-tolerance = foldl' tolerance' 0
+tolerance = foldl' tolerance' minTolerance
   where tolerance' t (FloatValue (Float32Value v)) = max t $ minTolerance * floatToDouble v
         tolerance' t (FloatValue (Float64Value v)) = max t $ minTolerance * v
         tolerance' t _                             = t
