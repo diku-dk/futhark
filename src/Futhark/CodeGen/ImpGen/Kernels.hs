e950ff81f903c63efaffbc7f05cffd9943559fb5
Troels Henriksen <athas@sigkill.dk>
2016-05-28 11:29:20 +0200

    Handle map-out parts in scan kernels.

--- src/Futhark/CodeGen/ImpGen/Kernels.hs
+++ src/Futhark/CodeGen/ImpGen/Kernels.hs
@@ -27,6 +27,7 @@ import qualified Futhark.Analysis.ScalExp as SE
 import qualified Futhark.Representation.ExplicitMemory.IndexFunction as IxFun
 import Futhark.CodeGen.SetDefaultSpace
 import Futhark.Tools (partitionChunkedKernelLambdaParameters)
+import Futhark.Util (splitAt3)
 import Futhark.Util.IntegralExp (quotRoundingUp, quot)
 
 type CallKernelGen = ImpGen.ImpM Imp.HostOp
@@ -406,7 +407,8 @@ kernelCompiler
 kernelCompiler
   (ImpGen.Destination dest)
   (ScanKernel _ _ kernel_size lam foldlam nes arrs) = do
-    let (arrs_dest, partials_dest) = splitAt (length nes) dest
+    let (arrs_dest, partials_dest, map_dest) =
+          splitAt3 (length nes) (length nes) dest
     (local_id, group_id, wave_size, skip_waves, global_id) <- kernelSizeNames
     thread_chunk_size <- newVName "thread_chunk_size"
     chunks_per_group <- newVName "chunks_per_group"
@@ -460,13 +462,23 @@ kernelCompiler
             (Imp.sizeToExp num_elements `quotRoundingUp`
              Imp.sizeToExp num_threads)
 
-        fold_x_dest <- ImpGen.destinationFromParams fold_x_params
+        ImpGen.Destination fold_x_targets <- ImpGen.destinationFromParams fold_x_params
+
+        let indexMapTarget (ImpGen.ArrayDestination
+                              (ImpGen.CopyIntoMemory dest_loc) (_:dest_dims)) = do
+              let dest_loc' = ImpGen.sliceArray dest_loc [ImpGen.varIndex fold_lam_i]
+              return $ ImpGen.ArrayDestination (ImpGen.CopyIntoMemory dest_loc') $
+                Nothing : dest_dims
+            indexMapTarget _ =
+              throwError "indexMapTarget: invalid target for map-out."
+
+        mapout_targets <- mapM indexMapTarget map_dest
+        let fold_dest = ImpGen.Destination $ fold_x_targets <> mapout_targets
 
         y_dest <- ImpGen.destinationFromParams y_params
 
         set_fold_x_to_ne <- ImpGen.collect $
-          zipWithM_ ImpGen.compileSubExpTo
-          (ImpGen.valueDestinations fold_x_dest) nes
+          zipWithM_ ImpGen.compileSubExpTo fold_x_targets nes
 
         ImpGen.emit set_fold_x_to_ne
 
@@ -488,7 +500,7 @@ kernelCompiler
 
           apply_fold_fun <- ImpGen.collect $ do
             zipWithM_ readFoldElement fold_y_params arrs
-            ImpGen.compileBody fold_x_dest $ lambdaBody foldlam
+            ImpGen.compileBody fold_dest $ lambdaBody foldlam
 
           let is_in_bounds = Imp.CmpOp (CmpUlt Int32)
                              (Imp.ScalarVar fold_lam_i) (Imp.sizeToExp num_elements)
