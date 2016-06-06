82d501e55386a356c360df67577e0c5fa11ca60d
Troels Henriksen <athas@sigkill.dk>
2016-06-06 15:45:20 +0200

    Reshape of manifest array is just manifest of the new shape.

--- src/Futhark/Representation/ExplicitMemory/IndexFunction.hs
+++ src/Futhark/Representation/ExplicitMemory/IndexFunction.hs
@@ -187,9 +187,8 @@ rotate ixfun offsets = Rotate ixfun offsets
 reshape :: (Eq num, IntegralCond num) =>
            IxFun num -> ShapeChange num -> IxFun num
 
-reshape (Direct oldshape) newshape
-  | length oldshape == length newshape =
-      Direct $ map newDim newshape
+reshape (Direct oldshape) newshape =
+  Direct $ map newDim newshape
 
 reshape (Reshape ixfun _) newshape =
   reshape ixfun newshape
