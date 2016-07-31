4e91a2c28ff3ff31624b928d0eb4068a5fa3e4b9
Troels Henriksen <athas@sigkill.dk>
2016-07-31 15:47:08 +0200

    Why was this check there?

--- src/Futhark/Pass/KernelBabysitting.hs
+++ src/Futhark/Pass/KernelBabysitting.hs
@@ -248,11 +248,10 @@ ensureCoalescedAccess expmap thread_gids boundOutside arr is = do
         perm /= [0..length perm-1] ->
           replace =<< lift (rearrangeInput (nonlinearInMemory arr expmap) perm arr)
 
-      -- We are not fully indexing the array, and the indices are not
-      -- a prefix of the thread indices, so we assume (HEURISTIC!)
-      -- that the remaining dimensions will be traversed sequentially.
-      | length is < arrayRank t,
-        is /= map Var (take (length is) thread_gids) -> do
+      -- We are not fully indexing the array, so we assume
+      -- (HEURISTIC!)  that the remaining dimensions will be traversed
+      -- sequentially.
+      | length is < arrayRank t -> do
           let perm = coalescingPermutation (length is) $ arrayRank t
           replace =<< lift (rearrangeInput (nonlinearInMemory arr expmap) perm arr)
 
