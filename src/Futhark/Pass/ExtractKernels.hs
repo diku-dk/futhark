28e692687de047f9cfba171f2874bf38bf697b4b
Troels Henriksen <athas@sigkill.dk>
2016-10-16 10:32:26 +0200

    Oops, the world is not ready for this.

--- src/Futhark/Pass/ExtractKernels.hs
+++ src/Futhark/Pass/ExtractKernels.hs
@@ -578,7 +578,7 @@ lambdaContainsParallelism = bodyContainsParallelism . lambdaBody
 -- Enable if you want the cool new versioned code.  Beware: may be
 -- slower in practice.  Caveat emptor (and you are the emptor).
 versionedCode :: Bool
-versionedCode = True
+versionedCode = False
 
 distributeInnerMap :: Pattern -> MapLoop -> KernelAcc
                    -> KernelM KernelAcc
