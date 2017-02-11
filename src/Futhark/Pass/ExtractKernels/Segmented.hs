7c78e1d579a9b26312acd15d889d99d63d5b1620
Rasmus Wriedt Larsen <rasmuswriedtlarsen@gmail.com>
2017-02-11 14:07:47 +0100

    segredomap: handle many small segments by segscan
    Currently doesn't support map-invariant variables (eg ex4.fut)

--- src/Futhark/Pass/ExtractKernels/Segmented.hs
+++ src/Futhark/Pass/ExtractKernels/Segmented.hs
@@ -102,6 +102,12 @@ regularSegmentedRedomap segment_size num_segments _nest_sizes flat_pat
                                                 other_offset_param :
                                                 lambdaParams reduce_lam
                                }
+  flag_reduce_lam <- addFlagToLambda nes reduce_lam
+  let flag_reduce_lam' = flag_reduce_lam { lambdaParams = my_index_param :
+                                                          other_offset_param :
+                                                          lambdaParams flag_reduce_lam
+                                         }
+
 
   -- FIXME: do we need to copy arrays here? :S
   -- see 'blockedReductionStream' in BlockedKernel.hs
@@ -110,11 +116,17 @@ regularSegmentedRedomap segment_size num_segments _nest_sizes flat_pat
   (ogps_ses, ogps_stms) <- runBinder $ oneGroupPerSeg all_arrs reduce_lam' kern_chunk_fold_lam
   (mgps_ses, mgps_stms) <- runBinder $ manyGroupPerSeg all_arrs reduce_lam' kern_chunk_fold_lam kern_chunk_reduce_lam
 
+  (ogms_ses, ogms_stms) <- runBinder $ oneGroupManySeg map_out_arrs flag_reduce_lam'
+
   -- TODO: should unify this calculation somewhere, with the one taking place in
   -- 'groupPerSegmentKernel'
   num_groups_hint <- letSubExp "num_groups_hint" $ Op NumGroups
   num_groups_per_segment <- letSubExp "num_groups_per_segment" =<<
     eDivRoundingUp Int32 (eSubExp num_groups_hint) (eSubExp num_segments)
+
+  group_size <- letSubExp "group_size" $ Op GroupSize
+  num_segments_per_group <- letSubExp "num_segments_per_group" $
+    BasicOp $ BinOp (UDiv Int32) group_size segment_size
 
   -- Instead of making this @if@ here, we could run "multiple groups per
   -- segment" first, and if the number of groups per segment was 1, we could
@@ -122,10 +134,16 @@ regularSegmentedRedomap segment_size num_segments _nest_sizes flat_pat
   -- chose not to do this, as the oneGroupPerSegment kernel will contain
   -- slightly fewer instructions -- and hopefully be a bit faster (at the cost
   -- of large code size). TODO: test how much we win by doing this.
-  e <- eIf (eCmpOp (CmpEq $ IntType Int32) (eSubExp num_groups_per_segment)
-                                           (eSubExp one))
-           (mkBodyM ogps_stms ogps_ses)
-           (mkBodyM mgps_stms mgps_ses)
+
+
+  let e_no_small_seg = eIf (eCmpOp (CmpEq $ IntType Int32) (eSubExp num_groups_per_segment)
+                                                           (eSubExp one))
+                        (mkBodyM ogps_stms ogps_ses)
+                        (mkBodyM mgps_stms mgps_ses)
+
+  e <- eIf (eCmpOp (CmpSlt Int32) (eSubExp one) (eSubExp num_segments_per_group))
+         (mkBodyM ogms_stms ogms_ses)
+         (eBody [e_no_small_seg])
 
   redres_pes <- forM (take num_redres (patternValueElements pat)) $ \pe -> do
     vn' <- newName $ patElemName pe
@@ -207,6 +225,22 @@ regularSegmentedRedomap segment_size num_segments _nest_sizes flat_pat
 
       let result_pes = second_redres_pes ++ mapres_pes
       return $ map (Var . patElemName) result_pes
+
+    ----------------------------------------------------------------------------
+    oneGroupManySeg map_out_arrs flag_reduce_lam' = do
+      red_scratch_arrs <- forM (take num_redres $ patternIdents pat) $ \(Ident name t) -> do
+        tmp <- letExp (baseString name <> "_redres_scratch") $
+               BasicOp $ Scratch (elemType t) (arrayDims t)
+        letExp (baseString name ++ "_redres_scratch") $
+                BasicOp $ Reshape cs [DimNew num_segments] tmp
+
+      let scratch_arrays = red_scratch_arrs ++ map_out_arrs
+
+      e <- oneGroupManySegmentKernel segment_size num_segments cs
+                          arrs_flat scratch_arrays
+                          comm flag_reduce_lam' fold_lam
+                          nes w ispace inps
+      letTupExp' "kernel_result" $ Op e
 
 groupPerSegmentKernel :: (MonadBinder m, Lore m ~ Kernels) =>
           SubExp            -- segment_size
@@ -378,6 +412,266 @@ groupPerSegmentKernel segment_size num_segments cs all_arrs comm
       letSubExp "offset" e
     makeOffsetExp (SplitStrided _) index_within_segment _elements_per_thread segment_index = do
       e <- eBinOp (Add Int32) (eSubExp index_within_segment)
+             (eBinOp (Mul Int32) (eSubExp segment_size) (eSubExp segment_index))
+      letSubExp "offset" e
+
+oneGroupManySegmentKernel :: (MonadBinder m, Lore m ~ Kernels) =>
+          SubExp            -- segment_size
+       -> SubExp            -- num_segments
+       -> Certificates      -- cs
+       -> [VName]           -- redin_arrs: flat arrays (containing input to fold_lam)
+       -> [VName]           -- scratch_arrs: Preallocated space that we can write into
+       -> Commutativity     -- comm
+       -> Lambda InKernel   -- flag_reduce_lam'
+       -> Lambda InKernel   -- fold_lam
+       -> [SubExp]          -- nes
+       -> SubExp            -- w = total_num_elements
+       -> [(VName, SubExp)] -- ispace = pair of (gtid, size) for the maps on "top" of this redomap
+       -> [KernelInput]     -- inps = inputs that can be looked up by using the gtids from ispace
+       -> m (Kernel InKernel)
+oneGroupManySegmentKernel segment_size num_segments cs redin_arrs scratch_arrs
+                      _comm flag_reduce_lam' fold_lam_unrenamed
+                      nes w ispace inps = do
+  let num_redres = length nes -- number of reduction results (tuple size for
+                              -- reduction operator)
+
+  fold_lam <- renameLambda fold_lam_unrenamed
+
+  group_size <- letSubExp "group_size" $ Op GroupSize
+
+  num_segments_per_group <- letSubExp "num_segments_per_group" $
+    BasicOp $ BinOp (UDiv Int32) group_size segment_size
+
+  num_groups <- letSubExp "num_groups" =<<
+    eDivRoundingUp Int32 (eSubExp num_segments) (eSubExp num_segments_per_group)
+
+  num_threads <- letSubExp "num_threads" $
+    BasicOp $ BinOp (Mul Int32) num_groups group_size
+
+  active_threads_per_group <- letSubExp "active_threads_per_group" $
+    BasicOp $ BinOp (Mul Int32) segment_size num_segments_per_group
+
+  let remainder_last_group = eBinOp (UMod Int32) (eSubExp num_segments) (eSubExp num_segments_per_group)
+
+  segments_in_last_group <- letSubExp "seg_in_last_group" =<<
+    eIf (eCmpOp (CmpEq $ IntType Int32) remainder_last_group
+                                        (eSubExp zero))
+        (eBody [eSubExp num_segments_per_group])
+        (eBody [remainder_last_group])
+
+  active_threads_in_last_group <- letSubExp "active_threads_last_group" $
+    BasicOp $ BinOp (Mul Int32) segment_size segments_in_last_group
+
+  total_wanted_threads <- letSubExp "totalwantedthreads" =<<
+    eBinOp (Add Int32)
+        (eBinOp (Mul Int32)
+            (eBinOp (Sub Int32) (eSubExp num_groups) (eSubExp one))
+            (eSubExp group_size))
+        (eSubExp active_threads_in_last_group)
+
+  gtid_vn <- newVName "gtid"
+
+  -- FIXME: Can't handle map-invariant variables right now, eg. ex4.fut
+
+  -- the array passed here is the structure for how to layout the kernel space
+  space <- newKernelSpace (num_groups, group_size, num_threads) $
+    FlatThreadSpace []
+
+  ------------------------------------------------------------------------------
+  -- What follows is the statements used in the kernel
+  ------------------------------------------------------------------------------
+
+  let lid = Var $ spaceLocalId space
+
+  ((segment_index, index_within_segment), calc_segindex_stms) <- runBinder $ do
+    segment_index <- letSubExp "segment_index" =<<
+      eBinOp (Add Int32)
+        (eBinOp (UDiv Int32) (eSubExp $ Var $ spaceLocalId space) (eSubExp segment_size))
+        (eBinOp (Mul Int32) (eSubExp $ Var $ spaceGroupId space) (eSubExp num_segments_per_group))
+
+    index_within_segment <- letSubExp "index_within_segment" =<<
+      eBinOp (UMod Int32) (eSubExp $ Var $ spaceLocalId space) (eSubExp segment_size)
+
+    return (segment_index, index_within_segment)
+
+  (offset, offset_stms) <- runBinder $ makeOffsetExp index_within_segment segment_index
+
+
+  let (red_ts, map_ts) = splitAt num_redres $ lambdaReturnType fold_lam
+  let kernel_return_types = red_ts ++ map_ts
+
+  let wasted_thread = do
+        unless (all primType kernel_return_types) $ fail "TODO: segreodmap, when using GroupScan, cannot handle non-PrimType return types for fold_lam"
+        let dummy_vals = map (Constant . blankPrimValue . elemType) kernel_return_types
+        return (negone : negone : dummy_vals)
+
+  let normal_thread = do
+        let red_ts' = Prim Bool : red_ts
+
+        red_pes <- forM red_ts $ \red_t -> do
+          pe_name <- newVName "fold_red"
+          return $ PatElem pe_name BindVar red_t
+        map_pes <- forM map_ts $ \map_t -> do
+          pe_name <- newVName "fold_map"
+          return $ PatElem pe_name BindVar map_t
+
+        -- kernel_input_stms <- forM inps $ \kin -> do
+        --   let pe = PatElem (kernelInputName kin) BindVar (kernelInputType kin)
+        --   let arr = kernelInputArray kin
+        --   arrtp <- lookupType arr
+        --   let slice = fullSlice arrtp [DimFix se | se <- kernelInputIndices kin]
+        --   return $ Let (Pattern [] [pe]) () $ BasicOp $ Index cs arr slice
+
+        -- Index input array to get arguments to fold_lam
+        let arr_params = drop num_redres $ lambdaParams fold_lam
+        let nonred_lamparam_pes = map
+              (\p -> PatElem (paramName p) BindVar (paramType p)) arr_params
+        forM_ (zip redin_arrs nonred_lamparam_pes) $ \(arr, pe) -> do
+          tp <- lookupType arr
+          let slice = fullSlice tp [DimFix offset]
+          addStm $ Let (Pattern [] [pe]) () $ BasicOp $ Index cs arr slice
+
+        -- Bind neutral element (serves as the reduction arguments to fold_lam)
+        forM_ (zip nes (take num_redres $ lambdaParams fold_lam)) $ \(ne,param) -> do
+          let pe = PatElem (paramName param) BindVar (paramType param)
+          addStm $ Let (Pattern [] [pe]) () $ BasicOp $ SubExp ne
+
+        mapM_ addStm $ bodyStms $ lambdaBody fold_lam
+
+        -- we add the lets here, as we practially don't know if the resulting subexp
+        -- is a Constant or a Var, so better be safe (?)
+        mapM_ addStm [ Let (Pattern [] [pe]) () $ BasicOp $ SubExp se
+                    | (pe,se) <- zip (red_pes ++ map_pes) (bodyResult $ lambdaBody fold_lam) ]
+
+        isfirstinsegment <- letExp "isfirstinsegment" =<<
+          eCmpOp (CmpEq $ IntType Int32)
+            (eBinOp (UMod Int32) (eSubExp lid) (eSubExp segment_size))
+            (eSubExp zero)
+
+        -- We will perform a segmented-scan, so all the prime variables here
+        -- include the flag, which is the first argument to flag_reduce_lam
+        let red_pes' = PatElem isfirstinsegment BindVar (Prim Bool) : red_pes
+
+        -- Combine the reduction results from each thread. This will put results in
+        -- local memory, so a GroupReduce/GroupScan can be performed on them
+        combine_red_pes' <- forM red_ts' $ \red_t -> do
+          pe_name <- newVName "chunk_fold_red"
+          return $ PatElem pe_name BindVar $ red_t `arrayOfRow` group_size
+        mapM_ addStm [ Let (Pattern [] [pe']) () $ Op $
+                             Combine [(spaceLocalId space, group_size)] [patElemType pe]
+                             (constant True) $
+                             Body () [] [Var $ patElemName pe]
+                           | (pe', pe) <- zip combine_red_pes' red_pes' ]
+
+        scan_red_pes' <- forM red_ts' $ \red_t -> do
+          pe_name <- newVName "scanned"
+          return $ PatElem pe_name BindVar $ red_t `arrayOfRow` group_size
+        let scan_red_pes = drop 1 scan_red_pes'
+        addStm $ Let (Pattern [] scan_red_pes') () $ Op $
+          GroupScan group_size flag_reduce_lam' $
+          zip (false:nes) (map patElemName combine_red_pes')
+
+        islastinsegment <- letExp "islastinseg" =<< eCmpOp (CmpEq $ IntType Int32)
+            (eBinOp (UMod Int32) (eSubExp lid) (eSubExp segment_size))
+            (eBinOp (Sub Int32) (eSubExp segment_size) (eSubExp one))
+
+        redoffset <- letSubExp "redoffset" =<<
+            eIf (eSubExp $ Var islastinsegment)
+              (eBody [eSubExp segment_index])
+              (mkBodyM [] [negone])
+        let mapoffset = offset
+
+        redret_elems <- fmap (map Var) $ letTupExp "red_return_elem" =<<
+          eIf (eSubExp $ Var islastinsegment)
+            (eBody [return $ BasicOp $ Index [] (patElemName pe) (fullSlice (patElemType pe) [DimFix lid])
+                   | pe <- scan_red_pes])
+            (mkBodyM [] nes)
+
+        let mapret_elems = map (Var . patElemName) map_pes
+
+        return (redoffset : mapoffset : redret_elems++mapret_elems)
+
+
+  let picknchoose = do
+        is_last_group <- letSubExp "islastgroup" =<<
+            eCmpOp (CmpEq $ IntType Int32)
+                (eSubExp $ Var $ spaceGroupId space)
+                (eBinOp (Sub Int32) (eSubExp num_groups) (eSubExp one))
+
+        active_threads_this_group <- letSubExp "active_thread_this_group" =<<
+            eIf (eSubExp is_last_group)
+               (eBody [eSubExp active_threads_in_last_group])
+               (eBody [eSubExp active_threads_per_group])
+
+        isactive <- letSubExp "isactive" =<<
+          eCmpOp (CmpSlt Int32) (eSubExp lid) (eSubExp active_threads_this_group)
+
+        (normal_res, normal_stms) <- runBinder normal_thread
+        (wasted_res, wasted_stms) <- runBinder wasted_thread
+
+        -- we could just have used letTupExp, but this would not give as nice
+        -- names in the generated code
+        redoffset_pe <- (\vn -> PatElem vn BindVar i32) <$> newVName "redoffset"
+        mapoffset_pe <- (\vn -> PatElem vn BindVar i32) <$> newVName "mapoffset"
+        red_pes <- forM red_ts $ \red_t -> do
+          pe_name <- newVName "red_res"
+          return $ PatElem pe_name BindVar red_t
+        map_pes <- forM map_ts $ \map_t -> do
+          pe_name <- newVName "map_res"
+          return $ PatElem pe_name BindVar map_t
+        let all_res_pes = redoffset_pe:mapoffset_pe:red_pes++map_pes
+
+        e <- eIf (eSubExp isactive)
+            (mkBodyM normal_stms normal_res)
+            (mkBodyM wasted_stms wasted_res)
+        addStm $ Let (Pattern [] all_res_pes) () e
+
+        return $ map (Var . patElemName) all_res_pes
+
+  (redoffset:mapoffset:redmapres, stms) <- runBinder $
+    localScope (scopeOf $ offset_stms ++ calc_segindex_stms) picknchoose
+  let (finalredvals, finalmapvals) = splitAt num_redres redmapres
+
+  -- To be able to only return elements from some threads, we exploit the fact
+  -- that WriteReturn with offset=-1, won't do anything.
+  red_returns <- forM (zip finalredvals $ take num_redres scratch_arrs) $ \(se, scarr) ->
+    return $ WriteReturn num_segments scarr redoffset se
+  map_returns <- forM (zip finalmapvals $ drop num_redres scratch_arrs) $ \(se, scarr) ->
+    return $ WriteReturn w scarr mapoffset se
+  let kernel_returns = red_returns ++ map_returns
+
+  let kerneldebughints = KernelDebugHints kernelname
+                         [ ("num_segment", num_segments)
+                         , ("segment_size", segment_size)
+                         , ("num_groups", num_groups)
+                         , ("group_size", group_size)
+                         , ("num_segments_per_group", num_segments_per_group)
+                         , ("active_threads_per_group", active_threads_per_group)
+                         , ("segments_in_last_group", segments_in_last_group)
+                         , ("active_threads_in_last_group", active_threads_in_last_group)
+                         ]
+
+  let kernel = Kernel kerneldebughints cs space kernel_return_types $
+                  KernelBody () (--FIXME: add these back: kernel_input_stms ++
+                                 calc_segindex_stms ++
+                                 offset_stms ++
+                                 stms)
+                  kernel_returns
+
+  return kernel
+
+  where
+    i32 = Prim $ IntType Int32
+    zero = constant (0 :: Int32)
+    one = constant (1 :: Int32)
+    negone = constant (-1 :: Int32)
+    false = constant False
+
+    kernelname = "segmented_redomap__one_group_many_segment"
+
+    makeOffsetExp index_within_segment segment_index = do
+      e <- eBinOp (Add Int32)
+             (eSubExp index_within_segment)
              (eBinOp (Mul Int32) (eSubExp segment_size) (eSubExp segment_index))
       letSubExp "offset" e
 
