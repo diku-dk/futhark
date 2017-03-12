6b205cad818fa71f589ebd0d4510088fd027f643
Rasmus Wriedt Larsen <rasmuswriedtlarsen@gmail.com>
2017-03-12 20:45:02 +0100

    panic.py insert name of Futhark program

--- rts/python/panic.py
+++ rts/python/panic.py
@@ -1,5 +1,4 @@
 def panic(exitcode, fmt, *args):
-    # FIXME: Insert the name of the Futhark program, but I don't know where it can be found
-    sys.stderr.write('<Futhark program>: ')
+    sys.stderr.write('%s: ' % sys.argv[0])
     sys.stderr.write(fmt % args)
     sys.exit(exitcode)
