c26f4756252744eb8d0ee1b2a55920fca026ae7e
Troels Henriksen <athas@sigkill.dk>
2017-05-23 15:02:44 +0200

    This was a mistake.
    Revert "Be more liberal when segmenting."
    
    This reverts commit 79cd845fbb4858834fb2458711f44348d6929684.

--- src/Futhark/Pass/ExtractKernels.hs
+++ src/Futhark/Pass/ExtractKernels.hs
@@ -1009,7 +1009,7 @@ isSegmentedOp nest perm segment_size ret free_in_op _free_in_fold_op nes arrs m 
 
       prepareArr arr =
         case find ((==arr) . kernelInputName) kernel_inps of
-          Just inp | not (kernelInputArray inp `S.member` bound_by_nest) ->
+          Just inp | kernelInputIndices inp == map Var indices ->
             return $ return $ kernelInputArray inp
           Nothing | not (arr `S.member` bound_by_nest) ->
                       -- This input is something that is free inside
