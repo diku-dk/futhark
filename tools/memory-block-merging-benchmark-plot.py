a07aa927040067d79ca868f4f0f2b56ed94b56ec
Niels G. W. Serup <ngws@metanohi.name>
2017-06-01 22:32:15 +0200

    Improve memory block merging benchmarking and plotting.

--- tools/memory-block-merging-benchmark-plot.py
+++ tools/memory-block-merging-benchmark-plot.py
@@ -13,6 +13,10 @@ import matplotlib.pyplot as plt
 out_dir = sys.argv[1]
 json_paths = sys.argv[2:]
 
+# fix later
+assert 'with.json' in json_paths
+assert 'without.json' in json_paths
+
 raw_results = {}
 for json_path in json_paths:
     with open(json_path) as f:
@@ -38,36 +42,51 @@ for benchmark_name in benchmark_names:
     datasets = []
     for dataset_name in sorted(dataset_names[benchmark_name]):
         runtimes = {}
+        total_alloc = {}
         for json_path, local_benchmarks in sorted(raw_results.items()):
-            runtimes[json_path] = np.mean(local_benchmarks[benchmark_name]['datasets'][dataset_name]['runtimes'])
-        improvement = (runtimes['without.json'] - runtimes['with.json']) / runtimes['without.json']
-        datasets.append((dataset_name, runtimes, improvement))
-    datasets.sort(key=lambda t: t[2], reverse=True)
+            try:
+                runtimes[json_path] = np.mean(local_benchmarks[benchmark_name]['datasets'][dataset_name]['runtimes'])
+                total_alloc[json_path] = local_benchmarks[benchmark_name]['datasets'][dataset_name]['total_allocated']
+            except TypeError:
+                pass
+        if 'without.json' in runtimes.keys() and 'with.json' in runtimes.keys():
+            improvement_time = (runtimes['without.json'] - runtimes['with.json']) / runtimes['without.json']
+            improvement_alloc = (total_alloc['without.json'] - total_alloc['with.json']) / total_alloc['without.json']
+            datasets.append((dataset_name, runtimes, total_alloc, improvement_time, improvement_alloc))
+    datasets.sort(key=lambda t: t[3:], reverse=True)
     benchmarks.append((benchmark_name, datasets))
 
-def average_improvement(datasets):
-    i = 0
-    for _, _, improvement in datasets:
-        i += improvement
-    i /= len(datasets)
-    return i
+def average_improvements(datasets):
+    t, a = 0, 0
+    for _, _, _, improvement_time, improvement_alloc in datasets:
+        t += improvement_time
+        a += improvement_alloc
+    t /= len(datasets)
+    a /= len(datasets)
+    return (t, a)
 
-benchmarks.sort(key=lambda t: average_improvement(t[1]), reverse=True)
+benchmarks.sort(key=lambda t: average_improvements(t[1]), reverse=True)
+# benchmarks.sort(key=lambda t: average_improvements(t[1])[0], reverse=True)
+# benchmarks.sort(key=lambda t: average_improvements(t[1])[1], reverse=True)
 
 def cut_desc(s):
     if s[0] == '#':
         return s.split(' ')[0]
     else:
-        return s
+        return os.path.split(s)[1]
 
 for benchmark_name, datasets in benchmarks:
     print('# {}'.format(benchmark_name))
-    print('Average improvement: {:.3f}'.format(average_improvement(datasets)))
+    t, a = average_improvements(datasets)
+    print('Average improvement in runtime: {:.3f}'.format(t))
+    print('Average improvement in total allocation: {:.3f}'.format(t))
     print('Datasets:')
-    for dataset_name, runtimes, improvement in datasets:
+    for dataset_name, runtimes, total_alloc, improvement_time, improvement_alloc in datasets:
         print('  ## Dataset: {}'.format(cut_desc(dataset_name)))
         print('  Runtimes: Without: {} us, with: {} us'.format(runtimes['without.json'], runtimes['with.json']))
-        print('  Improvement: {}'.format(improvement))
+        print('  Total allocation: Without: {} us, with: {} us'.format(total_alloc['without.json'], total_alloc['with.json']))
+        print('  Improvement in runtime: {}'.format(improvement_time))
+        print('  Improvement in total allocation: {}'.format(improvement_alloc))
     print('')
 
 
@@ -94,15 +113,15 @@ def short_path_name(path):
         return path
 
 for benchmark_name, datasets in benchmarks:
-    pdf_path = os.path.normpath(os.path.join(out_dir, benchmark_name + '.pdf'))
+    pdf_path = os.path.normpath(os.path.join(out_dir, benchmark_name + '-runtimes.pdf'))
     os.makedirs(os.path.dirname(pdf_path), exist_ok=True)
     flat_results = []
-    for dataset_name, results, _ in datasets:
+    for dataset_name, runtimes, total_alloc, _, _ in datasets:
         for json_path in ['without.json', 'with.json']:
-            runtime = results[json_path]
+            runtime = runtimes[json_path]
             color = json_colors[json_path]
-            flat_results.append(('{}: {}'.format(short_path_name(json_path),
-                                                 cut_desc(dataset_name)),
+            flat_results.append(('{}:\n{}'.format(short_path_name(json_path),
+                                                  cut_desc(dataset_name)),
                                  runtime, color))
 
     print('Plotting {} into {}.'.format(benchmark_name, pdf_path))
@@ -116,6 +135,44 @@ for benchmark_name, datasets in benchmarks:
     ax.set_ylim([0, maximum * 1.1])
     ax.set_title('Runtimes of {}'.format(benchmark_name))
     ax.set_ylabel('Microseconds')
+    ax.set_xticks(ind)
+    ax.set_xticklabels(list(map(lambda x: x[0], flat_results)))
+    plt.tick_params(axis='x', which='major', labelsize=4)
+
+    plt.bar(ind,
+            list(map(lambda x: x[1], flat_results)),
+            width,
+            color=list(map(lambda x: x[2], flat_results)))
+
+    plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+
+    plt.rc('text')
+    plt.savefig(pdf_path, format='pdf')
+    plt.close()
+
+
+    pdf_path = os.path.normpath(os.path.join(out_dir, benchmark_name + '-allocations.pdf'))
+    os.makedirs(os.path.dirname(pdf_path), exist_ok=True)
+    flat_results = []
+    for dataset_name, runtimes, total_alloc, _, _ in datasets:
+        for json_path in ['without.json', 'with.json']:
+            alloc = total_alloc[json_path]
+            color = json_colors[json_path]
+            flat_results.append(('{}:\n{}'.format(short_path_name(json_path),
+                                                  cut_desc(dataset_name)),
+                                 alloc, color))
+
+    print('Plotting {} into {}.'.format(benchmark_name, pdf_path))
+
+    ind = np.arange(len(flat_results))
+    width = 0.35
+
+    maximum = max(map(lambda x: x[1], flat_results))
+
+    fig, ax = plt.subplots()
+    ax.set_ylim([0, maximum * 1.1])
+    ax.set_title('Cumulative allocations in {}'.format(benchmark_name))
+    ax.set_ylabel('Bytes')
     ax.set_xticks(ind)
     ax.set_xticklabels(list(map(lambda x: x[0], flat_results)))
     plt.tick_params(axis='x', which='major', labelsize=4)
--- tools/memory-block-merging-benchmark.sh
+++ tools/memory-block-merging-benchmark.sh
@@ -5,6 +5,9 @@ set -e # Exit on first error.
 # Assumes your futhark-benchmarks directory is next to your futhark directory.
 cd "$(dirname "$0")/../../futhark-benchmarks/"
 
-futhark-bench --json without.json .
+# Ignore too large datasets.
+flags='--timeout 60'
 
-MEMORY_BLOCK_MERGING=1 futhark-bench --json with.json .
+futhark-bench $flags --json without.json .
+
+MEMORY_BLOCK_MERGING=1 futhark-bench $flags --json with.json .
