fd6ace12572f0094f4e4449d3168da4b40d7254b
Troels Henriksen <athas@sigkill.dk>
2017-06-10 07:59:49 +0200

    Fixes to type conversion simplification.

--- src/Futhark/Optimise/Simplifier/Rules.hs
+++ src/Futhark/Optimise/Simplifier/Rules.hs
@@ -529,17 +529,17 @@ simplifyConvOp _ _ (ConvOp op (Constant v)) =
 simplifyConvOp _ _ (ConvOp op se)
   | (from, to) <- convTypes op, from == to =
   Just $ SubExp se
-simplifyConvOp lookupVar _ (ConvOp (SExt _ t1) (Var v))
-  | Just (BasicOp (ConvOp (SExt t3 _) se)) <- lookupVar v =
+simplifyConvOp lookupVar _ (ConvOp (SExt t2 t1) (Var v))
+  | Just (BasicOp (ConvOp (SExt t3 _) se)) <- lookupVar v, t2 >= t3 =
       Just $ ConvOp (SExt t3 t1) se
-simplifyConvOp lookupVar _ (ConvOp (ZExt _ t1) (Var v))
-  | Just (BasicOp (ConvOp (ZExt t3 _) se)) <- lookupVar v =
+simplifyConvOp lookupVar _ (ConvOp (ZExt t2 t1) (Var v))
+  | Just (BasicOp (ConvOp (ZExt t3 _) se)) <- lookupVar v, t2 >= t3 =
       Just $ ConvOp (ZExt t3 t1) se
-simplifyConvOp lookupVar _ (ConvOp (SIToFP _ t1) (Var v))
-  | Just (BasicOp (ConvOp (SExt t3 _) se)) <- lookupVar v =
+simplifyConvOp lookupVar _ (ConvOp (SIToFP t2 t1) (Var v))
+  | Just (BasicOp (ConvOp (SExt t3 _) se)) <- lookupVar v, t2 >= t3 =
       Just $ ConvOp (SIToFP t3 t1) se
-simplifyConvOp lookupVar _ (ConvOp (UIToFP _ t1) (Var v))
-  | Just (BasicOp (ConvOp (ZExt t3 _) se)) <- lookupVar v =
+simplifyConvOp lookupVar _ (ConvOp (UIToFP t2 t1) (Var v))
+  | Just (BasicOp (ConvOp (ZExt t3 _) se)) <- lookupVar v, t2 >= t3 =
       Just $ ConvOp (UIToFP t3 t1) se
 simplifyConvOp _ _ _ =
   Nothing
