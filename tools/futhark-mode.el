432c50c4ffaae3f9f07e37cc6ee2dc54133dd3db
Niels G. W. Serup <ngws@metanohi.name>
2017-06-22 16:01:46 +0200

    futhark-mode.el: Clean up documentation.
    In particular, don't include manual installation instructions in the
    file.  This looks silly on MELPA.  Put it in the README.

--- tools/futhark-mode.el
+++ tools/futhark-mode.el
@@ -17,21 +17,10 @@
 ;; efficient GPU code.  This Emacs mode provides syntax highlighting and
 ;; conservative automatic indentation for Futhark source code.
 ;;
-;; Define your local keybindings in `futhark-mode-map'.  Add startup
+;; Files with the ".fut" extension are automatically handled by this mode.
+;;
+;; For extensions: Define local keybindings in `futhark-mode-map'.  Add startup
 ;; functions to `futhark-mode-hook'.
-;;
-;; Manual installation: To load futhark-mode automatically on Emacs
-;; startup, put this file in your load path and require the mode,
-;; e.g. something like this:
-;;
-;;   (add-to-list 'load-path "~/.emacs.d/futhark-mode")
-;;   (require 'futhark-mode)
-;;
-;; In this case, you have to create the directory
-;; "~/.emacs.d/futhark-mode" and store this file in that directory.
-;;
-;; This will also tell your Emacs that ".fut" files are to be handled by
-;; futhark-mode.
 
 ;;; Code:
 
