41902f396714a5df5155ba1fd85ff37e7a440e1c
Niels G. W. Serup <ngws@metanohi.name>
2017-08-07 16:13:18 +0200

    Allow using a different compiler than futhark-c in memblock testing.

--- memory-block-merging/tests/test_all.sh
+++ memory-block-merging/tests/test_all.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 #
 # Run all the tests in the subdirectories.
-#
-# With -w, exclude the wip directories.
 
 cd "$(dirname "$0")"
 
-./coalescing/test.sh "$1" \
-    && ./reuse/test.sh "$1" \
-    && ./mix/test.sh "$1"
+./coalescing/test.sh "$@" \
+    && ./reuse/test.sh "$@" \
+    && ./mix/test.sh "$@"
