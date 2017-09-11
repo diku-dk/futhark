ca1389bb1989a0d3ba5ddd4e235e12e27cc8df14
Niels G. W. Serup <ngws@metanohi.name>
2017-09-11 12:04:23 +0200

    Fix test scripts.

--- memory-block-merging/tools/futhark-mbm-test-all.sh
+++ memory-block-merging/tools/futhark-mbm-test-all.sh
@@ -1,19 +1,36 @@
 #!/bin/sh
+#
+# Perform all possible memory block merging tests.
 
-set -x
+section() {
+    echo "# $*"
+}
 
 with_opencl() {
     prog="$1"
     shift
-    "$prog" futhark-opencl "$@"
+    time "$prog" futhark-opencl "$@"
 }
 
 base="$(dirname "$0")/../../../futhark/memory-block-merging/tests/" # Makes assumption.
-"$base"/test_all.sh
+
+section Memory block merging tests, futhark-c.
+time "$base"/test_all.sh
+
+section Memory block merging tests, futhark-opencl.
 with_opencl "$base"/test_all.sh
 
+
 base="$(dirname "$0")"
-"$base"/futhark-mbm-failures
+
+section Futhark tests, futhark-c.
+time "$base"/futhark-mbm-failures
+
+section Futhark tests, futhark-opencl.
 with_opencl "$base"/futhark-mbm-failures
-"$base"/futhark-mbm-failures-benchmarks
-with_opencl "$base"/futhark-mbm-failures-benchmarks
+
+section Futhark benchmarks, futhark-c.
+time "$base"/futhark-mbm-failures-benchmarks
+
+section Futhark benchmarks, futhark-opencl.
+with_opencl time "$base"/futhark-mbm-failures-benchmarks
