a2048927409777153fbe48041205473c7723fa92
Troels Henriksen <athas@sigkill.dk>
2018-01-06 16:10:22 +0100

    Simplify call graph computation.

--- src/Futhark/Analysis/CallGraph.hs
+++ src/Futhark/Analysis/CallGraph.hs
@@ -6,31 +6,19 @@ module Futhark.Analysis.CallGraph
   )
   where
 
-import Control.Monad.Reader
-import Control.Monad.State
+import Control.Monad.Writer.Strict
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
 import Data.Maybe (isJust)
+import Data.List
 
 import Futhark.Representation.SOACS
 
 type FunctionTable = M.Map Name FunDef
 
 buildFunctionTable :: Prog -> FunctionTable
-buildFunctionTable =
-  foldl expand M.empty . progFunctions
-  where expand ftab f =
-          M.insert (funDefName f) f ftab
-
--- | The symbol table for functions
-newtype CGEnv = CGEnv { envFtable  :: FunctionTable }
-
-type CGM = Reader CGEnv
-
--- | Building the call grah runs in this monad.  There is no
--- mutable state.
-runCGM :: CGM a -> CGEnv -> a
-runCGM = runReader
+buildFunctionTable = foldl expand M.empty . progFunctions
+  where expand ftab f = M.insert (funDefName f) f ftab
 
 -- | The call graph is just a mapping from a function name, i.e., the
 -- caller, to a list of the names of functions called by the function.
@@ -40,54 +28,39 @@ type CallGraph = M.Map Name (S.Set Name)
 -- | @buildCallGraph prog@ build the program's Call Graph. The representation
 -- is a hashtable that maps function names to a list of callee names.
 buildCallGraph :: Prog -> CallGraph
-buildCallGraph prog = do
-  let ftable = buildFunctionTable prog
-  runCGM (foldM buildCGfun M.empty entry_points) $ CGEnv ftable
+buildCallGraph prog = foldl' (buildCGfun ftable) M.empty entry_points
   where entry_points = map funDefName $ filter (isJust . funDefEntryPoint) $ progFunctions prog
+        ftable = buildFunctionTable prog
 
--- | @buildCallGraph cg f@ updates Call Graph @cg@ with the contributions of function
--- @fname@, and recursively, with the contributions of the callees of @fname@.
-buildCGfun :: CallGraph -> Name -> CGM CallGraph
-buildCGfun cg fname  = do
-  bnd <- asks $ M.lookup fname . envFtable
-  case bnd of
-    Nothing -> return cg -- Must be builtin or similar.
-    Just f ->
-      case M.lookup fname cg of
-        Just _  -> return cg
-        Nothing -> do let callees = buildCGbody mempty $ funDefBody f
-                      let cg' = M.insert fname callees cg
+-- | @buildCallGraph ftable cg fname@ updates Call Graph @cg@ with the
+-- contributions of function @fname@, and recursively, with the
+-- contributions of the callees of @fname@.
+buildCGfun :: FunctionTable -> CallGraph -> Name -> CallGraph
+buildCGfun ftable cg fname  =
+  -- Check if function is a non-builtin that we have not already
+  -- processed.
+  case M.lookup fname ftable of
+    Just f | Nothing <- M.lookup fname cg -> do
+               let callees = buildCGbody $ funDefBody f
+                   cg' = M.insert fname callees cg
+               -- recursively build the callees
+               foldl' (buildCGfun ftable) cg' callees
+    _ -> cg
 
-                      -- recursively build the callees
-                      foldM buildCGfun cg' callees
+buildCGbody :: Body -> S.Set Name
+buildCGbody = mconcat . map (buildCGexp . stmExp) . stmsToList . bodyStms
 
-buildCGbody :: S.Set Name -> Body -> S.Set Name
-buildCGbody callees = foldl (\x -> buildCGexp x . stmExp) callees . bodyStms
-
-buildCGexp :: S.Set Name -> Exp -> S.Set Name
-buildCGexp callees (Apply fname _ _ _)
-  | fname `elem` callees = callees
-  | otherwise            = S.insert fname callees
-buildCGexp callees (Op op) =
-  case op of Map _ lam _ ->
-               buildCGbody callees $ lambdaBody lam
-             Reduce _ _ lam _ ->
-               buildCGbody callees $ lambdaBody lam
-             Scan _ lam _ ->
-               buildCGbody callees $ lambdaBody lam
-             Redomap _ _ lam0 lam1 _ _ ->
-               buildCGbody (buildCGbody callees $ lambdaBody lam0) (lambdaBody lam1)
-             Scanomap _ lam0 lam1 _ _ ->
-               buildCGbody (buildCGbody callees $ lambdaBody lam0) (lambdaBody lam1)
-             Stream _ (Parallel _ _ lam0 _) lam _ ->
-               buildCGbody (buildCGbody callees $ lambdaBody lam0) (extLambdaBody lam)
-             Stream _ _ lam _ ->
-               buildCGbody callees (extLambdaBody lam)
-             Scatter {} ->
-               callees
-buildCGexp callees e =
-  execState (mapExpM folder e) callees
+buildCGexp :: Exp -> S.Set Name
+buildCGexp (Apply fname _ _ _) = S.singleton fname
+buildCGexp (Op op) = execWriter $ mapSOACM folder op
+  where folder = identitySOACMapper {
+          mapOnSOACLambda = \lam -> do tell $ buildCGbody $ lambdaBody lam
+                                       return lam,
+          mapOnSOACExtLambda = \lam -> do tell $ buildCGbody $ extLambdaBody lam
+                                          return lam
+          }
+buildCGexp e = execWriter $ mapExpM folder e
   where folder = identityMapper {
-          mapOnBody = \_ body -> do put =<< (buildCGbody <$> get <*> pure body)
+          mapOnBody = \_ body -> do tell $ buildCGbody body
                                     return body
           }
