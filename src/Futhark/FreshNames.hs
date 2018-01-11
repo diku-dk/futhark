7253ece38eeb57a39aec6dad737eef46f5bd80b6
Troels Henriksen <athas@sigkill.dk>
2018-01-11 08:19:33 +0100

    Add possibly-handy Ord instance.

--- src/Futhark/FreshNames.hs
+++ src/Futhark/FreshNames.hs
@@ -18,8 +18,11 @@ import Language.Futhark.Core
 -- no repeating entries.  In practice, when asked for a name, the name
 -- source will return the name along with a new name source, which
 -- should then be used in place of the original.
+--
+-- The 'Ord' instance is based on how many names have been extracted
+-- from the name source.
 newtype VNameSource = VNameSource Int
-  deriving (Lift)
+  deriving (Lift, Eq, Ord)
 
 instance Sem.Semigroup VNameSource where
   VNameSource x <> VNameSource y = VNameSource (x `max` y)
