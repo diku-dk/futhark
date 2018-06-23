8201b06213ec48c5f343d09f6b83a30ab46ea07d
Troels Henriksen <athas@sigkill.dk>
2018-06-23 18:58:19 +0200

    Fix explicit prelude import on Windows.
    Maybe.  It's hard to get Haskell running on Windows, apparently.

--- src/Language/Futhark/Futlib/Prelude.hs
+++ src/Language/Futhark/Futlib/Prelude.hs
@@ -9,9 +9,10 @@ module Language.Futhark.Futlib.Prelude (preludeBasis) where
 import Futhark.Compiler.Program
 import Futhark.Compiler.TH
 import Language.Futhark.Futlib()
+import System.FilePath ((</>))
 
 -- | The Futlib basis constructed from @futlib/prelude.fut@.  This is
 -- only a subset of the full futlib, but contains pre-type checked
 -- ASTs.
 preludeBasis :: Basis
-preludeBasis = $(embedBasis "futlib/prelude.fut" "/futlib/prelude")
+preludeBasis = $(embedBasis ("futlib" </> "prelude.fut") "/futlib/prelude")
