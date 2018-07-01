d44f8fae27d1370241bd12b669d016be61661d3f
Troels Henriksen <athas@sigkill.dk>
2018-07-01 10:20:46 +0200

    futharki: Handle import.
    Also remove :load, which is now redundant.

--- src/futharki.hs
+++ src/futharki.hs
@@ -4,7 +4,6 @@
 {-# LANGUAGE QuasiQuotes #-}
 module Main (main) where
 
-import Control.Exception
 import Data.Char
 import Data.Loc
 import Data.Version
@@ -26,7 +25,6 @@ import Language.Futhark.Parser
 import Language.Futhark.TypeChecker
 import Language.Futhark.Semantic
 import Futhark.MonadFreshNames
-import Futhark.Pipeline
 import Futhark.Interpreter
 import Futhark.Version
 import Futhark.Passes
@@ -120,32 +118,53 @@ readEvalPrint = do
         Nothing -> liftIO $ T.putStrLn $ "Unknown command '" <> cmdname <> "'"
         Just (cmdf, _) -> cmdf arg
     _ -> do
-      imports <- gets interpImports
-      decs <- gets interpDecs
-      src <- gets interpNameSource
       -- Read a declaration or expression.
       maybe_dec_or_e <- parseDecOrExpIncrM (inputLine "  ") ("[" ++ show i ++ "]") line
 
       case maybe_dec_or_e of
         Left err -> liftIO $ print err
-        Right (Left d) -> do
-          -- See if it type checks with the new declaration.
-          let decs' = decs ++ [d]
-              prog' = mkProg decs' $ TupLit [] noLoc
-          ok <- runProgram imports src prog'
-          when ok $ modify $ \s -> s { interpDecs = decs' }
-        Right (Right e) -> do
-          -- Generate a 0-ary function with empty name with the
-          -- expression as its body, append it to the stored program,
-          -- then run it.
-          let prog' = mkProg decs e
-          void $ runProgram imports src prog'
+        Right (Left d) -> onDec d
+        Right (Right e) -> onExp e
   where inputLine prompt = do
           inp <- lift $ Haskeline.getInputLine prompt
           case inp of
             Just s -> return $ T.pack s
             Nothing -> liftIO $ do putStrLn "Leaving futharki."
                                    exitSuccess
+
+onDec :: UncheckedDec -> FutharkiM ()
+onDec d = do
+  decs <- gets interpDecs
+  -- See if it type checks with the new declaration.
+  let decs' = decs ++ [d]
+      prog' = mkProg decs' $ TupLit [] noLoc
+  -- We have to read in any new imports done by the declaration.
+  basis <- curBasis
+  res <- runExceptT $ readLibrary basis $
+         map ((<.> "fut") . fst) $ progImports $ Prog Nothing [d]
+  case res of
+    Left err -> liftIO $ print err
+    Right (_, imports, src) -> do
+      ok <- runProgram imports src prog'
+      when ok $ modify $ \s -> s { interpDecs = decs'
+                                 , interpNameSource = src
+                                 , interpImports = imports }
+  where curBasis = do
+          imports <- gets interpImports
+          src <- gets interpNameSource
+          return $ Basis { basisImports = imports
+                         , basisNameSource = src
+                         , basisRoots = basisRoots preludeBasis }
+
+onExp :: UncheckedExp -> FutharkiM ()
+onExp e = do
+  imports <- gets interpImports
+  decs <- gets interpDecs
+  src <- gets interpNameSource
+  -- Generate a 0-ary function with empty name with the expression as
+  -- its body, append it to the stored program, then run it.
+  let prog' = mkProg decs e
+  void $ runProgram imports src prog'
 
 mkProg :: [UncheckedDec] -> UncheckedExp -> UncheckedProg
 mkProg decs e =
@@ -176,41 +195,13 @@ runProgram imports src prog = liftIO $
 type Command = T.Text -> FutharkiM ()
 
 commands :: [(T.Text, (Command, T.Text))]
-commands = [("load", (loadCommand, [text|
-Load a Futhark source file.  Usage:
-
-  > :load foo.fut
-
-If the loading succeeds, any subsequentialy entered expressions entered
-subsequently will have access to the definition (such as function definitions)
-in the source file.
-
-Only one source file can be loaded at a time.  Using the :load command a
-second time will replace the previously loaded file.
-
-|])),
-            ("help", (helpCommand, [text|
+commands = [("help", (helpCommand, [text|
 Print a list of commands and a description of their behaviour.
 |])),
             ("quit", (quitCommand, [text|
 Quit futharki.
 |]))]
-  where loadCommand :: Command
-        loadCommand file = do
-          liftIO $ T.putStrLn $ "Reading " <> file
-          res <- liftIO $ runExceptT (readProgram preludeBasis (T.unpack file))
-                 `Haskeline.catch` \(err::IOException) ->
-                 return (Left (ExternalError (T.pack $ show err)))
-          case res of
-            Left err -> liftIO $ dumpError newFutharkConfig err
-            Right (_, imports, src) ->
-              modify $ \env -> env { interpImports = imports
-                                   , interpDecs = map mkOpen $ basisRoots preludeBasis ++
-                                                  [dropExtension $ T.unpack file]
-                                   , interpNameSource = src
-                                   }
-
-        helpCommand :: Command
+  where helpCommand :: Command
         helpCommand _ = liftIO $ forM_ commands $ \(cmd, (_, desc)) -> do
             T.putStrLn $ ":" <> cmd
             T.putStrLn $ T.replicate (1+T.length cmd) "-"
