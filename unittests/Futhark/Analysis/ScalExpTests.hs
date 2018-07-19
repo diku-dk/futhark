7039a9597c2d7da868482bec295fb2e7a3bf017d
Troels Henriksen <athas@sigkill.dk>
2018-07-19 12:46:32 +0200

    Also migrate to megaparsec in tests.

--- unittests/Futhark/Analysis/ScalExpTests.hs
+++ unittests/Futhark/Analysis/ScalExpTests.hs
@@ -10,12 +10,13 @@ where
 import Test.Framework
 
 import Control.Applicative
-import Control.Monad.Identity
+import Control.Monad.State
 import qualified Data.Map as M
-import Text.Parsec hiding (token, (<|>), many)
-import Text.Parsec.Expr
-import Text.Parsec.Language
-import qualified Text.Parsec.Token as Token
+import Data.Void
+import Text.Megaparsec hiding (token, (<|>), many, State)
+import Text.Megaparsec.Expr
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
 
 import Futhark.Analysis.ScalExp
 import Futhark.Representation.AST hiding (constant, SDiv)
@@ -23,75 +24,55 @@ import Futhark.Representation.AST hiding (constant, SDiv)
 tests :: [Test]
 tests = []
 
-languageDef :: GenLanguageDef String u Identity
-languageDef =
-  emptyDef { Token.commentStart    = "/*"
-           , Token.commentEnd      = "*/"
-           , Token.commentLine     = "//"
-           , Token.identStart      = letter
-           , Token.identLetter     = alphaNum
-           , Token.reservedNames   = ["max", "min", "False", "True"]
-           , Token.reservedOpNames = ["+", "-", "*", "/"
-                                     , "<", ">", "<=", ">="
-                                     , "&&", "||", "not"
-                                     ]
-           }
-
-lexer :: Token.GenTokenParser String ParserState Identity
-lexer = Token.makeTokenParser languageDef
-
 parseScalExp :: String -> ScalExp
 parseScalExp = parseScalExp' M.empty
 
 parseScalExp' :: M.Map String (Int, Type) -> String -> ScalExp
-parseScalExp' m s = case runParser expr (0, m) ("string: " ++ s) s of
+parseScalExp' m s = case evalState (runParserT expr ("string: " ++ s) s) (0, m) of
   Left err -> error $ show err
   Right e  -> e
 
 type ParserState = (Int, M.Map String (Int, Type))
-type Parser = ParsecT String ParserState Identity
+type Parser = ParsecT Void String (State ParserState)
 
 newVar :: String -> Type -> Parser Ident
-newVar s t = do (x, m) <- getState
+newVar s t = do (x, m) <- lift get
                 case M.lookup s m of
                   Just _ -> fail $ "Variable " ++ s ++ " double-declared."
-                  Nothing -> do setState (x+1, M.insert s (x,t) m)
+                  Nothing -> do lift $ put (x+1, M.insert s (x,t) m)
                                 return $ Ident (VName (nameFromString s) x) t
 
 knownVar :: String -> Parser Ident
-knownVar s = do (_, m) <- getState
+knownVar s = do (_, m) <- lift get
                 case M.lookup s m of
                   Just (y,t) -> return $ Ident (VName (nameFromString s) y) t
                   Nothing -> fail $ "Undeclared variable " ++ s
 
-reservedOp :: String -> Parser ()
-reservedOp = Token.reservedOp lexer
-
-reserved :: String -> Parser ()
-reserved = Token.reserved lexer
+token :: String -> Parser ()
+token = L.lexeme space . void . string
 
 parens :: Parser a -> Parser a
-parens = Token.parens lexer
+parens = between (token "(") (token ")")
 
 identifier :: Parser Ident
-identifier = do s <- (:) <$> letter <*> many alphaNum
+identifier = do s <- (:) <$> letterChar <*> many alphaNumChar
                 varDecl s <|> knownVar s
   where varDecl s = do
           t <- parens $
-               (reserved "int" >> pure (Prim $ IntType Int32)) <|>
-               (reserved "float32" >> pure (Prim $ FloatType Float32)) <|>
-               (reserved "float64" >> pure (Prim $ FloatType Float64)) <|>
-               (reserved "bool" >> pure (Prim Bool))
+               (token "int" >> pure (Prim $ IntType Int32)) <|>
+               (token "float32" >> pure (Prim $ FloatType Float32)) <|>
+               (token "float64" >> pure (Prim $ FloatType Float64)) <|>
+               (token "bool" >> pure (Prim Bool))
           newVar s t
 
 constant :: Parser ScalExp
-constant = (reserved "True" >> pure (Val $ BoolValue True)) <|>
-           (reserved "False" >> pure (Val $ BoolValue True)) <|>
+constant = (token "True" >> pure (Val $ BoolValue True)) <|>
+           (token "False" >> pure (Val $ BoolValue True)) <|>
            (Val . IntValue . Int32Value <$> integer)
-  where integer = fromIntegral <$> Token.integer lexer
+  where integer = L.lexeme space (L.signed space L.decimal)
 
 expr :: Parser ScalExp
-expr = buildExpressionParser operators prim
+expr = makeExprParser prim operators
 
 prim :: Parser ScalExp
 prim = parens expr <|>
@@ -99,27 +80,27 @@ prim = parens expr <|>
        maxapp <|>
        minapp <|>
        (scalExpId <$> identifier)
-  where maxapp = reserved "max" >> MaxMin False <$> parens (expr `sepBy` comma)
-        minapp = reserved "min" >> MaxMin True <$> parens (expr `sepBy` comma)
-        comma = Token.comma lexer
+  where maxapp = token "max" >> MaxMin False <$> parens (expr `sepBy` comma)
+        minapp = token "min" >> MaxMin True <$> parens (expr `sepBy` comma)
+        comma = token ","
         scalExpId (Ident name (Prim t)) = Id name t
         scalExpId (Ident name t) = error $
                                    pretty name ++ " is of type " ++ pretty t ++
                                    " but supposed to be a ScalExp."
 
-operators :: [[Operator String ParserState Identity ScalExp]]
-operators = [ [Prefix (reservedOp "-"   >> return SNeg)          ]
-            , [Infix  (reservedOp "*"   >> return STimes) AssocLeft]
-            , [Infix  (reservedOp "pow"   >> return SPow) AssocLeft]
-            , [Infix  (reservedOp "/"   >> return SDiv) AssocLeft]
-            , [Infix  (reservedOp "+"   >> return SPlus) AssocLeft]
-            , [Infix  (reservedOp "-"   >> return SMinus) AssocLeft]
-            , [Infix  (reservedOp "<="   >> return leq) AssocLeft]
-            , [Infix  (reservedOp "<"   >> return lth) AssocLeft]
-            , [Infix  (reservedOp ">="   >> return (flip leq)) AssocLeft]
-            , [Infix  (reservedOp ">"   >> return (flip lth)) AssocLeft]
-            , [Infix  (reservedOp "&&"   >> return SLogAnd) AssocLeft]
-            , [Infix  (reservedOp "||"   >> return SLogOr) AssocLeft]
+operators :: [[Operator Parser ScalExp]]
+operators = [ [Prefix (token "-"   >> return SNeg)]
+            , [InfixL (token "*"   >> return STimes)]
+            , [InfixL (token "pow" >> return SPow)]
+            , [InfixL (token "/"   >> return SDiv)]
+            , [InfixL (token "+"   >> return SPlus)]
+            , [InfixL (token "-"   >> return SMinus)]
+            , [InfixL (token "<="  >> return leq)]
+            , [InfixL (token "<"   >> return lth)]
+            , [InfixL (token ">="  >> return (flip leq))]
+            , [InfixL (token ">"   >> return (flip lth))]
+            , [InfixL (token "&&"  >> return SLogAnd)]
+            , [InfixL (token "||"  >> return SLogOr)]
             ]
   where leq x y =
           RelExp LEQ0 $ x `SMinus` y
