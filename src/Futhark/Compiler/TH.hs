c1e4bf20248323aa7679e770722a2cf3dda2461d
Troels Henriksen <athas@sigkill.dk>
2018-09-12 21:42:12 +0200

    Force dependencies with the builtin prelude.

--- src/Futhark/Compiler/TH.hs
+++ src/Futhark/Compiler/TH.hs
@@ -6,7 +6,8 @@ where
 
 import Control.Monad.Except (runExceptT, (<=<))
 import Language.Futhark.TH ()
-import Language.Haskell.TH.Syntax (Exp, Q, runIO, lift)
+import Language.Haskell.TH.Syntax (Exp, Q, runIO, lift, qAddDependentFile)
+import System.FilePath
 
 import Futhark.Compiler.Program
 import Language.Futhark.Semantic (mkInitialImport)
@@ -15,7 +16,9 @@ readBasis :: [String] -> Q Basis
 readBasis roots = do
   res <- runIO $ runExceptT $ readImports emptyBasis $ map mkInitialImport roots
   case res of
-    Right (_, imps, src) -> return $ Basis imps src roots
+    Right (_, imps, src) -> do
+      mapM_ (qAddDependentFile . (<.> "fut") . fst) imps
+      return $ Basis imps src roots
     Left err             -> error $ show err
 
 -- | At compile-time, produce an 'Exp' corresponding to a 'Basis'.
