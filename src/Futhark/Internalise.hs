580455c75a601c050b2afa58ad491e7a3d591e5f
zfnmxt <37446532+zfnmxt@users.noreply.github.com>
2018-11-07 00:49:52 +0100

    Thanks, Diving Beet.

--- src/Futhark/Internalise.hs
+++ src/Futhark/Internalise.hs
@@ -168,7 +168,11 @@ entryPoint params (retdecl, eret, crets) =
                        -> [EntryPointType]
         entryPointType (_, E.Prim E.Unsigned{}, _) =
           [I.TypeUnsigned]
+        entryPointType (_, E.Enum{}, _) =
+          [I.TypeUnsigned]
         entryPointType (_, E.Array (ArrayPrimElem Unsigned{} _) _ _, _) =
+          [I.TypeUnsigned]
+        entryPointType (_, E.Array (ArrayEnumElem _ _) _ _, _) =
           [I.TypeUnsigned]
         entryPointType (_, E.Prim{}, _) =
           [I.TypeDirect]
