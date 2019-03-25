2dc1bcc0f2071d81a85622455e9c83972744a284
Troels Henriksen <athas@sigkill.dk>
2019-03-25 15:41:52 +0100

    Add this, even if it is currently empty.

--- unittests/futhark_tests.hs
+++ unittests/futhark_tests.hs
@@ -7,6 +7,7 @@ import qualified Futhark.Representation.ExplicitMemory.IndexFunctionTests
 import qualified Futhark.Optimise.AlgSimplifyTests
 import qualified Futhark.Pkg.SolveTests
 import qualified Futhark.Representation.PrimitiveTests
+import qualified Futhark.Analysis.ScalExpTests
 
 import Test.Tasty
 
@@ -20,6 +21,7 @@ allTests =
   , Futhark.Pkg.SolveTests.tests
   , Futhark.Representation.ExplicitMemory.IndexFunctionTests.tests
   , Futhark.Representation.PrimitiveTests.tests
+  , Futhark.Analysis.ScalExpTests.tests
   ]
 
 main :: IO ()
