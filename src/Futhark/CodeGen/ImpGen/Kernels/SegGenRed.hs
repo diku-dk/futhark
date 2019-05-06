d0917c22f5b5bad5684942b6f76705a33e0f55da
Troels Henriksen <athas@sigkill.dk>
2019-05-06 13:29:07 +0200

    More debug prints.

--- src/Futhark/CodeGen/ImpGen/Kernels/SegGenRed.hs
+++ src/Futhark/CodeGen/ImpGen/Kernels/SegGenRed.hs
@@ -609,6 +609,10 @@ compileSegGenRed (Pattern _ pes) space ops kbody = do
   sUnless (h .==. 0) $ do
     lh <- dPrimV "lh" $ (g * t) `quotRoundingUp` h
 
+    emit $ Imp.DebugPrint "# SegGenRed\nCooperation level" int32 coop
+    emit $ Imp.DebugPrint "Memory per set of subhistograms" int32 h
+    emit $ Imp.DebugPrint "Desired group size" int32 g
+
     forM_ slugs $
       emit . Imp.DebugPrint "Number of subhistograms" int32 .
       (`Imp.var` int32) . slugNumSubhistos
