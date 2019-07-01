4a7018b500f5d0249ba121087dcfe35cdb67bc60
Troels Henriksen <athas@sigkill.dk>
2019-07-01 09:03:13 +0200

    Bump copyright year.

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Futhark'
-copyright = '2013-2018, DIKU, University of Copenhagen'
+copyright = '2013-2019, DIKU, University of Copenhagen'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
