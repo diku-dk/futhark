f5afb1f51ab1c8a0540655403f3a2f9f2d6b69d0
Troels Henriksen <athas@sigkill.dk>
2019-08-22 17:28:47 +0200

    Fix aliasing in record updates.

--- src/Language/Futhark/TypeChecker/Terms.hs
+++ src/Language/Futhark/TypeChecker/Terms.hs
@@ -1041,8 +1041,10 @@ checkExp (RecordUpdate src fields ve NoInfo loc) = do
   a <- expType src'
   let usage = mkUsage loc "record update"
   r <- foldM (flip $ mustHaveField usage) a fields
-  unify usage (toStructural r) . toStructural =<< expType ve'
-  return $ RecordUpdate src' fields ve' (Info a) loc
+  ve_t <- expType ve'
+  unify usage (toStructural r) (toStructural ve_t)
+  a' <- onRecordField (`setAliases` aliases ve_t) fields <$> expType src'
+  return $ RecordUpdate src' fields ve' (Info a') loc
 
 checkExp (Index e idxes NoInfo loc) = do
   (t, _) <- newArrayType (srclocOf e) "e" $ length idxes
---
+++ tests/uniqueness/uniqueness-error48.fut
@@ -0,0 +1,10 @@
+-- Record updates should respect uniqueness and aliases.
+-- ==
+-- error: `s`
+
+type state = { size: i32, world: []i32 }
+
+let init (size: i32): state = {size, world = replicate size 0}
+
+let main (size: i32) (s: state) : *[]i32 =
+  (init size with world = s.world).world
