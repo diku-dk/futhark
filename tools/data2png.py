cf9ab4891bb1529b5c36b8b1832bb8e4043bedb9
Troels Henriksen <athas@sigkill.dk>
2019-09-09 17:47:16 +0200

    Try handling a few more formats.

--- tools/data2png.py
+++ tools/data2png.py
@@ -16,28 +16,37 @@ import struct
 import numpy as np
 import png
 
-def read_image(f = sys.stdin.buffer):
+def read_image(f):
     # Skip binary header.
     assert f.read(1) == b'b', 'Invalid binary header'
     assert f.read(1) == b'\2', 'Invalid binary format'
     rank = np.int8(struct.unpack('<B', f.read(1))[0])
     type = f.read(4)
 
-    if rank == 2:
-        assert type in [b' i32', b' u32'], 'Invalid element type {}'.format(type)
+    if rank == 2 and type == b'  u8':
         height = np.int64(struct.unpack('<Q', f.read(8))[0])
         width = np.int64(struct.unpack('<Q', f.read(8))[0])
-        array = np.frombuffer(f.read(height*width*4), dtype=np.int32).reshape(height, width)
+        array = np.frombuffer(f.read(height*width), dtype=np.int8).reshape(height, width)
+
+        return (width, height, array)
+
+    if rank == 2 and type in [b' i32', b' u32', b' f32']:
+        height = np.int64(struct.unpack('<Q', f.read(8))[0])
+        width = np.int64(struct.unpack('<Q', f.read(8))[0])
 
         image=np.empty((height,width,3), dtype=np.int8)
-        image[:,:,0] = (array & 0xFF0000) >> 16
-        image[:,:,1] = (array & 0xFF00) >> 8
-        image[:,:,2] = (array & 0xFF)
+        if type == b' f32':
+            array = np.frombuffer(f.read(height*width*4), dtype=np.float32).reshape(height, width)
+            image[:,:,0] = image[:,:,1] = image[:,:,2] = np.int8(np.float32(array)*256)
+        else:
+            array = np.frombuffer(f.read(height*width*4), dtype=np.int32).reshape(height, width)
+            image[:,:,0] = (array & 0xFF0000) >> 16
+            image[:,:,1] = (array & 0xFF00) >> 8
+            image[:,:,2] = (array & 0xFF)
 
         return (width, height, np.reshape(image, (height, width*3)))
 
-    elif rank == 3:
-        assert type in [b'  i8', b'  u8'], 'Invalid element type {}'.format(type)
+    elif rank == 3 and type in [b'  i8', b'  u8']:
         height = np.int64(struct.unpack('<Q', f.read(8))[0])
         width = np.int64(struct.unpack('<Q', f.read(8))[0])
         depth = np.int64(struct.unpack('<Q', f.read(8))[0])
@@ -47,11 +56,15 @@ def read_image(f = sys.stdin.buffer):
         return (width, height, np.reshape(image, (height, width*3)))
 
     else:
-        raise Error('Invalid input rank {}'.format(rank))
+        raise Exception('Invalid rank \'{}\' and element type {}'.format(rank, type))
 
 if __name__ == '__main__':
-    fname = sys.argv[1]
-    width, height, img = read_image()
-    with open(fname, 'wb') as f:
-        w = png.Writer(width=width, height=height, alpha=False)
-        w.write(f, img)
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
+
+    with open(infile, 'rb') as f:
+        width, height, img = read_image(f)
+
+        with open(outfile, 'wb') as f:
+            w = png.Writer(width=width, height=height, alpha=False)
+            w.write(f, img)
