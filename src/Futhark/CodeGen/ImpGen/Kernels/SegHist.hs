0ab77f2db1309136ad52a20c533492ba04594847
Cosmin Oancea <cosmin.oancea@diku.dk>
2020-02-06 13:08:32 +0100

    adjusting num chunks factor

--- src/Futhark/CodeGen/ImpGen/Kernels/SegHist.hs
+++ src/Futhark/CodeGen/ImpGen/Kernels/SegHist.hs
@@ -728,8 +728,8 @@ histKernelLocal num_subhistos_per_group_var groups_per_segment map_pes num_group
 slugMaxLocalMemPasses :: SegHistSlug -> Int
 slugMaxLocalMemPasses slug =
   case slugAtomicUpdate slug of
-    AtomicPrim _ -> 1
-    AtomicCAS _ -> 2
+    AtomicPrim _ -> 2 --1
+    AtomicCAS _  -> 4 --2
     AtomicLocking _ -> 4
 
 localMemoryCase :: [PatElem ExplicitMemory]
