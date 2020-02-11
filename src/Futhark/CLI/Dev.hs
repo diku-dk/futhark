a6c0238205973923082a350aae20388748df5a0c
Troels Henriksen <athas@sigkill.dk>
2020-02-11 20:28:46 +0100

    Slightly better prettyprinting from futhark dev.

--- src/Futhark/CLI/Dev.hs
+++ src/Futhark/CLI/Dev.hs
@@ -3,6 +3,7 @@
 module Futhark.CLI.Dev (main) where
 
 import Data.Maybe
+import Data.List
 import Control.Category (id)
 import Control.Monad
 import Control.Monad.State
@@ -343,7 +344,12 @@ main = mainWithOptions newConfig commandLineOptions "options... program" compile
               Right () -> return ()
         compile _      _      =
           Nothing
-        m file config =
+        m file config = do
+          let p :: (Show a, PP.Pretty a) => [a] -> IO ()
+              p = mapM_ putStrLn .
+                  intersperse "" .
+                  map (if futharkPrintAST config then show else pretty)
+
           case futharkPipeline config of
             PrettyPrint -> liftIO $ do
               maybe_prog <- parseFuthark file <$> T.readFile file
@@ -359,17 +365,16 @@ main = mainWithOptions newConfig commandLineOptions "options... program" compile
                            else pretty $ fileProg fm
             Defunctorise -> do
               (_, imports, src) <- readProgram file
-              liftIO $ mapM_ (putStrLn . if futharkPrintAST config then show else pretty) $
-                evalState (Defunctorise.transformProg imports) src
+              liftIO $ p $ evalState (Defunctorise.transformProg imports) src
             Monomorphise -> do
               (_, imports, src) <- readProgram file
-              liftIO $ mapM_ (putStrLn . if futharkPrintAST config then show else pretty) $
+              liftIO $ p $
                 flip evalState src $
                 Defunctorise.transformProg imports
                 >>= Monomorphise.transformProg
             Defunctionalise -> do
               (_, imports, src) <- readProgram file
-              liftIO $ mapM_ (putStrLn . if futharkPrintAST config then show else pretty) $
+              liftIO $ p $
                 flip evalState src $
                 Defunctorise.transformProg imports
                 >>= Monomorphise.transformProg
