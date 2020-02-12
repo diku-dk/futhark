ba367542816a9776516387f81eafb36b81eb2016
Cosmin Oancea <cosmin.oancea@diku.dk>
2020-02-12 17:32:43 +0100

    adjusting the decision between local and global memory for histograms

--- src/Futhark/CodeGen/ImpGen/Kernels/SegHist.hs
+++ src/Futhark/CodeGen/ImpGen/Kernels/SegHist.hs
@@ -728,9 +728,9 @@ histKernelLocal num_subhistos_per_group_var groups_per_segment map_pes num_group
 slugMaxLocalMemPasses :: SegHistSlug -> Int
 slugMaxLocalMemPasses slug =
   case slugAtomicUpdate slug of
-    AtomicPrim _ -> 2 --1
-    AtomicCAS _  -> 4 --2
-    AtomicLocking _ -> 4
+    AtomicPrim _ -> 2
+    AtomicCAS _  -> 4
+    AtomicLocking _ -> 5
 
 localMemoryCase :: [PatElem ExplicitMemory]
                 -> Imp.Exp
