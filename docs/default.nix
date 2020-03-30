f3d7ba5a800b72be5773e62aefecf4444207131a
Troels Henriksen <athas@sigkill.dk>
2020-03-30 23:29:10 +0200

    Python is implicit here.

--- docs/default.nix
+++ docs/default.nix
@@ -1,5 +1,5 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
     name = "futhark-docs";
-    buildInputs = [ python37 python37Packages.sphinx ];
+    buildInputs = [ python37Packages.sphinx ];
 }
