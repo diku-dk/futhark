511425bb1004d4d09e656bc0b2cd483e9765a831
Philip Munksgaard <philip@munksgaard.me>
2020-05-27 10:15:04 +0200

    Perform style check in run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -5,20 +5,26 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+tools/style-check.sh src || echo "::error:: A style violation was found" && exit 1
+
 # cabal installs here.
 export PATH=$HOME/.cabal/bin:$PATH
+
+cabal update
+
+cabal test || echo "::error:: Unit tests failed" && exit 1
 
 # When running in a pure nix-shell, we need to tell git where it can
 # find certificates.
 export GIT_SSL_CAINFO=/etc/ssl/certs/ca-certificates.crt
 
-cabal install --install-method=copy --overwrite-policy=always --installdir=$HOME/.cabal/bin
+cabal install --install-method=copy --overwrite-policy=always --installdir=$HOME/.cabal/bin || echo "::error:: Could not build futhark" && exit 1
 
 echo "# Integration tests"
-futhark test --no-terminal tests examples --no-tuning
+futhark test --no-terminal tests examples --no-tuning || echo "::error:: Integration tests failed" && exit 1
 
 echo "# Library tests"
-make -C libtests/c
+make -C libtests/c || echo "::error:: Library tests failed" && exit 1
 
 echo "# Package tests"
-(cd pkgtests; sh test.sh)
+(cd pkgtests; sh test.sh) || echo "::error:: Package tests failed" && exit 1
