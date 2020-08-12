df7093014b8e63661f5bfadf2ed5299dcef498db
Troels Henriksen <athas@sigkill.dk>
2020-08-12 16:05:53 +0200

    Be more consistent.

--- src/Futhark/Pass/ExtractKernels/Intragroup.hs
+++ src/Futhark/Pass/ExtractKernels/Intragroup.hs
@@ -148,7 +148,7 @@ parallelMin ws = tell mempty { accMinPar = S.singleton ws
 
 intraGroupBody :: SegLevel -> Body -> IntraGroupM (Out.Body Out.Kernels)
 intraGroupBody lvl body = do
-  stms <- collectStms_ $ mapM_ (intraGroupStm lvl) $ bodyStms body
+  stms <- collectStms_ $ intraGroupStms lvl $ bodyStms body
   return $ mkBody stms $ bodyResult body
 
 intraGroupStm :: SegLevel -> Stm -> IntraGroupM ()
@@ -175,7 +175,7 @@ intraGroupStm lvl stm@(Let pat aux e) = do
 
     Op soac
       | "sequential_outer" `inAttrs` stmAuxAttrs aux ->
-          mapM_ (intraGroupStm lvl) . fmap (certify (stmAuxCerts aux)) =<<
+          intraGroupStms lvl . fmap (certify (stmAuxCerts aux)) =<<
           runBinder_ (FOT.transformSOAC pat soac)
 
     Op (Screma w form arrs)
@@ -248,7 +248,7 @@ intraGroupStm lvl stm@(Let pat aux e) = do
           replace se = se
           replaceSets (Acc x y log) =
             Acc (S.map (map replace) x) (S.map (map replace) y) log
-      censor replaceSets $ mapM_ (intraGroupStm lvl) stream_bnds
+      censor replaceSets $ intraGroupStms lvl stream_bnds
 
     Op (Scatter w lam ivs dests) -> do
       write_i <- newVName "write_i"
