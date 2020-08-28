f5852fe22c65037cba142598ab0bbac4fd069b68
HnimNart <minhtran1391@gmail.com>
2020-08-28 18:36:22 +0200

    No need to measure sequential runs

--- rts/c/scheduler.h
+++ rts/c/scheduler.h
@@ -282,23 +282,23 @@ static inline int scheduler_execute_task(struct scheduler *scheduler,
   int64_t task_timer = 0;
   /* Execute task sequential or parallel based on decision made earlier */
   if (task->info.nsubtasks == 1) {
-    int64_t start = get_wall_time_ns();
+    /* int64_t start = get_wall_time_ns(); */
     err = task->fn(task->args, 0, task->iterations, 0, worker->tid);
-    int64_t end = get_wall_time_ns();
-    task_timer = end - start;
-    worker_local->time_spent_working += task_timer;
-
+    /* int64_t end = get_wall_time_ns(); */
+    /* task_timer = end - start; */
+    /* worker_local->time_spent_working += task_timer; */
     // Report time measurements
     // TODO the update of both of these should really both be atomic!!
-    __atomic_fetch_add(task->info.total_time, task_timer, __ATOMIC_RELAXED);
-    __atomic_fetch_add(task->info.total_iter, task->iterations, __ATOMIC_RELAXED);
+    /* __atomic_fetch_add(task->info.total_time, task_timer, __ATOMIC_RELAXED); */
+    /* __atomic_fetch_add(task->info.total_iter, task->iterations, __ATOMIC_RELAXED); */
   }
   else
   {
     // Add "before" time if we already are inside a task
     int64_t time_before = 0;
-    if (worker->nested > 0)
+    if (worker->nested > 0) {
       time_before = total_now(worker->total, worker->timer);
+    }
 
     err = scheduler_execute_parallel(scheduler, task, &task_timer);
 
