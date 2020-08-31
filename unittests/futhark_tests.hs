c47d8e0a463ad1463c0e60bab2662190ef81b229
Philip Munksgaard <philip@munksgaard.me>
2020-08-31 10:47:12 +0200

    Add unittests

--- unittests/futhark_tests.hs
+++ unittests/futhark_tests.hs
@@ -1,10 +1,12 @@
 module Main (main) where
 
+import qualified Futhark.Analysis.InterferenceTests
 import qualified Futhark.BenchTests
 import qualified Futhark.IR.Mem.IxFunTests
 import qualified Futhark.IR.PrimitiveTests
 import qualified Futhark.IR.PropTests
 import qualified Futhark.IR.Syntax.CoreTests
+import qualified Futhark.Optimise.ReuseAllocations.GreedyColoringTests
 import qualified Futhark.Pkg.SolveTests
 import qualified Language.Futhark.SyntaxTests
 import Test.Tasty
@@ -19,7 +21,9 @@ allTests =
       Futhark.IR.Syntax.CoreTests.tests,
       Futhark.Pkg.SolveTests.tests,
       Futhark.IR.Mem.IxFunTests.tests,
-      Futhark.IR.PrimitiveTests.tests
+      Futhark.IR.PrimitiveTests.tests,
+      Futhark.Optimise.ReuseAllocations.GreedyColoringTests.tests,
+      Futhark.Analysis.InterferenceTests.tests
     ]
 
 main :: IO ()
