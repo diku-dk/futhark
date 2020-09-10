c807370fba52492bdbce92fafdb465072dd3a87e
HnimNart <minhtran1391@gmail.com>
2020-09-10 10:12:24 +0200

    Add comments

--- rts/c/timing.h
+++ rts/c/timing.h
@@ -26,17 +26,30 @@ static int64_t get_wall_time(void) {
   return time.tv_sec * 1000000 + time.tv_usec;
 }
 
+static int64_t get_wall_time_ns(void) {
+  struct timespec time;
+  assert(clock_gettime(CLOCK_REALTIME, &time) == 0);
+  return time.tv_sec * 1000000000 + time.tv_nsec;
+}
+
+
+
 static inline uint64_t rdtsc() {
   unsigned int hi, lo;
   __asm__ __volatile__("rdtsc" : "=a"(lo), "=d"(hi));
   return  ((uint64_t) lo) | (((uint64_t) hi) << 32);
 }
 
-static int64_t get_wall_time_ns(void) {
-  struct timespec time;
-  assert(clock_gettime(CLOCK_REALTIME, &time) == 0);
-  return time.tv_sec * 1000000000 + time.tv_nsec;
+static inline void rdtsc_wait(uint64_t n) {
+  const uint64_t start = rdtsc();
+  while (rdtsc() < (start + n)) {
+    __asm__("PAUSE");
+  }
 }
+static inline void spin_for(uint64_t nb_cycles) {
+  rdtsc_wait(nb_cycles);
+}
+
 
 
 // This does not return a consistent CPU frequency
