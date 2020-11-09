037771b5ae0e6ec1d60546261606e6c0956e7a78
Philip Lassen <philiplassen+git@gmail.com>
2020-11-09 17:57:36 +0100

    Catch error, instead of check

--- src/Futhark/CLI/Datacmp.hs
+++ src/Futhark/CLI/Datacmp.hs
@@ -3,19 +3,31 @@
 -- | @futhark datacmp@
 module Futhark.CLI.Datacmp (main) where
 
+import Control.Exception
 import qualified Data.ByteString.Lazy.Char8 as BS
+import qualified Data.Text.IO as T
 import Futhark.Test.Values
 import Futhark.Util.Options
-import System.Directory
 import System.Exit
+import System.IO.Error
+
+doesFileExistSafely :: String -> IO Bool
+doesFileExistSafely filepath =
+  (T.readFile filepath >> return True) `catch` couldNotRead
+  where
+    couldNotRead e
+      | isDoesNotExistError e =
+        return False
+      | otherwise =
+        return True
 
 -- | Run @futhark datacmp@
 main :: String -> [String] -> IO ()
 main = mainWithOptions () [] "<file> <file>" f
   where
     f [file_a, file_b] () = Just $ do
-      aExists <- doesFileExist file_a
-      bExists <- doesFileExist file_b
+      aExists <- doesFileExistSafely file_a
+      bExists <- doesFileExistSafely file_b
       case (aExists, bExists) of
         (False, _) ->
           putStrLn $ "No such file : " ++ file_a
