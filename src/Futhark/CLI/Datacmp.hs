9eabbe1dfc20cdd2307bcb4d2d33c74014475b28
Philip Lassen <philiplassen+git@gmail.com>
2020-11-10 10:53:38 +0100

    Fix toctou issue

--- src/Futhark/CLI/Datacmp.hs
+++ src/Futhark/CLI/Datacmp.hs
@@ -5,37 +5,40 @@ module Futhark.CLI.Datacmp (main) where
 
 import Control.Exception
 import qualified Data.ByteString.Lazy.Char8 as BS
-import qualified Data.Text.IO as T
 import Futhark.Test.Values
 import Futhark.Util.Options
 import System.Exit
 import System.IO.Error
 
-doesFileExistSafely :: String -> IO Bool
-doesFileExistSafely filepath =
-  (T.readFile filepath >> return True) `catch` couldNotRead
+readFileSafely :: String -> IO (Maybe (Either String BS.ByteString))
+readFileSafely filepath =
+  (Just . Right <$> BS.readFile filepath) `catch` couldNotRead
   where
     couldNotRead e
       | isDoesNotExistError e =
-        return False
+        return Nothing
       | otherwise =
-        return True
+        return $ Just $ Left $ show e
 
 -- | Run @futhark datacmp@
 main :: String -> [String] -> IO ()
 main = mainWithOptions () [] "<file> <file>" f
   where
     f [file_a, file_b] () = Just $ do
-      aExists <- doesFileExistSafely file_a
-      bExists <- doesFileExistSafely file_b
-      case (aExists, bExists) of
-        (False, _) ->
+      file_contents_a_maybe <- readFileSafely file_a
+      file_contents_b_maybe <- readFileSafely file_b
+      case (file_contents_a_maybe, file_contents_b_maybe) of
+        (Nothing, _) ->
           putStrLn $ "No such file : " ++ file_a
-        (_, False) ->
+        (_, Nothing) ->
           putStrLn $ "No such file : " ++ file_b
-        (True, True) -> do
-          vs_a_maybe <- readValues <$> BS.readFile file_a
-          vs_b_maybe <- readValues <$> BS.readFile file_b
+        (Just (Left err_msg), _) ->
+          error $ err_msg ++ " when reading " ++ file_a
+        (_, Just (Left err_msg)) ->
+          error $ err_msg ++ " when reading " ++ file_b
+        (Just (Right contents_a), Just (Right contents_b)) -> do
+          let vs_a_maybe = readValues contents_a
+          let vs_b_maybe = readValues contents_b
           case (vs_a_maybe, vs_b_maybe) of
             (Nothing, _) ->
               error $ "Error reading values from " ++ file_a
