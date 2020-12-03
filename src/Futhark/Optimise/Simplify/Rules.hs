732218efb2a2723dde220940e3383b6a51ec3659
Troels Henriksen <athas@sigkill.dk>
2020-12-03 21:24:54 +0100

    Fix #1194.

--- src/Futhark/Optimise/Simplify/Rules.hs
+++ src/Futhark/Optimise/Simplify/Rules.hs
@@ -488,25 +488,30 @@ simplifyBinOp look _ (BinOp Add {} e1 e2)
 simplifyBinOp _ _ (BinOp FAdd {} e1 e2)
   | isCt0 e1 = subExpRes e2
   | isCt0 e2 = subExpRes e1
-simplifyBinOp look _ (BinOp Sub {} e1 e2)
+simplifyBinOp look _ (BinOp sub@(Sub t _) e1 e2)
   | isCt0 e2 = subExpRes e1
   -- Cases for simplifying (a+b)-b and permutations.
+
+  -- (e1_a+e1_b)-e1_a == e1_b
   | Var v1 <- e1,
     Just (BasicOp (BinOp Add {} e1_a e1_b), cs) <- look v1,
     e1_a == e2 =
     Just (SubExp e1_b, cs)
+  -- (e1_a+e1_b)-e1_b == e1_a
   | Var v1 <- e1,
     Just (BasicOp (BinOp Add {} e1_a e1_b), cs) <- look v1,
     e1_b == e2 =
     Just (SubExp e1_a, cs)
+  -- e2_a-(e2_a+e2_b) == 0-e2_b
   | Var v2 <- e2,
     Just (BasicOp (BinOp Add {} e2_a e2_b), cs) <- look v2,
     e2_a == e1 =
-    Just (SubExp e2_b, cs)
-  | Var v2 <- e1,
+    Just (BinOp sub (intConst t 0) e2_b, cs)
+  -- e2_b-(e2_a+e2_b) == 0-e2_a
+  | Var v2 <- e2,
     Just (BasicOp (BinOp Add {} e2_a e2_b), cs) <- look v2,
     e2_b == e1 =
-    Just (SubExp e2_a, cs)
+    Just (BinOp sub (intConst t 0) e2_a, cs)
 simplifyBinOp _ _ (BinOp FSub {} e1 e2)
   | isCt0 e2 = subExpRes e1
 simplifyBinOp _ _ (BinOp Mul {} e1 e2)
