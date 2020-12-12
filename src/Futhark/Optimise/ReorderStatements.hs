708403c750b439e13ad5a0544e16046b59d3f6ed
Philip Munksgaard <philip@munksgaard.me>
2020-12-12 20:01:44 +0100

    Nicer

--- src/Futhark/Optimise/ReorderStatements.hs
+++ src/Futhark/Optimise/ReorderStatements.hs
@@ -1,8 +1,3 @@
-{-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE TypeFamilies #-}
-
 -- | This module contains an optimization that tries to reorder statements
 -- within bodies such that related statements are moved as close to each other
 -- as possible.
@@ -14,34 +9,30 @@ import Data.Functor.Identity
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Sequence ((|>))
+-- import Futhark.Analysis.Alias (aliasAnalysis)
+-- import Futhark.IR.Aliases (removeProgAliases)
 import Futhark.IR.KernelsMem
 import Futhark.Pass (Pass (..))
 
--- Computes the list of `VName` defined by a given `PatternT`
-namesInPat :: PatternT lore -> [VName]
-namesInPat p =
-  fmap patElemName (patternContextElements p)
-    <> fmap patElemName (patternValueElements p)
-
 reorderBody :: Names -> Body KernelsMem -> Body KernelsMem
-reorderBody alreadyDefined body@Body {bodyResult, bodyStms} =
-  let m = statementMap bodyStms
+reorderBody alreadyDefined body@Body {bodyResult = res, bodyStms = stms} =
+  let m = statementMap stms
    in body
         { bodyStms =
             reorderStatements
-              (namesToList $ freeIn bodyResult)
+              (namesToList $ freeIn res)
               alreadyDefined
               m
               mempty
         }
 
 reorderKernelBody :: Names -> KernelBody KernelsMem -> KernelBody KernelsMem
-reorderKernelBody alreadyDefined kbody@KernelBody {kernelBodyResult, kernelBodyStms} =
-  let m = statementMap kernelBodyStms
+reorderKernelBody alreadyDefined kbody =
+  let m = statementMap $ kernelBodyStms kbody
    in kbody
         { kernelBodyStms =
             reorderStatements
-              (namesToList $ freeIn kernelBodyResult)
+              (namesToList $ freeIn $ kernelBodyResult kbody)
               alreadyDefined
               m
               mempty
@@ -52,7 +43,7 @@ statementMap :: Stms lore -> Map VName (Stm lore)
 statementMap stms =
   toList stms
     & concatMap
-      (\stm -> [(vname, stm) | vname <- namesInPat $ stmPattern stm])
+      (\stm -> [(vname, stm) | vname <- patternNames $ stmPattern stm])
     & Map.fromList
 
 -- | Attemts to reorder statements by maintaining a stack of `VName` we need to
@@ -62,7 +53,7 @@ statementMap stms =
 -- `VName` used in the the computation of `x` and insert those at the front of
 -- the stack. If no no `VName` is required, insert the statement defining `x`
 -- and add `x` and all other `VName` defined in that statement to the set of
--- values in `scope`.
+-- values in `vtable`.
 reorderStatements ::
   [VName] ->
   Names ->
@@ -70,35 +61,26 @@ reorderStatements ::
   Stms KernelsMem ->
   Stms KernelsMem
 reorderStatements [] _ _ acc = acc
-reorderStatements (x : xs) scope m acc =
-  if x `nameIn` scope
-    then reorderStatements xs scope m acc
+reorderStatements (x : xs) vtable m acc =
+  if x `nameIn` vtable
+    then reorderStatements xs vtable m acc
     else case Map.lookup x m of
-      Just stm -> case namesToList $ freeIn stm `namesSubtract` scope of
+      Just stm -> case namesToList $ freeIn stm `namesSubtract` vtable of
         [] ->
-          let scope' =
-                namesFromList (namesInPat (stmPattern stm)) <> scope
-           in reorderStatements
-                xs
-                scope'
-                m
-                $ acc
-                  |> stm
-                    { stmExp =
-                        mapExp (mapper scope') $
-                          stmExp stm
-                    }
-        todo -> reorderStatements (todo <> (x : xs)) scope m acc
+          let vtable' = namesFromList (patternNames $stmPattern stm) <> vtable
+              acc' = acc |> stm {stmExp = mapExp (mapper vtable') $ stmExp stm}
+           in reorderStatements xs vtable' m acc'
+        todo -> reorderStatements (todo <> (x : xs)) vtable m acc
       Nothing ->
         -- The variable doesn't appear in the statement-map. We therefore assume
         -- that it comes from outside this body, and that it is already in
-        -- scope.
-        reorderStatements xs (oneName x <> scope) m acc
+        -- vtable.
+        reorderStatements xs (oneName x <> vtable) m acc
 
 mapper :: Names -> Mapper KernelsMem KernelsMem Identity
-mapper scope =
+mapper vtable =
   identityMapper
-    { mapOnBody = \_ b -> return $ reorderBody scope b,
+    { mapOnBody = \_ b -> return $ reorderBody vtable b,
       mapOnOp = onOp
     }
   where
@@ -113,20 +95,15 @@ mapper scope =
         }
 
     onKernelBody body = do
-      return $ reorderKernelBody scope body
+      return $ reorderKernelBody vtable body
 
     onLambda lam = do
-      return lam {lambdaBody = reorderBody scope $ lambdaBody lam}
+      return lam {lambdaBody = reorderBody vtable $ lambdaBody lam}
 
 optimise :: Pass KernelsMem KernelsMem
 optimise =
-  Pass "reorder statements" "reorder statements" $ \prog@Prog {progFuns} ->
-    return $
-      prog
-        { progFuns =
-            fmap
-              ( \f@FunDef {funDefBody, funDefParams} ->
-                  f {funDefBody = reorderBody (freeIn funDefParams) funDefBody}
-              )
-              progFuns
-        }
+  Pass "reorder statements" "reorder statements" $ \prog ->
+    return $ prog {progFuns = fmap funHelper $ progFuns prog}
+  where
+    funHelper f =
+      f {funDefBody = reorderBody (freeIn $ funDefParams f) $ funDefBody f}
