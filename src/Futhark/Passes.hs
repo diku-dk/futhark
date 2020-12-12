c8de2408137e1a157fb7520401b6eded387f1a1f
Philip Munksgaard <philip@munksgaard.me>
2020-12-12 10:46:58 +0100

    Add `ReorderStatements` pass
    This should unlock some further optimizations by the `ReuseAllocations` pass, by
    improving code-locality.
    
    More info: https://munksgaard.me/technical-diary/2020-10-07.html#orgbbfe6a7

--- src/Futhark/Passes.hs
+++ src/Futhark/Passes.hs
@@ -25,6 +25,7 @@ import Futhark.Optimise.DoubleBuffer
 import Futhark.Optimise.Fusion
 import Futhark.Optimise.InPlaceLowering
 import Futhark.Optimise.InliningDeadFun
+import qualified Futhark.Optimise.ReorderStatements as ReorderStatements
 import qualified Futhark.Optimise.ReuseAllocations as ReuseAllocations
 import Futhark.Optimise.Sink
 import Futhark.Optimise.TileLoops
@@ -101,6 +102,8 @@ gpuPipeline =
         performCSE False,
         simplifyKernelsMem,
         doubleBufferKernels,
+        simplifyKernelsMem,
+        ReorderStatements.optimise,
         simplifyKernelsMem,
         ReuseAllocations.optimise,
         simplifyKernelsMem,
