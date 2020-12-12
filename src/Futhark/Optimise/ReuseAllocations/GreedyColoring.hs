f24a1383a13b10854e3eac6ab1b4b4b9dc9e765d
Philip Munksgaard <philip@munksgaard.me>
2020-12-12 10:46:39 +0100

    Refactor and doc comments

--- src/Futhark/Optimise/ReuseAllocations/GreedyColoring.hs
+++ src/Futhark/Optimise/ReuseAllocations/GreedyColoring.hs
@@ -1,3 +1,4 @@
+-- | Provides a greedy graph-coloring algorithm.
 module Futhark.Optimise.ReuseAllocations.GreedyColoring (colorGraph, Coloring) where
 
 import Data.Function ((&))
@@ -8,10 +9,13 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import qualified Futhark.Analysis.Interference as Interference
 
+-- | A map of values to their color, identified by an integer.
 type Coloring a = Map a Int
 
+-- | A map of values to the set "neighbors" in the graph
 type Neighbors a = Map a (Set a)
 
+-- | Computes the neighbor map of a graph.
 neighbors :: Ord a => Interference.Graph a -> Neighbors a
 neighbors =
   Set.foldr
@@ -42,6 +46,10 @@ colorNode nbs (x, sp) (spaces, coloring) =
       (spaces', color) = firstAvailable spaces nb_colors 0 sp
    in (spaces', Map.insert x color coloring)
 
+-- | Graph coloring that takes into account the `space` of values. Two values
+-- can only share the same color if they live in the same space. The result is
+-- map from each color to a space and a map from each value in the input graph
+-- to it's new color.
 colorGraph ::
   (Ord a, Ord space) =>
   Map a space ->
