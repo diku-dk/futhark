f6b595b0fe43f51878c05af46372f0b4f9554230
Michaël El Kharroubi <michael.el-kharroubi@hesge.ch>
2020-12-22 17:47:23 +0100

    Useless comment

--- src/Futhark/CodeGen/ImpGen/MPI/SegMap.hs
+++ src/Futhark/CodeGen/ImpGen/MPI/SegMap.hs
@@ -35,7 +35,6 @@ compileSegMapBody flat_idx pat space (KernelBody _ kstms kres) = do
   kstms' <- mapM renameStm kstms
   collect $ do
     emit $ Imp.DebugPrint "SegMap fbody" Nothing
-    -- Continue here --------
     zipWithM_ dPrimV_ is $ map sExt64 $ unflattenIndex ns' $ tvExp flat_idx
     compileStms (freeIn kres) kstms' $
       zipWithM_ (writeResult is) (patternElements pat) kres
