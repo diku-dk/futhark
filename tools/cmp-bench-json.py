7ac13da28515bcc497ffb118db7257a8ba0726e4
Philip Munksgaard <philip@munksgaard.me>
2021-01-04 12:42:07 +0100

    Add memory info to futhark bench json output
    Also amend cmp-bench-json.py to be able to read and compare memory usage
    results, if present.

--- tools/cmp-bench-json.py
+++ tools/cmp-bench-json.py
@@ -7,6 +7,14 @@ import sys
 import numpy as np
 from collections import OrderedDict
 
+def mem_dict(a, b):
+    result = {}
+    try:
+        for k in set(a['bytes'].keys()) & set(b['bytes'].keys()):
+            result[k] = float(b['bytes'][k]) / float(a['bytes'][k])
+    finally:
+        return result
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -40,16 +48,19 @@ def compare(a_json, b_json):
                     b_dataset_results = b_prog_datasets[dataset]
                     if 'runtimes' in a_dataset_results:
                         a_runtimes = a_dataset_results['runtimes']
+                        a_bytes = a_dataset_results.get('bytes')
                         if not ('runtimes' in b_dataset_results):
                             print('In %s but failed in %s: program %s dataset %s' % (a_file, b_file, prog, dataset))
                             continue
                         b_runtimes = b_dataset_results['runtimes']
+                        b_bytes = b_dataset_results.get('bytes')
+                        mem_usage = mem_dict(a_dataset_results, b_dataset_results)
                         speedup = np.mean(a_runtimes)/np.mean(b_runtimes)
                         diff = abs(np.mean(a_runtimes)-np.mean(b_runtimes))
                         significant = diff > np.std(a_runtimes)/2 + np.std(a_runtimes)/2
                         # Apart from speedups, we also calculate whether the
                         # change is statistically significant.
-                        speedups[prog][dataset] = (speedup, significant)
+                        speedups[prog][dataset] = (speedup, mem_usage, significant)
 
     for prog,b_prog in b_json.items():
         if not prog in a_json:
@@ -67,14 +78,21 @@ def compare(a_json, b_json):
         prog_speedups = speedups[prog]
         if len(prog_speedups) > 0:
             print('\n%s%s%s' % (bcolors.HEADER+bcolors.BOLD, prog, bcolors.ENDC))
-            for dataset,(dataset_speedup,significant) in prog_speedups.items():
+            for dataset,(dataset_speedup,mem_usage,significant) in prog_speedups.items():
                 if significant and dataset_speedup > 1.01:
                     color = bcolors.OKGREEN
                 elif significant and dataset_speedup < 0.99:
                     color = bcolors.FAIL
                 else:
                     color = ''
-                print('  %s%s%10.2fx%s' % ((dataset+':').ljust(64), color, dataset_speedup, bcolors.ENDC))
+                mem_strings = []
+                for k, v in mem_usage.items():
+                    if v < 0.99:
+                        mem_strings.append("%s%4.2fx@%s%s" % (bcolors.OKGREEN, v, k, bcolors.ENDC))
+                    elif v > 1.01:
+                        mem_strings.append("%s%4.2fx@%s%s" % (bcolors.FAIL, v, k, bcolors.ENDC))
+                mem_string = "" if not mem_strings else " (mem: %s)" % ", ".join(mem_strings)
+                print('  %s%s%10.2fx%s%s' % ((dataset+':').ljust(64), color, dataset_speedup, bcolors.ENDC, mem_string))
 
 
 if __name__ == '__main__':
