2fcb87a5c563be173ecf8d07601683a21d565920
Troels Henriksen <athas@sigkill.dk>
2021-01-09 04:09:44 +0100

    Regenerate futhark.nix.

--- futhark.nix
+++ futhark.nix
@@ -1,13 +1,14 @@
 { mkDerivation, aeson, alex, ansi-terminal, array, base, binary
-, blaze-html, bytestring, cmark-gfm, containers, directory
-, directory-tree, dlist, file-embed, filepath, free, gitrev, happy
-, haskeline, language-c-quote, mainland-pretty, megaparsec, mtl
-, neat-interpolation, parallel, parser-combinators, pcg-random
-, process, process-extras, QuickCheck, regex-tdfa, sexp-grammar
-, srcloc, stdenv, tasty, tasty-hunit, tasty-quickcheck
-, template-haskell, temporary, terminal-size, text, time
-, transformers, unordered-containers, utf8-string, vector
-, vector-binary-instances, versions, zip-archive, zlib
+, blaze-html, bytestring, bytestring-to-vector, cmark-gfm
+, containers, directory, directory-tree, dlist, file-embed
+, filepath, free, gitrev, happy, haskeline, language-c-quote
+, mainland-pretty, megaparsec, mtl, neat-interpolation, parallel
+, parser-combinators, pcg-random, process, process-extras
+, QuickCheck, regex-tdfa, sexp-grammar, srcloc, stdenv, tasty
+, tasty-hunit, tasty-quickcheck, template-haskell, temporary
+, terminal-size, text, time, transformers, unordered-containers
+, utf8-string, vector, vector-binary-instances, versions
+, zip-archive, zlib
 }:
 mkDerivation {
   pname = "futhark";
@@ -17,13 +18,13 @@ mkDerivation {
   isExecutable = true;
   libraryHaskellDepends = [
     aeson ansi-terminal array base binary blaze-html bytestring
-    cmark-gfm containers directory directory-tree dlist file-embed
-    filepath free gitrev haskeline language-c-quote mainland-pretty
-    megaparsec mtl neat-interpolation parallel pcg-random process
-    process-extras regex-tdfa sexp-grammar srcloc template-haskell
-    temporary terminal-size text time transformers unordered-containers
-    utf8-string vector vector-binary-instances versions zip-archive
-    zlib
+    bytestring-to-vector cmark-gfm containers directory directory-tree
+    dlist file-embed filepath free gitrev haskeline language-c-quote
+    mainland-pretty megaparsec mtl neat-interpolation parallel
+    pcg-random process process-extras regex-tdfa sexp-grammar srcloc
+    template-haskell temporary terminal-size text time transformers
+    unordered-containers utf8-string vector vector-binary-instances
+    versions zip-archive zlib
   ];
   libraryToolDepends = [ alex happy ];
   executableHaskellDepends = [ base text ];
