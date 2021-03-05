c96d60af1b57fe136023f10f57c68e3077d8f2f3
Troels Henriksen <athas@sigkill.dk>
2021-03-05 20:31:42 +0100

    Fix #1263.

--- src/Language/Futhark/TypeChecker/Terms.hs
+++ src/Language/Futhark/TypeChecker/Terms.hs
@@ -2412,6 +2412,15 @@ causalityCheck binding_body = do
       onExp known (Var v (Info t) loc)
         | Just bad <- checkCausality (pquote (ppr v)) known t loc =
           bad
+      onExp known (ProjectSection _ (Info t) loc)
+        | Just bad <- checkCausality "projection section" known t loc =
+          bad
+      onExp known (OpSectionRight _ (Info t) _ _ _ loc)
+        | Just bad <- checkCausality "operator section" known t loc =
+          bad
+      onExp known (OpSectionLeft _ (Info t) _ _ _ loc)
+        | Just bad <- checkCausality "operator section" known t loc =
+          bad
       onExp known (ArrayLit [] (Info t) loc)
         | Just bad <- checkCausality "empty array" known t loc =
           bad
@@ -2430,15 +2439,7 @@ causalityCheck binding_body = do
         return e
       onExp
         known
-        e@( BinOp
-              (f, floc)
-              ft
-              (x, Info (_, xp))
-              (y, Info (_, yp))
-              _
-              (Info ext)
-              _
-            ) = do
+        e@(BinOp (f, floc) ft (x, Info (_, xp)) (y, Info (_, yp)) _ (Info ext) _) = do
           args_known <-
             lift $
               execStateT (sequencePoint known x y $ catMaybes [xp, yp]) mempty
