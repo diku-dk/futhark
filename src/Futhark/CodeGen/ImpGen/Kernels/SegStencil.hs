dab16f38852de29ec2b1146cb06067050c7ae37a
Jonathan WH <levestol@hotmail.com>
2021-03-10 23:19:02 +0100

    implement naive kernel-codegen for stencils

--- src/Futhark/CodeGen/ImpGen/Kernels/SegStencil.hs
+++ src/Futhark/CodeGen/ImpGen/Kernels/SegStencil.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeFamilies #-}
 
--- | Code generation for 'SegStencil' is quite straightforward. 
+-- | Code generation for 'SegStencil' is quite straightforward.
 module Futhark.CodeGen.ImpGen.Kernels.SegStencil (compileSegStencil) where
 
 import Control.Monad.Except
@@ -21,15 +21,19 @@ compileSegStencil ::
   StencilOp KernelsMem ->
   KernelBody KernelsMem ->
   CallKernelGen ()
-compileSegStencil pat lvl space stencilOp kbody = do
+compileSegStencil pat lvl space op kbody = do
   let (is, dims) = unzip $ unSegSpace space
       dims' = map toInt64Exp dims
       num_groups' = toInt64Exp <$> segNumGroups lvl
       group_size' = toInt64Exp <$> segGroupSize lvl
-      StencilOp stencil_is arrs lam' = stencilOp
-      parameters = (lambdaParams lam')
-      (invariantParams,variantParams) = splitAt ((length parameters) - (length arrs)) parameters
-  
+      stencil_ixss = map (map fromInteger) $ stencilIndexes op
+      invarElems = map kernelResultSubExp $ kernelBodyResult kbody
+      lam = stencilOp op
+      lamPars = lambdaParams lam
+      lamBody = lambdaBody lam
+      lamSplitPoint = length invarElems
+      (invariantParams,variantParams) =
+        splitAt lamSplitPoint lamPars
 
   case lvl of
     SegThread {} -> do
@@ -43,23 +47,39 @@ compileSegStencil pat lvl space stencilOp kbody = do
                 sExt64 group_id * sExt64 (unCount group_size')
                   + sExt64 local_tid
 
-          zipWithM_ dPrimV_ is $
-            map sExt64 $ unflattenIndex (map sExt64 dims') global_tid
+          -- create global ids for each axis (to check for out of bounds)
+          let gid_flat = map sExt64 $ unflattenIndex (map sExt64 dims') global_tid
+          zipWithM_ dPrimV_ is gid_flat
 
-          -- zipWithM dPrimV_ variantParams array[gtid + stencil_is_elem] 
+          -- check for out of bound on global id for each axis
+          sWhen (isActive $ unSegSpace space) $ do
+            -- compile invariant elements
+            compileStms mempty (kernelBodyStms kbody) $ pure ()
 
-          -- Imp.Call?
+            -- declare and attach invariant elements to invariantParams
+            zipWithM_ dPrimV_ (map paramName invariantParams)
+                =<< (map TPrimExp <$> mapM toExp invarElems)
 
+            -- calculate the unrolled variants parameter indexers
+            let bounded_ixs = flip map (zip3 gid_flat stencil_ixss dims') $
+                  \(axis_gid, axis_ixs, axis_len) -> flip map axis_ixs $
+                      \oi -> sMin64 (axis_len - 1) (sMax64 0 (axis_gid + oi))
+            let vname_ixs_for_tup =
+                  concatMap (\vn -> map ((,) vn) (transpose bounded_ixs)) $ stencilArrays op
 
-          --(\x y z -> x + y + z)
+            -- declare lambda variant parameters
+            dLParams variantParams
 
-          --x <- 1
-          -- ...
-          --x + y + z
+            -- load variants into lambda variant parameters
+            forM_ (zip variantParams vname_ixs_for_tup) $
+              \(vparam, (input_arr, ixs_tup)) -> do
+                  let pname = paramName vparam
+                  copyDWIMFix pname [] (Var input_arr) ixs_tup
 
-          sWhen (isActive $ unSegSpace space) $
-            compileStms mempty (kernelBodyStms kbody) $
+            -- compile lambda function and designate output style
+            compileStms mempty (bodyStms lamBody) $
               zipWithM_ (compileThreadResult space) (patternElements pat) $
-                kernelBodyResult kbody
+                map (Returns ResultMaySimplify) $ bodyResult lamBody
+
     SegGroup {} ->
       error "not implemented"
