f852d0c76c4b81a76f207774fb23907d8b995da8
Jonathan WH <levestol@hotmail.com>
2021-03-18 20:25:17 +0100

    do manual CSE on stencil big tile

--- src/Futhark/CodeGen/ImpGen/Kernels/SegStencil.hs
+++ src/Futhark/CodeGen/ImpGen/Kernels/SegStencil.hs
@@ -135,44 +135,45 @@ compileBigTileFlat pat lvl space op kbody = do
       lamPar = lambdaParams lam
       (invariantParams, variantParams) =
         splitAt (length invarElems) lamPar
-      a_mins = map (fromInteger . minimum) $ stencilIndexes op
-      a_maxs = map (fromInteger . maximum) $ stencilIndexes op
+      a_mins = map (fromInteger . (\x -> if x > 0 then error "invalid axis min" else x) . minimum) $ stencilIndexes op
+      a_maxs = map (fromInteger . (\x -> if x < 0 then error "invalid axis max" else x) . maximum) $ stencilIndexes op
       shared_sizes_exp = zipWith (+) group_sizes_exp $ zipWith (\x y -> x - y) a_maxs a_mins
-      headChunksOf _   [] = []
-      headChunksOf len xs =
+      chunksOf _   [] = []
+      chunksOf len xs =
         let (left, right) = splitAt len xs
-         in head left : headChunksOf len right
+         in left : chunksOf len right
       n_point_stencil = length $ head stencil_ixss
-      lamParTypes = map (elemType . paramType) $ headChunksOf n_point_stencil variantParams
+      lamParTypes = map (elemType . paramType) $ map head $ chunksOf n_point_stencil variantParams
+      unflattenIx _ [] _ = pure []
+      unflattenIx base (x:xs) i = do
+        dimIx <- dPrimVE base $ i `quot` x
+        (dimIx:) <$> unflattenIx base xs (i - (dimIx * x))
 
   case lvl of
     SegThread {} -> do
       emit $ Imp.DebugPrint "\n# SegStencil" Nothing
 
-      -- setup
+      -- Host side evaluated variables
       max_idxs <- mapM (dPrimVE "max_idx" . (+ (-1))) dims
       shared_sizes <- mapM (dPrimVE "shared_size") shared_sizes_exp
       shared_size_flat_var <- dPrimV "sh_flat" $ product shared_sizes
       group_sizes <- mapM (dPrimVE "group_sizes") group_sizes_exp
       readSet_iters <- mapM (dPrimV "readSet_iters") (zipWith divUp shared_sizes group_sizes)
       grid_sizes <- mapM (dPrimVE "grid_sizes") $ zipWith divUp dims group_sizes_exp
+      inner_grid <- mapM (dPrimVE "grid_sizes_inner") $ tail $ scanr (*) 1 grid_sizes
+      inner_group <- mapM (dPrimVE "group_sizes_inner") $ tail $ scanr (*) 1 group_sizes
 
       let dims_round_up = zipWith (*) grid_sizes group_sizes
-          virt_num_groups = -- may need correction
+          virt_num_groups =
             sExt32 $ product dims_round_up `divUp` unCount group_size_flat_c
       sKernelThread "segstencil" num_groups' group_size_flat_c (segFlat space) $
-        virtualiseGroups (segVirt lvl) virt_num_groups $ \group_id_flat_exp ->
-          let group_id_flat = sExt64 group_id_flat_exp
-              group_ids = unflattenIndex (map sExt64 grid_sizes) group_id_flat
-              lids = unflattenIndex (map sExt64 group_sizes) . sExt64 .
-                  kernelLocalThreadId . kernelConstants <$> askEnv
-           in do
-             local_ids <- mapM (dPrimVE "local_id") =<< lids
-             --compileBigTileFlatBody pat space op kbody group_sizes group_ids local_ids
+        virtualiseGroups (segVirt lvl) virt_num_groups $ \group_id_flat_exp -> do
+             group_id_flat <- dPrimVE "group_id_flat" $ sExt64 group_id_flat_exp
+             local_id_flat <- dPrimVE "local_id_flat" . sExt64 . kernelLocalThreadId . kernelConstants =<< askEnv
+             local_ids <- unflattenIx "local_id" (map sExt64 inner_group) local_id_flat
+             group_ids <- unflattenIx "group_id" (map sExt64 inner_grid) group_id_flat
 
-
-
-             tiles <- forM (lamParTypes) $ \ptype -> sAllocArray "tile_"  ptype (Shape [Var $ tvVar shared_size_flat_var]) (Space "local")
+             tiles <- forM (lamParTypes) $ \ptype -> sAllocArray "tile" ptype (Shape [Var $ tvVar shared_size_flat_var]) (Space "local")
              -- create max indexes for each axis, and the bound macro
              let bound_idxs = zipWith sMin64 max_idxs . map (sMax64 0)
 
@@ -181,16 +182,14 @@ compileBigTileFlat pat lvl space op kbody = do
                mapM (dPrimVE "writeSet_offset") $ zipWith (*) group_ids group_sizes
              -- create offsets for the readSet
              readSet_offsets <- mapM (dPrimVE "readSet_offset") $ zipWith (+) writeSet_offsets a_mins
-             --readSet_iters <- forM readSetIters (dPrimV "readSet_iters")
 
              sLoopNest (Shape $ map (Var . tvVar) readSet_iters) $ \ix_list -> do
-               tile_locals <- mapM (dPrimVE "tile_local_") $ zipWith (+) local_ids $ zipWith (*) ix_list group_sizes
-               tile_read_gids <- mapM (dPrimVE "tile_read_gid_") $ bound_idxs $ zipWith (+) readSet_offsets tile_locals
-               --tile_read_flat <- dPrimVE "tile_read_flat" $ flattenIndex dims tile_read_gids
-               sWhen (foldl1 (.&&.) (zipWith (.<.) tile_locals shared_sizes)) $
-                 let flat_local = flattenIndex shared_sizes tile_locals
-                  in forM_ (zip tiles $ stencilArrays op) $ \(tile, input_arr) ->
-                      copyDWIMFix tile [flat_local] (Var input_arr) tile_read_gids
+               tile_locals <- mapM (dPrimVE "tile_local") $ zipWith (+) local_ids $ zipWith (*) ix_list group_sizes
+               tile_read_gids <- mapM (dPrimVE "tile_read_gid") $ bound_idxs $ zipWith (+) readSet_offsets tile_locals
+               flat_local <- dPrimVE "tile_local_flat" $ flattenIndex shared_sizes tile_locals
+               sWhen (foldl1 (.&&.) (zipWith (.<.) tile_locals shared_sizes)) $ do
+                 forM_ (zip tiles $ stencilArrays op) $ \(tile, input_arr) -> do
+                     copyDWIMFix tile [flat_local] (Var input_arr) tile_read_gids
 
              -- group syncronize
              sOp $ Imp.Barrier Imp.FenceLocal
@@ -206,20 +205,17 @@ compileBigTileFlat pat lvl space op kbody = do
                zipWithM_ dPrimV_ (map paramName invariantParams) . map TPrimExp
                  =<< mapM toExp invarElems
 
-               sh_off <- mapM (dPrimVE "tile_offset") $ zipWith (-) local_ids a_mins
-               let param_ixs = flip map (zip sh_off stencil_ixss) $
-                     \(axis_lid, axis_ixs) -> map (axis_lid +) axis_ixs
-               let vname_ixs_for_tup =
-                     concatMap (mapM (,) (transpose param_ixs)) $ map Var tiles
-
+               sh_offs <- mapM (dPrimVE "tile_offset") $ zipWith (-) local_ids a_mins
                dLParams variantParams
 
-               -- load variants into lambda variant parameters
-               forM_ (zip variantParams vname_ixs_for_tup) $
-                 \(vparam, (ixs_tup, tile)) -> do
-                   --tile_ix <- dPrimVE "tile_ix" $ flattenIndex shared_sizes ixs_tup
-                   let tile_ix = flattenIndex shared_sizes ixs_tup
-                   copyDWIMFix (paramName vparam) [] tile [tile_ix]
+               let param_ixs = transpose $ zipWith (mapM (+)) stencil_ixss sh_offs
+               let params_ixs_ordered = transpose $ chunksOf n_point_stencil variantParams
+
+               ---- load variants into lambda variant parameters
+               forM_ (zip param_ixs params_ixs_ordered) $ \(parix, pars) -> do
+                   tile_ix <- dPrimVE "tile_ix" $ flattenIndex shared_sizes parix
+                   forM_ (zip pars tiles) $ \(par, tile) ->
+                     copyDWIMFix (paramName par) [] (Var tile) [tile_ix]
 
                -- compile lambda function and designate output style
                compileStms mempty (bodyStms lamBody) $
