2cda1fdf5269190a1ddb2ec64ab91a113dcc3a01
Jonathan WH <levestol@hotmail.com>
2021-03-18 21:32:54 +0100

    conform to code-style

--- src/Futhark/CodeGen/ImpGen/Kernels/SegStencil.hs
+++ src/Futhark/CodeGen/ImpGen/Kernels/SegStencil.hs
@@ -11,7 +11,7 @@ import Futhark.CodeGen.ImpGen
 import Futhark.CodeGen.ImpGen.Kernels.Base
 import Futhark.IR.KernelsMem
 import Futhark.Util.IntegralExp
-import Prelude hiding (quot, rem, div)
+import Prelude hiding (div, quot, rem)
 
 -- | Compile 'SegMap' instance code.
 compileSegStencil ::
@@ -21,23 +21,22 @@ compileSegStencil ::
   StencilOp KernelsMem ->
   KernelBody KernelsMem ->
   CallKernelGen ()
-compileSegStencil pat lvl space op kbody =
-  compileGlobalReadFlat pat lvl space op kbody
+compileSegStencil = compileGlobalReadFlat
 
 -- the provided one has a ton of common subexpressions so a new one was made
-unflattenIx
-  :: IntExp t
-  => String
-  -> [Imp.TExp t]
-  -> Imp.TExp t
-  -> ImpM lore r op [Imp.TExp t]
+unflattenIx ::
+  IntExp t =>
+  String ->
+  [Imp.TExp t] ->
+  Imp.TExp t ->
+  ImpM lore r op [Imp.TExp t]
 unflattenIx _ [] _ = pure []
-unflattenIx base (x:xs) i = do
+unflattenIx base (x : xs) i = do
   dimIx <- dPrimVE base $ i `quot` x
-  (dimIx:) <$> unflattenIx base xs (i - (dimIx * x))
+  (dimIx :) <$> unflattenIx base xs (i - (dimIx * x))
 
 chunksOf :: Int -> [a] -> [[a]]
-chunksOf _   [] = []
+chunksOf _ [] = []
 chunksOf len xs =
   let (left, right) = splitAt len xs
    in left : chunksOf len right
@@ -54,7 +53,7 @@ compileGlobalReadFlat pat lvl space op kbody = do
       dims = map toInt64Exp dims'
       num_groups' = toInt64Exp <$> segNumGroups lvl
       group_size_flat_c = toInt64Exp <$> segGroupSize lvl
-      group_size_flat = (unCount group_size_flat_c)
+      group_size_flat = unCount group_size_flat_c
       stencil_ixss = map (map fromInteger) $ stencilIndexes op
       invarElems = map kernelResultSubExp $ kernelBodyResult kbody
       lam = stencilOp op
@@ -65,7 +64,6 @@ compileGlobalReadFlat pat lvl space op kbody = do
 
   case lvl of
     SegThread {} -> do
-
       -- Host side evaluated variables
       max_idxs <- mapM (dPrimVE "max_idx" . (+ (-1))) dims
 
@@ -101,14 +99,13 @@ compileGlobalReadFlat pat lvl space op kbody = do
 
             -- load variants into lambda variant parameters
             forM_ (zip param_ixs params_ixs_ordered) $ \(parix, pars) -> do
-                forM_ (zip pars $ stencilArrays op) $ \(par, src) ->
-                  copyDWIMFix (paramName par) [] (Var src) $ bound_ixs parix
+              forM_ (zip pars $ stencilArrays op) $ \(par, src) ->
+                copyDWIMFix (paramName par) [] (Var src) $ bound_ixs parix
 
             -- compile lambda function and designate output style
             compileStms mempty (bodyStms lamBody) $
               zipWithM_ (compileThreadResult space) (patternElements pat) $
                 map (Returns ResultMaySimplify) $ bodyResult lamBody
-
     SegGroup {} ->
       error "not implemented"
 
@@ -120,15 +117,15 @@ compileBigTile ::
   KernelBody KernelsMem ->
   CallKernelGen ()
 compileBigTile pat lvl space op kbody = do
-  let
-      num_groups' = toInt64Exp <$> segNumGroups lvl
+  let num_groups' = toInt64Exp <$> segNumGroups lvl
       group_size_flat_c = toInt64Exp <$> segGroupSize lvl
-      group_size_flat = (unCount group_size_flat_c)
-      group_sizes_exp = -- assumes group_size_flat >= 64
+      group_size_flat = unCount group_size_flat_c
+      group_sizes_exp =
+        -- assumes group_size_flat >= 64
         case length dims of
           1 -> [group_size_flat]
           2 -> [group_size_flat `div` 32, 32]
-          3 -> [2, (group_size_flat `div` 64), 32]
+          3 -> [2, group_size_flat `div` 64, 32]
           _ -> error "not valid dimensions"
       (is, dims') = unzip $ unSegSpace space
       dims = map toInt64Exp dims'
@@ -141,9 +138,9 @@ compileBigTile pat lvl space op kbody = do
         splitAt (length invarElems) lamPar
       a_mins = map (fromInteger . (\x -> if x > 0 then error "invalid axis min" else x) . minimum) $ stencilIndexes op
       a_maxs = map (fromInteger . (\x -> if x < 0 then error "invalid axis max" else x) . maximum) $ stencilIndexes op
-      shared_sizes_exp = zipWith (+) group_sizes_exp $ zipWith (\x y -> x - y) a_maxs a_mins
+      shared_sizes_exp = zipWith (+) group_sizes_exp $ zipWith (-) a_maxs a_mins
       n_point_stencil = length $ head stencil_ixss
-      lamParTypes = map (elemType . paramType) $ map head $ chunksOf n_point_stencil variantParams
+      lamParTypes = map (elemType . paramType . head) $ chunksOf n_point_stencil variantParams
 
   case lvl of
     SegThread {} -> do
@@ -164,60 +161,58 @@ compileBigTile pat lvl space op kbody = do
             sExt32 $ product dims_round_up `divUp` unCount group_size_flat_c
       sKernelThread "segstencil" num_groups' group_size_flat_c (segFlat space) $
         virtualiseGroups (segVirt lvl) virt_num_groups $ \group_id_flat_exp -> do
-             group_id_flat <- dPrimVE "group_id_flat" $ sExt64 group_id_flat_exp
-             local_id_flat <- dPrimVE "local_id_flat" . sExt64 . kernelLocalThreadId . kernelConstants =<< askEnv
-             local_ids <- unflattenIx "local_id" (map sExt64 inner_group) local_id_flat
-             group_ids <- unflattenIx "group_id" (map sExt64 inner_grid) group_id_flat
+          group_id_flat <- dPrimVE "group_id_flat" $ sExt64 group_id_flat_exp
+          local_id_flat <- dPrimVE "local_id_flat" . sExt64 . kernelLocalThreadId . kernelConstants =<< askEnv
+          local_ids <- unflattenIx "local_id" (map sExt64 inner_group) local_id_flat
+          group_ids <- unflattenIx "group_id" (map sExt64 inner_grid) group_id_flat
 
-             tiles <- forM (lamParTypes) $ \ptype -> sAllocArray "tile" ptype (Shape [Var $ tvVar shared_size_flat_var]) (Space "local")
-             -- create max indexes for each axis, and the bound macro
-             let bound_idxs = zipWith sMin64 max_idxs . map (sMax64 0)
+          tiles <- forM lamParTypes $ \ptype -> sAllocArray "tile" ptype (Shape [Var $ tvVar shared_size_flat_var]) (Space "local")
+          -- create max indexes for each axis, and the bound macro
+          let bound_idxs = zipWith sMin64 max_idxs . map (sMax64 0)
 
-             -- create writeSet offSets
-             writeSet_offsets <-
-               mapM (dPrimVE "writeSet_offset") $ zipWith (*) group_ids group_sizes
-             -- create offsets for the readSet
-             readSet_offsets <- mapM (dPrimVE "readSet_offset") $ zipWith (+) writeSet_offsets a_mins
+          -- create writeSet offSets
+          writeSet_offsets <-
+            mapM (dPrimVE "writeSet_offset") $ zipWith (*) group_ids group_sizes
+          -- create offsets for the readSet
+          readSet_offsets <- mapM (dPrimVE "readSet_offset") $ zipWith (+) writeSet_offsets a_mins
 
-             sLoopNest (Shape $ map (Var . tvVar) readSet_iters) $ \ix_list -> do
-               tile_locals <- mapM (dPrimVE "tile_local") $ zipWith (+) local_ids $ zipWith (*) ix_list group_sizes
-               tile_read_gids <- mapM (dPrimVE "tile_read_gid") $ bound_idxs $ zipWith (+) readSet_offsets tile_locals
-               flat_local <- dPrimVE "tile_local_flat" $ flattenIndex shared_sizes tile_locals
-               sWhen (foldl1 (.&&.) (zipWith (.<.) tile_locals shared_sizes)) $ do
-                 forM_ (zip tiles $ stencilArrays op) $ \(tile, input_arr) -> do
-                     copyDWIMFix tile [flat_local] (Var input_arr) tile_read_gids
+          sLoopNest (Shape $ map (Var . tvVar) readSet_iters) $ \ix_list -> do
+            tile_locals <- mapM (dPrimVE "tile_local") $ zipWith (+) local_ids $ zipWith (*) ix_list group_sizes
+            tile_read_gids <- mapM (dPrimVE "tile_read_gid") $ bound_idxs $ zipWith (+) readSet_offsets tile_locals
+            flat_local <- dPrimVE "tile_local_flat" $ flattenIndex shared_sizes tile_locals
+            sWhen (foldl1 (.&&.) (zipWith (.<.) tile_locals shared_sizes)) $ do
+              forM_ (zip tiles $ stencilArrays op) $ \(tile, input_arr) -> do
+                copyDWIMFix tile [flat_local] (Var input_arr) tile_read_gids
 
-             -- group syncronize
-             sOp $ Imp.Barrier Imp.FenceLocal
+          -- group syncronize
+          sOp $ Imp.Barrier Imp.FenceLocal
 
-             let gids = zipWith (+) writeSet_offsets local_ids
-             zipWithM_ dPrimV_ is gids
-             -- check for out of bound on global id for each axis
-             sWhen (isActive $ unSegSpace space) $ do
-               -- compile invariant elements
-               compileStms mempty (kernelBodyStms kbody) $ pure ()
+          let gids = zipWith (+) writeSet_offsets local_ids
+          zipWithM_ dPrimV_ is gids
+          -- check for out of bound on global id for each axis
+          sWhen (isActive $ unSegSpace space) $ do
+            -- compile invariant elements
+            compileStms mempty (kernelBodyStms kbody) $ pure ()
 
-               -- declare and attach invariant elements to invariantParams
-               zipWithM_ dPrimV_ (map paramName invariantParams) . map TPrimExp
-                 =<< mapM toExp invarElems
+            -- declare and attach invariant elements to invariantParams
+            zipWithM_ dPrimV_ (map paramName invariantParams) . map TPrimExp
+              =<< mapM toExp invarElems
 
-               sh_offs <- mapM (dPrimVE "tile_offset") $ zipWith (-) local_ids a_mins
-               dLParams variantParams
+            sh_offs <- mapM (dPrimVE "tile_offset") $ zipWith (-) local_ids a_mins
+            dLParams variantParams
 
-               let param_ixs = transpose $ zipWith (mapM (+)) stencil_ixss sh_offs
-               let params_ixs_ordered = transpose $ chunksOf n_point_stencil variantParams
+            let param_ixs = transpose $ zipWith (mapM (+)) stencil_ixss sh_offs
+            let params_ixs_ordered = transpose $ chunksOf n_point_stencil variantParams
 
-               ---- load variants into lambda variant parameters
-               forM_ (zip param_ixs params_ixs_ordered) $ \(parix, pars) -> do
-                   tile_ix <- dPrimVE "tile_ix" $ flattenIndex shared_sizes parix
-                   forM_ (zip pars tiles) $ \(par, tile) ->
-                     copyDWIMFix (paramName par) [] (Var tile) [tile_ix]
+            ---- load variants into lambda variant parameters
+            forM_ (zip param_ixs params_ixs_ordered) $ \(parix, pars) -> do
+              tile_ix <- dPrimVE "tile_ix" $ flattenIndex shared_sizes parix
+              forM_ (zip pars tiles) $ \(par, tile) ->
+                copyDWIMFix (paramName par) [] (Var tile) [tile_ix]
 
-               -- compile lambda function and designate output style
-               compileStms mempty (bodyStms lamBody) $
-                 zipWithM_ (compileThreadResult space) (patternElements pat) $
-                   map (Returns ResultMaySimplify) $ bodyResult lamBody
-
+            -- compile lambda function and designate output style
+            compileStms mempty (bodyStms lamBody) $
+              zipWithM_ (compileThreadResult space) (patternElements pat) $
+                map (Returns ResultMaySimplify) $ bodyResult lamBody
     SegGroup {} ->
       error "not implemented"
-
