782622d04ab3281150f2612f1181de0858d91827
Philip Lassen <philiplassen+git@gmail.com>
2021-03-26 05:00:07 +0100

    Add read binary scalar

--- rts/javascript/values.js
+++ rts/javascript/values.js
@@ -2,7 +2,8 @@
 //
 //
 
-type_strs = { "Int8Array" : '  i8',
+
+var type_strs = { "Int8Array" : '  i8',
               "Int16Array" : ' i16',
               "Int32Array" : ' i32',
               "BigInt64Array" : ' i64',
@@ -13,7 +14,78 @@ type_strs = { "Int8Array" : '  i8',
               "Float32Array" : ' f32',
               "Float64Array" : ' f64',
               // TODO implement bool here
-             }
+             };
+var typToType = { '  i8' : Int8Array ,
+              ' i16' : Int16Array ,
+              ' i32' : Int32Array ,
+              ' i64' : BigInt64Array ,
+              '  u8' : BigUint8Array ,
+              ' u16' :  BigUint16Array ,
+              ' u32' :  BigUint32Array ,
+              ' u64' :  BigUint64Array ,
+              ' f32' : Float32Array ,
+              ' f64' : Float64Array ,
+              // TODO implement bool here
+             };
+
+function binToStringToArray(str, array) {
+  for (var i = 0; i < array.length; i++) {
+    array[i] = str.charCodeAt(i);
+  }
+  return array;
+}
+
+
+
+function read_binary(str) {
+  var str = str.trim();
+  if (str.charCodeAt(0) != 'b') {
+    throw "Not in binary format"
+  }
+  str = str.substr(1);
+  str = str.trim();
+  var version = str.charCodeAt(0);
+  if (version != '2') {
+    throw "Not version 2";
+  }
+  str = str.trim();
+  var num_dim = parseInt(str.charCodeAt(0));
+  str=str.substring(1);
+  str = str.trim();
+  var typ = str.substr(0, 4);
+  str = str.substr(4);
+  str.trim();
+  if (num_dim == 0) {
+    return read_bin_scalar(str, typ);
+  } else {
+    return read_bin_array(str, num_dim, typ);
+  }
+}
+
+typToSize = {
+  "bool" = 1;
+  "  u8" = 1;
+  "  i8" = 1;
+  " u16" = 2;
+  " i16" = 2;
+  " u32" = 4;
+  " i32" = 4;
+  " f32" = 4;
+  " u64" = 8;
+  " i64" = 8;
+  " f64" = 8;
+}
+
+
+function read_bin_scalar(str, typ) {
+  var size = typToSize[typ];
+  var u8_array = new Uint8Array(size);
+  binToStringArray(str, array);
+  array = new (typToType.get(typ))(u8_array.buffer);
+}
+
+  
+
 
 function construct_binary_value(v) {
 
