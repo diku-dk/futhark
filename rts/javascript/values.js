071ebdb91967069306d0d1031c35fdeff942f264
Philip Lassen <philiplassen+git@gmail.com>
2021-03-29 04:27:06 +0200

    working implementation for most of scalars

--- rts/javascript/values.js
+++ rts/javascript/values.js
@@ -19,69 +19,83 @@ var typToType = { '  i8' : Int8Array ,
               ' i16' : Int16Array ,
               ' i32' : Int32Array ,
               ' i64' : BigInt64Array ,
-              '  u8' : BigUint8Array ,
-              ' u16' :  BigUint16Array ,
-              ' u32' :  BigUint32Array ,
+              '  u8' : Uint8Array ,
+              ' u16' :  Uint16Array ,
+              ' u32' :  Uint32Array ,
               ' u64' :  BigUint64Array ,
               ' f32' : Float32Array ,
               ' f64' : Float64Array ,
               // TODO implement bool here
              };
 
-function binToStringToArray(str, array) {
+function binToStringArray(buff, array) {
   for (var i = 0; i < array.length; i++) {
-    array[i] = str.charCodeAt(i);
+    array[i] = buff[i];
   }
-  return array;
 }
 
+function fileToBuff(fname) {
+  var readline = require('readline');
+  var fs = require('fs');
+  // TODO set decoding flag so there is no wack byte reading
+  var buff =  fs.readFileSync(fname);
+  return buff;
+}
 
+function fileToValue(fname) {
+  var str = fileToBuff(fname);
+  return read_binary(str);
+}
 
-function read_binary(str) {
-  var str = str.trim();
-  if (str.charCodeAt(0) != 'b') {
+function read_binary(buff) {
+  // TODO Trim white space at the beggining
+  //var buff = buff.trimStart();
+  while (buff.slice(0, 1).toString().trim() == "") {
+    buff = buff.slice(1);
+  }
+  if (buff[0] != 'b'.charCodeAt(0)) {
     throw "Not in binary format"
   }
-  str = str.substr(1);
-  str = str.trim();
-  var version = str.charCodeAt(0);
-  if (version != '2') {
+  var version = buff[1];
+  if (version != 2) {
     throw "Not version 2";
   }
-  str = str.trim();
-  var num_dim = parseInt(str.charCodeAt(0));
-  str=str.substring(1);
-  str = str.trim();
-  var typ = str.substr(0, 4);
-  str = str.substr(4);
-  str.trim();
+  var num_dim = buff[2];
+  var typ = buff.slice(3, 7);
+  buff = buff.slice(7);
   if (num_dim == 0) {
-    return read_bin_scalar(str, typ);
+    return read_bin_scalar(buff, typ);
   } else {
-    return read_bin_array(str, num_dim, typ);
+    return read_bin_array(buff, num_dim, typ);
   }
 }
 
-typToSize = {
-  "bool" = 1;
-  "  u8" = 1;
-  "  i8" = 1;
-  " u16" = 2;
-  " i16" = 2;
-  " u32" = 4;
-  " i32" = 4;
-  " f32" = 4;
-  " u64" = 8;
-  " i64" = 8;
-  " f64" = 8;
+function read_bin_array(buff, num_dim, typ) {
+  console.log("not implemented yet");
+  return undefined;
+}
+
+var typToSize = {
+  "bool" : 1,
+  "  u8" : 1,
+  "  i8" : 1,
+  " u16" : 2,
+  " i16" : 2,
+  " u32" : 4,
+  " i32" : 4,
+  " f32" : 4,
+  " u64" : 8,
+  " i64" : 8,
+  " f64" : 8,
 }
 
 
-function read_bin_scalar(str, typ) {
+function read_bin_scalar(buff, typ) {
   var size = typToSize[typ];
   var u8_array = new Uint8Array(size);
-  binToStringArray(str, array);
-  array = new (typToType.get(typ))(u8_array.buffer);
+  binToStringArray(buff, u8_array);
+  array = new (typToType[typ])(u8_array.buffer);
+  return array[0];
 }
 
   
