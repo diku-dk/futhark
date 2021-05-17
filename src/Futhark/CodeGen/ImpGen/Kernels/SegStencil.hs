736c1241d7d46124ef80d5ab0ac111f4c41544a6
Jonathan Wraa <levestol@hotmail.com>
2021-05-17 18:42:31 +0200

    add comments

--- src/Futhark/CodeGen/ImpGen/Kernels/SegStencil.hs
+++ src/Futhark/CodeGen/ImpGen/Kernels/SegStencil.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeFamilies #-}
 
--- | Code generation for 'SegStencil' is quite straightforward.
+-- | Code generation for 'SegStencil'.
 module Futhark.CodeGen.ImpGen.Kernels.SegStencil (compileSegStencil) where
 
 import Control.Monad.Except
@@ -13,6 +13,7 @@ import Futhark.CodeGen.ImpGen.Kernels.Base
 import Futhark.IR.KernelsMem
 import Futhark.Util.IntegralExp
 import Prelude hiding (div, quot, rem)
+import qualified Prelude (div)
 
 -- | Compile 'SegMap' instance code.
 compileSegStencil ::
@@ -28,12 +29,12 @@ compileSegStencil pat lvl space op kbody =
     SegThread {} -> do
       emit $ Imp.DebugPrint "\n# SegStencil" Nothing
 
-      -- constexpr and constants
       let dims = map (TPrimExp . toExp' int32 . snd) $ unSegSpace space
           stencil_indexes = stencilIndexes op
           dimentionality = length stencil_indexes
           group_size_flat_subexp = unCount $ segGroupSize lvl
           group_size_flat_exp = TPrimExp . toExp' int32 $ group_size_flat_subexp
+          -- stuff to reshape the group-size depending on the macro variable 'groupsize'
           flNumT2 x = 1 + (sMin32 1 (group_size_flat_exp `quot` x))
           fl256t2 = flNumT2 256
           fl512t2 = flNumT2 512
@@ -42,8 +43,8 @@ compileSegStencil pat lvl space op kbody =
           group_sizes_exp =
               case dimentionality of
                   1 -> [group_size_flat_exp]
-                  2 -> [group_size_flat_exp `quot` 32, 32]
-                  3 -> [2*fl512t2,2*fl256t2*fl1024t2,32]
+                  2 -> [8 * fl256t2 * fl512t2 * fl1024t2, 32]
+                  3 -> [2*fl1024t2, 2*fl256t2*fl512t2, 32]
                   _ -> error "not valid dimensions"
           n_point_stencil = length $ head stencil_indexes
           n_invarElems = length $ map kernelResultSubExp $ kernelBodyResult kbody
@@ -58,88 +59,92 @@ compileSegStencil pat lvl space op kbody =
           halo_widths = map (\x-> x-1) $ zipWith (-) a_maxs_i a_mins_i
           halo_widths_exp = map fromInteger halo_widths
           -- Amount of Bytes required for a single index in tile(s).
+          memory_per_elem :: Integer
           memory_per_elem = sum $ map primByteSize lambdaInvarTypes
-
+          rescale_on_byte_size :: Integer -> Integer
+          rescale_on_byte_size mult =
+            let largest_type_size = maximum $ map primByteSize lambdaInvarTypes
+            in max (1 :: Integer) ((mult * 4) `Prelude.div` largest_type_size)
           strip_multiples :: [Integer]
           strip_multiples =
             case dimentionality of
-              1 -> [8]
+              1 -> [4]
               2 -> [2, 2]
-              3 -> [2, 1, 2]
+              3 -> [4, 2, 1]
               _ -> error "not valid dimensions"
           strip_multiples_exp :: [Imp.TExp Int32]
           strip_multiples_exp = map fromInteger strip_multiples
-          uses = map (length . nub) stencil_indexes
-          uses_exp :: [Imp.TExp Int32]
-          uses_exp = map fromIntegral uses
-          printf :: String -> Imp.TExp t -> ImpM lore r op ()
-          printf text = emit . Imp.DebugPrint text . Just . untyped
-          computeReuses works hws = do
-            let tileReads = zipWith (*) uses_exp works
-                weights = 1 : repeat 3
-            tileReadsWeighed <- mapM (dPrimVE "tileReadsWeighed") $ reverse $ zipWith (*) weights $ reverse tileReads
-            let tileWrites = zipWith (+) hws works
-            weighedReuseFactors <- mapM (dPrimVE "weighedReuseFactors") $ zipWith div tileReadsWeighed tileWrites
-            pure (     foldl1 (.||.) (map (5 .<=.) weighedReuseFactors)
-                  .&&. foldl1 (.&&.) (map (1 .<=.) weighedReuseFactors)
-                 )
-
+          debugPrintf :: String -> Imp.TExp t -> ImpM lore r op ()
+          debugPrintf text = emit . Imp.DebugPrint text . Just . untyped
+          computeFlatReuse works hws =
+            let unique_ixs = fromIntegral . length . nub . transpose $ stencil_indexes
+                flat_tile_reads = product works * unique_ixs
+                flat_tile_writes = product $ zipWith (+) works hws
+            in dPrimVE "flat_reuse_int" $ flat_tile_reads `quot` flat_tile_writes
       max_shared_bytes <- do
         name <- dPrim "max_shared_bytes" int32
         sOp $ Imp.GetSizeMax (tvVar name) Imp.SizeLocalMemory
         pure $ tvExp name
-      aboveMinBlock <- dPrimVE "aboveMinBlock" ((128 .<=. group_size_flat_exp))
-      printf "aboveMinBlock" aboveMinBlock
+      isAboveMinBlock <- dPrimVE "isAboveMinBlock" ((128 .<=. group_size_flat_exp))
+      debugPrintf "isAboveMinBlock" isAboveMinBlock
       can_and_should_run_stripTile <- do
         host_strip_sizes <- mapM (dPrimVE "host_strip_sizes") $ zipWith (*) group_sizes_exp strip_multiples_exp
         stripTileElems <- dPrimVE "stripTileElems" $ product $ zipWith (+) halo_widths_exp host_strip_sizes
         stripTileBytes <- dPrimVE "stripTileBytes" $ stripTileElems * fromInteger memory_per_elem
-        printf "shared_size_max" max_shared_bytes
-        forM_ group_sizes_exp $ printf "group_sizes"
-        printf "stripTileBytes" stripTileBytes
+        -- There should also be a check for whether the amount of shared
+        --   memory required per thread-block would harm the occupancy too
+        --   much for the stripmineBigTile design to be meaningfully run.
+        --   However as of writing this comment, there is no support for this
+        --   in the compiler, so it was simply (but incorrectly) assumed that
+        --   this was of no concern.
         canRunStripTile <- dPrimVE "canRunStripTile" $ stripTileBytes .<=. max_shared_bytes
-        reuseIsHighEnough <- dPrimVE "reuseIsHighEnoughST" =<< computeReuses host_strip_sizes halo_widths_exp
+        reuseIsHighEnough <- (dPrimVE "reuseIsHighEnoughST" . (2 .<=.)) =<< computeFlatReuse host_strip_sizes halo_widths_exp
         isNotSkewed <- dPrimVE "isNotSkewedST" $ foldl1 (.&&.) $ zipWith (.<=.) host_strip_sizes dims
-        printf "can_run_strips" canRunStripTile
-        printf "reuseIsHighEnough_strips" reuseIsHighEnough
-        printf "isNotSkewed_strips" isNotSkewed
-        dPrimVE "can_and_should_run_stripTile" $ canRunStripTile .&&. reuseIsHighEnough .&&. isNotSkewed .&&. aboveMinBlock
-
-      printf "group_size_flat" $ product group_sizes_exp
-      printf "can_and_should_run_stripTile" can_and_should_run_stripTile
-      emit $ Imp.DebugPrint "\n" Nothing
+        forM_ group_sizes_exp $ debugPrintf "group_sizes"
+        debugPrintf "shared_size_max" max_shared_bytes
+        debugPrintf "stripTileBytes" stripTileBytes
+        debugPrintf "can_run_strips" canRunStripTile
+        debugPrintf "reuseIsHighEnough_strips" reuseIsHighEnough
+        debugPrintf "isNotSkewed_strips" isNotSkewed
+        dPrimVE "can_and_should_run_stripTile" $ canRunStripTile .&&. reuseIsHighEnough .&&. isNotSkewed .&&. isAboveMinBlock
+      debugPrintf "can_and_should_run_stripTile" can_and_should_run_stripTile
       sIf can_and_should_run_stripTile
         (compileBigTileStripMinedSingleDim pat lvl space op kbody group_sizes_exp strip_multiples)
         (compileGlobalReadFlat pat lvl space op kbody)
 
+-- Creates the size spans given a list of sizes.
 createSpans :: Num a => [a] -> [a]
 createSpans = scanr1 (*) . tail
 
--- the provided one has a ton of common subexpressions so a new one was made
--- !!!! It does however need the span of the inner dimensions, so use
--- createSpans
+-- 'unflattenIndex' has a ton of common subexpressions,
+-- and manully calculate 'rem' so a new one was made.
+-- Use as:
+--   indexes <- unflattenIx name_base (createSpans sizes) flat_index
 unflattenIx ::
   IntExp t =>
   String ->
   [Imp.TExp t] ->
   Imp.TExp t ->
   ImpM lore r op [TV t]
-unflattenIx base [] i = (: []) <$> dPrimV base i
-unflattenIx base (x : xs) i = do
-  dimIx <- dPrimV base $ i `quot` x
+unflattenIx name [] i = (: []) <$> dPrimV name i
+unflattenIx name (x : xs) i = do
+  dimIx <- dPrimV name $ i `quot` x
   rem_val <- dPrimV "rem_val" $ i `rem` x
-  (dimIx :) <$> unflattenIx base xs (tvExp rem_val)
+  (dimIx :) <$> unflattenIx name xs (tvExp rem_val)
 
--- works better for 3d+ than the provided by using that multiplication is distributive over addition
-flattenIx :: Num b => [b] -> [b] -> b
-flattenIx dims ixs = foldl (\acc (d,n) -> acc*d + n) (head ixs) $ zip (tail dims) (tail ixs)
-
+-- Splits the lists 'xs' into chunks of 'len' size.
 chunksOf :: Int -> [a] -> [[a]]
 chunksOf _ [] = []
 chunksOf len xs =
   let (left, right) = splitAt len xs
    in left : chunksOf len right
 
+-- A rewrite of the pattern:
+--  iterations <- dPrimVE "iters" $ divUp (product sizes) added_flat
+--  sFor "i" iterations $ \i ->
+--    flat_ix <- dPrimVE "flat_ix" $ (i * added_flat) + start_flat
+--    ixs <- unflattenIndex "ixs" sizes flat_ix
+--    () <- body (ixs, flat_ix)
 sForUnflatten ::
   IntExp t =>
   [Imp.TExp t] ->
@@ -147,7 +152,7 @@ sForUnflatten ::
   Imp.TExp t ->
   (([Imp.TExp t], Imp.TExp t, Imp.TExp Bool)-> ImpM lore r op ()) ->
   ImpM lore r op ()
-sForUnflatten sizes start_flat added_flat m = do
+sForUnflatten sizes start_flat added_flat body = do
   size_span <- mapM (dPrimVE "size_span") $ createSpans sizes
   iterations <- dPrimVE "iterations" $ divUp (product sizes) added_flat
   starts <- unflattenIx "start" size_span start_flat
@@ -168,9 +173,12 @@ sForUnflatten sizes start_flat added_flat m = do
         zipWithM_ (\b ng -> b <-- (tvExp b + tvExp ng)) starts adds
         forM_ ls add_carry
   sFor "i" iterations $ \i -> do
-    () <- m (map tvExp starts, tvExp start_flat_var, i .==. (iterations-1))
+    () <- body (map tvExp starts, tvExp start_flat_var, i .==. (iterations-1))
     prepareNextIter
 
+-- Kernel for evaluating a single stencil.
+-- This is equivalent to just making a nest of maps and finding the neighbours from there.
+-- This kernel is primarily used as a fallback in case the others can't meaningfully be run
 compileGlobalReadFlat ::
   Pattern KernelsMem ->
   SegLevel ->
@@ -239,6 +247,19 @@ compileGlobalReadFlat pat lvl space op kbody = do
         zipWithM_ (compileThreadResult space) (patternElements pat) $
           map (Returns ResultMaySimplify) $ bodyResult lamBody
 
+-- Kernel for evaluating a single stencil.
+-- This kernel uses shared memory to store read elements,
+--   and does multiple writes per thread.
+-- The write-set of a thread-block is the set of indexes of the output array that are written to.
+-- The read-set of a thread-block is the set of indexes of the input array that it reads.
+--
+-- The approach is to find the the smallest rectangular shape containing all
+--   the required elements to do all writes of the write-set.
+--   This is then the read-set.
+-- There are 2 phases.
+--   In phase 1 it loads the entire read-set into the tile(s).
+--   In phase 2 it iterates through the write-set and evaluated the lambda
+--     function and performs writes.
 compileBigTileStripMinedSingleDim ::
   Pattern KernelsMem ->
   SegLevel ->
@@ -267,11 +288,8 @@ compileBigTileStripMinedSingleDim pat _ space op kbody group_sizes_exp strip_mul
       a_maxs = map fromInteger a_maxs_i
       n_point_stencil = length $ head stencil_ixss
       lamParTypes = map (elemType . paramType . head) $ chunksOf n_point_stencil variantParams
-
       strip_multiples_exp :: [Imp.TExp Int32]
       strip_multiples_exp = map fromInteger strip_multiples
-
-      -- constexpr variables
       strip_sizes_exp = zipWith (*) strip_multiples_exp group_sizes_exp
       shared_sizes_exp = zipWith (+) strip_sizes_exp $ zipWith (-) a_maxs a_mins
       shared_size_flat_exp = product shared_sizes_exp
@@ -290,43 +308,50 @@ compileBigTileStripMinedSingleDim pat _ space op kbody group_sizes_exp strip_mul
     tiles <- forM lamParTypes $ \ptype ->
       sAllocArray "tile" ptype (Shape [Var $ tvVar tile_length]) (Space "local")
     constants <- kernelConstants <$> askEnv
-    -- constexpr variables
+    -- compile-time constant variables
     group_sizes <- mapM (dPrimVE "group_sizes") group_sizes_exp
     group_spans <- mapM (dPrimVE "group_spans") $ createSpans group_sizes
     group_size_flat <- dPrimVE "group_size_flat" $ product group_sizes
     strip_sizes <- mapM (dPrimVE "strip_sizes") $ map sExt64 strip_sizes_exp
     shared_sizes <- mapM (dPrimVE "shared_sizes") shared_sizes_exp
     shared_size_flat <- dPrimVE "shared_size_flat" $ product shared_sizes
-    -- runtime variables
+    -- threadId/groupId variables
     local_id_flat <- dPrimVE "local_id_flat" . kernelLocalThreadId $ constants
     local_ids <- map tvExp <$> unflattenIx "local_id" group_spans local_id_flat
     strip_id_flat <- dPrimVE "strip_id_flat" . kernelGroupId $ constants
     strip_ids <- map tvExp <$> unflattenIx "strip_id" strip_grid_spans strip_id_flat
 
+    -- Phase 1: load the read-set into the tile(s).
     let bound_idxs = zipWith sMin64 max_ixs . map (sMax64 0)
     writeSet_offsets <- mapM (dPrimVE "writeSet_offset") $
       zipWith (*) (map sExt64 strip_ids) (map sExt64 strip_sizes)
-    -- run the data loader
+    -- iterate through the read-set and do reads.
     sForUnflatten shared_sizes local_id_flat group_size_flat $ \(loader_ids, loader_ids_flat, isLastIter) -> do
+      -- bounded indexes used for loading elements into the tile.
       loader_gids <- mapM (dPrimVE "loader_gid") $
          bound_idxs $ zipWith (+) writeSet_offsets $ map sExt64 $ zipWith (+) loader_ids a_mins
+      -- if inside the bounds of the tile, perform read from input / write to tile.
       sWhen ((bNot isLastIter) .||. loader_ids_flat .<. shared_size_flat) $
         forM_ (zip tiles (stencilArrays op)) $ \(tile, input_arr) ->
           copyDWIMFix tile [sExt64 loader_ids_flat] (Var input_arr) loader_gids
 
+    -- end of phase 1. Wait for all elements to be loaded.
     sOp $ Imp.Barrier Imp.FenceLocal
-    -- loader finished
 
+    -- Phase 2: Iterate through the write-set and evaluate the lambda function and do the writes.
     let nest_shape = Shape $ map (Constant . IntValue . intValue Int64) strip_multiples
     sLoopNest nest_shape $ \local_strip_ids -> do
+      -- create the centre index used to read from the tile(s).
       tile_local_ids <- mapM (dPrimVE "tile_local_id")
                       $ zipWith (+) local_ids
                       $ zipWith (*) (map sExt32 local_strip_ids) group_sizes
-      tile_local_id_flat <- dPrimVE "tile_local_id_flat" $ flattenIx shared_sizes tile_local_ids
+      tile_local_id_flat <- dPrimVE "tile_local_id_flat" $ flattenIndex shared_sizes tile_local_ids
+      -- assign the relevant values to the global-ids.
       let gids = zipWith (+) writeSet_offsets (map sExt64 tile_local_ids)
       zipWithM_ dPrimV_ is gids
 
-      let tile_offsets = map (flattenIx shared_sizes) $ transpose $ zipWith (mapM (-)) stencil_ixss a_mins
+      -- create the individual indexes used to load from the tile(s).
+      let tile_offsets = map (flattenIndex shared_sizes) $ transpose $ zipWith (mapM (-)) stencil_ixss a_mins
           tile_ixs = mapM (+) tile_offsets tile_local_id_flat
           variant_params_tuples = transpose $ chunksOf n_point_stencil variantParams
 
@@ -338,9 +363,8 @@ compileBigTileStripMinedSingleDim pat _ space op kbody group_sizes_exp strip_mul
         zipWithM_ dPrimV_ (map paramName invariantParams) . map TPrimExp
           =<< mapM toExp invarElems
 
+        -- declare the lambda function variant paramets and set them.
         dLParams variantParams
-
-        ---- load variants into lambda variant parameters
         forM_ (zip tile_ixs variant_params_tuples) $ \(tile_ix, pars) ->
           forM_ (zip pars tiles) $ \(par, tile) ->
             copyDWIMFix (paramName par) [] (Var tile) [sExt64 tile_ix]
