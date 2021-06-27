d76eb59b42a2c7e3e7a92d7c30d8c04111a6e5b6
Philip Lassen <philiplassen+git@gmail.com>
2021-06-27 09:22:02 -0700

    Free error string

--- src/Futhark/CodeGen/Backends/GenericWASM.hs
+++ src/Futhark/CodeGen/Backends/GenericWASM.hs
@@ -201,7 +201,9 @@ getErrorFun =
   get_error() {
     var ptr = _futhark_context_get_error(this.ctx);
     var len = HEAP8.subarray(ptr).indexOf(0);
-    return String.fromCharCode(...HEAP8.subarray(ptr, ptr + len));
+    var str = String.fromCharCode(...HEAP8.subarray(ptr, ptr + len));
+    _free(ptr);
+    return str;
   }
   |]
 
