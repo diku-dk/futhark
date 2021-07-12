a7a6f0be796c0298ca77c9d3dd8fbeed4116548e
Troels Henriksen <athas@sigkill.dk>
2021-07-12 18:15:54 +0200

    Weed some dead code.

--- src/Language/Futhark/Syntax.hs
+++ src/Language/Futhark/Syntax.hs
@@ -24,7 +24,6 @@ module Language.Futhark.Syntax
     ShapeDecl (..),
     shapeRank,
     stripDims,
-    unifyShapes,
     TypeName (..),
     typeNameFromQualName,
     qualNameFromTypeName,
@@ -315,13 +314,6 @@ stripDims :: Int -> ShapeDecl dim -> Maybe (ShapeDecl dim)
 stripDims i (ShapeDecl l)
   | i < length l = Just $ ShapeDecl $ drop i l
   | otherwise = Nothing
-
--- | @unifyShapes x y@ combines @x@ and @y@ to contain their maximum
--- common information, and fails if they conflict.
-unifyShapes :: ArrayDim dim => ShapeDecl dim -> ShapeDecl dim -> Maybe (ShapeDecl dim)
-unifyShapes (ShapeDecl xs) (ShapeDecl ys) = do
-  guard $ length xs == length ys
-  ShapeDecl <$> zipWithM unifyDims xs ys
 
 -- | A type name consists of qualifiers (for error messages) and a
 -- 'VName' (for equality checking).
