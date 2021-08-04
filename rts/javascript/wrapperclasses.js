e0b4d05d1cf5b1ba1d68a05a267d468312cd316f
Philip Lassen <philiplassen+git@gmail.com>
2021-08-04 14:22:57 -0700

    Wasm free check (#1427)
    * Fix line spacing
    
    * Throw errors when freed memory is used in JS classes.

--- rts/javascript/wrapperclasses.js
+++ rts/javascript/wrapperclasses.js
@@ -10,20 +10,41 @@ class FutharkArray {
     this.fshape = fshape;
     this.fvalues = fvalues;
     this.ffree = ffree;
+    this.valid = true;
   }
-  futharkType() { return this.type_name; }
-  free() { this.ffree(this.ctx.ctx, this.ptr); }
+
+  validCheck() {
+    if (!this.valid) {
+      throw "Using freed memory"
+    }
+  }
+
+  futharkType() {
+    return this.type_name;
+  }
+
+  free() {
+    this.validCheck();
+    this.ffree(this.ctx.ctx, this.ptr);
+    this.valid = false;
+  }
+
   shape() {
+    this.validCheck();
     var s = this.fshape(this.ctx.ctx, this.ptr) >> 3;
     return Array.from(this.ctx.wasm.HEAP64.subarray(s, s + this.dim));
   }
+
   toTypedArray(dims = this.shape()) {
+    this.validCheck();
     console.assert(dims.length === this.dim, "dim=%s,dims=%s", this.dim, dims.toString());
     var length = Number(dims.reduce((a, b) => a * b));
     var v = this.fvalues(this.ctx.ctx, this.ptr) / this.heap.BYTES_PER_ELEMENT;
     return this.heap.subarray(v, v + length);
   }
+
   toArray() {
+    this.validCheck();
     var dims = this.shape();
     var ta = this.toTypedArray(dims);
     return (function nest(offs, ds) {
@@ -39,8 +60,24 @@ class FutharkArray {
 }
 
 class FutharkOpaque {
-  constructor(ctx, ptr, ffree) { this.ctx = ctx; this.ptr = ptr; this.ffree = ffree; }
-  free() { this.ffree(this.ctx.ctx, this.ptr); }
+  constructor(ctx, ptr, ffree) {
+    this.ctx = ctx;
+    this.ptr = ptr;
+    this.ffree = ffree;
+    this.valid = true;
+  }
+
+  validCheck() {
+    if (!this.valid) {
+      throw "Using freed memory"
+    }
+  }
+
+  free() {
+    this.validCheck();
+    this.ffree(this.ctx.ctx, this.ptr);
+    this.valid = false;
+  }
 }
 
 // End of wrapperclasses.js
