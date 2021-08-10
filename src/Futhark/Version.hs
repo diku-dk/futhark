88a456bfa75ac4637830600823d8027e82c6bdac
Troels Henriksen <athas@sigkill.dk>
2021-08-10 17:45:37 +0200

    More reliable Git version embedding.
    Fixes #1438.

--- src/Futhark/Version.hs
+++ src/Futhark/Version.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE Trustworthy #-}
 
 -- | This module exports version information about the Futhark
 -- compiler.
@@ -10,35 +9,38 @@ module Futhark.Version
 where
 
 import Data.Version
-import Development.GitRev
+import GitHash
 import qualified Paths_futhark
+
+{-# NOINLINE version #-}
 
 -- | The version of Futhark that we are using.  This is equivalent to
 -- the version defined in the .cabal file.
 version :: Version
 version = Paths_futhark.version
 
+{-# NOINLINE versionString #-}
+
 -- | The version of Futhark that we are using, as a 'String'
 versionString :: String
 versionString =
   showVersion version
-    ++ if used_hash /= "UNKNOWN"
-      then "\n" ++ gitversion
-      else ""
+    ++ gitversion $$tGitInfoCwdTry
   where
-    used_hash = take 7 $(gitHash)
-
-    gitversion =
+    gitversion (Left _) = ""
+    gitversion (Right gi) =
       concat
-        [ "git: ",
+        [ "\n",
+          "git: ",
           branch,
-          used_hash,
+          take 7 $ giHash gi,
           " (",
-          $(gitCommitDate),
+          giCommitDate gi,
           ")",
           dirty
         ]
-    branch
-      | $(gitBranch) == "master" = ""
-      | otherwise = $(gitBranch) ++ " @ "
-    dirty = if $(gitDirtyTracked) then " [modified]" else ""
+      where
+        branch
+          | giBranch gi == "master" = ""
+          | otherwise = giBranch gi ++ " @ "
+        dirty = if giDirty gi then " [modified]" else ""
