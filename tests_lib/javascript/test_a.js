895cbd662550bd3dc1193d902f69922134738384
Philip Lassen <philiplassen+git@gmail.com>
2021-08-11 03:26:08 -0700

    Add comment explaining substring hack.

--- tests_lib/javascript/test_a.js
+++ tests_lib/javascript/test_a.js
@@ -5,8 +5,9 @@ import assert from 'assert/strict';
 // Hack for Running generated ES6 modules from Emscripten with Node
 // https://github.com/emscripten-core/emscripten/issues/11792#issuecomment-877120580
 import {dirname} from "path";
-globalThis.__dirname = dirname(import.meta.url).substring(7);
 import { createRequire } from 'module';
+// substring removes file:// from the filepath
+globalThis.__dirname = dirname(import.meta.url).substring(7);
 globalThis.require = createRequire(import.meta.url);
 
 // Imports from the generated ES6 Module
