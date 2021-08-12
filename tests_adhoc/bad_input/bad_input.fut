4d6e264fb022bd966aa0f501bd9557fbbb02d22c
Troels Henriksen <athas@sigkill.dk>
2021-08-12 21:45:31 +0200

    Better error message when test input has wrong type.
    This only handles the (very common) case where the value is provided
    explicitly.  In particular, data files are still passed directly to
    the server executable.  This is because, currently, extracting the
    type of the value contained within requires us to read the entire
    file, which is expensive for very large input.
    
    Closes #1441.

---
+++ tests_adhoc/bad_input/bad_input.fut
@@ -0,0 +1,4 @@
+-- ==
+-- input { 1i32 }
+
+let main (x: i64) = x
---
+++ tests_adhoc/bad_input/test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+! futhark test bad_input.fut > stderr
+if !(fgrep -q 'Expected input of types: [i64]' stderr) ||
+        !(fgrep -q 'Provided input of types: [i32]' stderr); then
+    cat stderr
+    exit 1
+fi
