65ea23a16a1815f9db418454a52df00513f023b8
Philip Lassen <philiplassen+git@gmail.com>
2021-08-14 16:12:26 -0700

    Delete unnecessary 128 bit cast Emscripten work around.

--- rts/c/scalar.h
+++ rts/c/scalar.h
@@ -823,28 +823,12 @@ static uint32_t futrts_mul_hi32(uint32_t a, uint32_t b) {
   return aa * bb >> 32;
 }
 
-#ifdef __EMSCRIPTEN__
-static uint64_t futrts_mul_hi64(uint64_t x, uint64_t y) {
-  uint64_t a = x >> 32, b = x & 0xffffffff;
-  uint64_t c = y >> 32, d = y & 0xffffffff;
-  uint64_t ac = a * c;
-  uint64_t bc = b * c;
-  uint64_t ad = a * d;
-  uint64_t bd = b * d;
-  uint64_t mid34 = (bd >> 32) + (bc & 0xffffffff) + (ad & 0xffffffff);
-  uint64_t upper64 = ac + (bc >> 32) + (ad >> 32) + (mid34 >> 32);
-
-  return upper64;
-}
-#else // Not Emscripten
 static uint64_t futrts_mul_hi64(uint64_t a, uint64_t b) {
   __uint128_t aa = a;
   __uint128_t bb = b;
 
   return aa * bb >> 64;
 }
-#endif
-
 #endif
 
 #if defined(__OPENCL_VERSION__)
