3ef7fa3fd005987b7bc76ce7cc7158a55f351dac
Philip Munksgaard <philip@munksgaard.me>
2021-08-18 17:01:23 +0200

    This doesn't need to be a monad

--- src/Futhark/Optimise/MemBlockCoalesce.hs
+++ src/Futhark/Optimise/MemBlockCoalesce.hs
@@ -5,14 +5,11 @@
 -- | Playground for work on merging memory blocks
 module Futhark.Optimise.MemBlockCoalesce (memoryBlockMerging, pass) where
 
-import Control.Monad.Reader
-import Control.Monad.State.Strict
 import Data.Bifunctor (first)
+import Data.Function ((&))
 import qualified Data.Map as M
 import Data.Maybe (fromMaybe)
 import qualified Futhark.Analysis.Alias as AnlAls
-import Futhark.Builder.Class
-import Futhark.Construct
 import Futhark.IR.Aliases
 import Futhark.IR.Mem
 import Futhark.IR.Mem.IxFun (substituteInIxFun)
@@ -46,18 +43,16 @@ pass =
      in Pass.intraproceduralTransformation (onStms coaltab) prog
   where
     onStms :: M.Map VName Coalesced -> Scope SeqMem -> Stms SeqMem -> PassM (Stms SeqMem)
-    onStms coaltab scope stms = do
-      let m = localScope scope $ replaceDecorations coaltab stms
-      fmap fst $ modifyNameSource $ runState $ runBuilderT m mempty
+    onStms coaltab _ stms = do
+      return $ replaceDecorations coaltab stms
 
-replaceDecorations :: LocalScope SeqMem m => M.Map VName Coalesced -> Stms SeqMem -> m (Stms SeqMem)
-replaceDecorations coaltab = mapM $ replaceInStm coaltab
+replaceDecorations :: M.Map VName Coalesced -> Stms SeqMem -> Stms SeqMem
+replaceDecorations coaltab = fmap $ replaceInStm coaltab
 
-replaceInStm :: LocalScope SeqMem m => M.Map VName Coalesced -> Stm SeqMem -> m (Stm SeqMem)
-replaceInStm coaltab stm@(Let (Pat elems) d e) =
-  inScopeOf stm $ do
-    e' <- replaceInExp coaltab e
-    return $ Let (Pat elems') d e'
+replaceInStm :: M.Map VName Coalesced -> Stm SeqMem -> Stm SeqMem
+replaceInStm coaltab (Let (Pat elems) d e) =
+  let e' = replaceInExp coaltab e
+   in Let (Pat elems') d e'
   where
     elems' = fmap replaceInPatElem elems
 
@@ -66,42 +61,56 @@ replaceInStm coaltab stm@(Let (Pat elems) d e) =
       fromMaybe p $ lookupAndReplace vname coaltab PatElem u
     replaceInPatElem p = p
 
-    replaceInExp :: LocalScope SeqMem m => M.Map VName Coalesced -> Exp SeqMem -> m (Exp SeqMem)
-    replaceInExp _ (BasicOp _) = return e
-    replaceInExp tab (If se then_body else_body dec) = do
-      then_body' <- replaceInIfBody tab then_body
-      else_body' <- replaceInIfBody tab else_body
-      let dec' = dec {ifReturns = zipWith (curry generalizeIxfun) elems' $ ifReturns dec}
-      return $ If se then_body' else_body' dec'
-    replaceInExp tab (DoLoop loop_inits loop_form (Body dec stms res)) = do
+    replaceInExp :: M.Map VName Coalesced -> Exp SeqMem -> Exp SeqMem
+    replaceInExp _ (BasicOp _) = e
+    replaceInExp tab (If se then_body else_body dec) =
+      let then_body' = replaceInIfBody tab then_body
+          else_body' = replaceInIfBody tab else_body
+          dec' = dec {ifReturns = zipWith (curry generalizeIxfun) elems' $ ifReturns dec}
+       in If se then_body' else_body' dec'
+    replaceInExp tab (DoLoop loop_inits loop_form (Body dec stms res)) =
       let loop_inits' = map (first $ replaceInFParam tab) loop_inits
-      stms' <- mapM (replaceInStm tab) stms
-      return $ DoLoop loop_inits' loop_form $ Body dec stms' res
-    replaceInExp _ (Op (Alloc _ _)) = return e
-    replaceInExp _ (Op (Inner ())) = return e
-    replaceInExp _ WithAcc {} = return e
-    replaceInExp _ Apply {} = return e
+          stms' = fmap (replaceInStm tab) stms
+       in DoLoop loop_inits' loop_form $ Body dec stms' res
+    replaceInExp _ (Op (Alloc _ _)) = e
+    replaceInExp _ (Op (Inner ())) = e
+    replaceInExp _ WithAcc {} = e
+    replaceInExp _ Apply {} = e
 
-    generalizeIxfun :: (PatElem SeqMem, MemInfo ExtSize NoUniqueness MemReturn) -> MemInfo ExtSize NoUniqueness MemReturn
-    generalizeIxfun (PatElem vname (MemArray _ _ _ (ArrayIn mem ixfun)), MemArray pt shp u _)
-      | vname `M.member` coaltab =
-        MemArray pt shp u $ ReturnsInBlock mem $ existentialiseIxFun (map patElemName elems) ixfun
+    generalizeIxfun :: (PatElem SeqMem, BodyReturns) -> BodyReturns
+    generalizeIxfun
+      ( PatElem vname (MemArray _ _ _ (ArrayIn mem ixfun)),
+        MemArray pt shp u _
+        )
+        | vname `M.member` coaltab =
+          existentialiseIxFun (map patElemName elems) ixfun
+            & ReturnsInBlock mem
+            & MemArray pt shp u
     generalizeIxfun (_, m) = m
 
-replaceInIfBody :: LocalScope SeqMem m => M.Map VName Coalesced -> Body SeqMem -> m (Body SeqMem)
-replaceInIfBody tab b@(Body _ stms _) = do
-  stms' <- mapM (replaceInStm tab) stms
-  return $ b {bodyStms = stms'}
+replaceInIfBody :: M.Map VName Coalesced -> Body SeqMem -> Body SeqMem
+replaceInIfBody tab b@(Body _ stms _) =
+  let stms' = fmap (replaceInStm tab) stms
+   in b {bodyStms = stms'}
 
 replaceInFParam :: M.Map VName Coalesced -> FParam SeqMem -> FParam SeqMem
 replaceInFParam tab p@(Param vname (MemArray _ _ u _)) =
   fromMaybe p $ lookupAndReplace vname tab Param u
 replaceInFParam _ p = p
 
-lookupAndReplace :: VName -> M.Map VName Coalesced -> (VName -> MemInfo SubExp u MemBind -> a) -> u -> Maybe a
+lookupAndReplace ::
+  VName ->
+  M.Map VName Coalesced ->
+  (VName -> MemInfo SubExp u MemBind -> a) ->
+  u ->
+  Maybe a
 lookupAndReplace vname coaltab f u =
   case M.lookup vname coaltab of
     Just (Coalesced InPlaceCoal (MemBlock pt shp mem ixfun) subs) ->
-      Just $ f vname $ MemArray pt shp u $ ArrayIn mem $ substituteInIxFun subs ixfun
+      substituteInIxFun subs ixfun
+        & ArrayIn mem
+        & MemArray pt shp u
+        & f vname
+        & Just
     Nothing -> Nothing
     _ -> undefined
