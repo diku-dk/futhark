dd48c5cf8c03194225760a3b0c999ffb2efb5912
Philip Munksgaard <philip@munksgaard.me>
2021-09-28 11:10:05 +0200

    Fix LiftAllocations handling of If-branches
    Also remove some commented code

--- src/Futhark/Pass/LiftAllocations.hs
+++ src/Futhark/Pass/LiftAllocations.hs
@@ -43,10 +43,22 @@ liftAllocationsInStms Empty lifted acc _ = lifted <> acc
 liftAllocationsInStms (stms :|> stm@(Let (Pat [PatElem vname _]) _ (Op (Alloc _ _)))) lifted acc to_lift =
   liftAllocationsInStms stms (stm :<| lifted) acc ((freeIn stm <> to_lift) `namesSubtract` oneName vname)
 liftAllocationsInStms (stms :|> stm@(Let pat _ (If cond then_body else_body dec))) lifted acc to_lift =
-  let stm' = stm {stmExp = If cond (liftAllocationsInBody then_body) (liftAllocationsInBody else_body) dec}
+  let then_body' = liftAllocationsInBody then_body
+      else_body' = liftAllocationsInBody else_body
+      stm' = stm {stmExp = If cond then_body' else_body' dec}
       pat_names = namesFromList $ patNames pat
    in if pat_names `namesIntersect` to_lift
-        then liftAllocationsInStms stms (stm' :<| lifted) acc (to_lift `namesSubtract` pat_names)
+        then
+          liftAllocationsInStms
+            stms
+            (stm' :<| lifted)
+            acc
+            ( (to_lift `namesSubtract` pat_names)
+                <> freeIn cond
+                <> freeIn then_body'
+                <> freeIn else_body'
+                <> freeIn dec
+            )
         else liftAllocationsInStms stms lifted (stm' :<| acc) to_lift
 liftAllocationsInStms (stms :|> stm@(Let pat _ (DoLoop params form body))) lifted acc to_lift =
   let stm' = stm {stmExp = DoLoop params form $ liftAllocationsInBody body}
@@ -59,98 +71,3 @@ liftAllocationsInStms (stms :|> stm@(Let pat _ _)) lifted acc to_lift =
    in if pat_names `namesIntersect` to_lift
         then liftAllocationsInStms stms (stm :<| lifted) acc ((to_lift `namesSubtract` pat_names) <> freeIn stm)
         else liftAllocationsInStms stms lifted (stm :<| acc) to_lift
-
--- reorderBody :: Names -> Body SeqMem -> Body SeqMem
--- reorderBody alreadyDefined body@Body {bodyResult, bodyStms} =
---   let m = statementMap bodyStms
---    in body
---         { bodyStms =
---             reorderStatements
---               (namesToList $ freeIn bodyResult)
---               alreadyDefined
---               m
---               mempty
---         }
-
--- reorderKernelBody :: Names -> KernelBody SeqMem -> KernelBody SeqMem
--- reorderKernelBody alreadyDefined kbody@KernelBody {kernelBodyResult, kernelBodyStms} =
---   let m = statementMap kernelBodyStms
---    in kbody
---         { kernelBodyStms =
---             reorderStatements
---               (namesToList $ freeIn kernelBodyResult)
---               alreadyDefined
---               m
---               mempty
---         }
-
--- -- | Computes a map from VName to the statement that defines it.
--- statementMap :: Stms lore -> Map VName (Stm lore)
--- statementMap stms =
---   toList stms
---     & concatMap
---       (\stm -> [(vname, stm) | vname <- namesInPat $ stmPattern stm])
---     & Map.fromList
-
--- -- | Attemts to reorder statements by maintaining a stack of `VName` we need to
--- -- compute.
--- --
--- -- When looking at the item at the top of the stack `x`, find the list of
--- -- `VName` used in the the computation of `x` and insert those at the front of
--- -- the stack. If no no `VName` is required, insert the statement defining `x`
--- -- and add `x` and all other `VName` defined in that statement to the set of
--- -- values in `scope`.
--- reorderStatements ::
---   [VName] ->
---   Names ->
---   Map VName (Stm SeqMem) ->
---   Stms SeqMem ->
---   Stms SeqMem
--- reorderStatements [] _ _ acc = acc
--- reorderStatements (x : xs) scope m acc =
---   if x `nameIn` scope
---     then reorderStatements xs scope m acc
---     else case Map.lookup x m of
---       Just stm -> case namesToList $ freeIn stm `namesSubtract` scope of
---         [] ->
---           let scope' =
---                 namesFromList (namesInPat (stmPattern stm)) <> scope
---            in reorderStatements
---                 xs
---                 scope'
---                 m
---                 $ acc
---                   |> stm
---                     { stmExp =
---                         mapExp (mapper scope') $
---                           stmExp stm
---                     }
---         todo -> reorderStatements (todo <> (x : xs)) scope m acc
---       Nothing ->
---         -- The variable doesn't appear in the statement-map. We therefore assume
---         -- that it comes from outside this body, and that it is already in
---         -- scope.
---         reorderStatements xs (oneName x <> scope) m acc
-
--- mapper :: Names -> Mapper SeqMem SeqMem Identity
--- mapper scope =
---   identityMapper
---     { mapOnBody = \_ b -> return $ reorderBody scope b,
---       mapOnOp = onOp
---     }
---   where
---     onOp (Inner (SegOp op)) =
---       Inner . SegOp <$> mapSegOpM opMapper op
---     onOp op = return op
-
---     opMapper =
---       identitySegOpMapper
---         { mapOnSegOpLambda = onLambda,
---           mapOnSegOpBody = onKernelBody
---         }
-
---     onKernelBody body = do
---       return $ reorderKernelBody scope body
-
---     onLambda lam = do
---       return lam {lambdaBody = reorderBody scope $ lambdaBody lam}
