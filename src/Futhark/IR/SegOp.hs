e15e4d76cd21bedbf7719e5a506315536ee3b36c
Troels Henriksen <athas@sigkill.dk>
2021-10-12 17:30:39 +0200

    Fix renaming of SegOps, again.

--- src/Futhark/IR/SegOp.hs
+++ src/Futhark/IR/SegOp.hs
@@ -766,12 +766,11 @@ identitySegOpMapper =
     }
 
 mapOnSegSpace ::
-  Monad f =>
-  SegOpMapper lvl frep trep f ->
-  SegSpace ->
-  f SegSpace
+  Monad f => SegOpMapper lvl frep trep f -> SegSpace -> f SegSpace
 mapOnSegSpace tv (SegSpace phys dims) =
-  SegSpace phys <$> traverse (traverse $ mapOnSegOpSubExp tv) dims
+  SegSpace
+    <$> mapOnSegOpVName tv phys
+    <*> traverse (bitraverse (mapOnSegOpVName tv) (mapOnSegOpSubExp tv)) dims
 
 mapSegBinOp ::
   Monad m =>
@@ -867,7 +866,9 @@ instance
   (ASTRep rep, FreeIn (LParamInfo rep), FreeIn lvl) =>
   FreeIn (SegOp lvl rep)
   where
-  freeIn' e = flip execState mempty $ mapSegOpM free e
+  freeIn' e =
+    fvBind (namesFromList $ M.keys $ scopeOfSegSpace (segSpace e)) $
+      flip execState mempty $ mapSegOpM free e
     where
       walk f x = modify (<> f x) >> return x
       free =
