50a3b3831ea11a65e8d08508d9f10899ffbfd763
Troels Henriksen <athas@sigkill.dk>
2021-10-31 00:51:04 +0200

    Interchange fixes.

--- src/Futhark/Pass/ExtractKernels/Interchange.hs
+++ src/Futhark/Pass/ExtractKernels/Interchange.hs
@@ -27,6 +27,7 @@ import Futhark.Pass.ExtractKernels.Distribution
   ( KernelNest,
     LoopNesting (..),
     kernelNestLoops,
+    scopeOfKernelNest,
   )
 import Futhark.Tools
 import Futhark.Transform.Rename
@@ -77,7 +78,7 @@ interchangeLoop
         res = varsRes $ patNames loop_pat_expanded
         pat' = Pat $ rearrangeShape perm $ patElems pat
 
-    return $
+    pure $
       SeqLoop perm pat' merge_expanded form $
         mkBody (pre_copy_stms <> oneStm map_stm) res
     where
@@ -89,12 +90,9 @@ interchangeLoop
         | otherwise =
           return $ Just (param, arr)
 
-      expandedInit param_name (Var v)
+      expandedInit _ (Var v)
         | Just arr <- isMapParameter v =
-          -- We need to copy because otherwise the result of the
-          -- loop will alias the input (if the number of iterations
-          -- is 0), which is a problem if the result is consumed.
-          letSubExp (param_name <> "_copied_init") $ BasicOp $ Copy arr
+          pure $ Var arr
       expandedInit param_name se =
         letSubExp (param_name <> "_expanded_init") $
           BasicOp $ Replicate (Shape [w]) se
@@ -103,8 +101,8 @@ interchangeLoop
         expanded_param <-
           newParam (param_name <> "_expanded") $
             -- FIXME: Unique here is a hack to make sure the copy from
-            -- expandedInit is not prematurely simplified away.  It'd
-            -- be better to fix this somewhere else...
+            -- makeCopyInitial is not prematurely simplified away.
+            -- It'd be better to fix this somewhere else...
             arrayOf (paramDeclType merge_param) (Shape [w]) Unique
         expanded_init <- expandedInit param_name merge_init
         return (expanded_param, expanded_init)
@@ -113,6 +111,23 @@ interchangeLoop
 
       expandPatElem (PatElem name t) =
         PatElem name $ arrayOfRow t w
+
+-- We need to copy some initial arguments because otherwise the result
+-- of the loop might alias the input (if the number of iterations is
+-- 0), which is a problem if the result is consumed.
+maybeCopyInitial ::
+  (MonadBuilder m) =>
+  (VName -> Bool) ->
+  SeqLoop ->
+  m SeqLoop
+maybeCopyInitial isMapInput (SeqLoop perm loop_pat merge form body) =
+  SeqLoop perm loop_pat <$> mapM f merge <*> pure form <*> pure body
+  where
+    f (p, Var arg)
+      | isMapInput arg =
+        (p,) <$> letSubExp (baseString (paramName p) <> "_inter_copy") (BasicOp $ Copy arg)
+    f (p, arg) =
+      pure (p, arg)
 
 -- | Given a (parallel) map nesting and an inner sequential loop, move
 -- the maps inside the sequential loop.  The result is several
@@ -125,15 +140,17 @@ interchangeLoops ::
   m (Stms SOACS)
 interchangeLoops nest loop = do
   (loop', stms) <-
-    runBuilder $
-      foldM (interchangeLoop isMapParameter) loop $
-        reverse $ kernelNestLoops nest
+    runBuilder . localScope (scopeOfKernelNest nest) $
+      maybeCopyInitial isMapInput
+        =<< foldM (interchangeLoop isMapParameter) loop (reverse $ kernelNestLoops nest)
   return $ stms <> oneStm (seqLoopStm loop')
   where
     isMapParameter v =
-      fmap snd $
-        find ((== v) . paramName . fst) $
-          concatMap loopNestingParamsAndArrs $ kernelNestLoops nest
+      fmap snd . find ((== v) . paramName . fst) $
+        concatMap loopNestingParamsAndArrs $ kernelNestLoops nest
+    isMapInput v =
+      elem v . map snd . concatMap loopNestingParamsAndArrs $
+        kernelNestLoops nest
 
 data Branch = Branch [Int] Pat SubExp Body Body (IfDec (BranchType SOACS))
 
