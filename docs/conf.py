05169fc270ae78df2467de029c03dba1627902f0
Troels Henriksen <athas@sigkill.dk>
2021-11-05 17:01:43 +0100

    Perhaps fix doc building.

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ from sphinx.highlighting import lexers
 # ones.
 extensions = [
     'sphinx.ext.todo',
-    'sphinx.ext.mathjax',
+    'sphinx.ext.mathjax'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -83,7 +83,7 @@ release = version
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'lib']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
