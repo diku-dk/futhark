6953ce0d3a245d6ec6f4bbab6e63b5c24848885f
Troels Henriksen <athas@sigkill.dk>
2022-03-11 09:58:00 +0100

    Move non-shared things out of Shared module.

--- src/Futhark/Optimise/BlkRegTiling.hs
+++ src/Futhark/Optimise/BlkRegTiling.hs
@@ -21,15 +21,260 @@ module Futhark.Optimise.BlkRegTiling (mmBlkRegTiling, doRegTiling3D) where
 
 import Control.Monad.Reader
 import qualified Data.List as L
+import Data.List.NonEmpty (NonEmpty (..))
 import qualified Data.Map.Strict as M
 import Data.Maybe
 import qualified Data.Sequence as Seq
 import Futhark.IR.GPU
+import qualified Futhark.IR.Mem.IxFun as IxFun
 import Futhark.MonadFreshNames
 import Futhark.Optimise.TileLoops.Shared
 import Futhark.Tools
 import Futhark.Transform.Rename
 import Futhark.Transform.Substitute
+
+se0 :: SubExp
+se0 = intConst Int64 0
+
+se1 :: SubExp
+se1 = intConst Int64 1
+
+se2 :: SubExp
+se2 = intConst Int64 2
+
+se4 :: SubExp
+se4 = intConst Int64 4
+
+se8 :: SubExp
+se8 = intConst Int64 8
+
+scratch :: MonadBuilder m => String -> PrimType -> [SubExp] -> m VName
+scratch se_name t shape = letExp se_name $ BasicOp $ Scratch t shape
+
+-- | Main helper function for Register-and-Block Tiling
+kkLoopBody ::
+  Env ->
+  ( (SubExp, SubExp, SubExp, SubExp, SubExp, SubExp, SubExp, SubExp),
+    SegLevel,
+    [Int],
+    (VName, SubExp, VName, SubExp, SubExp),
+    (SubExp, SubExp),
+    (VName, VName),
+    (Stm GPU, VName, PrimType, Stm GPU, VName, PrimType),
+    (Lambda GPU, Lambda GPU)
+  ) ->
+  VName ->
+  (VName, VName, VName) ->
+  Bool ->
+  Builder GPU [VName]
+kkLoopBody
+  env
+  ( (rx, ry, tx, ty, tk, tk_div_tx, _tk_div_ty, tx_rx),
+    segthd_lvl,
+    var_dims,
+    (gtid_x, width_B, gtid_y, height_A, common_dim),
+    (a_loc_sz, b_loc_sz),
+    (iii, jjj),
+    (load_A, inp_A, pt_A, load_B, inp_B, pt_B),
+    (map_lam, red_lam)
+    )
+  kk0
+  (thd_res_merge, a_loc_init', b_loc_init')
+  epilogue = do
+    let (map_t1, map_t2) = (pt_A, pt_B)
+    kk <- letExp "kk" =<< toExp (le64 kk0 * pe64 tk)
+    -- copy A to local memory
+    (a_loc, aCopyLoc2Reg) <-
+      copyGlb2ShMem kk (gtid_y, iii, map_t1, height_A, inp_A, load_A, a_loc_sz, a_loc_init')
+
+    -- copy B from global to shared memory
+    (b_loc, bCopyLoc2Reg) <-
+      copyGlb2ShMem kk (gtid_x, jjj, map_t2, width_B, inp_B, load_B, b_loc_sz, b_loc_init')
+
+    -- inner loop updating this thread's accumulator (loop k in mmm_kernels).
+    thd_acc <- forLoop tk [thd_res_merge] $ \k [acc_merge] ->
+      resultBodyM =<< letTupExp' "foo"
+        =<< eIf
+          ( toExp $
+              if epilogue
+                then le64 kk + le64 k .<. pe64 common_dim
+                else true -- if in prologue, always compute redomap.
+          )
+          ( do
+              reg_mem <- segMap2D "reg_mem" segthd_lvl ResultPrivate (ty, tx) $
+                \(ltid_y, ltid_x) -> do
+                  -- copy A from local memory to registers
+                  asss <- aCopyLoc2Reg k ltid_y
+                  -- copy B from local memory to registers
+                  bsss <- bCopyLoc2Reg k ltid_x
+                  return $ varsRes [asss, bsss]
+              let [asss, bsss] = reg_mem
+              mkRedomapOneTileBody acc_merge asss bsss True
+          )
+          (resultBodyM [Var acc_merge])
+    return [thd_acc, a_loc, b_loc]
+    where
+      mk_ik is_coal (thd_y, thd_x) (i0, k0)
+        | is_coal = do
+          -- not-transposed case (i.e., already coalesced)
+          let (t_par, t_seq) = (tx, tk)
+          k <- letExp "k" =<< toExp (le64 thd_x + le64 k0 * pe64 t_par)
+          i <- letExp "i" =<< toExp (le64 thd_y + le64 i0 * pe64 t_par)
+          -- we have padded to minimize bank conflicts,
+          -- hence the length of inner dim is (t_seq + 1)
+          let e = le64 k + le64 i * (pe64 t_seq + pe64 se1)
+          return (i, k, e)
+      mk_ik _ (thd_y, thd_x) (i0, k0) = do
+        -- matrix is transposed case (i.e., uncoalesced):
+        let (t_par, tr_par) = (tx, tx_rx)
+        k <- letExp "k" =<< toExp (le64 thd_y + le64 k0 * pe64 t_par)
+        i <- letExp "i" =<< toExp (le64 thd_x + le64 i0 * pe64 t_par)
+        -- we have padded to minimize bank conflicts,
+        -- hence the length of inner dim is (tr_par + 1)
+        let e = le64 i + le64 k * (pe64 tr_par + pe64 se1)
+        return (i, k, e)
+      isInnerCoal :: Env -> VName -> Stm GPU -> Bool
+      isInnerCoal (_, ixfn_env) slc_X (Let pat _ (BasicOp (Index x _)))
+        | [slc_X'] <- patNames pat,
+          slc_X == slc_X',
+          Nothing <- M.lookup x ixfn_env =
+          True -- if not in the table, we assume not-transposed!
+      isInnerCoal (_, ixfn_env) slc_X (Let pat _ (BasicOp (Index x _)))
+        | [slc_X'] <- patNames pat,
+          slc_X == slc_X',
+          Just ixf_fn <- M.lookup x ixfn_env,
+          (IxFun.IxFun (lmad :| []) _ _) <- ixf_fn =
+          let lmad_dims = IxFun.lmadDims lmad
+              q = length lmad_dims
+              last_perm = IxFun.ldPerm $ last lmad_dims
+              stride = IxFun.ldStride $ last lmad_dims
+              res = last_perm == q -1 && (stride == pe64 (intConst Int64 1))
+           in res
+      isInnerCoal _ _ _ = error "TileLoops/Shared.hs: not an error, but I would like to know why!"
+      --
+      mkRedomapOneTileBody acc_merge asss bsss fits_ij = do
+        -- the actual redomap.
+        redomap_res <- segMap2D "redomap_res" segthd_lvl ResultPrivate (ty, tx) $
+          \(ltid_y, ltid_x) -> do
+            as <- index "as" asss [ltid_y, ltid_x]
+            bs <- index "bs" bsss [ltid_y, ltid_x]
+            css_init <- index "css_init" acc_merge [ltid_y, ltid_x]
+
+            css <- forLoop ry [css_init] $ \i [css_merge] -> do
+              css <- forLoop rx [css_merge] $ \j [css_merge'] ->
+                resultBodyM =<< letTupExp' "foo"
+                  =<< eIf
+                    ( toExp $
+                        if fits_ij
+                          then true
+                          else -- this condition is never needed because
+                          -- if i and j are out of range than css[i,j]
+                          -- is garbage anyways and should not be written.
+                          -- so fits_ij should be always true!!!
+
+                            le64 iii + le64 i + pe64 ry * le64 ltid_y
+                              .<. pe64 height_A
+                                .&&. le64 jjj + le64 j + pe64 rx * le64 ltid_x
+                              .<. pe64 width_B
+                    )
+                    ( do
+                        a <- index "a" as [i]
+                        b <- index "b" bs [j]
+                        c <- index "c" css_merge' [i, j]
+
+                        map_lam' <- renameLambda map_lam
+                        red_lam' <- renameLambda red_lam
+
+                        -- the inputs to map are supposed to be permutted with the
+                        -- inverted permutation, so as to reach the original position;
+                        -- it just so happens that the inverse of [a,b] is [b,a]
+                        let map_inp_reg = if var_dims == [0, 1] then [a, b] else [b, a]
+
+                        map_res <- eLambda map_lam' (map (eSubExp . Var) map_inp_reg)
+                        ~[red_res] <- eLambda red_lam' (map eSubExp $ Var c : map resSubExp map_res)
+                        css <- update "css" css_merge' [i, j] (resSubExp red_res)
+
+                        resultBodyM [Var css]
+                    )
+                    (resultBodyM [Var css_merge'])
+              resultBodyM [Var css]
+            return [varRes css]
+        resultBodyM $ map Var redomap_res
+      --
+      copyGlb2ShMem ::
+        VName ->
+        (VName, VName, PrimType, SubExp, VName, Stm GPU, SubExp, VName) ->
+        Builder GPU (VName, VName -> VName -> Builder GPU VName)
+      copyGlb2ShMem kk (gtid, ii, ptp_X_el, parlen_X, inp_X, load_X, loc_sz_X, x_loc_init') = do
+        let (t_par, r_par, tseq_div_tpar) = (tx, rx, tk_div_tx)
+            is_inner_coal = isInnerCoal env inp_X load_X
+            str_A = baseString inp_X
+        x_loc <-
+          segScatter2D (str_A ++ "_glb2loc") loc_sz_X x_loc_init' segthd_lvl [r_par, tseq_div_tpar] (t_par, t_par) $
+            scatterFun is_inner_coal
+
+        return (x_loc, copyLoc2Reg is_inner_coal str_A x_loc)
+        where
+          copyLoc2Reg ::
+            Bool ->
+            String ->
+            VName ->
+            VName ->
+            VName ->
+            Builder GPU VName
+          copyLoc2Reg is_inner_coal str_A x_loc k ltid_yx = do
+            let (r_par, t_seq, tr_par) = (rx, tk, tx_rx)
+            xsss_init <- scratch (str_A ++ "_init_regs") ptp_X_el [r_par]
+            forLoop r_par [xsss_init] $ \ij [xsss_merge] -> do
+              x_loc_ind <-
+                letExp (str_A ++ "_loc_ind")
+                  =<< toExp
+                    ( if is_inner_coal
+                        then le64 k + (le64 ltid_yx * pe64 r_par + le64 ij) * (pe64 t_seq + pe64 se1)
+                        else le64 ij + le64 ltid_yx * pe64 r_par + le64 k * (pe64 tr_par + pe64 se1)
+                    )
+              xsss <-
+                update (str_A ++ "_regs") xsss_merge [ij] . Var
+                  =<< index (str_A ++ "_loc_elem") x_loc [x_loc_ind]
+              resultBodyM [Var xsss]
+          --
+          scatterFun ::
+            Bool ->
+            [VName] ->
+            (VName, VName) ->
+            Builder GPU (SubExp, SubExp)
+          scatterFun is_inner_coal [i0, k0] (thd_y, thd_x) = do
+            let str_A = baseString inp_X
+                t_seq = tk
+            (i, k, epx_loc_fi) <- mk_ik is_inner_coal (thd_y, thd_x) (i0, k0)
+            letBindNames [gtid] =<< toExp (le64 ii + le64 i)
+            a_seqdim_idx <- letExp (str_A ++ "_seqdim_idx") =<< toExp (le64 kk + le64 k)
+
+            a_elem <-
+              letSubExp (str_A ++ "_elem")
+                =<< eIf
+                  ( toExp $
+                      le64 gtid .<. pe64 parlen_X
+                        .&&. if epilogue
+                          then le64 a_seqdim_idx .<. pe64 common_dim
+                          else true
+                  )
+                  ( do
+                      addStm load_X
+                      res <- index "A_elem" inp_X [a_seqdim_idx]
+                      resultBodyM [Var res]
+                  )
+                  (eBody [eBlank $ Prim ptp_X_el])
+
+            a_loc_ind <-
+              letSubExp (str_A ++ "_loc_ind")
+                =<< eIf
+                  (toExp $ le64 k .<. pe64 t_seq)
+                  (eBody [toExp epx_loc_fi])
+                  (eBody [eSubExp $ intConst Int64 (-1)])
+            return (a_elem, a_loc_ind)
+          scatterFun _ _ _ = do
+            error "Function scatterFun in Shared.hs: 2nd arg should be an array with 2 elements!"
 
 -- ToDo: we need tx == ty (named t_par), and rx == ry (named r_par)
 --       in order to handle all the cases without transpositions.
@@ -656,21 +901,6 @@ processIndirections _ res_red_var acc stm'@(Let patt _ _)
     all (\p -> not (nameIn (patElemName p) res_red_var)) ps =
     Just (ss Seq.|> stm', tab)
   | otherwise = Nothing
-
-se0 :: SubExp
-se0 = intConst Int64 0
-
-se1 :: SubExp
-se1 = intConst Int64 1
-
-se2 :: SubExp
-se2 = intConst Int64 2
-
-se4 :: SubExp
-se4 = intConst Int64 4
-
-se8 :: SubExp
-se8 = intConst Int64 8
 
 getParTiles :: (String, String) -> (Name, Name) -> SubExp -> Builder GPU (SubExp, SubExp)
 getParTiles (t_str, r_str) (t_name, r_name) len_dim =
