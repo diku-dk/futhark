7d68ea14d6aca68d0eaa49e986886709f42c2d57
Troels Henriksen <athas@sigkill.dk>
2022-03-17 16:28:58 +0100

    Fix expected error.

--- tests_adhoc/bad_input/test.sh
+++ tests_adhoc/bad_input/test.sh
@@ -3,8 +3,8 @@
 set -e
 
 ! futhark test bad_input.fut > stderr
-if !(fgrep -q 'Expected input of types: [i64]' stderr) ||
-        !(fgrep -q 'Provided input of types: [i32]' stderr); then
+if !(fgrep -q 'Expected input of types: i64' stderr) ||
+        !(fgrep -q 'Provided input of types: i32' stderr); then
     cat stderr
     exit 1
 fi
