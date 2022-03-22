607d92ad79a44648d96d88cf836f51ec9b4c3ea4
haoranpb <haoranpb@outlook.com>
2022-03-22 11:13:48 +0100

    better error msg when find Hole
    the type is always NoInfo

--- src/Language/Futhark/TypeChecker/Terms.hs
+++ src/Language/Futhark/TypeChecker/Terms.hs
@@ -269,7 +269,7 @@ checkApplyExp e = do
 checkExp :: UncheckedExp -> TermTypeM Exp
 checkExp (Literal val loc) =
   pure $ Literal val loc
-checkExp (Hole _ _ _) = error $ "checkExp: unexpected hole"
+checkExp (Hole vn t loc) = typeError loc mempty $ "Found typed hole: " <+> text (show vn) <+> text (show t) <> "."
 checkExp (StringLit vs loc) =
   pure $ StringLit vs loc
 checkExp (IntLit val NoInfo loc) = do
