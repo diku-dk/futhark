acf08bcbd326961956738cb03a30f016848eab23
haoranpb <haoranpb@outlook.com>
2022-03-22 13:24:14 +0100

    refine checkExp

--- src/Language/Futhark/TypeChecker/Terms.hs
+++ src/Language/Futhark/TypeChecker/Terms.hs
@@ -269,7 +269,8 @@ checkApplyExp e = do
 checkExp :: UncheckedExp -> TermTypeM Exp
 checkExp (Literal val loc) =
   pure $ Literal val loc
-checkExp (Hole name t loc) = typeError loc mempty $ "Found typed hole: " <+> text (show name) <+> text (show t) <> "."
+checkExp (Hole name _ loc) =
+  typeError loc mempty $ "Found typed hole: " <+> text (show name) <> "."
 checkExp (StringLit vs loc) =
   pure $ StringLit vs loc
 checkExp (IntLit val NoInfo loc) = do
