0a322e20b2400aa5c4b35036637d2fb019f7a285
Kristoffer August Kortbaek <kak@hotmail.dk>
2022-03-28 10:53:12 +0200

    Starting to work on memblock set

--- rts/c/ispc_util.h
+++ rts/c/ispc_util.h
@@ -2,17 +2,17 @@
 
 typedef unsigned char uchar;
 
-static void free(void* ptr) {
+static inline void free(void* ptr) {
   delete ptr;
 }
 
-static void free(void* uniform ptr) {
+static inline void free(void* uniform ptr) {
   if (programIndex == 0) {
     delete ptr;
   }
 }
 
-static uniform int lexical_realloc_ispc(unsigned char uniform * uniform * uniform ptr,
+static inline uniform int lexical_realloc_ispc(unsigned char uniform * uniform * uniform ptr,
                                         int64_t uniform * uniform old_size,
                                         uniform int64_t new_size) {
   if (*ptr != NULL) free(*ptr);
@@ -22,13 +22,13 @@ static uniform int lexical_realloc_ispc(unsigned char uniform * uniform * unifor
   return FUTHARK_SUCCESS;
 }
 
-static uniform int lexical_realloc_ispc(unsigned char uniform * uniform * uniform ptr,
+static inline uniform int lexical_realloc_ispc(unsigned char uniform * uniform * uniform ptr,
                                         int64_t uniform * uniform old_size,
                                         varying int64_t new_size) {
   return lexical_realloc_ispc(ptr, old_size, extract(new_size, 0));
 }
 
-static uniform int lexical_realloc_ispc(unsigned char uniform * varying * uniform ptr,
+static inline uniform int lexical_realloc_ispc(unsigned char uniform * varying * uniform ptr,
                                         int64_t uniform * varying old_size,
                                         varying int64_t new_size) {
   if (*ptr != NULL) free(*ptr);
@@ -38,13 +38,13 @@ static uniform int lexical_realloc_ispc(unsigned char uniform * varying * unifor
   return FUTHARK_SUCCESS;
 }
 
-static uniform int lexical_realloc_ispc(unsigned char uniform * varying * uniform ptr,
+static inline uniform int lexical_realloc_ispc(unsigned char uniform * varying * uniform ptr,
                                         int64_t varying * uniform old_size,
                                         varying int64_t new_size) {
   return lexical_realloc_ispc(ptr, (uniform int64_t * varying)old_size, new_size);
 }
 
-static uniform int lexical_realloc_ispc(unsigned char uniform * varying * uniform ptr,
+static inline uniform int lexical_realloc_ispc(unsigned char uniform * varying * uniform ptr,
                                         size_t varying * uniform old_size,
                                         varying int64_t new_size) {
   if (*ptr != NULL) free(*ptr);
@@ -53,3 +53,15 @@ static uniform int lexical_realloc_ispc(unsigned char uniform * varying * unifor
   *old_size = new_size;
   return FUTHARK_SUCCESS;
 }
+static int $id:(fatMemSet space) (futhark_context *ctx, $ty:mty *lhs, $ty:mty *rhs, const char *lhs_desc) {
+  int ret = $id:(fatMemUnRef space)(ctx, lhs, 0); //TODO(K, oliber): Make error handling
+  if (rhs->references != NULL) {
+    (*(rhs->references))++;
+  }
+  *lhs = *rhs;
+  return ret;
+}
+
+
+
+
