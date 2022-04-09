1f68264d48da67464bade899496a4a00f93186eb
haoranpb <haoranpb@outlook.com>
2022-04-09 16:52:29 +0200

    publish diagnostic in correct file

--- src/Futhark/LSP/Compile.hs
+++ src/Futhark/LSP/Compile.hs
@@ -3,12 +3,11 @@ module Futhark.LSP.Compile (tryTakeStateFromMVar, tryReCompile) where
 import Control.Concurrent.MVar (MVar, putMVar, takeMVar)
 import Control.Monad.IO.Class (MonadIO (liftIO))
 import Futhark.Compiler.Program (LoadedProg, lpWarnings, noLoadedProg, reloadProg)
-import Futhark.LSP.Diagnostic (errorToDiagnostics, sendDiagnostics, warningsToDiagnostics)
+import Futhark.LSP.Diagnostic (publishErrorDiagnostics, publishWarningDiagnostics)
 import Futhark.LSP.State (State (..), emptyState)
 import Futhark.Util (debug)
 import Language.Futhark.Warnings (listWarnings)
 import Language.LSP.Server (LspT)
-import Language.LSP.Types (TextDocumentVersion, filePathToUri, toNormalizedUri)
 
 -- | Try to take state from MVar, if it's empty, try to compile.
 tryTakeStateFromMVar :: MVar State -> Maybe FilePath -> LspT () IO State
@@ -16,7 +15,7 @@ tryTakeStateFromMVar state_mvar file_path = do
   old_state <- liftIO $ takeMVar state_mvar
   case stateProgram old_state of
     Nothing -> do
-      new_state <- tryCompile file_path (State $ Just noLoadedProg) (Just 0) -- first compile, version 0
+      new_state <- tryCompile file_path (State $ Just noLoadedProg)
       liftIO $ putMVar state_mvar new_state
       pure new_state
     Just _ -> do
@@ -24,11 +23,11 @@ tryTakeStateFromMVar state_mvar file_path = do
       pure old_state
 
 -- | Try to (re)-compile, replace old state if successful.
-tryReCompile :: MVar State -> Maybe FilePath -> TextDocumentVersion -> LspT () IO ()
-tryReCompile state_mvar file_path version = do
+tryReCompile :: MVar State -> Maybe FilePath -> LspT () IO ()
+tryReCompile state_mvar file_path = do
   debug "(Re)-compiling ..."
   old_state <- liftIO $ takeMVar state_mvar
-  new_state <- tryCompile file_path old_state version
+  new_state <- tryCompile file_path old_state
   case stateProgram new_state of
     Nothing -> do
       debug "Failed to (re)-compile, using old state or Nothing"
@@ -39,20 +38,18 @@ tryReCompile state_mvar file_path version = do
 
 -- | Try to compile, publish diagnostics on warnings and errors, return newly compiled state.
 --  Single point where the compilation is done, and shouldn't be exported.
-tryCompile :: Maybe FilePath -> State -> TextDocumentVersion -> LspT () IO State
-tryCompile Nothing _ _ = pure emptyState
-tryCompile (Just path) state version = do
+tryCompile :: Maybe FilePath -> State -> LspT () IO State
+tryCompile Nothing _ = pure emptyState
+tryCompile (Just path) state = do
   let old_loaded_prog = getLoadedProg state
   res <- liftIO $ reloadProg old_loaded_prog [path]
   case res of
     Right new_loaded_prog -> do
-      let diags = warningsToDiagnostics $ listWarnings $ lpWarnings new_loaded_prog
-      sendDiagnostics (toNormalizedUri $ filePathToUri path) diags version
+      publishWarningDiagnostics $ listWarnings $ lpWarnings new_loaded_prog
       pure $ State (Just new_loaded_prog)
     Left prog_error -> do
       debug "Compilation failed, publishing diagnostics"
-      let diags = errorToDiagnostics prog_error
-      sendDiagnostics (toNormalizedUri $ filePathToUri path) diags version
+      publishErrorDiagnostics prog_error
       pure emptyState
 
 getLoadedProg :: State -> LoadedProg
--- src/Futhark/LSP/Diagnostic.hs
+++ src/Futhark/LSP/Diagnostic.hs
@@ -1,45 +1,71 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Futhark.LSP.Diagnostic
-  ( sendDiagnostics,
-    warningsToDiagnostics,
-    errorToDiagnostics,
+  ( publishWarningDiagnostics,
+    publishErrorDiagnostics,
   )
 where
 
+import Control.Lens ((^.))
+import Data.Foldable (for_)
 import qualified Data.List.NonEmpty as NE
+import Data.Map (assocs, empty, insertWith)
 import qualified Data.Text as T
 import Futhark.Compiler.Program (ProgError (ProgError))
+import Futhark.Util (debug)
 import Futhark.Util.Loc (Loc (Loc, NoLoc), Pos (Pos), SrcLoc, locOf)
 import Futhark.Util.Pretty (Doc, pretty)
 import Language.LSP.Diagnostics (partitionBySource)
-import Language.LSP.Server (LspT, publishDiagnostics)
+import Language.LSP.Server (LspT, getVersionedTextDoc, publishDiagnostics)
 import Language.LSP.Types
   ( Diagnostic (Diagnostic),
     DiagnosticSeverity (DsError, DsWarning),
-    NormalizedUri,
     Position (Position),
     Range (Range),
-    TextDocumentVersion,
+    TextDocumentIdentifier (TextDocumentIdentifier),
+    Uri,
+    filePathToUri,
+    toNormalizedUri,
   )
-
-sendDiagnostics :: NormalizedUri -> [Diagnostic] -> TextDocumentVersion -> LspT () IO ()
-sendDiagnostics uri diags version = publishDiagnostics 100 uri version (partitionBySource diags)
+import Language.LSP.Types.Lens (HasVersion (version))
 
 mkDiagnostic :: Range -> DiagnosticSeverity -> T.Text -> Diagnostic
 mkDiagnostic range severity msg = Diagnostic range (Just severity) Nothing (Just "futhark") msg Nothing Nothing
 
-warningsToDiagnostics :: [(SrcLoc, Doc)] -> [Diagnostic]
-warningsToDiagnostics =
-  map
-    ( \(srcloc, msg) ->
-        mkDiagnostic (rangeFromSrcLoc srcloc) DsWarning (T.pack $ pretty msg)
-    )
+-- | Publish diagnostics from a Uri to Diagnostics mapping.
+publish :: [(Uri, [Diagnostic])] -> LspT () IO ()
+publish uri_diags_map = for_ uri_diags_map $ \(uri, diags) -> do
+  doc <- getVersionedTextDoc $ TextDocumentIdentifier uri
+  debug $ "Publishing diagnostics for " ++ show uri ++ " Verion: " ++ show (doc ^. version)
+  publishDiagnostics 100 (toNormalizedUri uri) (doc ^. version) (partitionBySource diags)
 
-errorToDiagnostics :: NE.NonEmpty ProgError -> [Diagnostic]
-errorToDiagnostics prog_error = map onError (NE.toList prog_error)
-  where
-    onError (ProgError loc msg) = mkDiagnostic (rangeFromLoc loc) DsError (T.pack $ pretty msg)
+publishWarningDiagnostics :: [(SrcLoc, Doc)] -> LspT () IO ()
+publishWarningDiagnostics warnings = do
+  let diags_map =
+        foldr
+          ( \(srcloc, msg) acc -> do
+              let (Loc (Pos file _ _ _) _) = locOf srcloc
+                  uri = filePathToUri file
+                  diag = mkDiagnostic (rangeFromSrcLoc srcloc) DsWarning (T.pack $ pretty msg)
+              insertWith (++) uri [diag] acc
+          )
+          empty
+          warnings
+  publish $ assocs diags_map
+
+publishErrorDiagnostics :: NE.NonEmpty ProgError -> LspT () IO ()
+publishErrorDiagnostics errors = do
+  let diags_map =
+        foldr
+          ( \(ProgError loc msg) acc -> do
+              let (Loc (Pos file _ _ _) _) = loc
+                  uri = filePathToUri file
+                  diag = mkDiagnostic (rangeFromLoc loc) DsError (T.pack $ pretty msg)
+              insertWith (++) uri [diag] acc
+          )
+          empty
+          errors
+  publish $ assocs diags_map
 
 -- the ending appears to be one col too short
 rangeFromSrcLoc :: SrcLoc -> Range
