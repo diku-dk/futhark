65bd673db08ce43ac68b4aeb32df49531fcb5327
Kristoffer Kortbaek <kristoffer@kortbaek.dk>
2022-04-12 11:38:56 +0200

    Nix build now uses our version of lcq

--- default.nix
+++ default.nix
@@ -19,6 +19,8 @@ let
     packageOverrides = pkgs: rec {
       haskellPackages = pkgs.haskellPackages.override {
         overrides = haskellPackagesNew: haskellPackagesOld: rec {
+          language-c-quote =
+            haskellPackagesNew.callPackage ./nix/language-c-quote.nix { };
           futhark-data =
             haskellPackagesNew.callPackage ./nix/futhark-data.nix { };
 
