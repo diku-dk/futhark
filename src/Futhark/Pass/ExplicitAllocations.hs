51e812dc5a1e89fa7c7459ef3fcb3be91b885c91
Philip Børgesen <philip.borgesen@gmail.com>
2022-04-13 12:47:12 +0200

    Fix issue #1634

--- src/Futhark/Pass/ExplicitAllocations.hs
+++ src/Futhark/Pass/ExplicitAllocations.hs
@@ -746,7 +746,7 @@ allocInExp (DoLoop merge form (Body () bodystms bodyres)) =
 allocInExp (Apply fname args rettype loc) = do
   args' <- funcallArgs args
   -- We assume that every array is going to be in its own memory.
-  return $ Apply fname args' (mems ++ memoryInDeclExtType 0 rettype) loc
+  return $ Apply fname args' (mems ++ memoryInDeclExtType num_arrays rettype) loc
   where
     mems = replicate num_arrays (MemMem DefaultSpace)
     num_arrays = length $ filter ((> 0) . arrayRank . declExtTypeOf) rettype
