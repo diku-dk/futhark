98d1d3e79e73b3e2ba930b6d24bda410372f201f
Sun Haoran <haoranpb@outlook.com>
2022-04-14 09:58:06 +0200

    lsp: go to definition  (#1637)

--- src/Futhark/LSP/Tool.hs
+++ src/Futhark/LSP/Tool.hs
@@ -1,37 +1,87 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Futhark.LSP.Tool (getHoverInfoFromState) where
+module Futhark.LSP.Tool
+  ( getHoverInfoFromState,
+    findDefinitionRange,
+    rangeFromSrcLoc,
+    rangeFromLoc,
+    locToUri,
+  )
+where
 
 import qualified Data.Text as T
 import Futhark.Compiler.Program (lpImports)
 import Futhark.LSP.State (State (..))
-import Futhark.Util.Loc (srclocOf)
+import Futhark.Util.Loc (Loc (Loc, NoLoc), Pos (Pos), SrcLoc, locOf, srclocOf)
 import Futhark.Util.Pretty (pretty)
 import Language.Futhark.Core (locStr)
+import Language.Futhark.Prop (isBuiltin)
 import Language.Futhark.Query
   ( AtPos (AtName),
-    BoundTo (BoundModule, BoundModuleType, BoundTerm, BoundType),
-    Pos (Pos),
+    BoundTo (BoundTerm),
     atPos,
+    boundLoc,
+  )
+import Language.LSP.Types
+  ( Location (..),
+    Position (..),
+    Range (..),
+    Uri,
+    filePathToUri,
   )
 
-getHoverInfoFromState :: State -> Maybe FilePath -> Int -> Int -> IO (Maybe T.Text)
+getHoverInfoFromState :: State -> Maybe FilePath -> Int -> Int -> Maybe T.Text
 getHoverInfoFromState state (Just path) l c = do
+  case queryAtPos state $ Pos path l c 0 of
+    Nothing -> Nothing
+    Just (AtName qn def _loc) -> do
+      case def of
+        Nothing -> Nothing
+        Just (BoundTerm t defloc) -> do
+          Just $ T.pack $ pretty qn ++ " : " ++ pretty t ++ "\n\n" ++ "**Definition: " ++ locStr (srclocOf defloc) ++ "**"
+        Just bound ->
+          Just $ T.pack $ "Definition: " ++ locStr (boundLoc bound)
+getHoverInfoFromState _ _ _ _ = Nothing
+
+findDefinitionRange :: State -> Maybe FilePath -> Int -> Int -> Maybe Location
+findDefinitionRange state (Just path) l c = do
+  -- some unnessecary operations inside `queryAtPos` for this function
+  -- but shouldn't affect performance much since "Go to definition" is called less frequently
+  case queryAtPos state $ Pos path l c 0 of
+    Nothing -> Nothing
+    Just (AtName _qn def _loc) -> do
+      case def of
+        Nothing -> Nothing
+        Just bound -> do
+          let loc = boundLoc bound
+              Loc (Pos file_path _ _ _) _ = loc
+          if isBuiltin file_path
+            then Nothing
+            else Just $ Location (filePathToUri file_path) (rangeFromLoc loc)
+findDefinitionRange _ _ _ _ = Nothing
+
+queryAtPos :: State -> Pos -> Maybe AtPos
+queryAtPos state pos =
   case stateProgram state of
-    Nothing -> pure Nothing
-    Just loaded_prog -> do
-      let imports = lpImports loaded_prog
-      case atPos imports $ Pos path l c 0 of
-        Nothing -> pure Nothing
-        Just (AtName qn def _loc) -> do
-          case def of
-            Nothing -> pure Nothing
-            Just (BoundTerm t defloc) -> do
-              pure $ Just $ T.pack $ pretty qn ++ " : " ++ pretty t ++ "\n\n" ++ "**Definition: " ++ locStr (srclocOf defloc) ++ "**"
-            Just (BoundType defloc) ->
-              pure $ Just $ T.pack $ "Definition: " ++ locStr (srclocOf defloc)
-            Just (BoundModule defloc) ->
-              pure $ Just $ T.pack $ "Definition: " ++ locStr (srclocOf defloc)
-            Just (BoundModuleType defloc) ->
-              pure $ Just $ T.pack $ "Definition: " ++ locStr (srclocOf defloc)
-getHoverInfoFromState _ _ _ _ = pure Nothing
+    Nothing -> Nothing
+    Just loaded_prog -> atPos (lpImports loaded_prog) pos
+
+locToUri :: Loc -> Uri
+locToUri loc = do
+  let (Loc (Pos file _ _ _) _) = loc
+  filePathToUri file
+
+-- the ending appears to be one col too short
+rangeFromSrcLoc :: SrcLoc -> Range
+rangeFromSrcLoc srcloc = do
+  let Loc start end = locOf srcloc
+  Range (getPosition start) (getPosition end)
+
+rangeFromLoc :: Loc -> Range
+rangeFromLoc (Loc start end) = Range (getPosition start) (getPosition end)
+rangeFromLoc NoLoc = Range (Position 0 0) (Position 0 5) -- only when file not found, throw error after moving to vfs
+
+getPosition :: Pos -> Position
+getPosition pos = do
+  let Pos _ line col _ = pos
+  Position (toEnum line - 1) (toEnum col - 1)
