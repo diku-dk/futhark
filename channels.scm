9f9dbfe9620653c61ecfcf46a526340a03e34ef9
Philip Munksgaard <philip@munksgaard.me>
2022-04-21 13:53:04 +0200

    Add channels.scm
    Futhark can now be installed using the command:
    
    ```
    guix time-machine --channels=channels.scm -- install -f futhark.scm
    ```

---
+++ channels.scm
@@ -0,0 +1,11 @@
+(list (channel
+        (name 'guix)
+        (url "https://github.com/munksgaard/guix.git")
+        (branch "haskell-build-system-support-multiple-libraries")
+        (commit
+         "8a396a9803fc35ee63f01e608f87ffb16863bc6d")
+        (introduction
+         (make-channel-introduction
+          "8a396a9803fc35ee63f01e608f87ffb16863bc6d"
+          (openpgp-fingerprint
+           "5658 4D09 71AF E45F CC29 6BD7 4CE6 2A90 EFC0 B9B2")))))
