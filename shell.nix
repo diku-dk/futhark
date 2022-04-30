5395bdcd678d5a5469297955b8deab39424af21f
pema99 <pemamalling@gmail.com>
2022-04-30 02:24:54 +0200

    Fluff

--- shell.nix
+++ shell.nix
@@ -1,14 +1,6 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
-  ispc = pkgs.ispc.overrideAttrs (oldAttrs: rec {
-    version = "1.17.0";
-    src = builtins.fetchTarball {
-      url = "https://github.com/ispc/ispc/archive/refs/tags/v${version}.tar.gz";
-      sha256 = "1klk1mhxjvyhzf1kqznimdb2f96czp76k8riv8yda87gfrk7cmfn";
-    };
-    patches = [];
-  });
 in
 pkgs.stdenv.mkDerivation {
   name = "futhark";
@@ -37,7 +29,6 @@ pkgs.stdenv.mkDerivation {
     pkgs.python3Packages.sphinx
     pkgs.python3Packages.sphinxcontrib-bibtex
     pkgs.imagemagick # needed for literate tests
-    ispc
   ]
   ++ pkgs.lib.optionals (pkgs.stdenv.isLinux)
     [ pkgs.opencl-headers
