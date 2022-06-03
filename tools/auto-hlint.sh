7e67e600a5598ca7c59e2e3ab1d302b97f0ef947
Troels Henriksen <athas@sigkill.dk>
2022-06-03 08:42:53 +0200

    Add handy script.

---
+++ tools/auto-hlint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Automatically apply hlint rules to the given file(s) and directories.
+#
+# Note: you may need to run this tool to a fixed point manually, as
+# some rewrites only do partial improvements, and the resulting code
+# may still violate other rules.
+
+for d in $*; do
+    find $d -name \*.hs | parallel hlint --refactor --refactor-options=-i
+done
