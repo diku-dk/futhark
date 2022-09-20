2b8323cbb352190be66f8238a098e6fd74b68c9d
Troels Henriksen <athas@sigkill.dk>
2022-09-20 11:36:38 +0200

    Restore support for GHC 9.0.
    This is probably better while Nixpkgs is still stuck on an old version
    of GHC.

--- default.nix
+++ default.nix
@@ -23,7 +23,7 @@
 let
   config = {
     packageOverrides = pkgs: rec {
-      haskellPackages = pkgs.haskell.packages.ghc924.override {
+      haskellPackages = pkgs.haskellPackages.override {
         overrides = haskellPackagesNew: haskellPackagesOld: rec {
           futhark-data =
             haskellPackagesNew.callPackage ./nix/futhark-data.nix { };
