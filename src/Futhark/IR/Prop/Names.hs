50a3acffa110c582c3a6f32f3e1fc147a2888c8a
Troels Henriksen <athas@sigkill.dk>
2022-10-19 11:59:33 +0200

    Add FreeIn instance for Set.

--- src/Futhark/IR/Prop/Names.hs
+++ src/Futhark/IR/Prop/Names.hs
@@ -47,6 +47,7 @@ import Data.Foldable
 import Data.IntMap.Strict qualified as IM
 import Data.IntSet qualified as IS
 import Data.Map.Strict qualified as M
+import Data.Set qualified as S
 import Futhark.IR.Prop.Patterns
 import Futhark.IR.Prop.Scope
 import Futhark.IR.Syntax
@@ -213,6 +214,9 @@ instance (FreeIn a, FreeIn b, FreeIn c, FreeIn d) => FreeIn (a, b, c, d) where
   freeIn' (a, b, c, d) = freeIn' a <> freeIn' b <> freeIn' c <> freeIn' d
 
 instance FreeIn a => FreeIn [a] where
+  freeIn' = foldMap freeIn'
+
+instance FreeIn a => FreeIn (S.Set a) where
   freeIn' = foldMap freeIn'
 
 instance
