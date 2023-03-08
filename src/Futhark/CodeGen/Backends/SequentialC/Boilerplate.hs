232942df84790fef421282b1f324d9d92863269f
Troels Henriksen <athas@sigkill.dk>
2023-03-08 16:24:39 +0100

    Generate tuning param definitions in GenericC.
    This is a step towards #1884.  Now that GenericC is responsible for
    all the work (and has all the information), it can generate new API
    functions.

--- src/Futhark/CodeGen/Backends/SequentialC/Boilerplate.hs
+++ src/Futhark/CodeGen/Backends/SequentialC/Boilerplate.hs
@@ -11,11 +11,6 @@ import Language.C.Quote.OpenCL qualified as C
 -- | Generate the necessary boilerplate.
 generateBoilerplate :: GC.CompilerM op s ()
 generateBoilerplate = do
-  GC.earlyDecl [C.cedecl|static const int num_tuning_params = 0;|]
-  GC.earlyDecl [C.cedecl|static const char *tuning_param_names[1];|]
-  GC.earlyDecl [C.cedecl|static const char *tuning_param_vars[1];|]
-  GC.earlyDecl [C.cedecl|static const char *tuning_param_classes[1];|]
-  GC.earlyDecl [C.cedecl|static typename int64_t *tuning_param_defaults[1];|]
   GC.earlyDecl [C.cedecl|$esc:(T.unpack backendsCH)|]
   GC.generateProgramStruct
 {-# NOINLINE generateBoilerplate #-}
