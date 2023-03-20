e02ac6a4982e46f77788d6d0df961a4af75b1421
catvayor <lubin.bailly@ens.psl.eu>
2023-03-20 11:00:43 +0100

    hlint hint make error

--- src/Language/Futhark/Prop.hs
+++ src/Language/Futhark/Prop.hs
@@ -734,12 +734,8 @@ intrinsics =
                               ( BinOp
                                   (findOp "*", mempty)
                                   sizeBinOpInfo
-                                  (Var (qualName n) (Info i64) mempty),
-                                Info
-                                  (i64, Nothing)
-                                  (Var (qualName m) (Info i64) mempty),
-                                Info
-                                  (i64, Nothing)
+                                  ((Var (qualName n) (Info i64) mempty), Info (i64, Nothing))
+                                  ((Var (qualName m) (Info i64) mempty), Info (i64, Nothing))
                                   mempty
                               )
                             $ Info
@@ -772,12 +768,8 @@ intrinsics =
                           ( BinOp
                               (findOp "+", mempty)
                               sizeBinOpInfo
-                              (Var (qualName n) (Info i64) mempty),
-                            Info
-                              (i64, Nothing)
-                              (Var (qualName m) (Info i64) mempty),
-                            Info
-                              (i64, Nothing)
+                              ((Var (qualName n) (Info i64) mempty), Info (i64, Nothing))
+                              ((Var (qualName m) (Info i64) mempty), Info (i64, Nothing))
                               mempty
                           )
                         $ Info
