61aa9fe459adc36ff42aa123eb1fcf66d18cab12
Troels Henriksen <athas@sigkill.dk>
2023-05-28 09:48:32 +0200

    Fix #1952.

--- src/Futhark/Optimise/Simplify/Rules.hs
+++ src/Futhark/Optimise/Simplify/Rules.hs
@@ -127,13 +127,14 @@ emptyArrayToScratch _ _ = Skip
 
 simplifyIndex :: BuilderOps rep => BottomUpRuleBasicOp rep
 simplifyIndex (vtable, used) pat@(Pat [pe]) (StmAux cs attrs _) (Index idd inds)
-  | Just m <- simplifyIndexing vtable seType idd inds consumed = Simplify $ do
-      res <- certifying cs m
-      attributing attrs $ case res of
-        SubExpResult cs' se ->
-          certifying cs' $ letBindNames (patNames pat) $ BasicOp $ SubExp se
-        IndexResult extra_cs idd' inds' ->
-          certifying extra_cs $ letBindNames (patNames pat) $ BasicOp $ Index idd' inds'
+  | Just m <- simplifyIndexing vtable seType idd inds consumed =
+      Simplify $ certifying cs $ do
+        res <- m
+        attributing attrs $ case res of
+          SubExpResult cs' se ->
+            certifying cs' $ letBindNames (patNames pat) $ BasicOp $ SubExp se
+          IndexResult extra_cs idd' inds' ->
+            certifying extra_cs $ letBindNames (patNames pat) $ BasicOp $ Index idd' inds'
   where
     consumed = patElemName pe `UT.isConsumed` used
     seType (Var v) = ST.lookupType v vtable
