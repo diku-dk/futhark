830b0694339e4360e6bf68333588bb4ca115adb0
Troels Henriksen <athas@sigkill.dk>
2023-07-10 15:45:21 +0200

    Need this a little while longer to infer 'contiguous' metadata.
    Revert "Remove simplifyPatFromExpS machinery. (#1985)"
    
    This reverts commit d5b2ac80a6692952c5f99ecfa99befdafc5ece1e.

--- src/Futhark/Pass/ExplicitAllocations.hs
+++ src/Futhark/Pass/ExplicitAllocations.hs
@@ -1135,7 +1135,7 @@ simplifiable ::
   ) ->
   SimpleOps rep
 simplifiable innerUsage simplifyInnerOp =
-  SimpleOps mkExpDecS' mkBodyS' protectOp opUsage (simplifyMemOp simplifyInnerOp)
+  SimpleOps mkExpDecS' mkBodyS' protectOp opUsage simplifyPat (simplifyMemOp simplifyInnerOp)
   where
     mkExpDecS' _ pat e =
       pure $ Engine.mkWiseExpDec pat () e
@@ -1158,6 +1158,26 @@ simplifiable innerUsage simplifyInnerOp =
       mempty
     opUsage (Inner inner) =
       innerUsage inner
+
+    simplifyPat (Pat pes) e = do
+      rets <- fromMaybe (error "simplifyPat: ill-typed") <$> expReturns e
+      Pat <$> zipWithM update pes rets
+      where
+        names = map patElemName pes
+        update
+          (PatElem pe_v (MemArray pt shape u (ArrayIn mem _)))
+          (MemArray _ _ _ (Just (ReturnsInBlock _ ixfun)))
+            | Just ixfun' <- traverse (traverse inst) ixfun =
+                PatElem pe_v
+                  <$> ( MemArray pt
+                          <$> Engine.simplify shape
+                          <*> pure u
+                          <*> (ArrayIn <$> Engine.simplify mem <*> pure ixfun')
+                      )
+            where
+              inst (Ext i) = maybeNth i names
+              inst (Free v) = Just v
+        update pe _ = traverse Engine.simplify pe
 
 data ExpHint
   = NoHint
