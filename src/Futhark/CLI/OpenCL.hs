fadb6fec890d00670afa46fde69061bfc62f3fac
Troels Henriksen <athas@sigkill.dk>
2023-07-13 13:30:37 +0200

    Rename pipelines to match representations.

--- src/Futhark/CLI/OpenCL.hs
+++ src/Futhark/CLI/OpenCL.hs
@@ -3,7 +3,7 @@ module Futhark.CLI.OpenCL (main) where
 
 import Futhark.Actions (compileOpenCLAction)
 import Futhark.Compiler.CLI
-import Futhark.Passes (gpuPipeline)
+import Futhark.Passes (gpumemPipeline)
 
 -- | Run @futhark opencl@
 main :: String -> [String] -> IO ()
@@ -12,6 +12,6 @@ main = compilerMain
   []
   "Compile OpenCL"
   "Generate OpenCL/C code from optimised Futhark program."
-  gpuPipeline
+  gpumemPipeline
   $ \fcfg () mode outpath prog ->
     actionProcedure (compileOpenCLAction fcfg mode outpath) prog
