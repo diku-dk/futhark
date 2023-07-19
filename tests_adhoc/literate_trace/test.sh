34ba1800d211d67eb37721213f146d4b11767df8
Troels Henriksen <athas@sigkill.dk>
2023-07-19 16:24:10 +0200

    Off-by-1.

--- tests_adhoc/literate_trace/test.sh
+++ tests_adhoc/literate_trace/test.sh
@@ -3,4 +3,4 @@
 set -e
 
 rm -rf trace-img
-futhark literate trace.fut -v | tee | fgrep 'trace.fut:1:22-31: 1.000000 2.000000 3.000000'
+futhark literate trace.fut -v | tee | fgrep 'trace.fut:1:22-32: 1.000000 2.000000 3.000000'
