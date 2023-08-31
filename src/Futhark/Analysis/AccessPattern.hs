41ce9cc963e6095e54f997d3ab2bd4d2ad67b6e1
0scar <qgt268@alumni.ku.dk>
2023-08-31 21:44:25 +0200

    Fixup analyzeMemoryAccessPatterns signature

--- src/Futhark/Analysis/AccessPattern.hs
+++ src/Futhark/Analysis/AccessPattern.hs
@@ -1,22 +1,48 @@
-module Futhark.Analysis.AccessPattern ( analyseMemoryAccessPatterns, analyseMemoryAccessVariance )
+module Futhark.Analysis.AccessPattern
+  ( analyzeMemoryAccessPatterns
+  , MemoryAccessPattern
+  )
 where
 
+import Data.Map.Strict qualified as M
+import Futhark.IR.Syntax
 import Futhark.IR.GPU
+import Futhark.Tools
 
-data Pattern = Sequential
-             | Parallel
-             | Linear
-             | Scatter
-             | Random
+-- | Iteration type describes whether the index is iterated in a parallel or
+-- sequential way, ie. if the index expression comes from a sequential or
+-- parallel construct, like foldl or map.
+data IterationType =
+  Sequential | Parallel
+  deriving (Eq, Ord, Show)
+
+-- | Pattern determines what kind of physical pattern is used to index.
+-- This is subject to get removed.
+data Pattern = Linear | Random
+  deriving (Eq, Ord, Show)
+
+-- | Variance represents whether the index is variant, or invariant to the outer
+-- kernel/iteration function.
+data Variance = Variant | Invariant
+  deriving (Eq, Ord, Show)
+
+data MemoryAccessPattern =
+  MemoryAccessPattern
+    { iterationType :: IterationType
+    , pattern :: Pattern
+    , variance :: Variance
+    }
+
+data MemoryEntry = MemoryEntry
+    { idxExpr :: VName
+    , memAccessPatterns :: [MemoryAccessPattern]
+    }
+
+-- Theres probably a more readable way of doing this.
+type ArrayIndexDescriptors = M.Map VName [ MemoryEntry ]
+  --deriving (Eq, Ord, Show)
 
 
-data Variance = Variant Pattern
-              | Invariant Pattern
-
-
-analyseMemoryAccessPatterns :: GPU -> GPU
-analyseMemoryAccessPatterns = id
-
-
-analyseMemoryAccessVariance :: GPU -> GPU
-analyseMemoryAccessVariance = id
+analyzeMemoryAccessPatterns :: Prog GPU -> ArrayIndexDescriptors
+--analyzeMemoryAccessPatterns (Prog {}) = []
+analyzeMemoryAccessPatterns _ = M.empty
