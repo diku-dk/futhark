c9f03c1f9f091124c1a60332cfb5eac65f84242e
Bjarke <bjarke137@gmail.com>
2023-09-04 10:06:38 +0200

    Added temporary code to the pass to make the compiler happy

--- src/Futhark/Pass/CoalesceAccess.hs
+++ src/Futhark/Pass/CoalesceAccess.hs
@@ -41,3 +41,36 @@ analysis :: Prog GPU -> Prog GPU
 analysis prog = do
   let funaids = analyzeMemoryAccessPatterns prog
    in prog
+
+-- Code below is temporary and only to make the compiler happy.
+
+analyseBody :: Env -> Body GPU -> CoalesceM (Body GPU)
+analyseBody env (Body () stms res) =
+  Body () <$> analyseStms env stms <*> pure res
+
+analyseStms :: Env -> Stms GPU -> CoalesceM (Stms GPU)
+analyseStms env stms =
+  localScope (scopeOf stms) $ do
+    (_, stms') <- foldM foldfun (env, mempty) $ stmsToList stms
+    pure stms'
+  where
+    foldfun :: (Env, Stms GPU) -> Stm GPU -> CoalesceM (Env, Stms GPU)
+    foldfun (e, ss) s = do
+      (e', s') <- analyseStm_ e s
+      pure (e', ss <> s')
+
+analyseStm_ :: Env -> Stm GPU -> CoalesceM (Env, Stms GPU)
+-- analyseStm env stm@(Let _ _ (Op (SegOp (SegMap SegThread {} _ _ _)))) = do
+--   res_genred_opt <- genRedOpts env stm
+--   let stms' =
+--         case res_genred_opt of
+--           Just stms -> stms
+--           Nothing -> oneStm stm
+--   pure (env, stms')
+analyseStm_ env (Let pat aux e) = do
+  env' <- changeEnv env (head $ patNames pat) e
+  e' <- mapExpM (analyse env') e
+  -- pTrace "\nenv':\n" $ pTraceShow env' $ pTrace "\ne':\n" $ pTraceShow e' $
+  pure (env', oneStm $ Let pat aux e')
+  where
+    analyse env' = identityMapper {mapOnBody = \scope -> localScope scope . analyseBody env'}
