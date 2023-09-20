4d7619e70e987bc1e2374f607ef25705c3cb0992
Troels Henriksen <athas@sigkill.dk>
2023-09-20 21:31:34 +0200

    Start proper parser for format strings.

--- src/Futhark/Format.hs
+++ src/Futhark/Format.hs
@@ -1,19 +1,21 @@
 -- | Parsing of format strings.
 module Futhark.Format (parseFormatString) where
 
+import Data.Bifunctor
 import Data.Text qualified as T
+import Data.Void
+import Text.Megaparsec
 
-parseFormatString :: T.Text -> Either T.Text ([T.Text], [T.Text])
-parseFormatString printStr
-  | not balanced =
-      Left
-        "Invalid format string, possibly due to mismatched braces."
-  | otherwise = Right (strs, exps)
+pFormatString :: Parsec Void T.Text [Either T.Text T.Text]
+pFormatString =
+  many (choice [Left <$> pLiteral, Right <$> pInterpolation]) <* eof
   where
-    firstSplit = T.split (== '{') printStr
-    splits = map (T.split (== '}')) $ tail firstSplit
-    balanced =
-      (T.count "{" printStr == T.count "}" printStr)
-        && all (\x -> length x == 2) splits
-    strs = head firstSplit : map last splits
-    exps = map head splits
+    pInterpolation = "{" *> takeWhileP Nothing (`notElem` braces) <* "}"
+    pLiteral = takeWhile1P Nothing (`notElem` braces)
+    braces = "{}" :: String
+
+-- | The Lefts are pure text; the Rights are the contents of
+-- interpolations.
+parseFormatString :: T.Text -> Either T.Text [Either T.Text T.Text]
+parseFormatString =
+  first (T.pack . errorBundlePretty) . runParser pFormatString ""
