a84dd3352ec248c9ca752f538eeb4e359c5189f3
Bjarke <bjarke137@gmail.com>
2023-09-27 11:10:28 +0200

    Add CoalesceAccess boilerplate code

--- src/Futhark/Pass/CoalesceAccess.hs
+++ src/Futhark/Pass/CoalesceAccess.hs
@@ -7,8 +7,11 @@ import Control.Monad
 import Control.Monad.Reader
 import Control.Monad.State
 import Data.Map.Strict qualified as M
+import Data.Sequence.Internal qualified as S
 import Debug.Pretty.Simple
+import Debug.Trace
 import Futhark.Analysis.AccessPattern
+import Futhark.Analysis.Pretty
 import Futhark.Builder
 import Futhark.IR.GPU
 import Futhark.Optimise.TileLoops.Shared
@@ -31,49 +34,40 @@ coalesceAccess =
     "Transform kernel input arrays for better performance."
     -- \$ return
     $ \prog ->
-      let prog' = analysis prog
-       in intraproceduralTransformation onStms prog'
+      let analysisRes = analyzeDimIdxPats prog
+       in -- pTraceShow prog $ pTrace "\n------------------------\n" $ pTraceShow analysisRes $ pTrace "\n------------------------\n"
+          intraproceduralTransformation (onStms analysisRes) prog
   where
-    onStms scope stms =
-      modifyNameSource $
-        runState $
-          runReaderT (analyseStms (M.empty, M.empty) stms) scope
+    onStms analysisRes scope stms =
+      modifyNameSource $ runState $ runReaderT (transformStms analysisRes stms) scope
 
-analysis :: Prog GPU -> Prog GPU
-analysis prog = do
-  let funaids = analyzeDimIdxPats prog
-   in -- pTrace "\nprog:\n" $ pTraceShow prog $ pTrace "\nfunaids:\n" $ pTraceShow funaids
-      prog
+-- TODO: coalesce things here
+-- 1. Recurse through IR
+-- 2. For each segOp:
+--  1. For each array:
+--    1. Find optimal permutation
+--    2. If it doesn't already have optimal permutation, replace it with
+--       a manifest (not that simple tho! Figure out what to do with an
+--       array that should have multiple different manifestations)
 
--- Code below is temporary and only to make the compiler happy.
+type Ctx = ArrayIndexDescriptors
 
-analyseBody :: Env -> Body GPU -> CoalesceM (Body GPU)
-analyseBody env (Body () stms res) =
-  Body () <$> analyseStms env stms <*> pure res
-
-analyseStms :: Env -> Stms GPU -> CoalesceM (Stms GPU)
-analyseStms env stms =
-  localScope (scopeOf stms) $ do
-    (_, stms') <- foldM foldfun (env, mempty) $ stmsToList stms
-    pure stms'
+transformStms :: Ctx -> Stms GPU -> CoalesceM (Stms GPU)
+transformStms ctx stms = do
+  (_, stms') <- foldM foldfun (ctx, mempty) $ stmsToList stms
+  pure stms'
   where
-    foldfun :: (Env, Stms GPU) -> Stm GPU -> CoalesceM (Env, Stms GPU)
-    foldfun (e, ss) s = do
-      (e', s') <- analyseStm_ e s
-      pure (e', ss <> s')
+    foldfun :: (Ctx, Stms GPU) -> Stm GPU -> CoalesceM (Ctx, Stms GPU)
+    foldfun (c, ss) s = do
+      (c', s') <- transformStm c s
+      pure (c', ss <> s')
 
-analyseStm_ :: Env -> Stm GPU -> CoalesceM (Env, Stms GPU)
--- analyseStm env stm@(Let _ _ (Op (SegOp (SegMap SegThread {} _ _ _)))) = do
---   res_genred_opt <- genRedOpts env stm
---   let stms' =
---         case res_genred_opt of
---           Just stms -> stms
---           Nothing -> oneStm stm
---   pure (env, stms')
-analyseStm_ env (Let pat aux e) = do
-  env' <- changeEnv env (head $ patNames pat) e
-  e' <- mapExpM (analyse env') e
-  -- pTrace "\nenv':\n" $ pTraceShow env' $ pTrace "\ne':\n" $ pTraceShow e' $
-  pure (env', oneStm $ Let pat aux e')
-  where
-    analyse env' = identityMapper {mapOnBody = \scope -> localScope scope . analyseBody env'}
+transformStm :: Ctx -> Stm GPU -> CoalesceM (Ctx, Stms GPU)
+transformStm ctx stm@(Let pat aux (Op (SegOp op))) = do
+  let stm' = stm
+  let ctx' = ctx
+  pure (ctx', S.singleton stm')
+transformStm ctx stm@(Let pat aux e) = do
+  let stm' = stm
+  let ctx' = ctx
+  pure (ctx', S.singleton stm')
--- src/Futhark/Passes.hs
+++ src/Futhark/Passes.hs
@@ -95,7 +95,6 @@ gpuPipeline =
         -- tileLoops,
         simplifyGPU,
         histAccsGPU,
-        coalesceAccess,
         simplifyGPU,
         unstreamGPU,
         performCSE True,
@@ -107,7 +106,8 @@ gpuPipeline =
         mergeGPUBodies,
         simplifyGPU, -- Cleanup merged GPUBody kernels.
         sinkGPU, -- Sink reads within GPUBody kernels.
-        inPlaceLoweringGPU
+        inPlaceLoweringGPU,
+        coalesceAccess
       ]
 
 -- | Extend gpuPipeline by pretty-printing the true AST
