1e6cfac99406727d4ed8574fc986abe612e2b586
CKuke <marslev.c@gmail.com>
2023-10-05 09:40:11 +0200

    intra seq

--- src/Futhark/Optimise/IntraSeq.hs
+++ src/Futhark/Optimise/IntraSeq.hs
@@ -2,16 +2,10 @@ module Futhark.Optimise.IntraSeq (intraSeq) where
 
 import Futhark.Pass
 import Futhark.IR.GPU
-import Futhark.IR.Syntax
 import Futhark.Builder.Class
-import Futhark.FreshNames
-import Futhark.MonadFreshNames
 
-import Control.Monad.Identity
-import Control.Monad.State (runState, State)
 import Control.Monad.Reader
-import qualified Data.Map.Strict as M
-
+import Control.Monad.State
 -- mkLet [Ident (VName (nameFromString "abc") 0) (Prim $ IntType Int32 )] 
 --                              (BasicOp $ UnOp (Complement Int32) (Constant $ blankPrimValue (IntType Int32)))
 
@@ -46,6 +40,7 @@ import qualified Data.Map.Strict as M
 
 type SeqM = ReaderT (Scope GPU) (State VNameSource)
 
+
 intraSeq :: Pass GPU GPU
 intraSeq =
     Pass "name" "description" $
@@ -56,18 +51,98 @@ intraSeq =
           runState $
             runReaderT (seqStms stms) scope
 
+-- seqStms :: Stms GPU -> SeqM (Stms GPU)
+-- seqStms stms =
+--   localScope (scopeOf stms) $ do
+--     foldM foldfun mempty $ stmsToList stms
+  -- where
+--     foldfun :: Stms GPU -> Stm GPU -> SeqM (Stms GPU)
+--     foldfun ss s = do
+--       s' <- seqStm s 
+--       pure $ ss <> s'
+
+-- seqStm :: Stm GPU -> SeqM (Stms GPU)
+-- seqStm stm@(Let pat aux (Op (SegOp (SegMap lvl@SegGroup {} space ts kbody)))) = do
+
+
+-- segStm stm = undefined
+
+
 seqStms :: Stms GPU -> SeqM (Stms GPU)
-seqStms stms =
-  localScope (scopeOf stms) $ do
-    foldM foldfun mempty $ stmsToList stms
-  where
-    foldfun :: Stms GPU -> Stm GPU -> SeqM (Stms GPU)
-    foldfun ss s = do
-      s' <- seqStm s 
-      pure $ ss <> s'
+seqStms stms = do
+  stmsMapped <- mapM seqStm $ stmsToList stms
+  pure $ stmsFromList stmsMapped
 
-seqStm :: Stm GPU -> SeqM (Stms GPU)
-seqStm stm@(Let pat aux (Op (SegOp (SegMap lvl@SegGroup {} space ts kbody)))) = do
-  
+-- We need to somehow now in which segmented operation we are.
+-- e.g. when in a SegRed we should produce sequential reduce operations over
+-- a chunk using the same operator
 
-segStm stm = undefined
+seqStm :: Stm GPU -> SeqM (Stm GPU)
+-- seqStm (Let pat aux (Op (SegOp segop))) =
+--   case segLevel segop of
+--     (SegGroup _ _)  -> do
+--       kbody' <- seqBody $ segBody segop
+--       pure $ case segop of
+--               SegMap  lvl space       ts _ -> Let pat aux $ Op $ SegOp $ SegMap lvl space ts kbody'
+--               SegRed  lvl space binop ts _ -> Let pat aux $ Op $ SegOp $ SegRed lvl space binop ts kbody'
+--               SegScan lvl space binop ts _ -> Let pat aux $ Op $ SegOp $ SegScan lvl space binop ts kbody'
+--               SegHist lvl space binop ts _ -> Let pat aux $ Op $ SegOp $ SegHist lvl space binop ts kbody'
+--     (SegThread _ _) ->
+--       case segop of
+--         SegMap {} -> undefined
+--         SegRed {} -> undefined
+--         SegScan {} -> undefined
+--         SegHist {} -> undefined
+--     _ -> undefined
+
+seqStm (Let pat aux (Op (SegOp (SegMap lvl space ts kbody)))) =
+  case lvl of
+    SegGroup {} -> do
+      kbody' <- seqBody kbody
+      pure $ Let pat aux (Op (SegOp (SegMap lvl space ts kbody')))
+    SegThread {} -> undefined
+    _ -> undefined
+
+seqStm (Let pat aux (Op (SegOp (SegRed lvl space binops ts kbody)))) =
+  case lvl of
+    SegGroup {} -> do
+      kbody' <- seqBody kbody
+      pure $ Let pat aux (Op (SegOp (SegRed lvl space binops ts kbody')))
+    SegThread {} -> undefined
+    _ -> undefined
+
+seqStm (Let pat aux (Op (SegOp (SegScan lvl space binops ts kbody)))) =
+  case lvl of
+    SegGroup {} -> do
+      kbody' <- seqBody kbody
+      pure $ Let pat aux (Op (SegOp (SegScan lvl space binops ts kbody')))
+    SegThread {} -> undefined
+    _ -> undefined
+    
+seqStm (Let pat aux (Op (SegOp (SegHist lvl space binops ts kbody)))) =
+  case lvl of
+    SegGroup {} -> do
+      kbody' <- seqBody kbody
+      pure $ Let pat aux (Op (SegOp (SegHist lvl space binops ts kbody')))
+    SegThread {} -> undefined
+    _ -> undefined
+
+    
+seqStm (Let pat aux (Op op)) = undefined
+
+-- The catch all base case for now
+seqStm _ = undefined
+
+
+
+
+-- 
+seqBody :: KernelBody GPU -> SeqM (KernelBody GPU)
+seqBody (KernelBody dec stms result) = do
+  stms' <- seqStms stms
+  pure $ KernelBody dec stms' result
+
+
+-- Helper functions --
+
+-- segOpType :: SegOp GPU -> 
