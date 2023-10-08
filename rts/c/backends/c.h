6537a57f12fd5fd76a1deeaa7cc79e5124513343
Troels Henriksen <athas@sigkill.dk>
2023-10-08 21:25:22 +0200

    This should not be const.

--- rts/c/backends/c.h
+++ rts/c/backends/c.h
@@ -5,7 +5,7 @@ struct futhark_context_config {
   int debugging;
   int profiling;
   int logging;
-  const char *cache_fname;
+  char *cache_fname;
   int num_tuning_params;
   int64_t *tuning_params;
   const char** tuning_param_names;
