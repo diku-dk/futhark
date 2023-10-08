2c97436fdaec8c4ab0acdd4f53bb6d80b021649c
Troels Henriksen <athas@sigkill.dk>
2023-10-08 23:39:46 +0200

    Python backend: use more numpy and less ctypes.
    This is easier to understand and maintain.

--- src/Futhark/CodeGen/Backends/GenericPython/AST.hs
+++ src/Futhark/CodeGen/Backends/GenericPython/AST.hs
@@ -38,7 +38,6 @@ data PyExp
   | Cond PyExp PyExp PyExp
   | Index PyExp PyIdx
   | Call PyExp [PyArg]
-  | Cast PyExp String
   | Tuple [PyExp]
   | List [PyExp]
   | Field PyExp String
@@ -112,12 +111,6 @@ instance Pretty PyExp where
   pretty (BinOp s e1 e2) = parens (pretty e1 <+> pretty s <+> pretty e2)
   pretty (UnOp s e) = pretty s <> parens (pretty e)
   pretty (Cond e1 e2 e3) = pretty e2 <+> "if" <+> pretty e1 <+> "else" <+> pretty e3
-  pretty (Cast src bt) =
-    "ct.cast"
-      <> parens
-        ( pretty src <> ","
-            <+> "ct.POINTER" <> parens (pretty bt)
-        )
   pretty (Index src idx) = pretty src <> brackets (pretty idx)
   pretty (Call fun exps) = pretty fun <> parens (commasep $ map pretty exps)
   pretty (Tuple [dim]) = parens (pretty dim <> ",")
