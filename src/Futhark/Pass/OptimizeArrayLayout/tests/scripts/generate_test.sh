2bc05a704ce7cad0fda6137730e26b4b4c621afc
Bjarke <bjarke137@gmail.com>
2023-11-09 11:22:43 +0100

    Update tests

--- src/Futhark/Pass/OptimizeArrayLayout/tests/scripts/generate_test.sh
+++ src/Futhark/Pass/OptimizeArrayLayout/tests/scripts/generate_test.sh
@@ -4,7 +4,9 @@
 # expected output by running the pass on the generated IR.
 # NOTE: this will overwrite the existing test files, and make the tests
 # trivially pass. To generate the tests, the pass should be disabled in
-# the pippeline.
+# the pipeline.
+
+overwrite=false
 
 dir0="/Users/bak/Desktop/Speciale/futhark/tests_analysis"
 dir1="/Users/bak/Desktop/Speciale/futhark/src/Futhark/Pass/OptimizeArrayLayout/tests"
@@ -12,12 +14,13 @@ dir1="/Users/bak/Desktop/Speciale/futhark/src/Futhark/Pass/OptimizeArrayLayout/t
 # Get path of the latest build of the futhark executable
 futhark_dev=$(ls -tr $(find . -name futhark -type f ) | tail -1)
 
-# # loop counter to break after first iteration
-# i=0
+# loop counter to break after first iteration
+i=0
 
-for file in "$dir0"/*.fut
+for file in "$dir0"/*.fut_gpu
 do
-    # i=$((i+1))
+
+    i=$((i+1))
     # if [ $i -eq 1 ]
     # then
     #     continue
@@ -27,15 +30,22 @@ do
     #     break
     # fi
 
+    # Check if the file already has a test file
+    if [ -f "$dir1/$(basename -- "$file")" ] && [ "$overwrite" = false ]; then
+        printf "Test file already exists for $file. Skipping.\n"
+        continue
+    fi
+    printf "Generating test for $file\n"
+
     # Get result
-    res=$($futhark_dev dev -se --gpu $file)
+    res=$($futhark_dev dev $file)
     # Pipe result into file
-    new_file="$dir1/$(basename -- "$file")_gpu"
+    new_file="$dir1/$(basename -- "$file")"
     echo "$res" > "$new_file"
 
-    # # Append the following to the file
+    # Append the following to the file
     string="\n\n-- === Expected output after pass:\n"
-    printf "$string" >> "$dir1/$(basename -- "$file")_gpu"
+    printf "$string" >> "$dir1/$(basename -- "$file")"
 
     # Get expected output
     pass_res=$($futhark_dev dev --coalesce --simplify --cse $new_file)
