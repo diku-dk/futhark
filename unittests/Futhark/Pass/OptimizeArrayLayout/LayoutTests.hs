b0b70bc7aa7ef80363afd12159c2adbd78258099
0scar <qgt268@alumni.ku.dk>
2023-11-16 13:06:35 +0100

    Add test for const index elimination

--- unittests/Futhark/Pass/OptimizeArrayLayout/LayoutTests.hs
+++ unittests/Futhark/Pass/OptimizeArrayLayout/LayoutTests.hs
@@ -1,8 +1,10 @@
 module Futhark.Pass.OptimizeArrayLayout.LayoutTests (tests) where
 
 import Data.IntMap.Strict qualified as S
+import Data.Map.Strict qualified as M
 import Futhark.Analysis.AccessPattern
 import Futhark.FreshNames
+import Futhark.IR.GPU (GPU)
 import Futhark.Pass.OptimizeArrayLayout.Layout
 import Language.Futhark.Core
 import Test.Tasty
@@ -18,7 +20,7 @@ commonPermutationEliminatorsTests :: TestTree
 commonPermutationEliminatorsTests =
   testGroup
     "commonPermutationEliminators"
-    [permutationTests, nestTests, dimAccessTests]
+    [permutationTests, nestTests, dimAccessTests, constInLastIndexElimTests]
 
 permutationTests :: TestTree
 permutationTests =
@@ -65,6 +67,54 @@ nestTests = testGroup "Nests" $
 
 dimAccessTests :: TestTree
 dimAccessTests = testGroup "DimAccesses" [] -- TODO: Write tests for the part of commonPermutationEliminators that checks the complexity of the DimAccesses.
+
+constInLastIndexElimTests :: TestTree
+constInLastIndexElimTests =
+  testGroup
+    "constantInLastIndexElimination"
+    [ testCase "gpu eliminates indexes with constant in last dim" $
+        permutationTableFromIndexTable accessTableGPU @?= mempty,
+      testCase "gpu ignores when not last" $
+        let res = permutationTableFromIndexTable accessTableGPUrev
+         in assertBool ("Expected not mempty, got " ++ show (M.toList res)) $
+              not $
+                null res
+    ]
+  where
+    accessTableGPU :: IndexTable GPU
+    accessTableGPU =
+      singleAccess
+        [ singleParAccess 0 0 $ VName "gtid" 4,
+          singleSeqAccess 1 1 $ VName "i" 5,
+          DimAccess mempty 2
+        ]
+
+    accessTableGPUrev :: IndexTable GPU
+    accessTableGPUrev =
+      singleAccess
+        [ singleParAccess 0 1 $ VName "gtid" 4,
+          singleParAccess 1 2 $ VName "gtid" 5,
+          DimAccess mempty 2,
+          singleSeqAccess 3 1 $ VName "i" 6
+        ]
+
+singleAccess :: [DimAccess rep] -> IndexTable rep
+singleAccess dims =
+  M.fromList
+    [ ( sgOp,
+        M.fromList
+          [ ( (VName "A" 2, []),
+              M.fromList
+                [ ( VName "a" 3,
+                    dims
+                  )
+                ]
+            )
+          ]
+      )
+    ]
+  where
+    sgOp = SegmentedMap {vnameFromSegOp = VName "mapres" 1}
 
 singleParAccess :: Int -> Int -> VName -> DimAccess rep
 singleParAccess origDim level name =
