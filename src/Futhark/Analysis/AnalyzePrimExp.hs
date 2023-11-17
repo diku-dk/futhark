ce52547e08139126e81a77c613e2500455830464
Bjarke <bjarke137@gmail.com>
2023-11-17 10:46:33 +0100

    Handle conversion to PrimExp for all SegOps

--- src/Futhark/Analysis/AnalyzePrimExp.hs
+++ src/Futhark/Analysis/AnalyzePrimExp.hs
@@ -26,7 +26,7 @@ class (Analyze rep) => PrimExpAnalysis rep where
   opPrimExp :: (RepTypes rep) => Scope rep -> Op rep -> State PrimExpTable ()
 
 primExpAnalysis :: (PrimExpAnalysis rep, RepTypes rep) => Prog rep -> PrimExpTable
-primExpAnalysis prog = foldMap' (uncurry funPrimExp) scopesAndFuns
+primExpAnalysis prog = foldMap' (uncurry funToPrimExp) scopesAndFuns
   where
     scopesAndFuns = do
       let funDefs = progFuns prog
@@ -38,58 +38,76 @@ primExpAnalysis prog = foldMap' (uncurry funPrimExp) scopesAndFuns
 -- TODO: document
 type PrimExpTable = M.Map VName (Maybe (PrimExp VName))
 
-funPrimExp :: (PrimExpAnalysis rep, RepTypes rep) => Scope rep -> FunDef rep -> PrimExpTable
-funPrimExp scope fundef = execState (bodyPrimExps scope (funDefBody fundef)) mempty
+funToPrimExp :: (PrimExpAnalysis rep, RepTypes rep) => Scope rep -> FunDef rep -> PrimExpTable
+funToPrimExp scope fundef = execState (bodyToPrimExps scope (funDefBody fundef)) mempty
 
-bodyPrimExps :: (PrimExpAnalysis rep, RepTypes rep) => Scope rep -> Body rep -> State PrimExpTable ()
-bodyPrimExps scope body = mapM_ (stmPrimExps scope') (bodyStms body)
+-- | Adds the statements of a body to the PrimExpTable
+bodyToPrimExps :: (PrimExpAnalysis rep, RepTypes rep) => Scope rep -> Body rep -> State PrimExpTable ()
+bodyToPrimExps scope body = mapM_ (stmToPrimExps scope') (bodyStms body)
   where
     scope' = scope <> scopeOf (bodyStms body)
 
-kernelBodyPrimExps :: (PrimExpAnalysis rep, RepTypes rep) => Scope rep -> KernelBody rep -> State PrimExpTable ()
-kernelBodyPrimExps scope kbody = mapM_ (stmPrimExps scope') (kernelBodyStms kbody)
+-- | Adds the statements of a kernel body to the PrimExpTable
+kernelToBodyPrimExps :: (PrimExpAnalysis rep, RepTypes rep) => Scope rep -> KernelBody rep -> State PrimExpTable ()
+kernelToBodyPrimExps scope kbody = mapM_ (stmToPrimExps scope') (kernelBodyStms kbody)
   where
     scope' = scope <> scopeOf (kernelBodyStms kbody)
 
-stmPrimExps :: forall rep. (PrimExpAnalysis rep, RepTypes rep) => Scope rep -> Stm rep -> State PrimExpTable ()
-stmPrimExps scope stm = do
+-- | Adds a statement to the PrimExpTable. If it can't be resolved as a `PrimExp`,
+-- it will be added as `Nothing`.
+stmToPrimExps :: forall rep. (PrimExpAnalysis rep, RepTypes rep) => Scope rep -> Stm rep -> State PrimExpTable ()
+stmToPrimExps scope stm = do
   primExpTable <- get
   case stm of
     (Let (Pat patElems) aux exp)
-      | Just primExp <- primExpFromExp (toPrimExp primExpTable) exp ->
+      | Just primExp <- primExpFromExp (toPrimExp scope primExpTable) exp ->
+          -- The statement can be resolved as a `PrimExp`.
           -- For each pattern element, insert the PrimExp in the primExpTable
           forM_ patElems $ \(PatElem name _) -> modify $ M.insert name (Just primExp)
       | otherwise -> do
-          walkExpM (walker patElems) (stmExp stm) -- Traverse the rest of the AST
-          primExpTable' <- get
+          -- The statement can't be resolved as a `PrimExp`.
+          walkExpM walker (stmExp stm) -- Traverse the rest of the AST
+          primExpTable' <- get -- Get the updated PrimExpTable after traversing the AST
           -- Add pattern elements that can't be resolved as `PrimExp` to the `PrimExpTable` as `Nothing`
           forM_ patElems $ \(PatElem name _) -> case M.lookup name primExpTable' of
             Nothing -> modify $ M.insert name Nothing
             _ -> pure ()
   where
-    toPrimExp :: PrimExpTable -> VName -> Maybe (PrimExp VName)
-    toPrimExp primExpTable name = case M.lookup name primExpTable of
-      Just maybePrimExp
-        | Just primExp <- maybePrimExp -> Just primExp -- Already in the table
-      _ -> case fmap typeOf . M.lookup name $ scope of
-        (Just (Prim pt)) -> Just $ LeafExp name pt
-        _ -> Nothing
-
-    walker patElems =
+    walker =
       (identityWalker @rep)
-        { walkOnBody = \body_scope -> bodyPrimExps (scope <> body_scope),
+        { walkOnBody = \body_scope -> bodyToPrimExps (scope <> body_scope),
           walkOnOp = opPrimExp scope
         }
 
-segOpPrimExpsGPU :: Scope GPU -> SegOp SegLevel GPU -> State PrimExpTable ()
-segOpPrimExpsGPU scope (SegMap lvl space ts body) = kernelBodyPrimExps scope body
-segOpPrimExpsGPU scope (SegRed lvl space reds ts lam) = undefined -- TODO: Handle this.
-segOpPrimExpsGPU scope (SegScan lvl space scans ts body) = kernelBodyPrimExps scope body
-segOpPrimExpsGPU scope (SegHist lvl space ops ts body) = kernelBodyPrimExps scope body
+-- | Checks if a name is in the PrimExpTable and construct a `PrimExp` if it is not
+toPrimExp :: (RepTypes rep) => Scope rep -> PrimExpTable -> VName -> Maybe (PrimExp VName)
+toPrimExp scope primExpTable name = case M.lookup name primExpTable of
+  Just maybePrimExp
+    | Just primExp <- maybePrimExp -> Just primExp -- Already in the table
+  _ -> case fmap typeOf . M.lookup name $ scope of
+    (Just (Prim pt)) -> Just $ LeafExp name pt
+    _ -> Nothing
+
+-- | Adds the parameters of a SegOp as well as the statements in its body
+-- to the PrimExpTable
+segOpToPrimExps :: (PrimExpAnalysis rep, RepTypes rep) => Scope rep -> SegOp SegLevel rep -> State PrimExpTable ()
+segOpToPrimExps scope op = do
+  segOpParamsToPrimExps scope op -- Add the parameters of the SegOp to the PrimExpTable
+  kernelToBodyPrimExps scope (segBody op) -- Recurse into the kernel body
+
+-- | Adds the parameters of a SegOp to the PrimExpTable
+segOpParamsToPrimExps :: (RepTypes rep) => Scope rep -> SegOp SegLevel rep -> State PrimExpTable ()
+segOpParamsToPrimExps scope op = do
+  primExpTable <- get
+  let params = unSegSpace $ segSpace op :: [(VName, SubExp)]
+  forM_ params $ \(name, subExpr) ->
+    case primExpFromSubExpM (toPrimExp scope primExpTable) subExpr of
+      Just primExp -> modify $ M.insert name (Just primExp)
+      Nothing -> pure ()
 
 instance PrimExpAnalysis GPU where
   opPrimExp scope gpuOp
-    | (SegOp op) <- gpuOp = segOpPrimExpsGPU scope op
+    | (SegOp op) <- gpuOp = segOpToPrimExps scope op
     | (SizeOp op) <- gpuOp = pure () -- TODO: Handle this differently?
     | (GPUBody ts body) <- gpuOp = undefined -- TODO: Handle this.
     | (Futhark.IR.GPUMem.OtherOp op) <- gpuOp = undefined -- TODO: Handle this.
