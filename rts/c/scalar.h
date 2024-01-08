b982722f6e61769fb39e52c653a09b909f14f812
Dorota <43449960+dcz-self@users.noreply.github.com>
2024-01-08 18:59:00 +0100

    Really don't talk about double unless needed (#2075)
    Co-authored-by: dcz <gilaac.dcz@porcupinefactory.org>

--- rts/c/scalar.h
+++ rts/c/scalar.h
@@ -18,7 +18,6 @@
 // macro FUTHARK_F64_ENABLED is set.
 
 SCALAR_FUN_ATTR int32_t futrts_to_bits32(float x);
-SCALAR_FUN_ATTR int64_t futrts_to_bits64(double x);
 SCALAR_FUN_ATTR float futrts_from_bits32(int32_t x);
 
 SCALAR_FUN_ATTR uint8_t add8(uint8_t x, uint8_t y) {
@@ -2305,6 +2304,7 @@ SCALAR_FUN_ATTR float fsignum32(float x) {
 #ifdef FUTHARK_F64_ENABLED
 
 SCALAR_FUN_ATTR double futrts_from_bits64(int64_t x);
+SCALAR_FUN_ATTR int64_t futrts_to_bits64(double x);
 
 #if ISPC
 SCALAR_FUN_ATTR bool futrts_isinf64(float x) {
