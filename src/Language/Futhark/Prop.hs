dae379d8f808a42565b8270b8c76f4f00c5b29c0
Troels Henriksen <athas@sigkill.dk>
2024-02-07 13:59:08 +0100

    arrayRank: make type more restricted.
    This is to prevent accidental misuse in the AUTOMAP work.

--- src/Language/Futhark/Prop.hs
+++ src/Language/Futhark/Prop.hs
@@ -153,7 +153,7 @@ defaultEntryPoint = nameFromString "main"
 -- | Return the dimensionality of a type.  For non-arrays, this is
 -- zero.  For a one-dimensional array it is one, for a two-dimensional
 -- it is two, and so forth.
-arrayRank :: TypeBase dim as -> Int
+arrayRank :: TypeBase () u -> Int
 arrayRank = shapeRank . arrayShape
 
 -- | Return the shape of a type - for non-arrays, this is 'mempty'.
