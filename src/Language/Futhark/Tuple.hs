519d3f603fada0ff31007e29d46b10a7fb69044b
Troels Henriksen <athas@sigkill.dk>
2024-03-10 10:55:24 +0100

    Tuple can also be empty.

--- src/Language/Futhark/Tuple.hs
+++ src/Language/Futhark/Tuple.hs
@@ -17,7 +17,7 @@ import Language.Futhark.Core (Name, nameFromString, nameToText)
 areTupleFields :: M.Map Name a -> Maybe [a]
 areTupleFields fs =
   let fs' = sortFields fs
-   in if length fs' > 1
+   in if (null fs || length fs' > 1)
         && and (zipWith (==) (map fst fs') tupleFieldNames)
         then Just $ map snd fs'
         else Nothing
