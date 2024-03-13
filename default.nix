a3da9e74f179d98c006fd3f53664887340098bf0
Troels Henriksen <athas@sigkill.dk>
2024-03-13 16:46:36 +0100

    Bump newer everything.

--- default.nix
+++ default.nix
@@ -34,6 +34,9 @@ let
           futhark-manifest =
             haskellPackagesNew.callPackage ./nix/futhark-manifest.nix { };
 
+          zlib =
+            haskellPackagesNew.callPackage ./nix/zlib.nix {zlib=pkgs.zlib;};
+
           futhark =
             # callCabal2Nix does not do a great job at determining
             # which files must be included as source, which causes
--- nix/futhark-data.nix
+++ nix/futhark-data.nix
@@ -5,8 +5,8 @@
 }:
 mkDerivation {
   pname = "futhark-data";
-  version = "1.1.0.0";
-  sha256 = "a71cf118f67be4afdf252dc59ee78e24e9805c704f4a100dc6d27440d98dc44b";
+  version = "1.1.0.1";
+  sha256 = "c396542118f2532e59d03e423d9f51087a838e4c0dfbb50b7b99091963bf36c4";
   libraryHaskellDepends = [
     base binary bytestring bytestring-to-vector containers half
     megaparsec mtl scientific text vector vector-binary-instances
