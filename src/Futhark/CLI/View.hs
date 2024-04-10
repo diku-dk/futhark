8b46d5b0b62d358ff1f99064459ca806f9d086b8
Nikolaj Hey Hinnerskov <nhin@912134.xyz>
2024-04-10 15:04:29 +0200

    Slightly nicer testing based on prettyString.

--- src/Futhark/CLI/View.hs
+++ src/Futhark/CLI/View.hs
@@ -3,7 +3,6 @@
 module Futhark.CLI.View (main) where
 
 import Control.Monad
-import Data.Functor.Identity
 import Control.Monad.IO.Class
 import Futhark.Analysis.View
 import Futhark.Compiler
@@ -11,11 +10,9 @@ import Futhark.Util.Options
 import Futhark.Util.Pretty (hPutDoc, putDoc, Pretty (pretty), prettyString)
 import Language.Futhark.Warnings
 import System.IO
-import Futhark.Analysis.View.Representation
 import qualified Data.Map as M
-import qualified Futhark.SoP.SoP as SoP
-import Language.Futhark (VName (VName), nameFromString, nameToString, baseString)
-import Data.List.NonEmpty (fromList)
+import Data.Char (isSpace)
+import Language.Futhark.Core (baseString)
 
 -- import Futhark.Internalise.Defunctionalise as Defunctionalise
 -- import Futhark.Internalise.Defunctorise as Defunctorise
@@ -33,80 +30,41 @@ options :: [FunOptDescr RefineConfig]
 options =
   [ ]
 
-tests :: [(FilePath, View)]
+tests :: [(FilePath, [String])]
 tests =
-  [ ("tests/refinement/part2indices.fut",
-     View (Forall (nom "i") (Iota (Var (nom "n"))))
-          (Cases (fromList [
-            (Idx (Var (nom "conds")) (Var (nom "i")),
-             SoP (SoP.int2SoP (-1))
-               ~+~ Sum (Var (nom "j"))
-                       (SoP (SoP.int2SoP 0))
-                       (Var (nom "i"))
-                       (Indicator (Idx (Var (nom "conds")) (Var (nom "j"))))
-            ),
-            (Not (Idx (Var (nom "conds")) (Var (nom "i"))),
-               SoP (SoP.int2SoP (-1))
-                 ~+~ Sum (Var (nom "j"))
-                         (SoP (SoP.int2SoP 0))
-                         (Var (nom "n") ~-~ SoP (SoP.int2SoP 1))
-                         (Indicator (Idx (Var (nom "conds")) (Var (nom "j"))))
-                 ~+~ Sum (Var (nom "j"))
-                         (SoP (SoP.int2SoP 0))
-                         (Var (nom "i"))
-                         (Indicator (Not (Idx (Var (nom "conds")) (Var (nom "j")))))
-            )
-          ]))
-    )
+  [ ("tests/refinement/part2indices.fut", [
+      "∀i₆₁₇₆ ∈ iota n₆₀₆₈ .",
+      "    | (conds₆₀₇₀)[i₆₁₇₆] => -1 + Σj₆₁₇₂∈[0, ..., i₆₁₇₆] (⟦(conds₆₀₇₀)[j₆₁₇₂]⟧)",
+      "    | ¬((conds₆₀₇₀)[i₆₁₇₆]) => -1 + Σj₆₁₇₂∈[0, ..., -1 + n₆₀₆₈] (⟦(conds₆₀₇₀)[j₆₁₇₂]⟧) + Σj₆₁₇₄∈[0, ..., i₆₁₇₆] (⟦¬((conds₆₀₇₀)[j₆₁₇₄])⟧)"
+    ])
   ]
+
+-- Remove leading and trailing whitespace.
+trim :: String -> String
+trim = f . f
+   where f = reverse . dropWhile isSpace
+
+-- Trim whitespace and remove VName ids.
+strip :: String -> String
+strip = trim . filter stripId
   where
-    nom name = VName (nameFromString name) 0
-
-corruptId :: VName -> VName
-corruptId (VName vn _) = VName vn 1337
-
-corruptIds :: ASTMappable x => x -> x
-corruptIds x = do
-  runIdentity $ astMap substituter x
-  where
-    substituter =
-      ASTMapper
-        { mapOnExp = onExp }
-    onExp (Var vn) = pure $ Var (corruptId vn)
-    onExp e = astMap substituter e
-
-corruptDomIds :: Domain -> Domain
-corruptDomIds (Iota n) = Iota $ corruptIds n
-corruptDomIds (Range a b) = Range (corruptIds a) (corruptIds b)
-corruptDomIds (Union k m dom) = Union (corruptId k) (corruptIds m) (corruptDomIds dom)
-
--- Set all VName ids to the same value for janky equality.
-corruptView :: View -> View
-corruptView (View (Forall i dom) cases) =
-  corruptIds $ View (Forall i (corruptDomIds dom)) cases
-corruptView v = corruptIds v
-
+    nums :: [Char]
+    nums = "₀₁₂₃₄₅₆₇₈₉"
+    stripId c = c `notElem` nums
 
 -- | Run tests on all files in the "tests/refinement" directory.
 runAllTests :: IO ()
 runAllTests = do
-  -- files <- filter hasFutExt <$> listDirectory "tests/refinement"
-  -- let files' = filter hasFutExt files
   mapM_ runTest tests
-  -- where
-  --   hasFutExt fp = ".fut" == (reverse . take 4 . reverse $ fp)
 
 -- | Run test on a specific file.
-runTest :: (FilePath, View) -> IO ()
+runTest :: (FilePath, [String]) -> IO ()
 runTest (file, expected) = do
   putStrLn $ "Running test on file: " ++ file
   (_warnings, imports, src) <- readProgramOrDie file
   let res = M.mapKeys baseString $ mkViewProg src imports
   let actual = res M.! "inds"
-  putStrLn ("Actual:\n" <> prettyString (corruptView actual))
-  putStrLn ("Expected:\n" <> prettyString (corruptView expected))
-  putStrLn ("lol:\n" <> show (corruptView expected == corruptView actual))
-  if prettyString (corruptView expected) == prettyString (corruptView actual)
+  if map strip expected == map strip (lines (prettyString actual))
   then putStrLn $ "Test passed: " ++ file
   else error $ "Test failed: " ++ file
 
