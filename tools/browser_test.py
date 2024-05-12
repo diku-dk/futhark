3a72791035a0317465b5d40099030f0a254f939d
Sebastian Paarmann <git@s-paarmann.de>
2024-05-12 20:40:16 +0200

    Python formatting

--- tools/browser_test.py
+++ tools/browser_test.py
@@ -17,26 +17,47 @@ import numpy as np
 from selenium import webdriver
 
 parser = argparse.ArgumentParser()
-parser.add_argument("program",
-                    help="the program to wrap (without .js or other file extension)")
-parser.add_argument("--no-server-proxy",
-                    help=("only act as HTTP server, do not proxy Futhark server protocol\n"
-                          "(implies --no-browser)"),
-                    action="store_true")
-parser.add_argument("--no-browser",
-                    help=("do not start a browser, instead wait for one to connect.\n",
-                          "Can also be set via NO_BROWSER=1 env variable."),
-                    action="store_true")
-parser.add_argument("--show-browser",
-                    help=("disable headless mode for browser.\n"
-                          "Can also be set via HEADLESS=0 env variable."),
-                    action="store_true")
-parser.add_argument("--web-driver",
-                    help=("URL of a remote WebDriver to connec to.\n"
-                          "Can also be set via WEB_DRIVER_URL env variable."))
-parser.add_argument("--log",
-                    help=("log file for debug output.\n"
-                          "Can also be set via LOG_FILE env variable."))
+parser.add_argument(
+    "program", help="the program to wrap (without .js or other file extension)"
+)
+parser.add_argument(
+    "--no-server-proxy",
+    help=(
+        "only act as HTTP server, do not proxy Futhark server protocol\n"
+        "(implies --no-browser)"
+    ),
+    action="store_true",
+)
+parser.add_argument(
+    "--no-browser",
+    help=(
+        "do not start a browser, instead wait for one to connect.\n",
+        "Can also be set via NO_BROWSER=1 env variable.",
+    ),
+    action="store_true",
+)
+parser.add_argument(
+    "--show-browser",
+    help=(
+        "disable headless mode for browser.\n"
+        "Can also be set via HEADLESS=0 env variable."
+    ),
+    action="store_true",
+)
+parser.add_argument(
+    "--web-driver",
+    help=(
+        "URL of a remote WebDriver to connec to.\n"
+        "Can also be set via WEB_DRIVER_URL env variable."
+    ),
+)
+parser.add_argument(
+    "--log",
+    help=(
+        "log file for debug output.\n"
+        "Can also be set via LOG_FILE env variable."
+    ),
+)
 args = parser.parse_args()
 
 program_name = args.program
@@ -75,38 +96,43 @@ elif headless == "1":
 elif headless is None:
     headless = not args.show_browser
 
+
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, flush=True, **kwargs)
     if log_file is not None:
         print(*args, file=log_file, flush=True, **kwargs)
 
-index_page = (f"<!DOCTYPE html>\n"
-              f"<html>\n"
-              f"<head>\n"
-              f"   <title>futhark test</title>\n"
-              f"   <script src=\"{script_name}\"></script>\n"
-              f"</head>\n"
-              f"<body>\n"
-              f"   <h1>futhark test</h1>\n"
-              f"</body>\n"
-              f"</html>"
-             )
 
-default_headers = \
-    {
-        # This makes the site an "isolated context", most
-        # notably improving timing measurement resolution
-        # from 100 microseconds to 5 microseconds.
-        'Cross-Origin-Opener-Policy': 'same-origin',
-        'Cross-Origin-Embedder-Policy': 'require-corp'
-    }
+index_page = (
+    f"<!DOCTYPE html>\n"
+    f"<html>\n"
+    f"<head>\n"
+    f"   <title>futhark test</title>\n"
+    f'   <script src="{script_name}"></script>\n'
+    f"</head>\n"
+    f"<body>\n"
+    f"   <h1>futhark test</h1>\n"
+    f"</body>\n"
+    f"</html>"
+)
+
+default_headers = {
+    # This makes the site an "isolated context", most
+    # notably improving timing measurement resolution
+    # from 100 microseconds to 5 microseconds.
+    "Cross-Origin-Opener-Policy": "same-origin",
+    "Cross-Origin-Embedder-Policy": "require-corp",
+}
+
 
 async def handle_index(request):
-    return web.Response(text=index_page, content_type="text/html",
-                        headers=default_headers)
+    return web.Response(
+        text=index_page, content_type="text/html", headers=default_headers
+    )
+
 
 async def handle_file(request):
-    file = request.rel_url.path.lstrip('/')
+    file = request.rel_url.path.lstrip("/")
 
     content_type = "text/plain"
     if file.endswith(".js"):
@@ -120,8 +146,10 @@ async def handle_file(request):
     with open(file, "rb") as f:
         contents = f.read()
 
-    return web.Response(body=contents, content_type=content_type,
-                        headers=default_headers)
+    return web.Response(
+        body=contents, content_type=content_type, headers=default_headers
+    )
+
 
 def wrap_restore(args):
     fname = args[0]
@@ -129,22 +157,24 @@ def wrap_restore(args):
     with open(fname, "rb") as f:
         data = f.read()
 
-    data = base64.b64encode(data).decode('utf-8')
+    data = base64.b64encode(data).decode("utf-8")
 
     return [data] + args[1:]
 
+
 def wrap_store_resp(fname, resp):
-    data = base64.b64decode(resp['data'].encode('utf-8'))
+    data = base64.b64decode(resp["data"].encode("utf-8"))
     with open(fname, "wb") as f:
         f.write(data)
     return ""
+
 
 async def handle_ws(request):
     ws = web.WebSocketResponse()
     await ws.prepare(request)
 
-    toWS = request.app['toWS']
-    toStdIO = request.app['toStdIO']
+    toWS = request.app["toWS"]
+    toStdIO = request.app["toStdIO"]
 
     # Notify that we have a browser connected
     toStdIO.put_nowait("connected")
@@ -161,7 +191,7 @@ async def handle_ws(request):
         elif cmd == "store":
             args = args[1:]
 
-        await ws.send_json({ "cmd": cmd, "args": args })
+        await ws.send_json({"cmd": cmd, "args": args})
         msg = await ws.receive()
 
         if msg.type == aiohttp.WSMsgType.ERROR:
@@ -172,18 +202,19 @@ async def handle_ws(request):
         eprint("Got response:", resp)
 
         text = ""
-        if cmd == "store" and resp['status'] == 'ok':
+        if cmd == "store" and resp["status"] == "ok":
             text = wrap_store_resp(orig_args[0], resp)
         else:
-            text = resp['text']
+            text = resp["text"]
 
-        toStdIO.put_nowait((resp['status'], text))
+        toStdIO.put_nowait((resp["status"], text))
 
     if not ws.closed:
         await ws.close()
 
     eprint("WS connection closed, stopping server")
-    app['stop'].set()
+    app["stop"].set()
+
 
 def start_browser():
     options = webdriver.ChromeOptions()
@@ -192,7 +223,9 @@ def start_browser():
         options.add_argument("--headless=new")
 
     if remote_driver_url is not None:
-        driver = webdriver.Remote(command_executor=remote_driver_url, options=options)
+        driver = webdriver.Remote(
+            command_executor=remote_driver_url, options=options
+        )
     else:
         # Need these extra options when running properly on Linux, but
         # specifying them on Windows is not allowed. For now, just assume a
@@ -210,31 +243,32 @@ def start_browser():
 
         driver = webdriver.Chrome(options=options)
 
-
     loop = asyncio.get_running_loop()
     get_task = loop.run_in_executor(None, driver.get, "http://localhost:8100")
 
     return driver, get_task
 
+
 def stop_browser(driver):
     driver.quit()
 
-async def start_server(app, toWS, toStdIO):
-    app['toWS'] = toWS
-    app['toStdIO'] = toStdIO
 
-    app['stop'] = asyncio.Event()
+async def start_server(app, toWS, toStdIO):
+    app["toWS"] = toWS
+    app["toStdIO"] = toStdIO
+
+    app["stop"] = asyncio.Event()
 
     runner = web.AppRunner(app)
     await runner.setup()
-    site = web.TCPSite(runner, '0.0.0.0', 8100)
+    site = web.TCPSite(runner, "0.0.0.0", 8100)
 
     await site.start()
 
     if not no_browser:
         driver, get_task = start_browser()
 
-    await app['stop'].wait()
+    await app["stop"].wait()
     if not no_browser:
         await get_task
     await runner.cleanup()
@@ -242,19 +276,25 @@ async def start_server(app, toWS, toStdIO):
     if not no_browser:
         stop_browser(driver)
 
+
 app = web.Application()
 app.add_routes(
-    [web.get('/', handle_index),
-     web.get('/index.html', handle_index),
-     web.get(f'/{script_name}', handle_file),
-     web.get(f'/{wasm_name}', handle_file),
-     web.get(f'/{wasm_map_name}', handle_file),
-     web.get(f'/{source_name}', handle_file),
-     web.get('/ws', handle_ws)])
+    [
+        web.get("/", handle_index),
+        web.get("/index.html", handle_index),
+        web.get(f"/{script_name}", handle_file),
+        web.get(f"/{wasm_name}", handle_file),
+        web.get(f"/{wasm_map_name}", handle_file),
+        web.get(f"/{source_name}", handle_file),
+        web.get("/ws", handle_ws),
+    ]
+)
+
 
 async def read_stdin_line():
     loop = asyncio.get_event_loop()
     return await loop.run_in_executor(None, sys.stdin.readline)
+
 
 async def handle_stdio(toWS, toStdIO):
     # Wait for an initial signal that a web browser client has connected.
@@ -284,6 +324,7 @@ async def handle_stdio(toWS, toStdIO):
             print(text, flush=True)
             print("%%% OK", flush=True)
 
+
 async def main():
     toWS = asyncio.Queue()
     toStdIO = asyncio.Queue()
@@ -294,7 +335,8 @@ async def main():
     else:
         eprint(f"Wrapping {script_name}; proxying the Futhark server protocol")
         await asyncio.gather(
-                start_server(app, toWS, toStdIO),
-                handle_stdio(toWS, toStdIO))
-    
+            start_server(app, toWS, toStdIO), handle_stdio(toWS, toStdIO)
+        )
+
+
 asyncio.run(main())
