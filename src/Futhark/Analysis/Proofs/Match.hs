438e8eee74f8b3210624383cfea46f4cfaaf8065
Nikolaj Hey Hinnerskov <nhin@912134.xyz>
2024-08-22 13:22:27 +0200

    question is how to handle replace here

--- src/Futhark/Analysis/Proofs/Match.hs
+++ src/Futhark/Analysis/Proofs/Match.hs
@@ -7,42 +7,68 @@ where
 
 import Data.Map.Strict qualified as M
 import Data.Set qualified as S
-import Language.Futhark qualified as E
-import Futhark.SoP.SoP (SoP, sym2SoP, Substitute(..))
+import Futhark.SoP.SoP (SoP, Substitute(..))
 import Language.Futhark (VName)
+import Futhark.MonadFreshNames (VNameSource, MonadFreshNames (getNameSource), newNameFromString, putNameSource)
 
-class Ord a => FreeIn a where
-  freeIn :: a -> S.Set VName
+class Ord a => FreeVariables a where
+  fv :: a -> S.Set VName
+
+class Nameable u where
+  mkName :: VNameSource -> (u, VNameSource)
 
 class Renameable u where
-  -- Rename bound variables in u.
-  rename :: u -> u -- Implement subC(id,id,u) from Sieg and Kaufmann.
+  -- Implements subC(id,tau,e) from Sieg and Kaufmann where
+  -- tau is a renaming of bound variables. The context C is given by 
+  -- VNameSource in MonadFreshNames.
+  rename_ :: MonadFreshNames m => M.Map VName VName -> u -> m u
+  -- Rename bound variables in u. Equivalent to subC(id,id,e).
+  rename :: MonadFreshNames m => u -> m u
+  rename = rename_ mempty
+
+class Replaceable u where
+  -- Implements the replacement operation from Sieg and Kaufmann.
+  rep :: Substitution u -> u -> u
+
+instance Substitute a b (Constraint u) where
+  -- TODO implement rep from Sieg and Kaufmann.
+  -- replace _ _ = undefined
+  -- Lemma 1.1: subC(sigma,tau,e) = rep(sigma,subC(id,tau,e))
+  -- Then implement substitute as subC(sigma, id, e) using Lemma 1.1:
+  --   subC(sigma, id, e) = rep(sigma,subC(id,id,e)) = rep(sigma,rename(e))
+  substitute _ _ = undefined
 
 type Substitution u = M.Map VName (SoP u)
+-- data Substitution u v = Substitution
+--   { usub :: M.Map VName u,
+--     vsub :: M.Map VName v
+--   }
 
 data Constraint u = u := u
 infixr 4 :=
 
-instance Substitute a b (Constraint u) where
-  substitute _ _ = undefined
-
--- TODO derive MonadFail/Maybe rather than returning Maybes?
-class (Renameable u, FreeIn u) => Unify u v where
-  -- (One step of?) the unification algorithm from Sieg and Kauffmann,
+class (MonadFreshNames m, MonadFail m, Renameable u, FreeVariables u) => Unify u v m where
+  -- `unify_ k eq` is the unification algorithm from Sieg and Kauffmann,
   -- Unification for quantified formulae, 1993.
-  unify_ :: Int -> Constraint u -> Maybe (Substitution v)
-  unify :: Int -> u -> u -> Maybe (Substitution v)
-  unify k a b = unify_ k (rename a := rename b)
+  -- Check whether x is a bound variable by `x >= k`.
+  unify_ :: VName -> Constraint u -> m (Substitution v)
+  unify :: u -> u -> m (Substitution v)
+  unify e e' = do
+    -- Unification on {subC(id,id,e) ~= subC(id,id,e')}
+    --                  = {rename(e) ~= rename(e')}.
+    k <- newNameFromString "k"
+    c <- getNameSource
+    a <- rename e
+    putNameSource c
+    b <- rename e'
+    unify_ k (a := b)
 
-isBoundVar :: Int -> VName -> Bool
-isBoundVar k vn = E.baseTag vn >= k
-
-instance Ord u => FreeIn (SoP u) where
-  freeIn _ = undefined
+instance Ord u => FreeVariables (SoP u) where
+  fv _ = undefined
   
 instance Renameable (SoP u) where
-  rename _ = undefined
+  rename_ _tau _u = undefined
 
-instance Unify u u => Unify (SoP u) u where
+instance Unify u u m => Unify (SoP u) u m where
   unify_ _ (_sop1 := _sop2) = undefined
   -- Unify sop1 with k terms with all k-combinations of terms in sop2?
--- src/Futhark/Analysis/Proofs/Term.hs
+++ src/Futhark/Analysis/Proofs/Term.hs
@@ -3,9 +3,10 @@ where
 
 import Data.Set qualified as S
 import Data.Map.Strict qualified as M
-import Language.Futhark (VName)
-import Futhark.Analysis.Proofs.Match (FreeIn(freeIn), Renameable(rename), Unify(..), Constraint(..), isBoundVar)
+import Language.Futhark (VName (VName))
+import Futhark.Analysis.Proofs.Match (FreeVariables(fv), Renameable(rename_), Unify(..), Constraint(..), Nameable (..))
 import Futhark.SoP.SoP (SoP, sym2SoP, Substitute(..))
+import Futhark.MonadFreshNames
 
 data Term =
     Var VName
@@ -20,21 +21,38 @@ data Term =
   | Recurrence
   deriving (Show, Eq, Ord)
 
-instance FreeIn Term where
-  freeIn (Var vn) = S.singleton vn
-  freeIn (Sum _ lb ub e) = freeIn lb <> freeIn ub <> freeIn e
-  freeIn (Idx xs i) = freeIn xs <> freeIn i
-  freeIn Recurrence = mempty
+instance FreeVariables Term where
+  fv (Var vn) = S.singleton vn
+  fv (Sum i lb ub e) = fv lb <> fv ub <> fv e S.\\ S.singleton i
+  fv (Idx xs i) = fv xs <> fv i
+  fv Recurrence = mempty
 
 instance Substitute VName (SoP Term) Term where
-  substitute _ _ = undefined
+  substitute _ = undefined
+
+instance Nameable Term where
+  mkName (VNameSource i) = (Var $ VName "x" i, VNameSource $ i + 1)
 
 instance Renameable Term where
-  rename _ = undefined
+  rename_ tau (Var x) =
+    pure $ Var $ M.findWithDefault x x tau
+  rename_ tau (Idx xs i) =
+    Idx <$> rename_ tau xs <*> rename_ tau i
+  rename_ tau (Sum xn lb ub e) = do
+    xm <- newNameFromString "i"
+    let tau' = M.insert xn xm tau
+    Sum xm <$> rename_ tau' lb <*> rename_ tau' ub <*> rename_ tau e
+  rename_ _ Recurrence =
+    pure Recurrence
 
-instance Unify Term Term where
-  unify_ _ (e1 := e2) | e1 == e2 =
-    Just mempty
+-- NOTE 2.b.iii says "if x occurs in some other equation",
+-- but we don't have access to other equations here.
+-- I reckon, we can always just do this substitution
+-- at the call-site regardless.
+-- Further, they keep (Var x := t) in the equations, but
+-- that's relegated to the substitution here.
+-- NOTE 3.a irrelevant here given that we are post type checking?
+instance (MonadFreshNames m, MonadFail m) => Unify Term Term m where
   -- 1. Exchange.
   unify_ k (t := Var x) | not (isVar t) =
     unify_ k (Var x := t)
@@ -43,32 +61,26 @@ instance Unify Term Term where
       isVar _ = False
   -- 2.a. Equation elimination.
   unify_ _ (Var x := t) | Var x == t =
-      Just mempty
+      pure mempty
   -- 2.b.
-  unify_ k (Var x := t) | Var x /= t && isBoundVar k x =
-      fail "2.b.i"
+  unify_ k (Var x := t) | Var x /= t && x >= k =
+    fail "2.b.i"
   unify_ k (Var x := t)
     | Var x /= t =
-    let fvs = freeIn t
-    in if x `S.member` fvs || any (isBoundVar k) fvs then fail "2.b.ii"
-       else Just $ M.singleton x (sym2SoP t) -- 2.b.iii. Variable elimination.
-       -- NOTE 2.b.iii says "if x occurs in some other equation",
-       -- but we don't have access to other equations here.
-       -- I reckon, we can always just do this substitution
-       -- at the call-site regardless.
-       -- Further, they keep (Var x := t) in the equations, but
-       -- that's relegated to the substitution here.
-  -- 3.a irrelevant here given that we are post type checking?
+    case fv t of
+      fvs | x `S.member` fvs || any (>= k) fvs -> fail "2.b.ii"
+      -- 2.b.iii. Variable elimination. 
+      _ -> pure $ M.singleton x (sym2SoP t)
   -- 3.b
   unify_ k (Sum _ a1 b1 e1 := Sum _ a2 b2 e2) = do
-    -- foldM (\s c -> do
-    --          s' <- unify_ k (substitute s c)
-    --          Just $ s <> s'
-    --       ) mempty [a1 := a2, b1 := b2]
-    sub1 <- unify_ k (a1 := a2)
-    sub2 <- unify_ k (substitute sub1 (b1 := b2))
-    sub3 <- unify_ k (substitute (sub1 <> sub2) (e1 := e2))
-    Just $ mconcat [sub1, sub2, sub3]
-  unify_ _ (Idx xs i := Idx ys j) =
-    undefined
-  unify_ _ _ = undefined
+    -- TODO unsure if substitute should just be replace?
+    s1 <- unify_ k (a1 := a2)
+    s2 <- unify_ k (substitute s1 (b1 := b2))
+    s3 <- unify_ k (substitute (s1 <> s2) (e1 := e2))
+    pure $ mconcat [s1, s2, s3]
+  unify_ k (Idx xs i := Idx ys j) = do
+    s1 <- unify_ k (xs := ys)
+    s2 <- unify_ k (substitute s1 (i := j))
+    pure $ s1 <> s2
+  unify_ _ _ =
+    pure mempty
