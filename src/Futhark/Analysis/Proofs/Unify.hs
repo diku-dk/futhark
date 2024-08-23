aff205da8ff3c83c747a454ea88c2e30fa3e779c
Nikolaj Hey Hinnerskov <nhin@912134.xyz>
2024-08-23 17:51:19 +0200

    gtg

--- src/Futhark/Analysis/Proofs/Unify.hs
+++ src/Futhark/Analysis/Proofs/Unify.hs
@@ -7,9 +7,13 @@ where
 
 import Data.Map.Strict qualified as M
 import Data.Set qualified as S
-import Futhark.SoP.SoP (SoP)
+import Futhark.SoP.SoP (SoP, sopToList)
+import Futhark.SoP.SoP qualified as SoP
 import Language.Futhark (VName)
 import Futhark.MonadFreshNames (VNameSource, MonadFreshNames (getNameSource), newNameFromString, putNameSource)
+import qualified Data.List as L
+import Data.Bifunctor (bimap)
+import Control.Monad (foldM)
 
 class Ord a => FreeVariables a where
   fv :: a -> S.Set VName
@@ -51,9 +55,15 @@ class (MonadFreshNames m, MonadFail m, Renameable u) => Unify u v m where
     b <- rename e'
     unify_ k a b
 
+instance Renameable VName where
+  rename_ tau x = pure $ M.findWithDefault x x tau
+
+instance FreeVariables VName where
+  fv = S.singleton
+
 instance Ord u => FreeVariables (SoP u) where
   fv _ = undefined
-  
+
 instance Renameable (SoP u) where
   rename_ _tau _u = undefined
 
@@ -63,16 +73,72 @@ instance Replaceable (SoP u) (SoP u) where
 instance SubstitutionBuilder (SoP u) (SoP u) where
   addSub = M.insert
 
-instance (MonadFail m, MonadFreshNames m) => Unify (SoP u) (SoP u) m where
-  unify_ _ _sop1 _sop2 = undefined
+unifies :: (MonadFreshNames m, MonadFail m, Replaceable u (SoP u)) => VName -> [(u, u)] -> m (Substitution (SoP u))
+unifies _ [] = pure mempty
+unifies k us = do
+  foldM (\s (a, b) -> do
+          s' <- unify_ k (rep s a) (rep s b)
+          pure $ s <> s'
+        ) mempty us
+
+instance (MonadFail m, MonadFreshNames m, Replaceable u (SoP u)) => Unify (SoP u) (SoP u) m where
+  unify_ k a b
+    | length ts1 == length ts2 =
+        let terms = do
+              ts1_perm <- L.permutations ts1
+              zip ts1_perm ts2
+            subs = unifySoPTerms terms
+        in fmap head subs
+    | otherwise = pure mempty
+    where
+      ts1 = sopToList a
+      ts2 = sopToList b
+
+      unifySoPTerms ::
+        [((SoP.Term u, Integer), (SoP.Term u, Integer))] ->
+        m [Substitution (SoP u)]
+      unifySoPTerms [] = pure mempty
+      unifySoPTerms ((t1, t2):ts) = do
+        s <- unifyOneSoPTerm t1 t2
+        undefined
+
+      unifyOneSoPTerm ::
+        (SoP.Term u, Integer) ->
+        (SoP.Term u, Integer) ->
+        m [Substitution (SoP u)]
+      unifyOneSoPTerm (t1, c1) (t2, c2)
+        | length xs == length ys,
+          c1 == c2 = undefined
+            -- let xs'' = do
+            --   xs' <- L.permutations xs
+            --   zip xs' ys
+            -- unifies $ 
+        | otherwise = pure mempty
+        where
+          xs = SoP.termToList t1
+          ys = SoP.termToList t2
+
+  -- unifyOneSoP x y
+  --   | length xs == length ys = do
+  --       xs' <- L.permutations xs
+  --       noSubProblems $
+  --         unifySoPTerms $
+  --           zip xs' ys
+  --   | otherwise = mempty
+  --   where
+  --     xs = SoP.sopToList x
+  --     ys = SoP.sopToList y
+  --     unifySoPTerms :: [((SoP.Term Term, Integer), (SoP.Term Term, Integer))] -> [Subst]
+  --     unifySoPTerms [] = pure mempty
+  --     unifySoPTerms ((t1, t2) : es) = do
+  --       s <- unifyOneSoPTerm t1 t2
+  --       let es' =
+  --             map
+  --               ( \((t1', a), (t2', b)) ->
+  --                   ((replace s t1', a), (replace s t2', b))
+  --               )
+  --               es
+  --       fmap (s <>) $ unifySoPTerms es'
+
 
 -- Unify sop1 with k terms with all k-combinations of terms in sop2?
-
-instance Renameable VName where
-  rename_ tau x = pure $ M.findWithDefault x x tau
-
-instance FreeVariables VName where
-  fv = S.singleton
-
-instance (MonadFreshNames m, MonadFail m) => Unify VName u m where
-  unify_ _ _ = undefined
