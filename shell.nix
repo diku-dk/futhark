6a9b5dec9cb356662781d81b1e7c7a98432dc8d7
Troels Henriksen <athas@sigkill.dk>
2024-08-23 10:21:34 +0200

    Use check-jsonschema for validation.

--- shell.nix
+++ shell.nix
@@ -13,6 +13,7 @@ pkgs.stdenv.mkDerivation {
       cabal-install
       cacert
       curl
+      check-jsonschema
       file
       git
       parallel
@@ -37,7 +38,6 @@ pkgs.stdenv.mkDerivation {
       python.numpy
       python.pyopencl
       python.matplotlib
-      python.jsonschema
       python.sphinx
       python.sphinxcontrib-bibtex
       imagemagick # needed for literate tests
