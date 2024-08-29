bda2fe1e626f2730ca2dafcfb3c579402167f541
Nikolaj Hey Hinnerskov <nhin@912134.xyz>
2024-08-29 14:27:27 +0200

    Add unittests for terms with bound variables.

--- unittests/Futhark/Analysis/Proofs/UnifyTests.hs
+++ unittests/Futhark/Analysis/Proofs/UnifyTests.hs
@@ -39,8 +39,8 @@ evalTestM (TestM m) vns = fst $ evalRWS m () s
 runTest :: MaybeT TestM Sub -> Maybe Sub
 runTest test = evalTestM (runMaybeT test) blankNameSource
 
-getNames :: MaybeT TestM a -> a
-getNames test = fromJust $ evalTestM (runMaybeT test) blankNameSource
+getValue :: MaybeT TestM a -> a
+getValue test = fromJust $ evalTestM (runMaybeT test) blankNameSource
 
 
 tests :: TestTree
@@ -81,10 +81,6 @@ tests = testGroup "Proofs.Unify"
       run_xyzw (\(x,y,z,w) ->
         unify (name2SoP x .+. name2SoP y .+. int2SoP 2) (name2SoP z .+. name2SoP w .+. int2SoP 2)
       ) @?= x2z_y2w
-  , testCase "Substitute only some symbols" $
-      run_xyzw (\(x,y,z,_) ->
-        unify (name2SoP x .*. name2SoP y .*. name2SoP w) (name2SoP z .*. name2SoP w .*. name2SoP w)
-      ) @?= x2z_y2w
   -- Empty substitutions (e.g., simply permuting symbols or terms).
   , testCase "Permuted terms" $
       run_xyzw (\(x,y,_,_) ->
@@ -94,13 +90,54 @@ tests = testGroup "Proofs.Unify"
       run_xyzw (\(x,y,z,_) ->
         unify (name2SoP x .*. name2SoP y .*. name2SoP z) (name2SoP z .*. name2SoP y .*. name2SoP x)
       ) @?= Just mempty
+  -- Indexing.
+  , testCase "Indexing" $
+      run_xyzw (\(x,y,z,w) ->
+        unify (Idx (Var x) (name2SoP y .+. int2SoP 1)) (Idx (Var z) (name2SoP w .+. int2SoP 1))
+      ) @?= x2z_y2w
+  , testCase "Indexing different constant" $
+      run_xyzw (\(x,y,z,w) ->
+        unify (Idx (Var x) (name2SoP y .+. int2SoP 1)) (Idx (Var z) (name2SoP w .+. int2SoP 2))
+      ) @?= Nothing
+  -- Substituting with quantifiers.
+  , testCase "LinComb empty" $
+      run_xyzw (\(x,y,z,w) ->
+        unify (LinComb x (name2SoP y) (name2SoP z) (Var w)) (LinComb x (name2SoP y) (name2SoP z) (Var w))
+      ) @?= Just mempty
+  , testCase "Bound names are not substituted" $
+      run_xyzw_abcd (\((x,y,z,w), (a,b,c,d)) ->
+        unify (LinComb x (name2SoP y) (name2SoP z) (Var w)) (LinComb a (name2SoP b) (name2SoP c) (Var d))
+      ) @?= yzw2bcd
+  , testCase "Bound names are renamed" $
+      run_xyzw_abcd (\((x,_,_,_), (a,b,c,d)) ->
+        unify (Var x) (LinComb a (name2SoP b) (name2SoP c) (Var d))
+      ) @?= Just (s x renamed_lin_comb)
+  -- TODO This test shouldn't be allowed since we assume VNames in first argument are holes?
+  -- , testCase "Substitute only some symbols" $
+  --     run_xyzw (\(x,y,z,_) ->
+  --       unify (name2SoP x .*. name2SoP y .*. name2SoP w) (name2SoP z .*. name2SoP w .*. name2SoP w)
+  --     ) @?= x2z_y2w
   ]
   where
     name2SoP = sym2SoP . Var
     s = M.singleton
+
     xyzwM =
       (,,,) <$> newVName "x" <*> newVName "y" <*> newVName "z" <*> newVName "w"
-    (x,y,z,w) = getNames xyzwM
+    (x,y,z,w) = getValue xyzwM
+    abcdM =
+      (,,,) <$> newVName "a" <*> newVName "b" <*> newVName "c" <*> newVName "d"
+    (_,b,c,d) = getValue (xyzwM >> abcdM)
+
     x2z_y2w = Just (s x (name2SoP z) <> s y (name2SoP w)) 
     x2w_y2z = Just (s x (name2SoP w) <> s y (name2SoP z)) 
+    yzw2bcd = Just (s y (name2SoP b) <> s z (name2SoP c) <> s w (name2SoP d)) 
+
+    renamed_lin_comb = getValue $ do
+          _ <- xyzwM
+          (a,b,c,d) <- abcdM
+          _ <- newVName "k" -- Simulate "k" introduced by Unify.
+          rename $ sym2SoP (LinComb a (name2SoP b) (name2SoP c) (Var d))
+
     run_xyzw f = runTest (xyzwM >>= f)
+    run_xyzw_abcd f = runTest ((,) <$> xyzwM <*> abcdM >>= f)
