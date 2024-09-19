6d4652af0e6888d1003f3db5cb70b1a2bf7e82c6
Nikolaj Hey Hinnerskov <nhin@912134.xyz>
2024-09-19 11:20:03 +0200

    Format.

--- src/Futhark/Analysis/Proofs/Refine.hs
+++ src/Futhark/Analysis/Proofs/Refine.hs
@@ -1,22 +1,21 @@
 -- Rewrite using range environment and Fourier-Motzkin elimination.
-module Futhark.Analysis.Proofs.Refine
-where
+module Futhark.Analysis.Proofs.Refine where
 
-import Futhark.Analysis.Proofs.Symbol (Symbol(..))
-import Futhark.SoP.FourierMotzkin (($==$), ($/=$), ($>=$), ($>$), ($<$), ($<=$))
 import Futhark.Analysis.Proofs.IndexFn (IndexFnM)
+import Futhark.Analysis.Proofs.Symbol (Symbol (..))
+import Futhark.SoP.FourierMotzkin (($/=$), ($<$), ($<=$), ($==$), ($>$), ($>=$))
 
 refineSymbol :: Symbol -> IndexFnM Symbol
 refineSymbol symbol = case symbol of
-    x :== y -> refineRelation (:==) x y
-    x :/= y -> refineRelation (:/=) x y
-    x :> y -> refineRelation (:>) x y
-    x :>= y -> refineRelation (:>=) x y
-    x :< y -> refineRelation (:<) x y
-    x :<= y -> refineRelation (:<=) x y
-    x :&& y -> refineRelation (:&&) x y
-    x :|| y -> refineRelation (:||) x y
-    x -> pure x
+  x :== y -> refineRelation (:==) x y
+  x :/= y -> refineRelation (:/=) x y
+  x :> y -> refineRelation (:>) x y
+  x :>= y -> refineRelation (:>=) x y
+  x :< y -> refineRelation (:<) x y
+  x :<= y -> refineRelation (:<=) x y
+  x :&& y -> refineRelation (:&&) x y
+  x :|| y -> refineRelation (:||) x y
+  x -> pure x
   where
     refineRelation rel x y = do
       b <- solve (x `rel` y)
@@ -28,8 +27,8 @@ refineSymbol symbol = case symbol of
     solve (Bool True) = pure True
     solve (a :== b) = a $==$ b
     solve (a :/= b) = a $/=$ b
-    solve (a :> b)  = a $>$ b
+    solve (a :> b) = a $>$ b
     solve (a :>= b) = a $>=$ b
-    solve (a :< b)  = a $<$ b
+    solve (a :< b) = a $<$ b
     solve (a :<= b) = a $<=$ b
     solve _ = pure False
