1aa39491d18718ca6ab606fdf0ddaee402e00233
Therese Lyngby <thereselyngby@gmail.com>
2024-09-23 16:18:09 +0200

    Started writing idempotence test for fmt. Problem: how to compare AST?

--- unittests/Futhark/CLI/FmtTests.hs
+++ unittests/Futhark/CLI/FmtTests.hs
@@ -3,7 +3,7 @@ module Futhark.CLI.FmtTests (tests) where
 
 import Data.Bifunctor
 import Test.Tasty
-import Test.Tasty.HUnit
+import Test.Tasty.HUnit(testCase, assertFailure, (@?=))
 import Futhark.CLI.Fmt(fmtText)
 import Language.Futhark
 import Language.Futhark.Parser
@@ -18,22 +18,42 @@ programs = filter ((".fut" `isExtensionOf`) . fst) $(embedDir "tests")
 
 tests :: TestTree
 tests =
-  testGroup "Futhark.CLI.Fmt" [fmtParseTests]
+  testGroup "Futhark.CLI.Fmt" [fmtParseTests, fmtIdempotenceTests]
+
+fmtParse :: FilePath -> BS.ByteString -> Either T.Text UncheckedProg
+fmtParse file bs = do 
+      t <- first (const "Error: Can not parse file as UTF-8.") $ T.decodeUtf8' bs 
+      fmt <- fmtText file t
+      first (\(SyntaxError loc err) -> locText loc <> ": " <> prettyText err) $ 
+        parseFuthark file fmt
 
 -- | Formats and compiles a file   
 fmtParseTest :: (FilePath, BS.ByteString) -> TestTree
 fmtParseTest (file, bs) = 
     testCase ("formatting and compiling: " ++ file) $ 
-      case result of 
+      case fmtParse file bs of 
         Left err -> 
           assertFailure $ "Could not parse: " <> T.unpack err
         Right _ -> pure ()
-  where
-    result = do 
-      t <- first (const "Error: Can not parse file as UTF-8.") $ T.decodeUtf8' bs 
-      fmt <- fmtText file t
-      first (\(SyntaxError loc err) -> locText loc <> ": " <> prettyText err)
-        $ parseFutharkWithComments file fmt
+
+
+-- | Checks that the AST resulting from parsing a file is the same before and after 
+--   formatting
+fmtIdempotenceTest :: (FilePath, BS.ByteString) -> TestTree
+fmtIdempotenceTest = undefined
+-- fmtIdempotenceTest (file, bs) =  
+--   -- Problem: how to compare abstract syntax trees? Also parsing does not give AST but CST
+--   testCase ("Comparing AST of " ++ file) $ beforeAST @?= fmtAST 
+--   where beforeAST = do
+--           t <- first (const "Error: Can not parse file as UTF-8.") $ T.decodeUtf8' bs 
+--           first (\(SyntaxError loc err) -> locText loc <> ": " <> prettyText err) $ 
+--             parseFuthark file t
+--         fmtAST = fmtParse file bs
+          
 
 fmtParseTests :: TestTree 
 fmtParseTests = testGroup "format and parse tests" $ map fmtParseTest programs
+
+fmtIdempotenceTests :: TestTree
+fmtIdempotenceTests = testGroup "Idempotence of formatter" $
+  map fmtIdempotenceTest programs
