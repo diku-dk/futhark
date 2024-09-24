6a9c28d5b53be47d3f8ada794d9cfc9b0ddaf4b4
Cosmin Oancea <cosmin.oancea@diku.dk>
2024-09-24 15:06:57 +0200

    Fusion accs (#2180)
    Implement accumulator fusion.

--- src/Futhark/Optimise/Fusion/GraphRep.hs
+++ src/Futhark/Optimise/Fusion/GraphRep.hs
@@ -41,6 +41,10 @@ module Futhark.Optimise.Fusion.GraphRep
     mkDepGraph,
     mkDepGraphForFun,
     pprg,
+    isWithAccNodeT,
+    isWithAccNodeId,
+    vFusionFeasability,
+    hFusionFeasability,
   )
 where
 
@@ -431,3 +435,25 @@ isInf (_, _, e) = case e of
 isCons :: EdgeT -> Bool
 isCons (Cons _) = True
 isCons _ = False
+
+-- | Is this a withAcc?
+isWithAccNodeT :: NodeT -> Bool
+isWithAccNodeT (StmNode (Let _ _ (WithAcc _ _))) = True
+isWithAccNodeT _ = False
+
+isWithAccNodeId :: G.Node -> DepGraph -> Bool
+isWithAccNodeId node_id (DepGraph {dgGraph = g}) =
+  let (_, _, nT, _) = G.context g node_id
+   in isWithAccNodeT nT
+
+unreachableEitherDir :: DepGraph -> G.Node -> G.Node -> Bool
+unreachableEitherDir g a b =
+  not (reachable g a b || reachable g b a)
+
+vFusionFeasability :: DepGraph -> G.Node -> G.Node -> Bool
+vFusionFeasability dg@DepGraph {dgGraph = g} n1 n2 =
+  (isWithAccNodeId n2 dg || not (any isInf (edgesBetween dg n1 n2)))
+    && not (any (reachable dg n2) (filter (/= n2) (G.pre g n1)))
+
+hFusionFeasability :: DepGraph -> G.Node -> G.Node -> Bool
+hFusionFeasability = unreachableEitherDir
