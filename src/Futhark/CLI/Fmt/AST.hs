958af54fc1399b6971e5e968bcd195c97f76413c
Therese Lyngby <thereselyngby@gmail.com>
2024-10-01 17:04:48 +0200

    Finished setup of fmt-type and renamed Fmt.hs to Printer.hs

--- src/Futhark/CLI/Fmt/AST.hs
+++ src/Futhark/CLI/Fmt/AST.hs
@@ -4,23 +4,35 @@ module Futhark.CLI.Fmt.AST (
     nil, 
     nest, 
     code,
+    space,
     line,
+    comment,
     group,
+
+    -- functions for manipulating fmt
+    flatten,
+    pretty
+
     ) where 
 
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
+import qualified Data.Text.Lazy.Lens as T
 
 infixr 6 :<> 
 infixr 5 :<|>
+
+type Comment = T.Text
+type Line = T.Text
 
 data Fmt = Nil
          | Fmt :<> Fmt
          | Nest Int Fmt
          | Code T.Text 
-         | Line
+         | Line (Maybe Comment) 
+         | Space
          | Fmt :<|> Fmt
-         | Comment T.Text deriving (Eq, Ord, Show)
+         deriving (Eq, Show)
 
 instance Semigroup Fmt where
     (<>) = (:<>)
@@ -39,16 +51,46 @@ code :: T.Text -> Fmt
 code = Code 
 
 line :: Fmt 
-line = Line
+line = Line Nothing
+
+space :: Fmt
+space = Space
+
+comment :: T.Text -> Fmt
+comment = Line . Just
 
 group :: Fmt -> Fmt 
 group x = flatten x :<|> x 
 
 flatten :: Fmt -> Fmt
-flatten Nil = Nil
-flatten (x :<> y) = flatten x :<> flatten y
-flatten (Nest i x) = Nest i (flatten x)
-flatten (Code x) = Code x
-flatten Line = Code " "
-flatten (x :<|> y) = flatten x
-flatten (Comment x) = Comment x
+flatten fmt = 
+    let (cs, a') = flatten' fmt
+        cs' = mconcat $ map (Line . Just) cs
+    in a' :<> cs' -- should comment appear before or after flat-line?
+    where flatten' :: Fmt -> ([T.Text], Fmt)
+          flatten' Nil = ([], nil)
+          flatten' (x :<> y) = 
+            let (a, x') = flatten' x
+                (b, y') = flatten' y
+            in (a ++ b, x' <> y')
+          flatten' (Nest _i x) = flatten' x
+          flatten' (Code x) = ([], code x)
+          flatten' (Line Nothing) = ([], space)
+          flatten' (Line (Just c)) = ([c], space)
+          flatten' Space = ([], space)
+          flatten' (x :<|> _) = flatten' x
+
+pretty :: Fmt -> Line
+pretty = layout 
+    where layout :: Fmt -> T.Text
+          layout Nil = ""
+          layout (x :<> y) = layout x <> layout y    
+          layout (Nest i x) = indnt <> layout x
+            where indnt = T.pack $ replicate i ' '
+          layout (Code s) = s
+          layout (Line Nothing) = "\n"
+          layout (Line (Just c)) = c <> "\n"  
+          layout Space = " "
+          -- always choose the first format for now
+          -- TO DO: Have a better way to choose layout
+          layout (x :<|> _) = layout x
