ee147066659e113897af1a2e0234995564eabe77
Troels Henriksen <athas@sigkill.dk>
2024-10-03 19:34:24 +0200

    futhark fmt: allow input on stdin as well.

--- src/Futhark/CLI/Fmt.hs
+++ src/Futhark/CLI/Fmt.hs
@@ -1,11 +1,11 @@
 -- | @futhark fmt@
 module Futhark.CLI.Fmt (main) where
 
-import Futhark.CLI.Fmt.Printer
 import Data.Text.IO qualified as T
+import Futhark.CLI.Fmt.Printer
 import Futhark.Util.Options
 import Language.Futhark
-import Language.Futhark.Parser ( SyntaxError (..) )
+import Language.Futhark.Parser (SyntaxError (..))
 import System.Exit
 import System.IO
 
@@ -13,11 +13,13 @@ import System.IO
 main :: String -> [String] -> IO ()
 main = mainWithOptions () [] "program" $ \args () ->
   case args of
-    [file] -> Just $ do
-      res <- fmtText file <$> T.readFile file
-      case res of
+    [] -> Just $ onInput =<< T.getContents
+    [file] -> Just $ onInput =<< T.readFile file
+    _any -> Nothing
+  where
+    onInput s = do
+      case fmtText "<stdin>" s of
         Left (SyntaxError loc err) -> do
           T.hPutStr stderr $ locText loc <> ":\n" <> prettyText err
           exitFailure
         Right fmt -> T.hPutStr stdout fmt
-    _any -> Nothing
