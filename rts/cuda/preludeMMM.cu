31b8a40267b9d1d87cbffaea290e14239f350b7e
TheRune22 <rune.e.b.lejboelle@gmail.com>
2024-10-24 14:57:11 +0200

    Fixed race condition

--- rts/cuda/preludeMMM.cu
+++ rts/cuda/preludeMMM.cu
@@ -15,6 +15,7 @@ struct get_mma_config {};
 
 template<class SizeM, class SizeN, class BlockSize>
 struct get_mma_config<half_t, half_t, half_t, SizeM, SizeN, BlockSize> {
+//     This is how BlockSize was calculated, no need to check
     using WarpsM = Int<ceil_div(SizeM{}, 64)>;
     using WarpsN = Int<ceil_div(SizeN{}, 64)>;
 
@@ -34,6 +35,7 @@ struct get_mma_config<half_t, half_t, half_t, SizeM, SizeN, BlockSize> {
 
 template<class SizeM, class SizeN, class BlockSize>
 struct get_mma_config<half_t, half_t, float, SizeM, SizeN, BlockSize> {
+//     This is how BlockSize was calculated, no need to check
     using WarpsM = Int<ceil_div(SizeM{}, 64)>;
     using WarpsN = Int<ceil_div(SizeN{}, 64)>;
 
@@ -55,93 +57,92 @@ struct get_mma_config<half_t, half_t, float, SizeM, SizeN, BlockSize> {
 template<class ElmTypeIn, class SizeY, class SizeX, class BlockSize>
 FUTHARK_FUN_ATTR void futrts_copyGlobalShared(unsigned char **mem_out_p, unsigned char *global_mem, unsigned char *shared_mem, int64_t globalOuterDim, ElmTypeIn, SizeY, SizeX, BlockSize)
 {
-    using ElmType = typename convert_type<ElmTypeIn>::TypeOut;
+    *mem_out_p = shared_mem;
 
-// TODO: check compute capability, check if transposed?
-//     using CopyOpGlobalShared = UniversalCopy<ElmType>;
-//     using CopyOpGlobalShared = UniversalCopy<uint128_t>;
-    using CopyOpGlobalShared = SM80_CP_ASYNC_CACHEGLOBAL<uint128_t>;
+    int flatThreadIdx = threadIdx.z * blockDim.y * blockDim.x + threadIdx.y * blockDim.x + threadIdx.x;
 
-    constexpr int elmsPerLoad = 16 / sizeof(ElmType);
-    constexpr int threadsX = SizeX{} / elmsPerLoad;
-    constexpr int threadsY = BlockSize{} / threadsX;
+    if (flatThreadIdx < BlockSize{}) {
+      using ElmType = typename convert_type<ElmTypeIn>::TypeOut;
 
-    auto s_layout = make_layout(Shape<SizeY, SizeX>{}, LayoutRight{});
-    //     TODO: change if swizzling
-    auto g_layout = s_layout;
+  // TODO: check compute capability, check if transposed?
+  //     using CopyOpGlobalShared = UniversalCopy<ElmType>;
+  //     using CopyOpGlobalShared = UniversalCopy<uint128_t>;
+      using CopyOpGlobalShared = SM80_CP_ASYNC_CACHEGLOBAL<uint128_t>;
 
-    TiledCopy copy_global_shared = make_tiled_copy(Copy_Atom<CopyOpGlobalShared, ElmType>{},
-        make_layout(Shape<Int<threadsY>, Int<threadsX>>{}, LayoutRight{}),
-        Layout<Shape<_1,Int<elmsPerLoad>>>{}
-    );
+      constexpr int elmsPerLoad = 16 / sizeof(ElmType);
+      constexpr int threadsX = SizeX{} / elmsPerLoad;
+      constexpr int threadsY = BlockSize{} / threadsX;
 
-    Tensor s = make_tensor(make_smem_ptr(reinterpret_cast<ElmType *>(shared_mem)), s_layout);
-//     TODO: blockIdx.x should be arg
-    Tensor g = make_tensor(make_gmem_ptr(&reinterpret_cast<ElmType *>(global_mem)[blockIdx.x * SizeY{} * SizeX{}]), g_layout);
+      auto s_layout = make_layout(Shape<SizeY, SizeX>{}, LayoutRight{});
+      //     TODO: change if swizzling
+      auto g_layout = s_layout;
 
-    ThrCopy thr_copy_global_shared = copy_global_shared.get_slice(threadIdx.x);
-    Tensor tAgA = thr_copy_global_shared.partition_S(g);
-    Tensor tAsA = thr_copy_global_shared.partition_D(s);
+      TiledCopy copy_global_shared = make_tiled_copy(Copy_Atom<CopyOpGlobalShared, ElmType>{},
+          make_layout(Shape<Int<threadsY>, Int<threadsX>>{}, LayoutRight{}),
+          Layout<Shape<_1,Int<elmsPerLoad>>>{}
+      );
 
-    copy(copy_global_shared, tAgA, tAsA);
+      Tensor s = make_tensor(make_smem_ptr(reinterpret_cast<ElmType *>(shared_mem)), s_layout);
+  //     TODO: blockIdx.x should be arg
+      Tensor g = make_tensor(make_gmem_ptr(&reinterpret_cast<ElmType *>(global_mem)[blockIdx.x * SizeY{} * SizeX{}]), g_layout);
 
-//     TODO: use async?
-    cp_async_fence();
+      ThrCopy thr_copy_global_shared = copy_global_shared.get_slice(flatThreadIdx);
+      Tensor tAgA = thr_copy_global_shared.partition_S(g);
+      Tensor tAsA = thr_copy_global_shared.partition_D(s);
 
-    //    TODO: remove
-//    for (int i = 0; i < 8; i++) {
-//        reinterpret_cast<half_t *>(shared_mem_6287)[i * blockDim.x + threadIdx.x] = reinterpret_cast<half_t *>(global_mem_6286)[blockIdx.x * 16 * 16 + i * blockDim.x + threadIdx.x];
-//    }
+      copy(copy_global_shared, tAgA, tAsA);
 
-// TODO: simplify
-    unsigned char *mem_out;
+  //     TODO: use async?
+      cp_async_fence();
+    }
 
-    mem_out = shared_mem;
-    *mem_out_p = mem_out;
+//     TODO: should ideally be just before gemm, could do in function
+    cp_async_wait<0>();
+    __syncthreads();
 }
 
 template<class ElmTypeAIn, class ElmTypeBIn, class ElmTypeCIn, class SizeM, class SizeN, class BlockSize, int numRegs>
 FUTHARK_FUN_ATTR void futrts_copyRegistersShared(unsigned char **mem_out_p, ElmTypeCIn (&registers_mem)[numRegs], unsigned char *shared_mem, ElmTypeAIn, ElmTypeBIn, SizeM, SizeN, BlockSize)
 {
-    using ElmTypeA = typename convert_type<ElmTypeAIn>::TypeOut;
-    using ElmTypeB = typename convert_type<ElmTypeBIn>::TypeOut;
-    using ElmTypeC = typename convert_type<ElmTypeCIn>::TypeOut;
+    *mem_out_p = shared_mem;
 
-    using MMAConfig = get_mma_config<ElmTypeA, ElmTypeB, ElmTypeC, SizeM, SizeN, BlockSize>;
-    using TiledMMA_ = typename MMAConfig::TiledMma;
+    int flatThreadIdx = threadIdx.z * blockDim.y * blockDim.x + threadIdx.y * blockDim.x + threadIdx.x;
 
-//    TODO: try tiledcopy instead?
-    auto s_layout = make_layout(Shape<SizeM, SizeN>{}, LayoutRight{});
-    TiledMMA_ tiled_mma;
+// TODO: handle non flat indices
+    if (flatThreadIdx < BlockSize{}) {
 
-    ThrMMA thr_mma = tiled_mma.get_slice(threadIdx.x);
+        using ElmTypeA = typename convert_type<ElmTypeAIn>::TypeOut;
+        using ElmTypeB = typename convert_type<ElmTypeBIn>::TypeOut;
+        using ElmTypeC = typename convert_type<ElmTypeCIn>::TypeOut;
 
-    auto rC_layout = partition_shape_C(thr_mma, s_layout.shape());
-    Tensor tCrC = make_tensor(make_rmem_ptr(reinterpret_cast<ElmTypeC *>(registers_mem)), rC_layout);
+        using MMAConfig = get_mma_config<ElmTypeA, ElmTypeB, ElmTypeC, SizeM, SizeN, BlockSize>;
+        using TiledMMA_ = typename MMAConfig::TiledMma;
 
-    Tensor s = make_tensor(make_gmem_ptr(reinterpret_cast<ElmTypeC *>(shared_mem)), s_layout);
-    Tensor tCgC = thr_mma.partition_C(s);
+    //    TODO: try tiledcopy instead?
+        auto s_layout = make_layout(Shape<SizeM, SizeN>{}, LayoutRight{});
+        TiledMMA_ tiled_mma;
 
-//  TODO: take as input
-    auto alpha = _1{};
-    auto beta = _0{};
-    axpby(alpha, tCrC, beta, tCgC);
+        ThrMMA thr_mma = tiled_mma.get_slice(flatThreadIdx);
 
-    //    TODO: remove
-//    for (int i = 0; i < 8; i++) {
-//        reinterpret_cast<half_t *>(global_mem_6287)[i * blockDim.x + threadIdx.x] = reinterpret_cast<half_t *>(registers_mem_6286)[i];
-//    }
+        auto rC_layout = partition_shape_C(thr_mma, s_layout.shape());
+        Tensor tCrC = make_tensor(make_rmem_ptr(reinterpret_cast<ElmTypeC *>(registers_mem)), rC_layout);
 
-// TODO: simplify
-    unsigned char *mem_out;
+        Tensor s = make_tensor(make_gmem_ptr(reinterpret_cast<ElmTypeC *>(shared_mem)), s_layout);
+        Tensor tCgC = thr_mma.partition_C(s);
 
-    mem_out = shared_mem;
-    *mem_out_p = mem_out;
+    //  TODO: take as input
+        auto alpha = _1{};
+        auto beta = _0{};
+        axpby(alpha, tCrC, beta, tCgC);
+    }
+    __syncthreads();
 }
 
 template<class ElmTypeAIn, class ElmTypeBIn, class ElmTypeCIn, class SizeM, class SizeN, class SizeK, class BlockSize, int numRegs>
 FUTHARK_FUN_ATTR void futrts_tensorMMM(ElmTypeCIn (*mem_out_p)[numRegs], unsigned char *A_mem, unsigned char *B_mem, ElmTypeCIn (&C_mem)[numRegs], ElmTypeAIn, ElmTypeBIn, SizeM, SizeN, SizeK, BlockSize)
 {
+    int flatThreadIdx = threadIdx.z * blockDim.y * blockDim.x + threadIdx.y * blockDim.x + threadIdx.x;
+
     using ElmTypeA = typename convert_type<ElmTypeAIn>::TypeOut;
     using ElmTypeB = typename convert_type<ElmTypeBIn>::TypeOut;
     using ElmTypeC = typename convert_type<ElmTypeCIn>::TypeOut;
@@ -156,7 +157,7 @@ FUTHARK_FUN_ATTR void futrts_tensorMMM(ElmTypeCIn (*mem_out_p)[numRegs], unsigne
 
     TiledMMA_ tiled_mma;
 
-    ThrMMA thr_mma = tiled_mma.get_slice(threadIdx.x);
+    ThrMMA thr_mma = tiled_mma.get_slice(flatThreadIdx);
 
     auto rC_layout = partition_shape_C(thr_mma, sC_layout.shape());
     Tensor tCrC = make_tensor(make_rmem_ptr(reinterpret_cast<ElmTypeC *>(C_mem)), rC_layout);
@@ -168,34 +169,13 @@ FUTHARK_FUN_ATTR void futrts_tensorMMM(ElmTypeCIn (*mem_out_p)[numRegs], unsigne
     Tensor tCsB = thr_mma.partition_B(sB);
 
 //     TODO: use async?
-    cp_async_wait<0>();
-    __syncthreads();
-    // TODO: add tDrD?
+//     cp_async_wait<0>();
+//     __syncthreads();
+    // TODO: add tDrD? try cooperative gemm
     gemm(tiled_mma, tCsA, tCsB, tCrC);
 //     TODO: probably not needed since not reusing buffers
 //    __syncthreads();
 
-//    for (int i = 0; i < 8; i++) {
-//        int m = (i * blockDim.x + threadIdx.x) / 16;
-//        int n = (i * blockDim.x + threadIdx.x) % 16;
-//        for (int k = 0; k < 16; k++)
-//        {
-//            half_t a_elm = reinterpret_cast<half_t *>(A_mem_6286)[m * 16 + k];
-//            half_t b_elm = reinterpret_cast<half_t *>(B_mem_6287)[k * 16 + n];
-//            reinterpret_cast<half_t *>(C_mem_6288)[i] += a_elm * b_elm;
-//        }
-//    }
-
-// TODO: remove
-//     for (int i = 0; i < 8; i++) {
-//         reinterpret_cast<half_t *>(*mem_out_p_0)[i] = reinterpret_cast<half_t *>(B_mem_6287)[i * blockDim.x + threadIdx.x];
-//     }
-
-// TODO: simplify
-    ElmTypeC mem_out[numRegs];
-
-    for (int32_t i_1 = 0; i_1 < numRegs; i_1++)
-        mem_out[i_1] = C_mem[i_1];
-    for (int32_t i_2 = 0; i_2 < numRegs; i_2++)
-        (*mem_out_p)[i_2] = mem_out[i_2];
+    for (int32_t i = 0; i < numRegs; i++)
+        (*mem_out_p)[i] = C_mem[i];
 }
