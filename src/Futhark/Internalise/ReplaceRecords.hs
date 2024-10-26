c546620be64cfc327aa1dd859e1cc07d0c5c6626
Troels Henriksen <athas@sigkill.dk>
2024-10-26 11:13:08 +0200

    Run record replacement early in compiler pipeline.
    This is necessary to avoid creating huge closures when lambda lifting.

--- src/Futhark/Internalise/ReplaceRecords.hs
+++ src/Futhark/Internalise/ReplaceRecords.hs
@@ -7,6 +7,8 @@ module Futhark.Internalise.ReplaceRecords (transformProg) where
 import Control.Monad
 import Control.Monad.Reader
 import Control.Monad.State
+import Data.Bifunctor
+import Data.Bitraversable
 import Data.Map.Strict qualified as M
 import Futhark.MonadFreshNames
 import Language.Futhark
@@ -23,23 +25,31 @@ newtype Env = Env
   { envRecordReplacements :: RecordReplacements
   }
 
+data S = S
+  { stateNameSource :: VNameSource,
+    stateStructTypeMemo :: M.Map StructType StructType,
+    stateParamTypeMemo :: M.Map ParamType ParamType
+  }
+
 -- The monomorphization monad.
 newtype RecordM a
-  = RecordM (ReaderT Env (State VNameSource) a)
+  = RecordM (ReaderT Env (State S) a)
   deriving
     ( Functor,
       Applicative,
       Monad,
-      MonadReader Env
+      MonadReader Env,
+      MonadState S
     )
 
 instance MonadFreshNames RecordM where
-  getNameSource = RecordM get
-  putNameSource = RecordM . put
+  getNameSource = RecordM $ gets stateNameSource
+  putNameSource src = RecordM $ modify $ \s -> s {stateNameSource = src}
 
 runRecordM :: VNameSource -> RecordM a -> (a, VNameSource)
 runRecordM src (RecordM m) =
-  runState (runReaderT m (Env mempty)) src
+  second stateNameSource $
+    runState (runReaderT m (Env mempty)) (S src mempty mempty)
 
 withRecordReplacements :: RecordReplacements -> RecordM a -> RecordM a
 withRecordReplacements rr = local $ \env ->
@@ -54,8 +64,39 @@ wildcard (Scalar (Record fs)) loc =
 wildcard t loc =
   Wildcard (Info t) loc
 
-transformPat :: Pat (TypeBase Size u) -> RecordM (Pat (TypeBase Size u), RecordReplacements)
-transformPat (Id v (Info (Scalar (Record fs))) loc) = do
+memoParamType :: ParamType -> RecordM ParamType -> RecordM ParamType
+memoParamType t m = do
+  prev <- gets $ M.lookup t . stateParamTypeMemo
+  case prev of
+    Just t' -> pure t'
+    Nothing -> do
+      t' <- m
+      modify $ \s -> s {stateParamTypeMemo = M.insert t t' $ stateParamTypeMemo s}
+      pure t'
+
+memoStructType :: StructType -> RecordM StructType -> RecordM StructType
+memoStructType t m = do
+  prev <- gets $ M.lookup t . stateStructTypeMemo
+  case prev of
+    Just t' -> pure t'
+    Nothing -> do
+      t' <- m
+      modify $ \s -> s {stateStructTypeMemo = M.insert t t' $ stateStructTypeMemo s}
+      pure t'
+
+-- No need to keep memoisation cache between top level functions.
+memoClear :: RecordM ()
+memoClear = modify $ \s ->
+  s
+    { stateStructTypeMemo = mempty,
+      stateParamTypeMemo = mempty
+    }
+
+transformPat ::
+  (TypeBase Size u -> RecordM (TypeBase Size u)) ->
+  Pat (TypeBase Size u) ->
+  RecordM (Pat (TypeBase Size u), RecordReplacements)
+transformPat _ (Id v (Info (Scalar (Record fs))) loc) = do
   let fs' = M.toList fs
   (fs_ks, fs_ts) <- fmap unzip $
     forM fs' $ \(f, ft) ->
@@ -66,29 +107,38 @@ transformPat (Id v (Info (Scalar (Record fs))) loc) = do
         loc,
       M.singleton v $ M.fromList $ zip (map fst fs') $ zip fs_ks $ map toStruct fs_ts
     )
-transformPat (Id v t loc) =
-  pure (Id v t loc, mempty)
-transformPat (TuplePat pats loc) = do
-  (pats', rrs) <- mapAndUnzipM transformPat pats
+transformPat onType (Id v t loc) = do
+  t' <- traverse onType t
+  pure (Id v t' loc, mempty)
+transformPat onType (TuplePat pats loc) = do
+  (pats', rrs) <- mapAndUnzipM (transformPat onType) pats
   pure (TuplePat pats' loc, mconcat rrs)
-transformPat (RecordPat fields loc) = do
+transformPat onType (RecordPat fields loc) = do
   let (field_names, field_pats) = unzip fields
-  (field_pats', rrs) <- mapAndUnzipM transformPat field_pats
+  (field_pats', rrs) <- mapAndUnzipM (transformPat onType) field_pats
   pure (RecordPat (zip field_names field_pats') loc, mconcat rrs)
-transformPat (PatParens pat loc) = do
-  (pat', rr) <- transformPat pat
+transformPat onType (PatParens pat loc) = do
+  (pat', rr) <- transformPat onType pat
   pure (PatParens pat' loc, rr)
-transformPat (PatAttr attr pat loc) = do
-  (pat', rr) <- transformPat pat
+transformPat onType (PatAttr attr pat loc) = do
+  (pat', rr) <- transformPat onType pat
   pure (PatAttr attr pat' loc, rr)
-transformPat (Wildcard (Info t) loc) =
-  pure (wildcard t loc, mempty)
-transformPat (PatAscription pat _ _) =
-  transformPat pat
-transformPat (PatLit e t loc) = pure (PatLit e t loc, mempty)
-transformPat (PatConstr name t all_ps loc) = do
-  (all_ps', rrs) <- mapAndUnzipM transformPat all_ps
+transformPat onType (Wildcard (Info t) loc) = do
+  t' <- onType t
+  pure (wildcard t' loc, mempty)
+transformPat onType (PatAscription pat _ _) =
+  transformPat onType pat
+transformPat _ (PatLit e t loc) =
+  pure (PatLit e t loc, mempty)
+transformPat onType (PatConstr name t all_ps loc) = do
+  (all_ps', rrs) <- mapAndUnzipM (transformPat onType) all_ps
   pure (PatConstr name t all_ps' loc, mconcat rrs)
+
+transformParamType :: ParamType -> RecordM ParamType
+transformParamType t = memoParamType t $ bitraverse transformExp pure t
+
+transformStructType :: StructType -> RecordM StructType
+transformStructType t = memoStructType t $ bitraverse transformExp pure t
 
 transformExp :: Exp -> RecordM Exp
 transformExp (Project n e t loc) = do
@@ -102,7 +152,7 @@ transformExp (Project n e t loc) = do
     _ -> do
       e' <- transformExp e
       pure $ Project n e' t loc
-transformExp e@(Var fname _ loc) = do
+transformExp (Var fname t loc) = do
   maybe_fs <- lookupRecordReplacement $ qualLeaf fname
   case maybe_fs of
     Just fs -> do
@@ -111,25 +161,37 @@ transformExp e@(Var fname _ loc) = do
             pure $ RecordFieldExplicit f f_v' loc
       RecordLit <$> mapM toField (M.toList fs) <*> pure loc
     Nothing ->
-      pure e
+      Var fname <$> traverse transformStructType t <*> pure loc
 transformExp (AppExp (LetPat sizes pat e body loc) res) = do
   e' <- transformExp e
-  (pat', rr) <- transformPat pat
+  (pat', rr) <- transformPat transformStructType pat
   body' <- withRecordReplacements rr $ transformExp body
   pure $ AppExp (LetPat sizes pat' e' body' loc) res
-transformExp (AppExp (LetFun {}) _) = do
-  error "transformExp: LetFun is not supposed to occur"
-transformExp (Lambda {}) =
-  error "transformExp: Lambda is not supposed to occur"
+transformExp (AppExp (LetFun fname (tparams, params, retdecl, ret, funbody) body loc) res) = do
+  (params', rrs) <- mapAndUnzipM (transformPat transformParamType) params
+  funbody' <- withRecordReplacements (mconcat rrs) $ transformExp funbody
+  body' <- transformExp body
+  pure $ AppExp (LetFun fname (tparams, params', retdecl, ret, funbody') body' loc) res
+transformExp (Lambda params body retdecl ret loc) = do
+  (params', rrs) <- mapAndUnzipM (transformPat transformParamType) params
+  body' <- withRecordReplacements (mconcat rrs) $ transformExp body
+  pure $ Lambda params' body' retdecl ret loc
 transformExp e = astMap m e
   where
     m = identityMapper {mapOnExp = transformExp}
 
 onValBind :: ValBind -> RecordM ValBind
 onValBind vb = do
-  (params', rrs) <- mapAndUnzipM transformPat $ valBindParams vb
+  (params', rrs) <- mapAndUnzipM (transformPat transformParamType) $ valBindParams vb
   e' <- withRecordReplacements (mconcat rrs) $ transformExp $ valBindBody vb
-  pure $ vb {valBindBody = e', valBindParams = params'}
+  ret <- traverse (bitraverse transformExp pure) $ valBindRetType vb
+  memoClear
+  pure $
+    vb
+      { valBindBody = e',
+        valBindParams = params',
+        valBindRetType = ret
+      }
 
 -- | Monomorphise a list of top-level declarations. A module-free input program
 -- is expected, so only value declarations and type declaration are accepted.
