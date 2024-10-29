cc8129c69c99ad49bb6eae6de2530f422934b3d5
Nikolaj Hey Hinnerskov <nhin@912134.xyz>
2024-10-29 09:44:29 +0100

    Fix harmless range leak. Increase laziness.

--- src/Futhark/Analysis/Proofs/Query.hs
+++ src/Futhark/Analysis/Proofs/Query.hs
@@ -18,15 +18,14 @@ import Data.Maybe (catMaybes, fromJust)
 import Futhark.Analysis.Proofs.AlgebraBridge (Answer (..), addRelIterator, addRelSymbol, algebraContext, assume, rollbackAlgEnv, simplify, toRel, ($<), ($<=), ($>))
 import Futhark.Analysis.Proofs.AlgebraPC.Symbol qualified as Algebra
 import Futhark.Analysis.Proofs.IndexFn (Domain (Iota), IndexFn (..), Iterator (..), casesToList, getCase)
-import Futhark.Analysis.Proofs.Monad (IndexFnM, debugM, debugPrettyM, debugPrettyM2, debugPrintAlgEnv, debugT)
+import Futhark.Analysis.Proofs.Monad (IndexFnM, debugPrettyM, debugPrintAlgEnv, debugT)
 import Futhark.Analysis.Proofs.Symbol (Symbol (..), neg, toDNF)
 import Futhark.Analysis.Proofs.Unify (mkRep, rep)
 import Futhark.MonadFreshNames (newNameFromString, newVName)
-import Futhark.SoP.Monad (MonadSoP, addEquiv, addRange, mkRange, mkRangeLB, mkRangeUB)
+import Futhark.SoP.Monad (MonadSoP, addRange, mkRange, mkRangeLB, mkRangeUB)
 import Futhark.SoP.Refine (addRel)
 import Futhark.SoP.SoP (SoP, int2SoP, justSym, sym2SoP, (.-.))
 import Language.Futhark (VName)
-import Prelude hiding (and, or)
 
 data MonoDir = Inc | IncStrict | Dec | DecStrict
   deriving (Show, Eq, Ord)
@@ -88,7 +87,7 @@ prove (PermutationOfZeroTo m) fn@(IndexFn (Forall iter (Iota n)) cs) = algebraCo
           addRange (Algebra.Var j) (mkRangeLB (int2SoP 0))
           assume (fromJust . justSym $ p @ Var i)
           assume (fromJust . justSym $ not_p @ Var j)
-          -- 2. Prove no overlap in cases.
+          -- 2. Prove no overlap between cases.
           -- It is sufficient to show one case is always smaller than the other.
           -- Specifically, for two indices i and j where p(i) is true
           -- and p(j) is false, we need to show:
@@ -116,16 +115,16 @@ prove (PermutationOfZeroTo m) fn@(IndexFn (Forall iter (Iota n)) cs) = algebraCo
           -- Case i /= j => f(i) > g(j):
           let f_GT_g = debugT "f_GT_g" $ f_rel_g ($>)
           let no_overlap = debugT "no_overlap" $ f_LT_g `orM` f_GT_g
-          -- 3. Prove both cases are bounded from below by 0.
-          -- 4. Prove both cases are bounded from above by m.
-          addRelIterator (Forall i (Iota n))
-          addRelIterator (Forall j (Iota n))
-          -- Putting the most expensive test last to enjoy short-circuiting.
-          (int2SoP 0 $<= (f @ Var i))
-            `andM` (int2SoP 0 $<= (g @ Var j))
-            `andM` ((f @ Var i) $< n)
-            `andM` ((g @ Var j) $< n)
-            `andM` no_overlap
+          -- 3. Prove both cases are bounded from below by 0 and above by m.
+          -- Order matters here for short-circuiting lazy actions.
+          let within_bounds = rollbackAlgEnv $ do
+                addRelIterator (Forall i (Iota n))
+                addRelIterator (Forall j (Iota n))
+                (int2SoP 0 $<= (f @ Var i))
+                  `andM` (int2SoP 0 $<= (g @ Var j))
+                  `andM` ((f @ Var i) $< n)
+                  `andM` ((g @ Var j) $< n)
+          within_bounds `andM` no_overlap
         _ -> undefined
   where
     f @ x = rep (mkRep iter x) f
@@ -187,10 +186,6 @@ isUnknown :: Answer -> Bool
 isUnknown Unknown = True
 isUnknown _ = False
 
-and :: Answer -> Answer -> Answer
-and Yes Yes = Yes
-and _ _ = Unknown
-
 -- Short-circuit evaluation `and`.
 andF :: (Applicative f) => Answer -> f Answer -> f Answer
 andF Yes m = m
@@ -201,14 +196,9 @@ andM m1 m2 = do
   ans <- m1
   ans `andF` m2
 
-or :: Answer -> Answer -> Answer
-or Yes _ = Yes
-or _ Yes = Yes
-or _ _ = Unknown
-
--- To increase laziness when used with short-circuited `and`.
 orM :: (Monad m) => m Answer -> m Answer -> m Answer
 orM m1 m2 = do
   a1 <- m1
-  a2 <- m2
-  pure $ a1 `or` a2
+  case a1 of
+    Yes -> pure Yes
+    Unknown -> m2
