5c14a4593dceaf70416e6a64d8f30271607abea2
Troels Henriksen <athas@sigkill.dk>
2024-10-31 14:45:48 +0100

    Adapt tests for new "futhark fmt" behaviour.

--- tests_fmt/test.sh
+++ tests_fmt/test.sh
@@ -5,11 +5,11 @@ diff_error=0
 rm -rf "$test_dir" && mkdir "$test_dir"
 for file in *.fut; do
     fmtFile=$test_dir/$(basename -s .fut $file).fmt.fut
-    futhark fmt $file > $fmtFile
+    futhark fmt < $file > $fmtFile
     if ! cmp --silent expected/$file $fmtFile; then
         echo "$file didn't format as expected"
         diff_error=1
-    else 
+    else
         rm $fmtFile
     fi
 done
