7630497295b20d87b1ab6295edc6cef58a390614
Troels Henriksen <athas@sigkill.dk>
2024-11-02 10:49:41 +0100

    Factor tuple and record printing into generic functions.
    This loses some of the fine-grained handling of trailing comments, but
    at this point I consider code simplicity more important. Programmers
    can move their comments.

--- src/Futhark/Util/Pretty.hs
+++ src/Futhark/Util/Pretty.hs
@@ -60,10 +60,11 @@ hPutDoc h d = do
   colours <- hIsTerminalDevice h
   if colours
     then Prettyprinter.Render.Terminal.renderIO h (layouter d)
-    else Prettyprinter.Render.Text.hPutDoc h d
+    else Prettyprinter.Render.Text.renderIO h (layouter d)
   where
     layouter =
-      layoutSmart defaultLayoutOptions {layoutPageWidth = Unbounded}
+      removeTrailingWhitespace
+        . layoutSmart defaultLayoutOptions {layoutPageWidth = Unbounded}
 
 -- | Like 'hPutDoc', but with a final newline.
 hPutDocLn :: Handle -> Doc AnsiStyle -> IO ()
@@ -87,7 +88,7 @@ putDocLn d = do
 docTextForHandle :: Handle -> Doc AnsiStyle -> IO T.Text
 docTextForHandle h d = do
   colours <- hIsTerminalDevice h
-  let sds = layoutSmart defaultLayoutOptions d
+  let sds = removeTrailingWhitespace $ layoutSmart defaultLayoutOptions d
   pure $
     if colours
       then Prettyprinter.Render.Terminal.renderStrict sds
@@ -111,7 +112,8 @@ docText :: Doc a -> T.Text
 docText = Prettyprinter.Render.Text.renderStrict . layouter
   where
     layouter =
-      layoutSmart defaultLayoutOptions {layoutPageWidth = Unbounded}
+      removeTrailingWhitespace
+        . layoutSmart defaultLayoutOptions {layoutPageWidth = Unbounded}
 
 -- | Convert a 'Doc' to a 'String', through 'docText'. Intended for
 -- debugging.
