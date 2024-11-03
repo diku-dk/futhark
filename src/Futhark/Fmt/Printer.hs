99186436a2323fd2b183fb37d374436267eacee1
Troels Henriksen <athas@sigkill.dk>
2024-11-03 11:17:32 +0100

    We also want a space here.

--- src/Futhark/Fmt/Printer.hs
+++ src/Futhark/Fmt/Printer.hs
@@ -464,12 +464,7 @@ instance Format UncheckedSpec where
         | null ps = fmtName bindingStyle name
         | otherwise = fmtName bindingStyle name </> align (sep line $ map fmt ps)
   fmt (ValSpec name ps te _ doc loc) =
-    addComments loc $
-      fmt doc
-        <> "val"
-          <+> sub
-        <> ":"
-          <+> fmt te
+    addComments loc $ fmt doc <> "val" <+> sub <+> ":" <+> fmt te
     where
       sub
         | null ps = fmtName bindingStyle name
