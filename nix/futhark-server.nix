daf4e625eb9f3b9e5b9e0f842042a37a614ae069
Troels Henriksen <athas@sigkill.dk>
2024-11-04 16:30:23 +0100

    Bump futhark-server dependency.

--- nix/futhark-server.nix
+++ nix/futhark-server.nix
@@ -3,8 +3,8 @@
 }:
 mkDerivation {
   pname = "futhark-server";
-  version = "1.2.2.1";
-  sha256 = "5845ca6f4dd8395ef860164a7e8193e4094dc342a5b7187e314a7842e70b163d";
+  version = "1.2.3.0";
+  sha256 = "4bd26a908ae3c41b4eb18343a8fedb193a06c802c9e8a31d99a4f87dc781f189";
   libraryHaskellDepends = [
     base binary bytestring directory futhark-data mtl process temporary
     text
