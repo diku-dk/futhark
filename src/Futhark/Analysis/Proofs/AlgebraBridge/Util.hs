92ccc1d676ee581d2d713dfc9f692f286ab9b5c5
Nikolaj Hey Hinnerskov <nhin@912134.xyz>
2024-11-13 14:56:35 +0100

    Trippy.

--- src/Futhark/Analysis/Proofs/AlgebraBridge/Util.hs
+++ src/Futhark/Analysis/Proofs/AlgebraBridge/Util.hs
@@ -20,22 +20,15 @@ import Futhark.Util.Pretty (Pretty (pretty), viaShow, prettyString)
 
 assume :: Symbol -> IndexFnM ()
 assume (p :&& q) = assume p >> assume q
-assume x = do
-  booltype <- isBooleanM x
-  unless booltype (error $ "assume on non-boolean: " <> prettyString x)
-  -- Symbol is in NNF, so x is _not_ on the form Not (p && q).
-  case x of
-    Not p -> do
-      addRelSymbol (neg p)
-      flip addEquiv (int2SoP 0) =<< toAlgebraSymbol p
-      flip addEquiv (int2SoP 1) =<< toAlgebraSymbol (neg p)
-    p -> do
-      addRelSymbol p
-      flip addEquiv (int2SoP 1) =<< toAlgebraSymbol p
-      flip addEquiv (int2SoP 0) =<< toAlgebraSymbol (neg p)
-      -- Add that pairwise disjoint symbols are false.
-      qs <- getPairwiseDisjoint p
-      forM_ qs (flip addEquiv (int2SoP 0) <=< toAlgebraSymbol)
+assume p = do
+  booltype <- isBooleanM p
+  unless booltype (error $ "assume on non-boolean: " <> prettyString p)
+  addRelSymbol p
+  flip addEquiv (int2SoP 1) =<< toAlgebraSymbol p
+  flip addEquiv (int2SoP 0) =<< toAlgebraSymbol (neg p)
+  -- Add that pairwise disjoint symbols are false.
+  qs <- getPairwiseDisjoint p
+  forM_ qs (flip addEquiv (int2SoP 0) <=< toAlgebraSymbol)
 
 -- | Adds a relation on symbols to the algebraic environment.
 -- No-op if `p` is not a relation.
