e35d8202cc57030e5d8c42357cce3d93df8b5644
Nikolaj Hey Hinnerskov <nhin@912134.xyz>
2024-12-02 17:52:15 +0100

    Add Algebra test that I would expect to pass.

--- unittests/Futhark/Analysis/Proofs/AlgebraPC/SolveTests.hs
+++ unittests/Futhark/Analysis/Proofs/AlgebraPC/SolveTests.hs
@@ -360,6 +360,42 @@ tests =
               simplify $ sym2SoP (Idx b a_sum) .+. b_sum
           )
           @??= sym2SoP (Sum b a_sum (sVar i2)),
+      testCase "Monotonicity (from part2indicesL)" $
+        -- j < i, shp >= 0, c[i] = 1, c[j] = 1
+          -- => ∑⟦cª₄₉₈₉₅⟧[∑shpª₃₃₇₈₀[0 : -1 + k₄₉₈₄₈] : i₉₆₆₄]
+        --        > ∑⟦cª₄₉₈₉₅⟧[∑shpª₃₃₇₈₀[0 : -1 + k₄₉₈₄₈] : j₅₀₁₃₁]
+        run
+          ( do
+              clearAlgEnv
+              i <- newNameFromString "i"
+              k <- newNameFromString "k"
+              j <- newNameFromString "j"
+              m <- newNameFromString "m"
+              vn_shp <- newNameFromString "shp"
+              let shp = One vn_shp
+              -- \s -> ∑shpª₃₃₇₈₀[0 : -1 + s]
+              let shp_sum' s = sym2SoP $ Sum shp (int 0) (s .-. int 1)
+              let shp_sum = shp_sum' . sVar
+              -- max{0} <= shapeª₃₃₇₈₀ <= min{}
+              addRange (Var vn_shp) $ mkRangeLB (int 0)
+              -- max{0} <= k₄₉₈₄₈ <= min{-1 + m₄₆₇₈}
+              addRange (Var k) $ mkRange (int 0) (sVar m .-. int 1)
+              -- max{0, 1, ∑shapeª₃₃₇₈₀[0 : -1 + k₄₉₈₄₈]}
+              --   <= i₉₆₆₄
+              --   <= min{-1 + ∑shapeª₃₃₇₈₀[0 : -1 + m₄₆₇₈], -1 + ∑shapeª₃₃₇₈₀[0 : k₄₉₈₄₈]}
+              addRange (Var i) $ mkRange (int 1) (shp_sum m .-. int 1)
+              addRange (Var i) $ mkRange (shp_sum k) (shp_sum' (sVar k .+. int 1) .-. int 1)
+              -- max{0} <= j₅₀₁₃₁ <= min{-1 + i₉₆₆₄}
+              addRange (Var j) $ mkRange (int 0) (sVar i .-. int 1)
+              -- c is disjoint with some other predicate d.
+              addProperty (Var c0) (Disjoint $ S.singleton d0)
+              addProperty (Var c0) Boolean
+              addRange (Var c0) $ mkRange (int 0) (int 1)
+              -- \s -> ∑⟦cª₄₉₈₉₅⟧[∑shpª₃₃₇₈₀[0 : -1 + k₄₉₈₄₈] : s]
+              let c_sum s = sym2SoP $ Sum c (shp_sum k) (sVar s)
+              c_sum i FM.$>$ c_sum j
+          )
+          @??= True,
       --
       testCase "FME1" $
         run
