dd085376eb73284e11b68fa04187e39848fc220d
Kristoffer Kortbaek <kristoffer@kortbaek.dk>
2024-12-18 16:52:18 +0100

    Hmm forgot this

--- src/Futhark/CodeGen/Backends/TensorCore.hs
+++ src/Futhark/CodeGen/Backends/TensorCore.hs
@@ -2,23 +2,22 @@
 
 module Futhark.CodeGen.Backends.TensorCore (compileGemmFun) where
 
+import Data.Text.Internal as T
 import Futhark.CodeGen.Backends.GenericC.Code
 import Futhark.CodeGen.Backends.GenericC.Monad
-import Language.C.Syntax qualified as C
-import Language.C.Quote.OpenCL qualified as C
-import Data.Text.Internal as T
 import Futhark.CodeGen.ImpCode
+import Language.C.Quote.OpenCL qualified as C
+import Language.C.Syntax qualified as C
 import NeatInterpolation (untrimming)
-
 
 -- TODO: remove or use
 gemmName :: T.Text
 gemmName = "gemm_123456"
 
-compileGemmFun
-  :: [C.BlockItem]
-  -> Function op
-  -> CompilerM op s ()
+compileGemmFun ::
+  [C.BlockItem] ->
+  Function op ->
+  CompilerM op s ()
 compileGemmFun get_constants func@(Function _ outputs inputs body) =
   mapM_ earlyDecl [C.cunit| $esc:("#define memblock_shared memblock_device") |]
 
