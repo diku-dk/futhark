19e113287991fdcdefa139e8aac5c977e0d875b9
Kristoffer Kortbaek <kristoffer@kortbaek.dk>
2024-12-18 16:09:20 +0100

    Remove TODOs and add more comments

--- src/Futhark/Optimise/TensorCores.hs
+++ src/Futhark/Optimise/TensorCores.hs
@@ -1,7 +1,5 @@
 module Futhark.Optimise.TensorCores (extractTensorCores, tensorCoreMemFixup) where
 
--- TODO: add specific imports, clean up, reorganize to multiple files
-
 import Control.Monad
 import Control.Monad.Identity
 import Control.Monad.RWS.Strict
@@ -36,7 +34,7 @@ divUp :: Int -> Int -> Int
 divUp x y = (x + y - 1) `div` y
 
 -- | Transforms intragroup kernels corresponding to matrix multiplication into
--- function calls the uses the Tensor Cores.
+-- function calls that use the Tensor Cores.
 extractTensorCores :: Pass GPU GPU
 extractTensorCores =
   Pass
@@ -57,7 +55,6 @@ data ExtractTcEnv = ExtractTcEnv
 
 type TensorCoreM = RWS ExtractTcEnv TcFuns VNameSource
 
--- TODO: use newtype above to fix warning
 instance HasScope GPU TensorCoreM where
   askScope = asks envScope
 
@@ -92,8 +89,6 @@ mkInt64Const :: Int -> SubExp
 mkInt64Const = Constant . IntValue . intValue Int64
 
 -- IR generation
-
--- TODO: pass sizes in args or encoded in name? todo also pass types in args?
 mkGemmFun ::
   (MonadFreshNames m) =>
   PrimType ->
@@ -104,14 +99,30 @@ mkGemmFun ::
   Int ->
   Int ->
   m TcFunDef
--- TODO: use record?
 mkGemmFun elmTypeA elmTypeB elmTypeC sizeM sizeN sizeK sizeRegs = do
-  let typeA = Array elmTypeA (Shape [mkInt64Const sizeM, mkInt64Const sizeK]) Nonunique
-      typeB = Array elmTypeB (Shape [mkInt64Const sizeK, mkInt64Const sizeN]) Nonunique
-      typeCin = Array elmTypeC (Shape [mkInt64Const sizeRegs]) Unique
+  let typeA =
+        Array
+          elmTypeA
+          (Shape [mkInt64Const sizeM, mkInt64Const sizeK])
+          Nonunique
+      typeB =
+        Array
+          elmTypeB
+          (Shape [mkInt64Const sizeK, mkInt64Const sizeN])
+          Nonunique
+      typeCin =
+        Array
+          elmTypeC
+          (Shape [mkInt64Const sizeRegs])
+          Unique
       --  TODO: use Free or Ext?
       typeCout =
-        [ (Array elmTypeC (Shape [Free $ mkInt64Const sizeRegs]) Unique, RetAls [] [])
+        [ ( Array
+              elmTypeC
+              (Shape [Free $ mkInt64Const sizeRegs])
+              Unique,
+            RetAls [] []
+          )
         ]
 
   aParam <- newParam "A" typeA
@@ -148,7 +159,6 @@ mkGemmFun elmTypeA elmTypeB elmTypeC sizeM sizeN sizeK sizeRegs = do
         resultBody [Var $ paramName cParam]
     )
 
--- TODO: entire global as input or only slize, if entire add index as argument
 mkCopyGlobalShared :: (MonadFreshNames m) => PrimType -> Int -> Int -> m TcFunDef
 mkCopyGlobalShared elmType sizeY sizeX = do
   let arrShape =
@@ -191,7 +201,6 @@ mkCopyGlobalShared elmType sizeY sizeX = do
         resultBody [Var $ paramName sharedParam]
     )
 
--- TODO: copy to global instead of shared?
 mkCopyRegistersShared ::
   (MonadFreshNames m) =>
   PrimType ->
@@ -256,7 +265,6 @@ scratchMem :: PrimType -> [Int] -> Exp GPU
 scratchMem elmType dims = BasicOp $ Scratch elmType $ map mkInt64Const dims
 
 buildMMM :: VName -> Int -> TensorCoreMatch -> Builder GPU TcFuns
--- TODO: fix args
 buildMMM
   resName
   actualBlockSize
@@ -269,25 +277,11 @@ buildMMM
     --    let optimalWarpTileM = 64 in
     --    let optimalWarpTileN = 64 in
     --    let (warpsM, warpsN) = (sizeM `divUp` optimalWarpTileM, sizeN `divUp` optimalWarpTileN)
-
+    -- Get the best tile of warps given the block size.
     let (warpsM, warpsN) = getOptimalWarps actualBlockSize match
     let blockSize = warpsM * warpsN * 32
     let cValsPerThread = sizeM * sizeN `div` blockSize
-
-    -- TODO: remove
-    traceM $ "blockSize: " ++ show blockSize
-    traceM $ "actualBlockSize: " ++ show actualBlockSize
-    traceM $ "warpsM: " ++ show warpsM
-    traceM $ "warpsN: " ++ show warpsN
-
-    --  TODO: would be better to check if copy global shared is needed here?
-
-    -- TODO: do we need to init regs when used only once?
-    -- TODO: use SegNoVirtFull instead of loop and avoid setting the block size?
-
-    -- TODO: A and B should probably have same type
-
-    -- TODO: use letSubExp
+    
     let elmTypeC = typeC kernelBodyMatch
         elmTypeB = typeB kernelBodyMatch
         elmTypeA = typeA kernelBodyMatch
@@ -311,13 +305,11 @@ buildMMM
           ]
 
     let thrdInBlock = SegThreadInBlock SegNoVirt
-    -- TODO: make version without regs for dynamic arguments, gemm outputs shared mem no regs as input
     cRegs_list <-
       segMap1D "cRegs" thrdInBlock ResultPrivate (mkInt64Const blockSize) $ \_ -> do
         cScratch <- letExp "cScratch" $ scratchMem elmTypeC [cValsPerThread]
         cLoop <- forLoop (mkInt64Const cValsPerThread) [cScratch] $ \i [cMerge] -> do
           cZeroed <- update "cZeroed" cMerge [i] ne
-          --        TODO: use pure instead of resultBodyM?
           resultBodyM [Var cZeroed]
         pure [varRes cLoop]
     let [cRegs] = cRegs_list
@@ -426,7 +418,6 @@ buildMMM
     inBlockMMAres_list <-
       segMap1D "inBlockMMAres" thrdInBlock ResultPrivate blksize $ \thread_idx -> do
         threadCregs <- index "threadCregs" cRegs [thread_idx]
-        --  TODO: is ObservePrim correct?
         let mmmArgs =
               [ (Var aCopied, ObservePrim),
                 (Var bCopied, ObservePrim),
@@ -549,7 +540,6 @@ transformBody (Body dec stms res) =
 
 transformOp :: Op GPU -> TensorCoreM (Op GPU)
 transformOp (SegOp sOp) = SegOp <$> transformSegOp sOp
--- TODO: handle these separately?
 transformOp op = pure op
 
 transformSegOp :: SegOp SegLevel GPU -> TensorCoreM (SegOp SegLevel GPU)
@@ -566,7 +556,6 @@ transformSegOp
     scope <- askScope
     case execWriter $ runReaderT (maxBlockSizeStms stms) scope of
       Known (Max maxBlockSize) -> do
-        --    TODO: just call transformStms or transformBody?
         transformedBody <-
           localBlockSize (const $ Just maxBlockSize) $
             transformKernelBody body
@@ -657,9 +646,10 @@ getFactorPairs n = [(x, n `div` x) | x <- [1 .. n], n `mod` x == 0]
 getRatio :: Int -> Int -> Float
 getRatio m n = fromIntegral m / fromIntegral n
 
+-- Note: This should in the future depend on the type.
+-- F64 might require a different warp layout than f16 for best performance.
 getOptimalWarps :: Int -> TensorCoreMatch -> (Int, Int)
 getOptimalWarps blockSize (TensorCoreMatch _ _ sizeM sizeN _) =
-  -- TODO: should depend on type
   -- warp tiles will be as close to square as possible, which should maximize register reuse
   let Arg _ (warpsM, warpsN) = minimum $ map ratioDifference usedfactorPairs
    in (warpsM, warpsN)
@@ -680,10 +670,13 @@ getOptimalWarps blockSize (TensorCoreMatch _ _ sizeM sizeN _) =
             else
               factorPairs
 
+-- NOTE: In the future the optimal block size should depend on:
+--  1. The array element type (f16 or f64)
+--  2. Architecture
+--  3. Type of program. Sometimes more threads can be good.
+-- The current estimate might not be optimal in all cases.
 getOptimalBlockSize :: TensorCoreMatch -> Int
-getOptimalBlockSize (TensorCoreMatch _ _ sizeM sizeN _sizeK) =
-  --  TODO: Should also depend on types, check if this is actually always optimal
-  --  TODO: maybe use 2048 instead?
+getOptimalBlockSize (TensorCoreMatch _ _ sizeM sizeN _sizeK) =  
   let optimalElmsPerWarp = 4096
    in ((sizeM * sizeN) `divUp` optimalElmsPerWarp) * 32
 
@@ -698,8 +691,10 @@ data TensorCoreMatch = TensorCoreMatch
   }
   deriving (Show, Eq, Ord)
 
+-- NOTE: The only type currently supported is f16 for A and B and f32 for C.
+-- Future work should also support f64 or maybe even mixed u8/s32.
 data KernelBodyMatch = KernelBodyMatch
-  { --  TODO: add types
+  {
     innerIndecesA :: [VName],
     innerIndecesB :: [VName],
     outerIndecesA :: [SubExp],
@@ -724,12 +719,13 @@ innerSegOpExpMatch _ _ = Nothing
 innerOpMatch :: Scope GPU -> Op GPU -> Maybe TensorCoreMatch
 innerOpMatch
   scope
-  -- TODO: check if better to match segmap with inner reduction
   ( SegOp
       segRed@(SegRed (SegThreadInBlock _) space segBinOps _ts body)
     )
     | Just ne <- segBinOpsMatch segBinOps =
         do
+          -- TODO: Do we need to check the type of ts or can we rely on the
+          -- types in the kernelbody to match?
           let (dimVars, segDims) = unzip $ unSegSpace space
           --  TODO: check this, probably not correct when not maintaining scope
           let freeVars = freeIn segRed
@@ -739,17 +735,17 @@ innerOpMatch
             --  TODO: also match bodyMatch? allow more sizes?
             [m, n, k]
               | all sizeMatches constSegDims ->
-                  --    TODO: pass ts? allow more segDims
                   Just (TensorCoreMatch bodyMatch ne m n k)
             _ -> Nothing
--- TODO: extract A, B, types, shape, layout, lookup m, n, k shapes
 innerOpMatch _ _ = Nothing
 
+-- NOTE: This should definitely depend on the element type when more types are
+-- supported. The current bounds fit within the shared memory when using f16
+-- on the A100.
 sizeMatches :: Int -> Bool
 sizeMatches x =
   x `mod` 16 == 0
     && 0 < x
-    --  TODO: should depend on elm size
     && x <= 128
     --  Check if x is power of 2
     && popCount x == 1
@@ -767,8 +763,6 @@ hasCorrectMatMulOperandType :: Type -> PrimType -> Bool
 hasCorrectMatMulOperandType (Array typ _ _) pt = typ == pt
 hasCorrectMatMulOperandType _ _ = False
 
-
--- TODO: support more than 3 dimensions?
 -- The indexVars corresponds to the variables from the SegSpace
 inBlockKernelBodyMatch ::
   [VName] ->
@@ -783,7 +777,6 @@ inBlockKernelBodyMatch
   scope = do
     let f16_type = FloatType Float16
     let sTable = ST.insertStms (informStms stms) $ ST.fromScope $ addScopeWisdom scope
-    --  TODO: rename to use A, B, C?
     (resExp, _) <- ST.lookupExp res sTable
     -- Check that the result is optionally converted from f16->f32.
     -- In case it is not, then since the program type checks and the
@@ -828,7 +821,6 @@ inBlockKernelBodyMatch
     elemIndex k innerIndeces1 >>= guard . (== 1) -- [m, k] matrix
     elemIndex k innerIndeces2 >>= guard . (== 0) -- [k, n] matrix
     case (arr1Type, arr2Type, resType) of
-      --  TODO: check which is A and which is B?
       (Array type1 (Shape arr1Dims) _, Array type2 (Shape arr2Dims) _, Prim resTypePrim)
         | k == indexVar3 && all (`subExpFreeIn` freeVars) outerIndeces ->
             let arr1OuterDims = take (length arr1Dims - 2) arr1Dims
@@ -853,7 +845,6 @@ inBlockKernelBodyMatch
       _ -> Nothing
 inBlockKernelBodyMatch _ _ _ _ = Nothing
 
--- TODO: Should maybe return SubExp, such that the indexing also can be constants
 getIndexVar :: SubExp -> Maybe VName
 getIndexVar (Var v) = Just v
 getIndexVar _ = Nothing
@@ -871,7 +862,6 @@ matchesMulArg :: Exp GPU -> Maybe (VName, Slice SubExp)
 matchesMulArg (BasicOp (Index v s)) = Just (v, s)
 matchesMulArg _ = Nothing
 
--- TODO: also return binop?
 segBinOpsMatch :: [SegBinOp GPU] -> Maybe SubExp
 segBinOpsMatch [SegBinOp Commutative lambda nes _]
   | lambdaMatch lambda = nesMatch nes
@@ -930,19 +920,14 @@ tensorCoreMemFixup =
 
 -- \$ intraproceduralTransformation fixStmtsWithScope
 
--- TODO: use PassM below?
 type FixEnv = Scope GPUMem
 
 data SpaceType = Device | Shared | Scalar
 
--- TODO: use map?
--- TODO: add mapping like ("gemm_123456", ["shared", "shared", "regs"]) to make general?
 type FixState = [(VName, (VName, Bool))]
 
--- type FixMonad a = RWST FixEnv () FixState PassM a
-type FixMonad a = RWST FixEnv () FixState PassM a
-
--- TODO: With LMAD we could do swizzle
+-- The monad the memory fixup runs within.
+type FixM a = RWST FixEnv () FixState PassM a
 
 fixFuns :: Stms GPUMem -> FunDef GPUMem -> PassM (FunDef GPUMem)
 fixFuns consts fun
@@ -1041,18 +1026,15 @@ extToSubExp :: ExtSize -> SubExp
 extToSubExp (Ext n) = mkInt64Const n
 extToSubExp (Free se) = se
 
--- TODO: use scope, genearate scope?
 fixStmtsWithScope :: Scope GPUMem -> Stms GPUMem -> PassM (Stms GPUMem)
 fixStmtsWithScope scope stms = do
   (res, _, _) <- runRWST (fixStmts stms) scope []
   pure res
 
-fixStmts :: Stms GPUMem -> FixMonad (Stms GPUMem)
+fixStmts :: Stms GPUMem -> FixM (Stms GPUMem)
 fixStmts = mapStmsWithScope fixStmt
 
--- TODO: fix rets without breaking subsequent code, maybe do in explicit allocations pass instead
--- TODO: pass layout info here?
-fixStmt :: Stm GPUMem -> FixMonad (Stms GPUMem)
+fixStmt :: Stm GPUMem -> FixM (Stms GPUMem)
 fixStmt
   stm@( Let
           (Pat [PatElem resName (MemArray _ _ _ (ArrayIn resMem _))])
@@ -1112,14 +1094,13 @@ fixStmt
     --  TODO: check if need to handle uniqueness/consumption
     let newRets = fixRetType Shared rets
     (newArgs, _removedCopy) <- mapAndUnzipM replaceArg args
-    --  TODO: check this, maybe use case instead
     let ((Var srcMemMem, _) : _ : (Var srcArray, _) : _restArgs) = newArgs
     srcMemInfo <- lookupInfo srcMemMem
     case srcMemInfo of
       LetName (MemMem srcMemSpace)
-        | srcMemSpace == space ->
-            -- TODO: try to replace Futhark copy global -> shared with ours in this case?
-            -- Array is already in shared so add to replaceArgs map to remove call to copyGlobalShared
+        | srcMemSpace == space ->            
+            -- Array is already in shared. Therefore, the copyGlobalShared call
+            -- should be removed and we return removedCopy=True.
             modify ([(vName2, (srcArray, True)), (vName1, (srcMemMem, True))] <>)
       _ ->
         pure ()
@@ -1133,7 +1114,7 @@ fixStmt
         )
         aux
       $ Apply fName newArgs newRets info
--- TODO: check this
+
 fixStmt
   ( Let
       ( Pat
@@ -1159,7 +1140,7 @@ fixStmt
           (Apply fName newArgs newRets info)
 fixStmt stm = defaultFixStm stm
 
-defaultFixStm :: Stm GPUMem -> FixMonad (Stms GPUMem)
+defaultFixStm :: Stm GPUMem -> FixM (Stms GPUMem)
 defaultFixStm (Let pat aux e) = do
   e' <- fixExp e
   pure $ oneStm $ Let pat aux e'
@@ -1181,7 +1162,7 @@ boolToInt False = 0
 -- kernel code!!!.
 -- In case the argument was not caused by a manifest statement, it might already
 -- be in shared memory. The removedCopy indicates if a manifest was removed.
-replaceArg :: (SubExp, Diet) -> FixMonad ((SubExp, Diet), Bool)
+replaceArg :: (SubExp, Diet) -> FixM ((SubExp, Diet), Bool)
 replaceArg (Var v, d) = do
   manifestMap <- get
   case lookup v manifestMap of
@@ -1191,8 +1172,7 @@ replaceArg (Var v, d) = do
       pure ((Var v, d), False)
 replaceArg a = pure (a, False)
 
--- TODO: add stuff to scope in other places
-fixExp :: Exp GPUMem -> FixMonad (Exp GPUMem)
+fixExp :: Exp GPUMem -> FixM (Exp GPUMem)
 fixExp (Match subExps cases body matchDec) =
   Match subExps
   <$> mapM fixCase cases
@@ -1205,23 +1185,23 @@ fixExp (Loop params form body) =
 fixExp (Op op) = Op <$> fixOp op
 fixExp e = pure e
 
-fixCase :: Case (Body GPUMem) -> FixMonad (Case (Body GPUMem))
+fixCase :: Case (Body GPUMem) -> FixM (Case (Body GPUMem))
 fixCase (Case pat body) = Case pat <$> fixBody body
 
-fixBody :: Body GPUMem -> FixMonad (Body GPUMem)
+fixBody :: Body GPUMem -> FixM (Body GPUMem)
 fixBody (Body dec stms res) = Body dec <$> fixStmts stms <*> pure res
 
-fixOp :: Op GPUMem -> FixMonad (Op GPUMem)
+fixOp :: Op GPUMem -> FixM (Op GPUMem)
 fixOp (Inner hostOp) = Inner <$> fixHostOp hostOp
 -- TODO: recurse here?
 fixOp op = pure op
 
-fixHostOp :: HostOp NoOp GPUMem -> FixMonad (HostOp NoOp GPUMem)
+fixHostOp :: HostOp NoOp GPUMem -> FixM (HostOp NoOp GPUMem)
 fixHostOp (SegOp op) = SegOp <$> fixSegOp op
 -- TODO: run recurisvely?
 fixHostOp op = pure op
 
-fixSegOp :: SegOp SegLevel GPUMem -> FixMonad (SegOp SegLevel GPUMem)
+fixSegOp :: SegOp SegLevel GPUMem -> FixM (SegOp SegLevel GPUMem)
 fixSegOp (SegMap level space ts body) =
   SegMap level space ts <$> fixKernelBody body
 fixSegOp (SegRed level space ops ts body) =
@@ -1231,6 +1211,6 @@ fixSegOp (SegScan level space ops ts body) =
 fixSegOp (SegHist level space ops hist body) =
   SegHist level space ops hist <$> fixKernelBody body
 
-fixKernelBody :: KernelBody GPUMem -> FixMonad (KernelBody GPUMem)
+fixKernelBody :: KernelBody GPUMem -> FixM (KernelBody GPUMem)
 fixKernelBody (KernelBody desc stms res) =
   KernelBody desc <$> fixStmts stms <*> pure res
