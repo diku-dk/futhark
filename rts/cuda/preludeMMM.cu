b046acd00f673e0f01de3e38e5f776a62981676a
TheRune22 <rune.e.b.lejboelle@gmail.com>
2024-12-18 17:00:15 +0100

    Cleaned prelude

--- rts/cuda/preludeMMM.cu
+++ rts/cuda/preludeMMM.cu
@@ -13,15 +13,15 @@ struct convert_type<f16> {
 template<class ElmTypeAIn, class ElmTypeBIn, class ElmTypeCIn, class SizeM, class SizeN, class WarpsM, class WarpsN>
 struct get_mma_config {};
 
-// TODO: add fallback FMA?
+// TODO: use FMA when Tensor Cores not available?
 
 template<class SizeM, class SizeN, class WarpsM, class WarpsN>
 struct get_mma_config<half_t, half_t, half_t, SizeM, SizeN, WarpsM, WarpsN> {
-    // TODO: should depend on type and arch
+    // TODO: should depend on architecture available
     using MMATraits = MMA_Traits<SM80_16x8x16_F16F16F16F16_TN>;
-
     using ACopyOpSharedRegisters = SM75_U32x4_LDSM_N;
     using BCopyOpSharedRegisters = SM75_U16x8_LDSM_T;
+
     using MMATile = Tile<Int<16 * WarpsM{}>, Int<16 * WarpsN{}>, _16>;
     using TiledMMA = TiledMMA<
         MMA_Atom<MMATraits>,
@@ -32,11 +32,11 @@ struct get_mma_config<half_t, half_t, half_t, SizeM, SizeN, WarpsM, WarpsN> {
 
 template<class SizeM, class SizeN, class WarpsM, class WarpsN>
 struct get_mma_config<half_t, half_t, float, SizeM, SizeN, WarpsM, WarpsN>{
-    // TODO: should depend on type and arch
+    // TODO: should depend on architecture available
     using MMATraits = MMA_Traits<SM80_16x8x16_F32F16F16F32_TN>;
-
     using ACopyOpSharedRegisters = SM75_U32x4_LDSM_N;
     using BCopyOpSharedRegisters = SM75_U16x8_LDSM_T;
+
     using MMATile = Tile<Int<16 * WarpsM{}>, Int<16 * WarpsN{}>, _16>;
     using TiledMMA = TiledMMA<
         MMA_Atom<MMATraits>,
@@ -68,8 +68,6 @@ struct get_layout_config<SizeY, SizeX, _0, LayoutLeft, shift_len>{
     using SharedLayout = Layout<Shape<SizeY, SizeX>, Stride<_1, SizeY>>;
 };
 
-
-// TODO: forceinline?
 template<class ElmTypeIn, class SizeY, class SizeX, class WarpsM, class WarpsN>
 FUTHARK_FUN_ATTR void futrts_copyGlobalShared(unsigned char **mem_out_p, unsigned char *global_mem, unsigned char *shared_mem, int64_t offset, ElmTypeIn, SizeY, SizeX, WarpsM, WarpsN)
 {
@@ -80,9 +78,6 @@ FUTHARK_FUN_ATTR void futrts_copyGlobalShared(unsigned char **mem_out_p, unsigne
     if (flatThreadIdx < WarpsM{} * WarpsN{} * 32) {
       using ElmType = typename convert_type<ElmTypeIn>::TypeOut;
 
-  // TODO: check compute capability, check if transposed?
-  //     using CopyOpGlobalShared = UniversalCopy<ElmType>;
-  //     using CopyOpGlobalShared = UniversalCopy<uint128_t>;
       using CopyOpGlobalShared = SM80_CP_ASYNC_CACHEGLOBAL<uint128_t>;
 
       constexpr int elmsPerLoad = 16 / sizeof(ElmType);
@@ -127,7 +122,6 @@ FUTHARK_FUN_ATTR void futrts_copyRegistersShared(unsigned char **mem_out_p, ElmT
     int flatThreadIdx = threadIdx.z * blockDim.y * blockDim.x + threadIdx.y * blockDim.x + threadIdx.x;
 
     if (flatThreadIdx < WarpsM{} * WarpsN{} * 32) {
-
         using ElmTypeA = typename convert_type<ElmTypeAIn>::TypeOut;
         using ElmTypeB = typename convert_type<ElmTypeBIn>::TypeOut;
         using ElmTypeC = typename convert_type<ElmTypeCIn>::TypeOut;
@@ -139,16 +133,12 @@ FUTHARK_FUN_ATTR void futrts_copyRegistersShared(unsigned char **mem_out_p, ElmT
 
         ThrMMA thr_mma = tiled_mma.get_slice(flatThreadIdx);
 
-        auto rC_layout = partition_shape_C(thr_mma, s_layout.shape());
-        Tensor tCrC = make_tensor(make_rmem_ptr(reinterpret_cast<ElmTypeC *>(registers_mem)), rC_layout);
+        auto r_layout = partition_shape_C(thr_mma, s_layout.shape());
+        Tensor tCrC = make_tensor(make_rmem_ptr(reinterpret_cast<ElmTypeC *>(registers_mem)), r_layout);
 
         Tensor s = make_tensor(make_smem_ptr(reinterpret_cast<ElmTypeC *>(shared_mem)), s_layout);
         Tensor tCsC = thr_mma.partition_C(s);
 
-        //  TODO: take as input, or just use copy instead?
-//         auto alpha = _1{};
-//         auto beta = _0{};
-//         axpby(alpha, tCrC, beta, tCsC);
         copy(AutoVectorizingCopy{}, tCrC, tCsC);
     }
     __syncthreads();
@@ -187,37 +177,33 @@ FUTHARK_FUN_ATTR void futrts_tensorMMM(ElmTypeCIn (*mem_out_p)[numRegs], unsigne
     Tensor sA = make_tensor(make_smem_ptr(reinterpret_cast<ElmTypeA *>(A_mem)), sA_layout);
     Tensor sB = make_tensor(make_smem_ptr(reinterpret_cast<ElmTypeB *>(B_mem)), sB_layout);
 
-    TiledCopy smem_tiled_copy_A = make_tiled_copy_A(Copy_Atom<typename MMAConfig::ACopyOpSharedRegisters, ElmTypeA>{}, tiled_mma);
-    TiledCopy smem_tiled_copy_B = make_tiled_copy_B(Copy_Atom<typename MMAConfig::BCopyOpSharedRegisters, ElmTypeB>{}, tiled_mma);
+    TiledCopy copyA_shared_registers = make_tiled_copy_A(Copy_Atom<typename MMAConfig::ACopyOpSharedRegisters, ElmTypeA>{}, tiled_mma);
+    TiledCopy copyB_shared_registers = make_tiled_copy_B(Copy_Atom<typename MMAConfig::BCopyOpSharedRegisters, ElmTypeB>{}, tiled_mma);
 
-    // Create register tensors for the MMA to operate on
     Tensor tCrA  = thr_mma.partition_fragment_A(sA);
     Tensor tCrB  = thr_mma.partition_fragment_B(sB);
 
-    auto smem_thr_copy_A   = smem_tiled_copy_A.get_thread_slice(threadIdx.x);
+    auto smem_thr_copy_A   = copyA_shared_registers.get_thread_slice(threadIdx.x);
     Tensor tCsA            = smem_thr_copy_A.partition_S(sA);
     Tensor tCrA_copy_view  = smem_thr_copy_A.retile_D(tCrA);
 
-    auto smem_thr_copy_B   = smem_tiled_copy_B.get_thread_slice(threadIdx.x);
+    auto smem_thr_copy_B   = copyB_shared_registers.get_thread_slice(threadIdx.x);
     Tensor tCsB            = smem_thr_copy_B.partition_S(sB);
     Tensor tCrB_copy_view  = smem_thr_copy_B.retile_D(tCrB);
-
-    // TODO: add tDrD? try cooperative gemm
 
     // Wait for data copied asynchronously by futrts_copyGlobalShared
     cp_async_wait<0>();
     __syncthreads();
 
-    // Inner loop
     constexpr int K_BLOCK_MAX = size<2>(tCrA);
     CUTE_UNROLL
     for (int k_block = 0; k_block < K_BLOCK_MAX; ++k_block)
     {
-        // Copy shared -> registers
-        copy(smem_tiled_copy_A, tCsA(_,_,k_block), tCrA_copy_view(_,_,k_block));
-        copy(smem_tiled_copy_B, tCsB(_,_,k_block), tCrB_copy_view(_,_,k_block));
+        // Copy shared->registers
+        copy(copyA_shared_registers, tCsA(_,_,k_block), tCrA_copy_view(_,_,k_block));
+        copy(copyB_shared_registers, tCsB(_,_,k_block), tCrB_copy_view(_,_,k_block));
 
-        // GEMM on k_block in registers
+        // Perform mma on k_block in registers
         gemm(tiled_mma, tCrA(_,_,k_block), tCrB(_,_,k_block), tCrC);
     }
 
