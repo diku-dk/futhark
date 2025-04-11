0b438a1e88dba092cd3ab3456dce0d5dfe63f1d4
Troels Henriksen <athas@sigkill.dk>
2025-04-11 10:32:23 +0200

    Fiddle more.

--- rts/cuda/prelude.cu
+++ rts/cuda/prelude.cu
@@ -4,14 +4,14 @@
 #define FUTHARK_FUN_ATTR __device__ static
 #define FUTHARK_F64_ENABLED
 
-typedef __int8_t int8_t;
-typedef __int16_t int16_t;
-typedef __int32_t int32_t;
-typedef __int64_t int64_t;
-typedef __uint8_t uint8_t;
-typedef __uint16_t uint16_t;
-typedef __uint32_t uint32_t;
-typedef __uint64_t uint64_t;
+typedef char int8_t;
+typedef short int16_t;
+typedef int int32_t;
+typedef long long int64_t;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
 
 #define __global
 #define __local
