684dc35f573e9cf06d3d78f2b6254e5ac9467873
Troels Henriksen <athas@sigkill.dk>
2025-04-11 10:34:56 +0200

    Match types better.

--- rts/cuda/prelude.cu
+++ rts/cuda/prelude.cu
@@ -4,14 +4,14 @@
 #define FUTHARK_FUN_ATTR __device__ static
 #define FUTHARK_F64_ENABLED
 
-typedef char int8_t;
+typedef signed char int8_t;
 typedef short int16_t;
 typedef int int32_t;
-typedef long long int64_t;
+typedef long int64_t;
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
+typedef unsigned long uint64_t;
 
 #define __global
 #define __local
