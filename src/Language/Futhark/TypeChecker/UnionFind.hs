bf75ff8aeb16a2dce6a5c00584259830bd13a27f
Jacob Siegumfeldt <jacobi7th@gmail.com>
2025-04-13 21:50:59 +0200

    Update todo

--- src/Language/Futhark/TypeChecker/UnionFind.hs
+++ src/Language/Futhark/TypeChecker/UnionFind.hs
@@ -99,8 +99,8 @@ assignType node t = do
 --   r <- descrRef node
 --   modifySTRef r $ \i -> i { descr = f (descr i) }
 
--- TODO: Make sure we correctly handle level, liftedness, and if it's a
--- TODO: type parameter.
+-- TODO: Make sure we correctly handle level, liftedness, and if 
+-- TODO: type parameters are involved.
 -- | Join the equivalence classes of the nodes.
 union :: VarNode s -> VarNode s -> ST s ()
 union n1 n2 = do
