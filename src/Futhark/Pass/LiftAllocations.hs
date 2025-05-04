1da262e1866261203314d5ae211db06b2c46bec2
Troels Henriksen <athas@sigkill.dk>
2025-05-04 19:52:50 +0200

    Make LiftAllocations properly alias-aware.
    It is somewhat tragic that correct treatment of aliases is still such a manual
    endeavour. LiftAllocations suffers more than most by its bottom-up operation,
    however.

--- src/Futhark/Pass/LiftAllocations.hs
+++ src/Futhark/Pass/LiftAllocations.hs
@@ -13,6 +13,7 @@ module Futhark.Pass.LiftAllocations
 where
 
 import Control.Monad.Reader
+import Data.Map qualified as M
 import Data.Sequence (Seq (..))
 import Futhark.Analysis.Alias (aliasAnalysis)
 import Futhark.IR.Aliases
@@ -32,7 +33,7 @@ liftInProg onOp prog =
     }
   where
     onFun f = f {funDefBody = onBody (funDefBody f)}
-    onBody body = runReader (liftAllocationsInBody body) (Env onOp)
+    onBody body = runReader (liftAllocationsInBody body) (Env onOp mempty)
 
 liftAllocationsSeqMem :: Pass SeqMem SeqMem
 liftAllocationsSeqMem =
@@ -49,8 +50,10 @@ liftAllocationsMCMem =
   Pass "lift allocations mc" "lift allocations mc" $
     pure . liftInProg liftAllocationsInMCOp
 
-newtype Env inner = Env
-  {onInner :: inner -> LiftM inner inner}
+data Env inner = Env
+  { onInner :: inner -> LiftM inner inner,
+    envAliases :: AliasesAndConsumed
+  }
 
 type LiftM inner a = Reader (Env inner) a
 
@@ -59,7 +62,7 @@ liftAllocationsInBody ::
   Body rep ->
   LiftM (inner rep) (Body rep)
 liftAllocationsInBody body = do
-  stms <- liftAllocationsInStms (bodyStms body) mempty mempty mempty
+  stms <- liftAllocationsInStms (bodyStms body)
   pure $ body {bodyStms = stms}
 
 liftInsideStm ::
@@ -80,54 +83,65 @@ liftInsideStm stm@(Let _ _ (Loop params form body)) = do
 liftInsideStm stm = pure stm
 
 liftAllocationsInStms ::
+  forall rep inner.
   (Mem rep inner, Aliased rep) =>
-  -- | The input stms
   Stms rep ->
-  -- | The lifted allocations and associated statements
-  Stms rep ->
-  -- | The other statements processed so far
-  Stms rep ->
-  -- | (Names we need to lift, consumed names)
-  (Names, Names) ->
   LiftM (inner rep) (Stms rep)
-liftAllocationsInStms Empty lifted acc _ = pure $ lifted <> acc
-liftAllocationsInStms (stms :|> stm) lifted acc (to_lift, consumed) = do
-  stm' <- liftInsideStm stm
-  case stmExp stm' of
-    BasicOp Assert {} -> liftStm stm'
-    Op Alloc {} -> liftStm stm'
-    _ -> do
-      let pat_names = namesFromList $ patNames $ stmPat stm'
-      if (pat_names `namesIntersect` to_lift)
-        || namesIntersect consumed (freeIn stm)
-        then liftStm stm'
-        else dontLiftStm stm'
-  where
-    liftStm stm' =
-      liftAllocationsInStms stms (stm' :<| lifted) acc (to_lift', consumed')
-      where
-        to_lift' =
-          freeIn stm'
-            <> (to_lift `namesSubtract` namesFromList (patNames (stmPat stm')))
-        consumed' = consumed <> consumedInStm stm'
-    dontLiftStm stm' =
-      liftAllocationsInStms stms lifted (stm' :<| acc) (to_lift, consumed)
+liftAllocationsInStms stms_orig = do
+  outer_aliases <- asks envAliases
+  let aliases = foldl trackAliases outer_aliases stms_orig
+      go ::
+        -- The input stms
+        Stms rep ->
+        -- The lifted allocations and associated statements
+        Stms rep ->
+        -- The other statements processed so far
+        Stms rep ->
+        -- (Names we need to lift, consumed names)
+        (Names, Names) ->
+        LiftM (inner rep) (Stms rep)
+      go Empty lifted acc _ = pure $ lifted <> acc
+      go (stms :|> stm) lifted acc (to_lift, consumed) = do
+        stm' <- liftInsideStm stm
+        case stmExp stm' of
+          BasicOp Assert {} -> liftStm stm'
+          Op Alloc {} -> liftStm stm'
+          _ -> do
+            let pat_names = namesFromList $ patNames $ stmPat stm'
+                free_in_stm = freeIn stm
+                expand v = maybe [v] namesToList $ M.lookup v $ fst aliases
+            if (pat_names `namesIntersect` to_lift)
+              || any (`nameIn` free_in_stm) (foldMap expand $ namesToList consumed)
+              then liftStm stm'
+              else dontLiftStm stm'
+        where
+          liftStm stm' =
+            go stms (stm' :<| lifted) acc (to_lift', consumed')
+            where
+              to_lift' =
+                freeIn stm'
+                  <> (to_lift `namesSubtract` namesFromList (patNames (stmPat stm')))
+              consumed' = consumed <> consumedInStm stm'
+          dontLiftStm stm' =
+            go stms lifted (stm' :<| acc) (to_lift, consumed)
+  local (\env -> env {envAliases = aliases}) $
+    go stms_orig mempty mempty mempty
 
 liftAllocationsInSegOp ::
   (Mem rep inner, Aliased rep) =>
   SegOp lvl rep ->
   LiftM (inner rep) (SegOp lvl rep)
 liftAllocationsInSegOp (SegMap lvl sp tps body) = do
-  stms <- liftAllocationsInStms (kernelBodyStms body) mempty mempty mempty
+  stms <- liftAllocationsInStms (kernelBodyStms body)
   pure $ SegMap lvl sp tps $ body {kernelBodyStms = stms}
 liftAllocationsInSegOp (SegRed lvl sp tps body binops) = do
-  stms <- liftAllocationsInStms (kernelBodyStms body) mempty mempty mempty
+  stms <- liftAllocationsInStms (kernelBodyStms body)
   pure $ SegRed lvl sp tps (body {kernelBodyStms = stms}) binops
 liftAllocationsInSegOp (SegScan lvl sp tps body binops) = do
-  stms <- liftAllocationsInStms (kernelBodyStms body) mempty mempty mempty
+  stms <- liftAllocationsInStms (kernelBodyStms body)
   pure $ SegScan lvl sp tps (body {kernelBodyStms = stms}) binops
 liftAllocationsInSegOp (SegHist lvl sp tps body histops) = do
-  stms <- liftAllocationsInStms (kernelBodyStms body) mempty mempty mempty
+  stms <- liftAllocationsInStms (kernelBodyStms body)
   pure $ SegHist lvl sp tps (body {kernelBodyStms = stms}) histops
 
 liftAllocationsInHostOp ::
