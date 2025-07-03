4ad19c891b96392b05e88de96c9232581454b2ac
Troels Henriksen <athas@sigkill.dk>
2025-07-03 22:57:59 +0200

    Bump Python.

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
-  python = pkgs.python312Packages;
+  python = pkgs.python313Packages;
   haskell = pkgs.haskell.packages.ghc98;
 in
 pkgs.stdenv.mkDerivation {
