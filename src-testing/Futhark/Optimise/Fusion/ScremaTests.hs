e769e7ba7ce1f5f7467c1171bee1ca4287d7d382
Troels Henriksen <athas@sigkill.dk>
2025-08-28 17:10:39 +0200

    Put some sanity in tests.

--- src-testing/Futhark/Optimise/Fusion/ScremaTests.hs
+++ src-testing/Futhark/Optimise/Fusion/ScremaTests.hs
@@ -1,129 +1,175 @@
 module Futhark.Optimise.Fusion.ScremaTests (tests) where
 
+import Data.String (fromString)
 import Futhark.Analysis.HORep.SOAC as SOAC
+import Futhark.IR.SOACS
 import Futhark.IR.SOACSTests ()
 import Futhark.Optimise.Fusion.Screma (fuseLambda, splitLambdaByPar, splitLambdaByRes)
 import Test.Tasty
 import Test.Tasty.HUnit
 
+fromLines :: [String] -> Lambda SOACS
+fromLines = fromString . unlines
+
 tests :: TestTree
 tests =
   testGroup
-    "FuseScrema"
-    [ testCase "map-map" $
-        let lam_c = "\\{x_0 : i32} : {i32} -> {x_0}"
-            inp_c = [SOAC.Input mempty "xs_1" "[10i64]i32"]
-            out_c = ["ys_3"]
-            lam_p = "\\{x_2 : i32} : {i32} -> {x_2}"
-            out_p = ["zs_4"]
-         in fuseLambda lam_c inp_c out_c lam_p out_p
-              @?= Just
-                ( [SOAC.Input mempty "xs_1" "[10i64]i32"],
-                  "\\{x_2 : i32, x_0 : i32} : {i32,i32} -> {x_2, x_0}",
-                  ["zs_4", "ys_3"]
-                ),
-      testCase "splitLamdaByPar" $
-        let lam = "\\{x_0 : i32, x_1 : i32} : {i32, i32} -> {x_0, x_1}"
-            lam_x = "\\{x_0 : i32} : {i32} -> {x_0}"
-            lam_y = "\\{x_1 : i32} : {i32} -> {x_1}"
-            names = ["x_0"]
-         in splitLambdaByPar names lam @?= (lam_x, lam_y),
-      testCase "splitLamdaByPar" $
-        let lam =
-              "\\{x_0 : i32, x_1 : i32} : {i32, i32} -> \
-              \  let {x_2 : i32} = add32(x_0, x_1) \
-              \  in {x_0, x_2}"
-            lam_x =
-              "\\{x_0 : i32, x_1 : i32} : {i32} -> \
-              \  let {x_2 : i32} = add32(x_0, x_1) \
-              \  in {x_2}"
-            lam_y = "\\{x_0 : i32} : {i32} -> {x_0}"
-            names = ["x_1"]
-         in splitLambdaByPar names lam @?= (lam_x, lam_y),
-      testCase "splitLamdaByPar" $
-        let lam =
-              "\\{x_0 : i32, x_1 : i32} : {i32, i32} -> \
-              \  let {x_2 : i32} = add32(x_0, x_1) \
-              \  let {x_3 : i32} = add32(1i32, x_0) \
-              \  in {x_3, x_2}"
-            lam_x =
-              "\\{x_0 : i32, x_1 : i32} : {i32} -> \
-              \  let {x_2 : i32} = add32(x_0, x_1) \
-              \  in {x_2}"
-            lam_y =
-              "\\{x_0 : i32} : {i32} -> \
-              \  let {x_3 : i32} = add32(1i32, x_0) \
-              \  in {x_3}"
-            names = ["x_1"]
-         in splitLambdaByPar names lam @?= (lam_x, lam_y),
-      testCase "splitLamdaByPar" $
-        let lam =
-              "\\{x_0 : i32, x_1 : i32} : {i32, i32} -> \
-              \  let {x_3 : i32} = add32(1i32, x_0) \
-              \  let {x_2 : i32} = add32(x_0, x_1) \
-              \  let {x_4 : i32} = add32(1i32, x_3) \
-              \  in {x_4, x_2}"
-            lam_x =
-              "\\{x_0 : i32, x_1 : i32} : {i32} -> \
-              \  let {x_2 : i32} = add32(x_0, x_1) \
-              \  in {x_2}"
-            lam_y =
-              "\\{x_0 : i32} : {i32} -> \
-              \  let {x_3 : i32} = add32(1i32, x_0) \
-              \  let {x_4 : i32} = add32(1i32, x_3) \
-              \  in {x_4}"
-            names = ["x_1"]
-         in splitLambdaByPar names lam @?= (lam_x, lam_y),
-      testCase "splitLamdaByRes" $
-        let lam = "\\{x_0 : i32, x_1 : i32} : {i32, i32} -> {x_0, x_1}"
-            lam_x = "\\{x_0 : i32} : {i32} -> {x_0}"
-            lam_y = "\\{x_1 : i32} : {i32} -> {x_1}"
-            names = ["x_0"]
-         in splitLambdaByRes names lam @?= (lam_x, lam_y),
-      testCase "splitLamdaByRes" $
-        let lam =
-              "\\{x_0 : i32, x_1 : i32} : {i32, i32} -> \
-              \  let {x_2 : i32} = add32(x_0, x_1) \
-              \  in {x_0, x_2}"
-            lam_x =
-              "\\{x_0 : i32, x_1 : i32} : {i32} -> \
-              \  let {x_2 : i32} = add32(x_0, x_1) \
-              \  in {x_2}"
-            lam_y = "\\{x_0 : i32} : {i32} -> {x_0}"
-            names = ["x_2"]
-         in splitLambdaByRes names lam @?= (lam_x, lam_y),
-      testCase "splitLamdaByRes" $
-        let lam =
-              "\\{x_0 : i32, x_1 : i32} : {i32, i32} -> \
-              \  let {x_2 : i32} = add32(x_0, x_1) \
-              \  let {x_3 : i32} = add32(1i32, x_0) \
-              \  in {x_3, x_2}"
-            lam_x =
-              "\\{x_0 : i32, x_1 : i32} : {i32} -> \
-              \  let {x_2 : i32} = add32(x_0, x_1) \
-              \  in {x_2}"
-            lam_y =
-              "\\{x_0 : i32} : {i32} -> \
-              \  let {x_3 : i32} = add32(1i32, x_0) \
-              \  in {x_3}"
-            names = ["x_2"]
-         in splitLambdaByRes names lam @?= (lam_x, lam_y),
-      testCase "splitLamdaByRes" $
-        let lam =
-              "\\{x_0 : i32, x_1 : i32} : {i32, i32} -> \
-              \  let {x_3 : i32} = add32(1i32, x_0) \
-              \  let {x_2 : i32} = add32(x_0, x_1) \
-              \  let {x_4 : i32} = add32(1i32, x_3) \
-              \  in {x_4, x_2}"
-            lam_x =
-              "\\{x_0 : i32, x_1 : i32} : {i32} -> \
-              \  let {x_2 : i32} = add32(x_0, x_1) \
-              \  in {x_2}"
-            lam_y =
-              "\\{x_0 : i32} : {i32} -> \
-              \  let {x_3 : i32} = add32(1i32, x_0) \
-              \  let {x_4 : i32} = add32(1i32, x_3) \
-              \  in {x_4}"
-            names = ["x_2"]
-         in splitLambdaByRes names lam @?= (lam_x, lam_y)
+    "ScremaTests"
+    [ testGroup
+        "FuseScrema"
+        [ testCase "map-map" $
+            let lam_c = "\\{x_0 : i32} : {i32} -> {x_0}"
+                inp_c = [SOAC.Input mempty "xs_1" "[10i64]i32"]
+                out_c = ["ys_3"]
+                lam_p = "\\{x_2 : i32} : {i32} -> {x_2}"
+                out_p = ["zs_4"]
+             in fuseLambda lam_c inp_c out_c lam_p out_p
+                  @?= Just
+                    ( [SOAC.Input mempty "xs_1" "[10i64]i32"],
+                      "\\{x_2 : i32, x_0 : i32} : {i32,i32} -> {x_2, x_0}",
+                      ["zs_4", "ys_3"]
+                    )
+        ],
+      testGroup
+        "splitLambdaByPar"
+        [ testCase "no good name" $
+            let lam = "\\{x_0 : i32, x_1 : i32} : {i32, i32} -> {x_0, x_1}"
+                lam_x = "\\{x_0 : i32} : {i32} -> {x_0}"
+                lam_y = "\\{x_1 : i32} : {i32} -> {x_1}"
+                names = ["x_0"]
+             in splitLambdaByPar names lam @?= (lam_x, lam_y),
+          testCase "no good name" $
+            let lam =
+                  fromLines
+                    [ "\\{x_0 : i32, x_1 : i32} : {i32, i32} ->",
+                      "  let {x_2 : i32} = add32(x_0, x_1)",
+                      "  in {x_0, x_2}"
+                    ]
+                lam_x =
+                  fromLines
+                    [ "\\{x_0 : i32, x_1 : i32} : {i32} ->",
+                      "  let {x_2 : i32} = add32(x_0, x_1)",
+                      "  in {x_2}"
+                    ]
+                lam_y = "\\{x_0 : i32} : {i32} -> {x_0}"
+                names = ["x_1"]
+             in splitLambdaByPar names lam @?= (lam_x, lam_y),
+          testCase "no good name" $
+            let lam =
+                  fromLines
+                    [ "\\{x_0 : i32, x_1 : i32} : {i32, i32} -> ",
+                      "  let {x_2 : i32} = add32(x_0, x_1) ",
+                      "  let {x_3 : i32} = add32(1i32, x_0) ",
+                      "  in {x_3, x_2}"
+                    ]
+                lam_x =
+                  fromLines
+                    [ "\\{x_0 : i32, x_1 : i32} : {i32} -> ",
+                      "  let {x_2 : i32} = add32(x_0, x_1) ",
+                      "  in {x_2}"
+                    ]
+                lam_y =
+                  fromLines
+                    [ "\\{x_0 : i32} : {i32} -> ",
+                      "  let {x_3 : i32} = add32(1i32, x_0) ",
+                      "  in {x_3}"
+                    ]
+                names = ["x_1"]
+             in splitLambdaByPar names lam @?= (lam_x, lam_y),
+          testCase "no good name" $
+            let lam =
+                  fromLines
+                    [ "\\{x_0 : i32, x_1 : i32} : {i32, i32} -> ",
+                      "  let {x_3 : i32} = add32(1i32, x_0) ",
+                      "  let {x_2 : i32} = add32(x_0, x_1) ",
+                      "  let {x_4 : i32} = add32(1i32, x_3) ",
+                      "  in {x_4, x_2}"
+                    ]
+                lam_x =
+                  fromLines
+                    [ "\\{x_0 : i32, x_1 : i32} : {i32} -> ",
+                      "  let {x_2 : i32} = add32(x_0, x_1) ",
+                      "  in {x_2}"
+                    ]
+                lam_y =
+                  fromLines
+                    [ "\\{x_0 : i32} : {i32} -> ",
+                      "  let {x_3 : i32} = add32(1i32, x_0) ",
+                      "  let {x_4 : i32} = add32(1i32, x_3) ",
+                      "  in {x_4}"
+                    ]
+                names = ["x_1"]
+             in splitLambdaByPar names lam @?= (lam_x, lam_y)
+        ],
+      testGroup
+        "splitLambdaByRes"
+        [ testCase "no good name" $
+            let lam = "\\{x_0 : i32, x_1 : i32} : {i32, i32} -> {x_0, x_1}"
+                lam_x = "\\{x_0 : i32} : {i32} -> {x_0}"
+                lam_y = "\\{x_1 : i32} : {i32} -> {x_1}"
+                names = ["x_0"]
+             in splitLambdaByRes names lam @?= (lam_x, lam_y),
+          testCase "no good name" $
+            let lam =
+                  fromLines
+                    [ "\\{x_0 : i32, x_1 : i32} : {i32, i32} -> ",
+                      "  let {x_2 : i32} = add32(x_0, x_1) ",
+                      "  in {x_0, x_2}"
+                    ]
+                lam_x =
+                  fromLines
+                    [ "\\{x_0 : i32, x_1 : i32} : {i32} -> ",
+                      "  let {x_2 : i32} = add32(x_0, x_1) ",
+                      "  in {x_2}"
+                    ]
+                lam_y = "\\{x_0 : i32} : {i32} -> {x_0}"
+                names = ["x_2"]
+             in splitLambdaByRes names lam @?= (lam_x, lam_y),
+          testCase "no good name" $
+            let lam =
+                  fromLines
+                    [ "\\{x_0 : i32, x_1 : i32} : {i32, i32} -> ",
+                      "  let {x_2 : i32} = add32(x_0, x_1) ",
+                      "  let {x_3 : i32} = add32(1i32, x_0) ",
+                      "  in {x_3, x_2}"
+                    ]
+                lam_x =
+                  fromLines
+                    [ "\\{x_0 : i32, x_1 : i32} : {i32} -> ",
+                      "  let {x_2 : i32} = add32(x_0, x_1) ",
+                      "  in {x_2}"
+                    ]
+                lam_y =
+                  fromLines
+                    [ "\\{x_0 : i32} : {i32} -> ",
+                      "  let {x_3 : i32} = add32(1i32, x_0) ",
+                      "  in {x_3}"
+                    ]
+                names = ["x_2"]
+             in splitLambdaByRes names lam @?= (lam_x, lam_y),
+          testCase "no good name" $
+            let lam =
+                  fromLines
+                    [ "\\{x_0 : i32, x_1 : i32} : {i32, i32} -> ",
+                      "  let {x_3 : i32} = add32(1i32, x_0) ",
+                      "  let {x_2 : i32} = add32(x_0, x_1) ",
+                      "  let {x_4 : i32} = add32(1i32, x_3) ",
+                      "  in {x_4, x_2}"
+                    ]
+                lam_x =
+                  fromLines
+                    [ "\\{x_0 : i32, x_1 : i32} : {i32} -> ",
+                      "  let {x_2 : i32} = add32(x_0, x_1) ",
+                      "  in {x_2}"
+                    ]
+                lam_y =
+                  fromLines
+                    [ "\\{x_0 : i32} : {i32} -> ",
+                      "  let {x_3 : i32} = add32(1i32, x_0) ",
+                      "  let {x_4 : i32} = add32(1i32, x_3) ",
+                      "  in {x_4}"
+                    ]
+                names = ["x_2"]
+             in splitLambdaByRes names lam @?= (lam_x, lam_y)
+        ]
     ]
