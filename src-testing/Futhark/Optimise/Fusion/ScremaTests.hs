9ef1275145802ee6847e956a84d24ff31daf6e7f
Troels Henriksen <athas@sigkill.dk>
2025-10-07 14:00:57 +0200

    Uncomment test.

--- src-testing/Futhark/Optimise/Fusion/ScremaTests.hs
+++ src-testing/Futhark/Optimise/Fusion/ScremaTests.hs
@@ -43,20 +43,19 @@ tests =
     "ScremaTests"
     [ testGroup
         "FuseScrema"
-        [],
-      -- testCase "map-map" $
-      --   let lam_c = "\\{x_0 : i32} : {i32} -> {x_0}"
-      --       inp_c = [SOAC.Input mempty "xs_1" "[10i64]i32"]
-      --       out_c = ["ys_3"]
-      --       lam_p = "\\{x_2 : i32} : {i32} -> {x_2}"
-      --       out_p = ["zs_4"]
-      --    in fuseLambda lam_c inp_c out_c lam_p out_p
-      --         @?= Just
-      --           ( [SOAC.Input mempty "xs_1" "[10i64]i32"],
-      --             "\\{x_2 : i32, x_0 : i32} : {i32,i32} -> {x_2, x_0}",
-      --             ["zs_4", "ys_3"]
-      --           )
-
+        [ testCase "map-map" $
+            let lam_c = "\\{x_0 : i32} : {i32} -> {x_0}"
+                inp_c = ["xs_1"]
+                out_c = ["ys_3"]
+                lam_p = "\\{x_2 : i32} : {i32} -> {x_2}"
+                out_p = ["zs_4"]
+             in SP (fuseLambda lam_c inp_c out_c lam_p out_p)
+                  @?= SP
+                    ( ["xs_1" :: VName],
+                      "\\{x_2 : i32, x_0 : i32} : {i32,i32} -> {x_2, x_0}",
+                      ["zs_4", "ys_3"]
+                    )
+        ],
       testGroup
         "splitLambdaByPar"
         [ testCase "keeps params and result." $
