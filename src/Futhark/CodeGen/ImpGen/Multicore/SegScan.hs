36bafc4784cd8c71e931263e464c480fb9fd8bd8
Amirreza Hashemi <amir@Amirrezas-MacBook-Pro.local>
2025-10-15 00:45:00 +0200

    playground

--- src/Futhark/CodeGen/ImpGen/Multicore/SegScan.hs
+++ src/Futhark/CodeGen/ImpGen/Multicore/SegScan.hs
@@ -12,9 +12,16 @@ import Futhark.CodeGen.ImpCode.Multicore qualified as Imp
 import Futhark.CodeGen.ImpGen
 import Futhark.CodeGen.ImpGen.Multicore.Base
 import Futhark.IR.MCMem
-import Futhark.Util.IntegralExp (quot, rem)
+import Futhark.Util.IntegralExp (quot, divUp)
 import Prelude hiding (quot, rem)
 import Futhark.Util.Pretty (prettyString)
+
+
+
+-- the final implementation should use this
+blockSize64 :: Imp.TExp Int64
+blockSize64 = 2
+
 
 
 xParams, yParams :: SegBinOp MCMem -> [LParam MCMem]
@@ -63,21 +70,115 @@ nonsegmentedScan
 
     genBinOpParams [scan_op]
 
-    forM_ (zip (xParams scan_op) (segBinOpNeutral scan_op)) $ \(p, ne) ->
-      copyDWIMFix (paramName p) [] ne []
 
-    emit $ Imp.DebugPrint ("KERNEL BODY:\n" ++ prettyString kstms) Nothing
-    emit $ Imp.DebugPrint ("KERNEL Res:\n" ++ prettyString res) Nothing
-    emit $ Imp.DebugPrint ("Lambda BODY:\n" ++ prettyString (bodyStms $ lamBody scan_op)) Nothing
+    nblocks <- dPrim "nblocks"
+    nblocks <-- ((pe64 n + blockSize64 - 1) `quot` blockSize64)
 
-    sFor "j" (pe64 n) $ \j -> do
-      dPrimV_ i j
-      compileStms mempty kstms $ do
-        forM_ (zip (yParams scan_op) [res]) $ \(p, se) ->
-          copyDWIMFix (paramName p) [] se []
+    -- for now using head and considering only one
+    let pt  = elemType (head (lambdaReturnType (segBinOpLambda scan_op)))
+    let ne = head (segBinOpNeutral scan_op)
 
-      compileStms mempty (bodyStms $ lamBody scan_op) $ do
-        forM_ (zip (map resSubExp $ bodyResult $ lamBody scan_op) (xParams scan_op))  $ \(se,px) -> do 
-          copyDWIMFix (patElemName pe) [j] se []
-          copyDWIMFix (paramName px) [] se [] 
-          
+    -- allocate flags/aggr/prefix arrays of length nblocks
+    let nblocks_se = Var (tvVar nblocks)
+    flagsArr <- sAllocArray "scan_flags" int8 (Shape [nblocks_se]) DefaultSpace
+    aggrArr  <- sAllocArray "scan_aggr"  pt    (Shape [nblocks_se]) DefaultSpace
+    prefArr  <- sAllocArray "scan_pref"  pt    (Shape [nblocks_se]) DefaultSpace
+
+    accLili  <- sAllocArray "accLili"  int64    (Shape [intConst Int64 1]) DefaultSpace
+    z <- dPrimV "z" (0 :: Imp.TExp Int64)
+    copyDWIMFix accLili [0] (Var (tvVar z)) []
+
+
+    -- initialise
+    sFor "init" (tvExp nblocks) $ \j -> do
+      copyDWIMFix flagsArr [j] (intConst Int8 0) []
+      copyDWIMFix aggrArr  [j] ne []
+      copyDWIMFix prefArr  [j] ne []
+
+
+    -- let's create the seg
+    fbody <- collect $ do
+      blockID <- dPrim "blockID" :: MulticoreGen (TV Int64)
+      sOp $ Imp.GetTaskId (tvVar blockID)
+      num_tasks <- dPrimSV "num_tasks" int64
+      -- sOp $ Imp.GetNumTasks $ tvVar nt
+      blockSize643 <- dPrim "blockSize64"
+      blockSize643 <-- (pe64 n  `divUp` tvExp blockID)
+      emit $ Imp.DebugPrint "the number of blocks" (Just $ untyped (tvExp num_tasks))
+      chunk_no <- dPrimV "chunk_no" (tvExp blockID * tvExp num_tasks)
+      chunk_length <- dPrimV "chunk_length" (min (tvExp blockSize643) (pe64 n - tvExp chunk_no))
+
+      sFor "j" (tvExp chunk_length) $ \j -> do
+        let one = (1 :: Imp.TExp Int64)
+        oldVal <- dPrim "oldVal" :: MulticoreGen (TV Int64)
+        -- acc <- dPrim "acc" :: MulticoreGen (TV Int64)
+        -- acc <-- (0 :: Imp.TExp Int64)
+        -- accMem <- sAlloc "acc" (Imp.bytes 8) Imp.DefaultSpace
+
+        memAcc <- memLocName . entryArrayLoc <$> lookupArray accLili
+        let idx0 = Imp.elements (0 :: Imp.TExp Int32)
+
+        sOp $ Imp.Atomic $ Imp.AtomicAdd Int64 (tvVar oldVal) memAcc idx0 (untyped one)
+ 
+
+        -- sOp $ Imp.Atomic (Imp.AtomicAdd Int64 (tvVar oldVal) accLili (Imp.elements (1 :: Imp.TExp Int32)) (untyped one) )
+        -- print the value of accmem
+        emit $ Imp.DebugPrint "the value of accmem" (Just $ untyped (tvExp oldVal))
+        -- emit $ Imp.DebugPrint "entering the loop" Nothing
+        sWhen (tvExp chunk_no + j .<. pe64 n) $ do
+          i_val <- dPrimV "i" (tvExp chunk_no + j)
+          dPrimV_ i (tvExp i_val)
+          emit $ Imp.DebugPrint "the value of loop index" (Just $ untyped (tvExp i_val))
+          compileStms mempty kstms $ do
+            forM_ [res] $ \se -> copyDWIMFix (patElemName pe) [tvExp i_val] se []
+
+      -- emit $ Imp.DebugPrint "Inside block loop" (Just $ untyped (tvExp blockID))
+      -- emit $ Imp.DebugPrint "HEllo World" (Just $ untyped (tvExp nblocks))
+      -- emit $ Imp.DebugPrint "GetNumTasks =" (Just $ untyped (tvExp nt))
+
+    free_params <- freeParams fbody
+    emit $ Imp.Op $ Imp.ParLoop "segmap" fbody free_params
+
+
+
+    -- forM_ (zip (xParams scan_op) (segBinOpNeutral scan_op)) $ \(p, ne) ->
+    --   copyDWIMFix (paramName p) [] ne []
+
+    -- emit $ Imp.DebugPrint ("KERNEL BODY:\n" ++ prettyString kstms) Nothing
+    -- emit $ Imp.DebugPrint ("KERNEL Res:\n" ++ prettyString res) Nothing
+    -- emit $ Imp.DebugPrint ("Lambda BODY:\n" ++ prettyString (bodyStms $ lamBody scan_op)) Nothing
+
+    -- sFor "j" (pe64 n) $ \j -> do
+    --   dPrimV_ i j
+    --   compileStms mempty kstms $ do
+    --     forM_ (zip (yParams scan_op) [res]) $ \(p, se) ->
+    --       copyDWIMFix (paramName p) [] se []
+
+    --   compileStms mempty (bodyStms $ lamBody scan_op) $ do
+    --     forM_ (zip (map resSubExp $ bodyResult $ lamBody scan_op) (xParams scan_op))  $ \(se,px) -> do 
+    --       copyDWIMFix (patElemName pe) [j] se []
+    --       copyDWIMFix (paramName px) [] se [] 
+
+
+
+
+
+      -- fbody <- collect $ do
+      --   -- Get the current block ID for this parallel task
+      --   bid <- dPrim "block_id" :: MulticoreGen (TV Int64)
+      --   sOp $ Imp.GetTaskId (tvVar bid)
+
+      --   emit $ Imp.DebugPrint "Inside block loop" (Just $ untyped (tvExp bid))
+
+      --   -- Sequential inner loop over elements in this block
+      --   sFor "j" blockSize64 $ \j -> do
+      --     sComment "process element j of this block" $
+      --       emit $ Imp.DebugPrint "  element" (Just $ untyped j)
+
+      -- free_params <- freeParams fbody
+
+
+      -- emit $ Imp.Op $ Imp.ParLoop "segmap" fbody free_params
+
+
+
