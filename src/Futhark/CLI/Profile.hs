9f864f692c93b4b157ddb42cb88ce55a64b59a41
VegOwOtenks <github@vegowotenks.de>
2025-12-29 13:36:47 +0100

    Handling of the first two cases

--- src/Futhark/CLI/Profile.hs
+++ src/Futhark/CLI/Profile.hs
@@ -315,6 +315,72 @@ writeHtml htmlDirPath evSummaryMap = do
 
   pure ()
 
+data OneTwoThree a = One a | Two a a | Three a a a
+  deriving (Show, Functor, Foldable)
+
+-- | Assumes that the ranges overlap
+mergeRanges ::
+  (Semigroup s) =>
+  (SourceRange, s) ->
+  (SourceRange, s) ->
+  OneTwoThree (SourceRange, s)
+mergeRanges a@(rangeA, auxA) b@(rangeB, auxB)
+  -- they have exactly the same source-range
+  -- ccc
+  | rangeA == rangeB = One (rangeA, auxA <> auxB)
+  -- they must not be the same, there are a few remaining cases
+  -- \* one of the ranges is longer than the other: cca, ccb
+  -- \* one of the ranges
+  -- \* end overlap: acc
+  | otherwise =
+      let earlier =
+            minimumBy
+              (comparing $ sourceRangeStart . fst)
+              [a, b]
+          earlierRange = fst earlier
+          earlierEnd = sourceRangeEnd earlierRange
+          earlierStart = sourceRangeStart earlierRange
+
+          later =
+            maximumBy
+              (comparing $ sourceRangeStart . fst)
+              [a, b]
+          laterRange = fst later
+          laterEnd = sourceRangeEnd laterRange
+          laterStart = sourceRangeStart laterRange
+       in if
+            -- ccb
+            | earlierStart == laterStart && earlierEnd < laterEnd ->
+                let SourceRange (Pos fname _ _ _) _ _ = earlierRange
+                    (earlierEndLine, earlierEndCol) = earlierEnd
+                    secondStartPos =
+                      Pos fname earlierEndLine earlierEndCol (-1)
+                    secondSourceRange =
+                      laterRange
+                        { sourceRangeStartPos = secondStartPos
+                        }
+                 in Two (earlierRange, auxA <> auxB) (secondSourceRange, auxB)
+            -- cca
+            | earlierStart == laterStart && laterEnd <= earlierEnd ->
+                let (overlapStartLine, overlapStartCol) = laterStart
+                    firstSourceRange =
+                      earlierRange
+                        { sourceRangeEndLine = overlapStartLine,
+                          sourceRangeEndColumn = overlapStartCol
+                        }
+                 in Two (firstSourceRange, auxA) (laterRange, auxA <> auxB)
+            | False -> _
+            -- none of the three cases
+            | otherwise ->
+                error $
+                  "Impossible case: Ranges did not overlap.\n"
+                    <> "First: "
+                    <> show (fst a)
+                    <> "\n"
+                    <> "Second: "
+                    <> show (fst b)
+                    <> "\n"
+
 summarizeAndSplitByFile ::
   -- | Names of all text files
   S.Set T.Text ->
@@ -341,55 +407,6 @@ summarizeAndSplitByFile files summaries =
         M.Map SourceRange (Seq.Seq (T.Text, EvSummary))
       separate = foldl' accumulateRange M.empty . fmap (second Seq.singleton)
         where
-          -- \| Assumes that the ranges overlap
-          mergeRanges ::
-            (Semigroup s) =>
-            (SourceRange, s) ->
-            (SourceRange, s) ->
-            NonEmpty (SourceRange, s)
-          mergeRanges a@(rangeA, auxA) b@(rangeB, auxB)
-            -- they have exactly the same source-range
-            | rangeA == rangeB = NE.singleton (rangeA, auxA <> auxB)
-            -- they must not be the same, there are a few remaining cases
-            -- - start overlap: ccb
-            -- - middle overlap: aca
-            -- - end overlap: acc
-            -- - start consumption: aca
-            -- - end consumption: cb
-            | otherwise =
-                let earlier =
-                      minimumBy
-                        (comparing $ sourceRangeStart . fst)
-                        [a, b]
-                    earlierRange = fst earlier
-                    earlierEnd = sourceRangeEnd earlierRange
-                    earlierStart = sourceRangeStart earlierRange
-
-                    later =
-                      maximumBy
-                        (comparing $ sourceRangeStart . fst)
-                        [a, b]
-                    laterRange = fst later
-                    laterEnd = sourceRangeEnd laterRange
-                    laterStart = sourceRangeStart laterRange
-                 in if
-                      -- start is the same
-                      | earlierStart == laterStart -> if earlierEnd < laterEnd
-                        -- start overlap
-                        then (earlierRange, auxA <> auxB) NE.:| [(_, auxB)]
-                        -- start consumption
-                        else _
-                      -- none of the three cases
-                      | otherwise ->
-                          error $
-                            "Impossible case: Ranges did not overlap.\n"
-                              <> "First: "
-                              <> show (fst a)
-                              <> "\n"
-                              <> "Second: "
-                              <> show (fst b)
-                              <> "\n"
-
           accumulateRange ::
             -- \| Mapping of non-overlapping ranges, the T.Text is a ccName
             M.Map SourceRange (Seq.Seq (T.Text, EvSummary)) ->
