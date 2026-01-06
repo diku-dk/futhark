4359b06d940a916766d26089a4f3a727226c222d
Troels Henriksen <athas@sigkill.dk>
2026-01-06 22:07:10 +0100

    Style fix.

--- src/Futhark/CLI/Profile.hs
+++ src/Futhark/CLI/Profile.hs
@@ -186,7 +186,7 @@ writeHtml htmlDirPath evSummaryMap = do
   let costCentreOverview = generateCCOverviewHtml costCentres
 
   liftIO $ do
-    createDirectoryIfMissing True $ htmlDirPath
+    createDirectoryIfMissing True htmlDirPath
     forM_ (M.toList htmlFiles) $ \(srcFilePath, html) -> do
       let absPath =
             htmlDirPath </> makeRelative "/" (srcFilePath <> ".html")
