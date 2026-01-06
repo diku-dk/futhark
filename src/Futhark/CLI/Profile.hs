f432a80ff8d409b4650f6d73f1a6f49f1caa4275
Troels Henriksen <athas@sigkill.dk>
2026-01-06 21:52:05 +0100

    Style fix.

--- src/Futhark/CLI/Profile.hs
+++ src/Futhark/CLI/Profile.hs
@@ -38,7 +38,7 @@ import Futhark.Profile.EventSummary qualified as ES
 import Futhark.Profile.Html (generateCCOverviewHtml, generateHeatmapHtml)
 import Futhark.Profile.SourceRange (SourceRange)
 import Futhark.Profile.SourceRange qualified as SR
-import Futhark.Util (showText, hashText)
+import Futhark.Util (hashText, showText)
 import Futhark.Util.Options (mainWithOptions)
 import System.Directory (createDirectoryIfMissing, removePathForcibly)
 import System.Exit (ExitCode (ExitFailure), exitWith)
@@ -211,8 +211,8 @@ generateHtmlHeatmaps ::
   ExceptT T.Text IO (M.Map FilePath H.Html)
 generateHtmlHeatmaps fileToRanges = do
   sourceFiles <- loadAllFiles (M.keys fileToRanges)
-  let disambiguatePath (p :: FilePath)
-        = T.unpack (hashText $ T.pack p) <> "-" <> takeFileName p
+  let disambiguatePath (p :: FilePath) =
+        T.unpack (hashText $ T.pack p) <> "-" <> takeFileName p
   let disambiguatedSourceFiles = M.mapKeys disambiguatePath sourceFiles
   let renderSingle path text =
         generateHeatmapHtml path text (fileToRanges M.! path)
