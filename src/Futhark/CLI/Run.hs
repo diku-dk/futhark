a9e30f35f336f9acbc306a80a028705491f5c49b
Troels Henriksen <athas@sigkill.dk>
2026-02-09 15:23:12 +0100

    futhark run: add --program option.
    This is initial work towards supporting the invocation of compiled code from the
    interpreter. This commit demonstrates how to start up a server program and
    communicate with it.

--- src/Futhark/CLI/Run.hs
+++ src/Futhark/CLI/Run.hs
@@ -9,10 +9,22 @@ import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.ByteString.Lazy qualified as BS
 import Data.Map qualified as M
 import Data.Maybe
+import Data.Text qualified as T
 import Data.Text.IO qualified as T
 import Futhark.Compiler
 import Futhark.Data.Reader (readValues)
 import Futhark.Pipeline
+import Futhark.Server
+  ( InputType (..),
+    OutputType (..),
+    Server,
+    cmdEither,
+    cmdEntryPoints,
+    cmdInputs,
+    cmdOutputs,
+    newServerCfg,
+    startServer,
+  )
 import Futhark.Util.Options
 import Futhark.Util.Pretty (AnsiStyle, Doc, align, hPutDoc, hPutDocLn, pretty, unAnnotate, (<+>))
 import Language.Futhark
@@ -30,10 +42,31 @@ main = mainWithOptions interpreterConfig options "options... <program.fut>" run
     run [prog] config = Just $ interpret config prog
     run _ _ = Nothing
 
+describeEntryPoints :: Server -> IO ()
+describeEntryPoints server = (either (error . T.unpack) pure <=< runExceptT) $ do
+  liftIO $ putStrLn "Program contains these entry points:"
+  entry_points <- cmdEither $ cmdEntryPoints server
+  forM_ entry_points $ \ep -> do
+    liftIO $ T.putStr $ ep <> " : "
+    inputs <- cmdEither $ cmdInputs server ep
+    outputs <- cmdEither $ cmdOutputs server ep
+    let onInput (InputType True t) = "*" <> t
+        onInput (InputType False t) = t
+        onOutput (OutputType True t) = "*" <> t
+        onOutput (OutputType False t) = t
+    liftIO $
+      T.putStrLn $
+        T.intercalate " -> " (map onInput inputs)
+          <> " -> "
+          <> "("
+          <> T.intercalate "," (map onOutput outputs)
+          <> ")"
+    pure ()
+
 interpret :: InterpreterConfig -> FilePath -> IO ()
 interpret config fp = do
   pr <- newFutharkiState config fp
-  (tenv, ienv) <- case pr of
+  (server, tenv, ienv) <- case pr of
     Left err -> do
       hPutDocLn stderr err
       exitFailure
@@ -59,12 +92,14 @@ interpret config fp = do
         T.hPutStrLn stderr $ "Invalid entry point: " <> prettyText entry
         exitFailure
 
+  mapM_ describeEntryPoints server
+
   case I.interpretFunction ienv (qualLeaf fname) inps of
     Left err -> do
       T.hPutStrLn stderr err
       exitFailure
     Right run -> do
-      run' <- runInterpreter' run
+      run' <- runInterpreter' server run
       case run' of
         Left err -> do
           hPrint stderr err
@@ -81,11 +116,12 @@ putValue v t
 
 data InterpreterConfig = InterpreterConfig
   { interpreterEntryPoint :: Name,
-    interpreterPrintWarnings :: Bool
+    interpreterPrintWarnings :: Bool,
+    interpreterProgram :: Maybe FilePath
   }
 
 interpreterConfig :: InterpreterConfig
-interpreterConfig = InterpreterConfig defaultEntryPoint True
+interpreterConfig = InterpreterConfig defaultEntryPoint True Nothing
 
 options :: [FunOptDescr InterpreterConfig]
 options =
@@ -103,14 +139,26 @@ options =
       "w"
       ["no-warnings"]
       (NoArg $ Right $ \config -> config {interpreterPrintWarnings = False})
-      "Do not print warnings."
+      "Do not print warnings.",
+    Option
+      ""
+      ["program"]
+      ( ReqArg
+          (\fname -> Right $ \config -> config {interpreterProgram = Just fname})
+          "PROGRAM"
+      )
+      "Load this compiled program."
   ]
 
 newFutharkiState ::
   InterpreterConfig ->
   FilePath ->
-  IO (Either (Doc AnsiStyle) (T.Env, I.Ctx))
+  IO (Either (Doc AnsiStyle) (Maybe Server, T.Env, I.Ctx))
 newFutharkiState cfg file = runExceptT $ do
+  server <- case interpreterProgram cfg of
+    Nothing -> pure Nothing
+    Just fname -> Just <$> liftIO (startServer (newServerCfg fname []))
+
   (ws, imports, _src) <-
     badOnLeft prettyCompilerError
       =<< liftIO
@@ -125,7 +173,7 @@ newFutharkiState cfg file = runExceptT $ do
 
   let loadImport ctx =
         badOnLeft I.prettyInterpreterError
-          <=< runInterpreter' . I.interpretImport ctx
+          <=< runInterpreter' server . I.interpretImport ctx
 
   ictx <- foldM loadImport I.initialCtx $ map (fmap fileProg) imports
   let (tenv, ienv) =
@@ -134,14 +182,14 @@ newFutharkiState cfg file = runExceptT $ do
               ictx {I.ctxEnv = I.ctxImports ictx M.! iname}
             )
 
-  pure (tenv, ienv)
+  pure (server, tenv, ienv)
   where
     badOnLeft :: (err -> err') -> Either err a -> ExceptT err' IO a
     badOnLeft _ (Right x) = pure x
     badOnLeft p (Left err) = throwError $ p err
 
-runInterpreter' :: (MonadIO m) => F I.ExtOp a -> m (Either I.InterpreterError a)
-runInterpreter' m = runF m (pure . Right) intOp
+runInterpreter' :: (MonadIO m) => Maybe Server -> F I.ExtOp a -> m (Either I.InterpreterError a)
+runInterpreter' _server m = runF m (pure . Right) intOp
   where
     intOp (I.ExtOpError err) = pure $ Left err
     intOp (I.ExtOpTrace w v c) = do
