486acea73395e2e94921392ff0b7c7c7ccb662e1
Due <williamhenrichdue@gmail.com>
2026-02-20 14:45:10 +0100

    More tests.

--- src-testing/Futhark/Optimise/Fusion/ScremaTests.hs
+++ src-testing/Futhark/Optimise/Fusion/ScremaTests.hs
@@ -564,7 +564,7 @@ tests =
                               [reduce_op']
                               (fromLines ["nilFn"])
                           )
-                          []
+                          ["out_a_12"]
                           [input_b]
                           ( ScremaForm
                               ( fromLines
@@ -576,7 +576,7 @@ tests =
                               [reduce_op]
                               (fromLines ["nilFn"])
                           )
-                          []
+                          ["out_b_14"]
                       )
                   )
                   @?= Tuple2
@@ -598,7 +598,7 @@ tests =
                         []
                         [reduce_op]
                         (fromLines ["nilFn"]),
-                      []
+                      ["out_a_12", "out_b_14"]
                     ),
           testCase "scan-scan fusion" $
             let scan_op' =
@@ -1124,6 +1124,76 @@ tests =
                                 ]
                             ),
                           ["b_out_8", identName ident_b]
+                        )
+                    ),
+          testCase "red-red fusion" $
+            let reduce_op' =
+                  Reduce
+                    Commutative
+                    ( fromLines
+                        [ "\\ {a_0 : i32, b_1 : i32} : {i32} ->",
+                          "let {c_2 : i32} = add32(a_0, b_1)",
+                          "in {c_2}"
+                        ]
+                    )
+                    ["0i32"]
+                reduce_op =
+                  Reduce
+                    Commutative
+                    ( fromLines
+                        [ "\\ {a_4 : f32, b_5 : f32} : {f32} ->",
+                          "let {c_6 : f32} = fadd32(a_4, b_5)",
+                          "in {c_6}"
+                        ]
+                    )
+                    ["0.0f32"]
+                ident_a = "input_a_7 : [d_9]i32"
+                input_a = SOAC.identInput ident_a
+                ident_b = "input_b_8 : [d_9]f32"
+                input_b = SOAC.identInput ident_b
+             in Tuple3
+                  ( withFreshNamesScopeError
+                      ( fuseScrema
+                          "d_9"
+                          [input_a]
+                          ( ScremaForm
+                              ( fromLines
+                                  [ "\\ {x_11 : i32} : {i32} ->",
+                                    "in {x_11}"
+                                  ]
+                              )
+                              []
+                              [reduce_op']
+                              (fromLines ["nilFn"])
+                          )
+                          ["out_a_12"]
+                          [input_b]
+                          ( ScremaForm
+                              ( fromLines
+                                  [ "\\ {x_13 : f32} : {f32} ->",
+                                    "in {x_13}"
+                                  ]
+                              )
+                              []
+                              [reduce_op]
+                              (fromLines ["nilFn"])
+                          )
+                          ["out_b_14"]
+                      )
+                  )
+                  @?= Tuple3
+                    ( Just
+                        ( [input_a, input_b],
+                          ScremaForm
+                        ( fromLines
+                            [ "\\ {x_11 : i32, x_10000 : f32}: {i32, f32} ->",
+                              "{x_11, x_10000}"
+                            ]
+                        )
+                        []
+                        [reduce_op', reduce_op]
+                        ( fromLines ["nilFn"]),
+                          ["out_a_12", "b_out_14"]
                         )
                     )
         ]
