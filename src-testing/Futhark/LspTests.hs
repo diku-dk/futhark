fbfd7759f4722be4c0f258bdd26546666d0cafa4
VegOwOtenks <github@vegowotenks.de>
2026-03-12 18:07:06 +0100

    Test the definition-finder

--- src-testing/Futhark/LspTests.hs
+++ src-testing/Futhark/LspTests.hs
@@ -1,23 +1,28 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE ExplicitNamespaces #-}
+{-# LANGUAGE MultilineStrings #-}
 
 module Futhark.LspTests (tests) where
 
 import Colog.Core (LogAction (LogAction))
 import Control.Concurrent (forkIO)
+import Control.Lens ((^.))
 import Control.Monad.IO.Class (liftIO)
 import Data.Functor (void)
 import Data.Text (Text)
 import Futhark.CLI.LSP (serverDefinition)
+import Language.LSP.Protocol.Lens (uri)
 import Language.LSP.Protocol.Types
-  ( Hover (Hover),
+  ( Definition (Definition),
+    Hover (Hover),
     LanguageKind (LanguageKind_Custom),
+    Location (..),
     MarkupContent (..),
     MarkupKind (MarkupKind_PlainText),
     Position (..),
     Range (..),
     TextDocumentIdentifier,
-    type (|?) (InL),
+    type (|?) (..),
   )
 import Language.LSP.Server (runServerWithHandles)
 import Language.LSP.Test
@@ -25,18 +30,20 @@ import Language.LSP.Test
     createDoc,
     defaultConfig,
     fullLatestClientCaps,
+    getDefinitions,
     getHover,
     runSessionWithHandles,
   )
 import System.Process (createPipe)
-import Test.Tasty (TestTree, testGroup)
+import Test.Tasty (TestName, TestTree, testGroup)
 import Test.Tasty.HUnit (assertFailure, testCase, (@?=))
 
 tests :: TestTree
 tests =
   testGroup
     "Futhark.LspTests"
-    [ testHoverInformation
+    [ testHoverInformation,
+      testDefinition
     ]
 
 runServerSessionPair :: Session a -> IO a
@@ -53,23 +60,24 @@ runServerSessionPair session = do
 
   runSessionWithHandles clientOut clientIn defaultConfig fullLatestClientCaps "." session
 
-
 createMainDoc :: Text -> Session TextDocumentIdentifier
 createMainDoc = createDoc "main.fut" futharkLanguage
   where
     futharkLanguage :: LanguageKind
     futharkLanguage = LanguageKind_Custom "Futhark"
 
+serverTestCase :: TestName -> Session () -> TestTree
+serverTestCase testName = testCase testName . runServerSessionPair
+
 testHoverInformation :: TestTree
-testHoverInformation = testCase "Hover" $
-  runServerSessionPair $ do
-    docIdent <- createMainDoc "def main = let xyz = 42i32 in xyz"
-    hoverInfo <- getHover docIdent $ Position {_character = 31, _line = 0}
-    case hoverInfo of
-      Nothing -> liftIO $ assertFailure "Did not receive any hover information"
-      Just (Hover content range) -> do
-        liftIO $ range @?= Just expectedRange
-        liftIO $ content @?= InL expectedMarkup
+testHoverInformation = serverTestCase "Hover" $ do
+  docIdent <- createMainDoc "def main = let xyz = 42i32 in xyz"
+  hoverInfo <- getHover docIdent $ Position {_character = 31, _line = 0}
+  case hoverInfo of
+    Nothing -> liftIO $ assertFailure "Did not receive any hover information"
+    Just (Hover content range) -> do
+      liftIO $ range @?= Just expectedRange
+      liftIO $ content @?= InL expectedMarkup
   where
     expectedMarkup =
       MarkupContent
@@ -80,4 +88,37 @@ testHoverInformation = testCase "Hover" $
       Range
         { _start = Position {_line = 0, _character = 30},
           _end = Position {_line = 0, _character = 34}
+        }
+
+testDefinition :: TestTree
+testDefinition = serverTestCase "Go To Definition" $
+  do
+    docIdent <- createMainDoc mainDocContents
+    definition <- getDefinitions docIdent fooBodyPosition
+
+    let expectedDefinition =
+          Definition $
+            InL $
+              Location
+                { _uri = docIdent ^. uri,
+                  _range =
+                    Range
+                      { _end = Position 0 15,
+                        _start = Position 0 0
+                      }
+                }
+
+    liftIO $ definition @?= InL expectedDefinition
+  where
+    mainDocContents =
+      """
+      def foo = 0i32
+      def bar = foo
+      """
+
+    fooBodyPosition :: Position
+    fooBodyPosition =
+      Position
+        { _line = 1,
+          _character = 12
         }
