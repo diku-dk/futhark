4129f3d2fd4f85c17c68a59c4acbc88653cf1220
VegOwOtenks <github@vegowotenks.de>
2026-03-12 17:39:22 +0100

    Test the markup content in the hover

--- src-testing/Futhark/LspTests.hs
+++ src-testing/Futhark/LspTests.hs
@@ -1,5 +1,5 @@
+{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE ExplicitNamespaces #-}
-{-# OPTIONS_GHC -fdefer-typed-holes #-}
 
 module Futhark.LspTests (tests) where
 
@@ -7,12 +7,16 @@ import Colog.Core (LogAction (LogAction))
 import Control.Concurrent (forkIO)
 import Control.Monad.IO.Class (liftIO)
 import Data.Functor (void)
+import Data.Text (Text)
 import Futhark.CLI.LSP (serverDefinition)
 import Language.LSP.Protocol.Types
   ( Hover (Hover),
     LanguageKind (LanguageKind_Custom),
+    MarkupContent (..),
+    MarkupKind (MarkupKind_PlainText),
     Position (..),
     Range (..),
+    TextDocumentIdentifier,
     type (|?) (InL),
   )
 import Language.LSP.Server (runServerWithHandles)
@@ -49,18 +53,29 @@ runServerSessionPair session = do
 
   runSessionWithHandles clientOut clientIn defaultConfig fullLatestClientCaps "." session
 
+
+createMainDoc :: Text -> Session TextDocumentIdentifier
+createMainDoc = createDoc "main.fut" futharkLanguage
+  where
+    futharkLanguage :: LanguageKind
+    futharkLanguage = LanguageKind_Custom "Futhark"
+
 testHoverInformation :: TestTree
-testHoverInformation = testCase "Hovering over i32 variable provides type hint" $
+testHoverInformation = testCase "Hover" $
   runServerSessionPair $ do
-    docIdent <- createDoc "main.fut" futharkLanguage "def main = let xyz = 42i32 in xyz"
+    docIdent <- createMainDoc "def main = let xyz = 42i32 in xyz"
     hoverInfo <- getHover docIdent $ Position {_character = 31, _line = 0}
     case hoverInfo of
       Nothing -> liftIO $ assertFailure "Did not receive any hover information"
       Just (Hover content range) -> do
         liftIO $ range @?= Just expectedRange
-        liftIO $ print content
+        liftIO $ content @?= InL expectedMarkup
   where
-    futharkLanguage = LanguageKind_Custom "Futhark"
+    expectedMarkup =
+      MarkupContent
+        { _value = "i32",
+          _kind = MarkupKind_PlainText
+        }
     expectedRange =
       Range
         { _start = Position {_line = 0, _character = 30},
