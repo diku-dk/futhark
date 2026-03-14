498ec4d7c3fbfcc6a5167a20720c8acb3757855d
Nikolaj Hey Hinnerskov <nhey@900901.xyz>
2026-03-14 21:21:17 +0100

    Add verify log-level subcommand.

--- src/Futhark/CLI/Verify.hs
+++ src/Futhark/CLI/Verify.hs
@@ -4,24 +4,34 @@ import Futhark.Analysis.Properties.Convert (mkIndexFnProg)
 import Futhark.Compiler
 import Futhark.Util.Options
 import System.Environment
+import Data.Maybe (fromMaybe)
 
 -- | The configuration for the futhark verify command.
-data VerifyConfig = VerifyConfig {}
+newtype VerifyConfig = VerifyConfig {
+  verifyIndexFnLevel :: Maybe String
+}
 
 -- | Initial configuration.
 initialVerifyConfig :: VerifyConfig
-initialVerifyConfig = VerifyConfig {}
+initialVerifyConfig = VerifyConfig {
+  verifyIndexFnLevel = Nothing
+}
 
 -- | Command line options.
 commandLineOptions :: [FunOptDescr VerifyConfig]
-commandLineOptions = []
+commandLineOptions = [
+  Option "f" ["log"]
+    (ReqArg (\s -> Right $ \config -> config {verifyIndexFnLevel = Just s}) "LEVEL")
+    "Set the log level (integer)."
+  ]
 
 -- | Run @futhark verify@.
 main :: String -> [String] -> IO ()
-main = mainWithOptions initialVerifyConfig commandLineOptions "program" $ \args _config ->
+main = mainWithOptions initialVerifyConfig commandLineOptions "program" $ \args config ->
   case args of
     [file] -> Just $ do
-      setEnv "FUTHARK_INDEXFN" "1"
+      let indexFnLevel = fromMaybe "1" (verifyIndexFnLevel config)
+      setEnv "FUTHARK_INDEXFN" indexFnLevel
       (_warnings, imports, vns) <- readProgramOrDie file
       let result = mkIndexFnProg vns (reverse imports)
       result `seq` pure ()
