8220d784f34149cb686eabce4de169123d742bb8
VegOwOtenks <github@vegowotenks.de>
2026-03-24 23:24:18 +0100

    Implement code actions for parenthesized types

--- src/Futhark/LSP/CodeAction.hs
+++ src/Futhark/LSP/CodeAction.hs
@@ -6,10 +6,9 @@ module Futhark.LSP.CodeAction (getCodeActions) where
 import Data.Function ((&))
 import Data.Loc (Pos (Pos))
 import Data.Map qualified as M
-import Data.Maybe (fromMaybe)
 import Futhark.LSP.State (State)
 import Futhark.LSP.Tool (bindingsInRange)
-import Futhark.LSP.TypeAscription (TypeAscription (TypeAscBare), missingAscriptions)
+import Futhark.LSP.TypeAscription (TypeAscription (TypeAscBare, TypeAscParens), missingAscriptions)
 import Language.LSP.Protocol.Types (CodeAction (..), CodeActionKind (CodeActionKind_Custom), Command, Position (Position), Range (Range), TextEdit (..), Uri, WorkspaceEdit (..), type (|?) (InR))
 
 getCodeActions :: Uri -> Range -> State -> FilePath -> [Command |? CodeAction]
@@ -23,32 +22,41 @@ getCodeActions file_uri range state filepath =
   where
     codeActions :: TypeAscription -> [CodeAction]
     codeActions (TypeAscBare text (Pos _ line column _)) =
+      pure $ mkAction [(text, line, column)]
+    codeActions (TypeAscParens openPos text pos) =
       pure $
-        CodeAction
-          { _isPreferred = Nothing,
-            _disabled = Nothing,
-            _diagnostics = Nothing,
-            _edit =
-              Just $
-                WorkspaceEdit
-                  { _documentChanges = Nothing,
-                    _changeAnnotations = Nothing,
-                    _changes =
-                      Just $
-                        M.singleton
-                          file_uri
-                          [ TextEdit
-                              { _range =
-                                  let toLsp = fromIntegral . pred
-                                      insertPos = Position (toLsp line) (toLsp column)
-                                   in Range insertPos insertPos,
-                                _newText = text
-                              }
-                          ]
-                  },
-            _title = "Insert type ascription",
-            _command = Nothing,
-            _data_ = Nothing,
-            _kind = Just $ CodeActionKind_Custom "TypeAscription"
-          }
-    codeActions _ = mempty
+        mkAction
+          [ let Pos _ line column _ = openPos in ("(", line, column),
+            let Pos _ line column _ = pos in (text, line, column)
+          ]
+
+    mkAction edits =
+      CodeAction
+        { _isPreferred = Nothing,
+          _disabled = Nothing,
+          _diagnostics = Nothing,
+          _edit =
+            Just $
+              WorkspaceEdit
+                { _documentChanges = Nothing,
+                  _changeAnnotations = Nothing,
+                  _changes =
+                    Just $
+                      M.singleton
+                        file_uri
+                        (map mkEdit edits)
+                },
+          _title = "Insert type ascription",
+          _command = Nothing,
+          _data_ = Nothing,
+          _kind = Just $ CodeActionKind_Custom "TypeAscription"
+        }
+
+    mkEdit (text, line, column) =
+      TextEdit
+        { _range =
+            let toLsp = fromIntegral . pred
+                insertPos = Position (toLsp line) (toLsp column)
+             in Range insertPos insertPos,
+          _newText = text
+        }
