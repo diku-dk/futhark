d623c5d9a381e3870436674be9239c643a63ac21
thedb <97967776+swagtop@users.noreply.github.com>
2026-05-04 11:25:12 +0200

    Moving Nix pinning system from 'niv' to flakes, while cleaning up. (#2438)
    This commit contains the following changes to the Nix code:
    
    1. Move pinning system from 'niv' to Nix flakes.
    
    2. Format code with official nixpkgs formatter.
    
    3. Update some expressions to modern conventions.
    
    4. Split futhark release-tarball derivation creation up into clear
    separate steps.
    
    5. Remove unused file (nix/zlib.nix).

--- nix/futhark-server.nix
+++ nix/futhark-server.nix
@@ -1,12 +1,29 @@
-{ mkDerivation, base, binary, bytestring, directory, futhark-data
-, lib, mtl, process, temporary, text
+{
+  mkDerivation,
+  base,
+  binary,
+  bytestring,
+  directory,
+  futhark-data,
+  lib,
+  mtl,
+  process,
+  temporary,
+  text,
 }:
 mkDerivation {
   pname = "futhark-server";
   version = "1.4.0.0";
   sha256 = "279763cea4f68e03e39aee0801c4185a1e913f09d5ee6aa022da3385a59dc616";
   libraryHaskellDepends = [
-    base binary bytestring directory futhark-data mtl process temporary
+    base
+    binary
+    bytestring
+    directory
+    futhark-data
+    mtl
+    process
+    temporary
     text
   ];
   description = "Client implementation of the Futhark server protocol";
