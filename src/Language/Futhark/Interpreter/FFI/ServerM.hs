a0ccebef635bdd7c5d7863e7cc0705d4ebef85d9
Troels Henriksen <athas@sigkill.dk>
2026-08-13 10:19:21 +0200

    Improve style.

--- src/Language/Futhark/Interpreter/FFI/ServerM.hs
+++ src/Language/Futhark/Interpreter/FFI/ServerM.hs
@@ -41,7 +41,7 @@ where
 
 import Control.Monad.Except (ExceptT, MonadError, runExceptT, throwError)
 import Control.Monad.IO.Class (MonadIO (liftIO))
-import Control.Monad.Reader (MonadReader, ReaderT, asks, runReaderT)
+import Control.Monad.Reader (ReaderT, asks, runReaderT)
 import Data.IORef (IORef, mkWeakIORef, newIORef, readIORef)
 import Data.List (intercalate)
 import Data.Map qualified as M
@@ -86,16 +86,6 @@ dToP (D.BoolValue _ vs) = BoolValue $ vs V.! 0
 
 newtype ValueRef = ValueRef (IORef S.VarName)
 
-mkValueRef :: (MonadIO m, MonadReader Server m) => S.VarName -> m ValueRef
-mkValueRef n = do
-  r <- liftIO $ newIORef n
-  q <- asks queue
-  _ <- liftIO $ mkWeakIORef r $ AL.prepend n q
-  pure $ ValueRef r
-
-varName :: ValueRef -> IO S.VarName
-varName (ValueRef r) = readIORef r
-
 data Server = Server
   { server :: S.Server,
     queue :: AL.AtomicList S.VarName
@@ -106,8 +96,15 @@ newtype ServerM a = ServerM (ReaderT Server (ExceptT String IO) a)
     ( Functor,
       Applicative,
       Monad,
-      MonadError String
+      MonadError String,
+      MonadIO
     )
+
+askServer :: ServerM S.Server
+askServer = ServerM $ asks server
+
+askQueue :: ServerM (AL.AtomicList S.VarName)
+askQueue = ServerM $ asks queue
 
 startServer :: S.ServerCfg -> IO Server
 startServer cfg = Server <$> S.startServer cfg <*> AL.new
@@ -115,136 +112,149 @@ startServer cfg = Server <$> S.startServer cfg <*> AL.new
 runServerM :: Server -> ServerM a -> IO (Either String a)
 runServerM s (ServerM m) = runExceptT $ runReaderT m s
 
+varName :: ValueRef -> ServerM S.VarName
+varName (ValueRef r) = liftIO $ readIORef r
+
+uniqueName :: ServerM S.VarName
+uniqueName = ("v" <>) . T.show . hashUnique <$> liftIO newUnique
+
+mkValueRef :: S.VarName -> ServerM ValueRef
+mkValueRef n = do
+  r <- liftIO $ newIORef n
+  q <- askQueue
+  _ <- liftIO $ mkWeakIORef r $ AL.prepend n q
+  pure $ ValueRef r
+
 gc :: ServerM ()
-gc = ServerM $ do
-  s <- asks server
-  vns <- asks queue >>= liftIO . AL.flush
-  liftIO (S.cmdFree s vns) >>= throwServerJust ("cmdFree failed on variables " ++ csList (map T.unpack vns) ++ ".")
+gc = do
+  s <- askServer
+  vns <- askQueue >>= liftIO . AL.flush
+  liftIO (S.cmdFree s vns)
+    >>= throwServerJust ("cmdFree failed on variables " ++ csList (map T.unpack vns) ++ ".")
 
 call :: Name -> [ValueRef] -> ServerM ValueRef
-call fn ps = ServerM $ do
-  s <- asks server
-  nps <- liftIO $ mapM varName ps
-  ndst <- liftIO uniqueName
+call fn ps = do
+  s <- askServer
+  nps <- mapM varName ps
+  ndst <- uniqueName
   _ <- liftIO (S.cmdCall s (nameToText fn) ndst nps) >>= throwServerLeft ("cmdCall failed on function " ++ nameToString fn ++ " with parameters " ++ csList (map T.unpack nps) ++ ".")
   mkValueRef ndst
 
-uniqueName :: IO S.VarName
-uniqueName = ("v" <>) . T.show . hashUnique <$> newUnique
-
 -- Interrogation
 inputs :: Name -> ServerM [S.TypeName]
-inputs fn = ServerM $ do
-  s <- asks server
+inputs fn = do
+  s <- askServer
   map S.inputType <$> (liftIO (S.cmdInputs s $ nameToText fn) >>= throwServerLeft ("cmdInputs failed on function " ++ nameToString fn ++ "."))
 
 output :: Name -> ServerM S.TypeName
-output fn = ServerM $ do
-  s <- asks server
+output fn = do
+  s <- askServer
   S.outputType <$> (liftIO (S.cmdOutput s $ nameToText fn) >>= throwServerLeft ("cmdOutput failed on function " ++ nameToString fn ++ "."))
 
 kind :: S.TypeName -> ServerM S.Kind
-kind tn = ServerM $ do
-  s <- asks server
+kind tn = do
+  s <- askServer
   liftIO (S.cmdKind s tn) >>= throwServerLeft ("cmdKind failed on type " ++ T.unpack tn ++ ".")
 
 vtype :: ValueRef -> ServerM S.TypeName
-vtype vr = ServerM $ do
-  s <- asks server
-  vn <- liftIO $ varName vr
+vtype vr = do
+  s <- askServer
+  vn <- varName vr
   liftIO (S.cmdType s vn) >>= throwServerLeft ("cmdType failed on variable " ++ T.unpack vn ++ ".")
 
 -- Primitives
 getPrim :: ValueRef -> ServerM PrimValue
-getPrim vr = ServerM $ do
-  s <- asks server
-  nsrc <- liftIO $ varName vr
+getPrim vr = do
+  s <- askServer
+  nsrc <- varName vr
   v <- liftIO (S.getValue s nsrc) >>= throwLeft ("Failed to get primitive variable " ++ T.unpack nsrc ++ ".")
   pure $ dToP v
 
 putPrim :: PrimValue -> ServerM ValueRef
-putPrim p = ServerM $ do
-  s <- asks server
-  ndst <- liftIO uniqueName
+putPrim p = do
+  s <- askServer
+  ndst <- uniqueName
   liftIO (S.putValue s ndst $ pToD p) >>= throwServerJust ("Failed to put primitive " ++ show p ++ ".")
   mkValueRef ndst
 
 -- Arrays
 rank :: S.TypeName -> ServerM Int
-rank tn = ServerM $ do
-  s <- asks server
+rank tn = do
+  s <- askServer
   liftIO (S.cmdRank s tn) >>= throwServerLeft ("cmdRank failed on type " ++ T.unpack tn ++ ".")
 
 elemType :: S.TypeName -> ServerM S.TypeName
-elemType tn = ServerM $ do
-  s <- asks server
+elemType tn = do
+  s <- askServer
   liftIO (S.cmdElemtype s tn) >>= throwServerLeft ("cmdElemtype failed on type " ++ T.unpack tn ++ ".")
 
 mkArray :: S.TypeName -> [Int64] -> [ValueRef] -> ServerM ValueRef
-mkArray tn dims vs = ServerM $ do
-  s <- asks server
-  vns <- liftIO $ mapM varName vs
-  dst <- liftIO uniqueName
+mkArray tn dims vs = do
+  s <- askServer
+  vns <- mapM varName vs
+  dst <- uniqueName
   liftIO (S.cmdNewArray s dst tn (map fromIntegral dims) vns) >>= throwServerJust ("cmdNewArray failed on type " ++ T.unpack tn ++ " with variables " ++ csList (map T.unpack vns) ++ ".")
   mkValueRef dst
 
 shape :: ValueRef -> ServerM [Int64]
-shape vr = ServerM $ do
-  s <- asks server
-  vn <- liftIO $ varName vr
+shape vr = do
+  s <- askServer
+  vn <- varName vr
   map fromIntegral <$> (liftIO (S.cmdShape s vn) >>= throwServerLeft ("cmdShape failed on variable " ++ T.unpack vn ++ "."))
 
 index :: [Int64] -> ValueRef -> ServerM ValueRef
-index is src = ServerM $ do
-  s <- asks server
-  nsrc <- liftIO $ varName src
-  ndst <- liftIO uniqueName
+index is src = do
+  s <- askServer
+  nsrc <- varName src
+  ndst <- uniqueName
   liftIO (S.cmdIndex s ndst nsrc $ map fromIntegral is) >>= throwServerJust ("cmdIndex failed on source " ++ T.unpack nsrc ++ ", destination " ++ T.unpack ndst ++ ", and index " ++ show is ++ ".")
   mkValueRef ndst
 
 -- Records
 fields :: S.TypeName -> ServerM (M.Map Name S.TypeName)
-fields tn = ServerM $ do
-  s <- asks server
+fields tn = do
+  s <- askServer
   fs <- liftIO (S.cmdFields s tn) >>= throwServerLeft ("cmdFields failed on type " ++ T.unpack tn ++ ".")
   pure $ M.fromList $ map (\f -> (nameFromText $ S.fieldName f, S.fieldType f)) fs
 
 mkRecord :: S.TypeName -> M.Map Name ValueRef -> ServerM ValueRef
-mkRecord tn vrm = ServerM $ do
-  s <- asks server
+mkRecord tn vrm = do
+  s <- askServer
   fns <- map (nameFromText . S.fieldName) <$> (liftIO (S.cmdFields s tn) >>= throwServerLeft ("cmdFields failed on type " ++ T.unpack tn ++ "."))
   vns <-
     mapM
       ( \fn ->
           throwNothing ("Mising field " ++ nameToString fn ++ " when constructing record of type " ++ T.unpack tn ++ ".") (M.lookup fn vrm)
-            >>= liftIO . varName
+            >>= varName
       )
       fns
-  dst <- liftIO uniqueName
+  dst <- uniqueName
   liftIO (S.cmdNew s dst tn vns) >>= throwServerJust ("cmdNew failed on type " ++ T.unpack tn ++ " with variables " ++ csList (map T.unpack vns) ++ ".")
   mkValueRef dst
 
 project :: ValueRef -> Name -> ServerM ValueRef
-project src fn = ServerM $ do
-  s <- asks server
-  nsrc <- liftIO $ varName src
-  ndst <- liftIO uniqueName
-  liftIO (S.cmdProject s ndst nsrc $ nameToText fn) >>= throwServerJust ("cmdKind failed on source " ++ T.unpack nsrc ++ ", destination " ++ T.unpack ndst ++ ", and field " ++ nameToString fn ++ ".")
+project src fn = do
+  s <- askServer
+  nsrc <- varName src
+  ndst <- uniqueName
+  liftIO (S.cmdProject s ndst nsrc $ nameToText fn)
+    >>= throwServerJust ("cmdKind failed on source " ++ T.unpack nsrc ++ ", destination " ++ T.unpack ndst ++ ", and field " ++ nameToString fn ++ ".")
   mkValueRef ndst
 
 -- Sums
 variants :: S.TypeName -> ServerM (M.Map Name [S.TypeName])
-variants tn = ServerM $ do
-  s <- asks server
+variants tn = do
+  s <- askServer
   vs <- liftIO (S.cmdVariants s tn) >>= throwServerLeft ("cmdVariants failed on type " ++ T.unpack tn ++ ".")
   pure $ M.fromList $ map (\v -> (nameFromText $ S.variantName v, S.variantTypes v)) vs
 
 mkSum :: S.TypeName -> Name -> [ValueRef] -> ServerM ValueRef
-mkSum tn vn vrs = ServerM $ do
-  s <- asks server
-  vns <- mapM (liftIO . varName) vrs
-  dst <- liftIO uniqueName
-  liftIO (S.cmdConstruct s dst tn (nameToText vn) vns) >>= throwServerJust ("cmdConstruct failed on type " ++ T.unpack tn ++ ", variant " ++ nameToString vn ++ " with variables " ++ csList (map T.unpack vns) ++ ".")
+mkSum tn vn vrs = do
+  s <- askServer
+  vns <- mapM varName vrs
+  dst <- uniqueName
+  liftIO (S.cmdConstruct s dst tn (nameToText vn) vns)
+    >>= throwServerJust ("cmdConstruct failed on type " ++ T.unpack tn ++ ", variant " ++ nameToString vn ++ " with variables " ++ csList (map T.unpack vns) ++ ".")
   mkValueRef dst
 
 destruct :: ValueRef -> ServerM [ValueRef]
@@ -252,18 +262,21 @@ destruct src = do
   vn <- variant src
   tn <- vtype src
   vts <- variants tn >>= throwNothing ("Variant " ++ nameToString vn ++ " is not part of its own sum type, " ++ T.unpack tn ++ ". This should be impossible.") . M.lookup vn
-  ServerM $ do
-    s <- asks server
-    nsrc <- liftIO $ varName src
-    ndsts <- liftIO $ mapM (const uniqueName) vts
-    liftIO (S.cmdDestruct s nsrc ndsts) >>= throwServerJust ("cmdVariants failed on source " ++ T.unpack nsrc ++ ", destinations " ++ csList (map T.unpack ndsts) ++ ".")
+  do
+    s <- askServer
+    nsrc <- varName src
+    ndsts <- mapM (const uniqueName) vts
+    liftIO (S.cmdDestruct s nsrc ndsts)
+      >>= throwServerJust ("cmdVariants failed on source " ++ T.unpack nsrc ++ ", destinations " ++ csList (map T.unpack ndsts) ++ ".")
     mapM mkValueRef ndsts
 
 variant :: ValueRef -> ServerM Name
-variant src = ServerM $ do
-  s <- asks server
-  nsrc <- liftIO $ varName src
-  vn <- liftIO (S.cmdVariant s nsrc) >>= throwServerLeft ("cmdIndex failed on variable " ++ T.unpack nsrc ++ ".")
+variant src = do
+  s <- askServer
+  nsrc <- varName src
+  vn <-
+    liftIO (S.cmdVariant s nsrc)
+      >>= throwServerLeft ("cmdIndex failed on variable " ++ T.unpack nsrc ++ ".")
   pure $ nameFromText vn
 
 -- Error handling convenience
